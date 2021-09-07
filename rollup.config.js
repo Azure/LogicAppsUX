5791f91ebba644d5a15225973df5cee0d451bc41
Travis Harris <travisharris@microsoft.com>
2021-09-07 15:14:58 -0700

    fix: Fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,29 @@
-import typescript from 'rollup-plugin-typescript2';
-import pkg from './package.json';
-import {terser} from "rollup-plugin-terser";
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
+import { terser } from "rollup-plugin-terser";
+import less from "rollup-plugin-less";
+import postcss from "rollup-plugin-postcss";
 export default {
- input: 'src/index.tsx', // our source file
- output: [
-  {
-   file: pkg.main,
-   format: 'cjs'
-  },
-  {
-   file: pkg.module,
-   format: 'es' // the preferred format
-  },
- ],
- external: [
-  ...Object.keys(pkg.dependencies || {})
- ],
- plugins: [
-  typescript({
-   typescript: require('typescript'),
-   tsconfig: './tsconfig.lib.json'
-  }),
-  terser() // minifies generated bundles
- ]
+  input: "src/index.tsx", // our source file
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      file: pkg.module,
+      format: "es", // the preferred format
+    },
+  ],
+  external: [...Object.keys(pkg.dependencies || {})],
+  plugins: [
+    typescript({
+      typescript: require("typescript"),
+      tsconfig: "./tsconfig.lib.json",
+    }),
+    postcss({
+      plugins: [],
+    }),
+    terser(), // minifies generated bundles
+  ],
 };
