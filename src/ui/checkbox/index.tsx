42fc9d991aa27d0b3d70fc95ce6b9733e9f63410
Travis Harris <travisharris@microsoft.com>
2021-09-09 01:09:24 -0700

    fix: rework checkbox to correctly use translated strings

--- src/ui/checkbox/index.tsx
+++ src/ui/checkbox/index.tsx
@@ -3,7 +3,8 @@ import { Callout, DirectionalHint } from '@fluentui/react/lib/Callout';
 import { Checkbox as FabricCheckbox, ICheckbox } from '@fluentui/react/lib/Checkbox';
 import { Icon } from '@fluentui/react/lib/Icon';
 import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { useState, useRef } from 'react';
+import { injectIntl, useIntl, WrappedComponentProps } from 'react-intl';
 import { calloutContentStyles, checkboxStyles } from '../fabric';
 import './checkbox.less';
 export interface CheckboxProps {
@@ -22,115 +23,81 @@ export interface CheckboxState {
   checked: boolean;
 }
 
-export class Checkbox extends React.Component<CheckboxProps, CheckboxState> {
-  private _checkboxDescriptionButtonRef = React.createRef<HTMLButtonElement>();
-  private _checkboxRef: ICheckbox | undefined;
+export const Checkbox = (props: CheckboxProps) => {
+  const [checkboxDescriptionExpanded, setCheckboxDescriptionExpanded] = useState(false);
+  const [checked, setChecked] = useState<boolean>(!!props.initChecked);
+  const checkboxDescriptionButtonRef = useRef<HTMLButtonElement | null>();
+  const checkboxRef = useRef<ICheckbox>();
+  const intl = useIntl();
 
-  private moreInfoMessage = (
-    <FormattedMessage id="checkbox_more_info_message" description="More Info Tooltip title for a checkbox" defaultMessage="More Info" />
-  );
-  constructor(props: CheckboxProps) {
-    super(props);
+  const handleCheckboxDescriptionDismiss = (): void => {
+    setCheckboxDescriptionExpanded(false);
+  };
 
-    this.state = {
-      checkboxDescriptionExpanded: false,
-      checked: props.initChecked as any,
-    };
-  }
-
-  focus(): void {
-    if (this._checkboxRef) {
-      this._checkboxRef.focus();
+  const focus = () => {
+    if (checkboxRef.current) {
+      checkboxRef.current.focus();
     }
-  }
+  };
 
-  render(): JSX.Element {
-    const { ariaLabel, className, id, text, disabled } = this.props;
-    const { checked } = this.state;
+  const handleChange: React.MouseEventHandler<HTMLInputElement> = () => {
+    setChecked(!checked);
 
-    return (
-      <div className={css(className, 'msla-checkbox')}>
-        <FabricCheckbox
-          ariaLabel={ariaLabel}
-          componentRef={(e) => (this._checkboxRef = e as any)}
-          checked={checked}
-          className="msla-checkbox-label"
-          id={id}
-          label={text}
-          styles={checkboxStyles}
-          disabled={disabled}
-          onChange={this._handleChange as any}
-        />
-        {this._renderCheckboxDescriptionButton()}
-        {this._renderCheckboxDescription()}
-      </div>
-    );
-  }
-
-  private _renderCheckboxDescription(): JSX.Element | null {
-    if (!this.state.checkboxDescriptionExpanded) {
-      return null;
-    }
-
-    const { descriptionText } = this.props;
-    return (
-      <Callout
-        ariaLabel={descriptionText}
-        className="msla-checkbox-description-callout"
-        directionalHint={DirectionalHint.rightCenter}
-        gapSpace={0}
-        setInitialFocus={true}
-        styles={calloutContentStyles}
-        target={this._checkboxDescriptionButtonRef.current}
-        onDismiss={this._handleCheckboxDescriptionDismiss}>
-        <div data-is-focusable={true} role="dialog" tabIndex={0}>
-          {descriptionText}
-        </div>
-      </Callout>
-    );
-  }
-
-  private _renderCheckboxDescriptionButton(): JSX.Element | null {
-    if (!this.props.descriptionText) {
-      return null;
-    }
-
-    return (
-      <button
-        ref={this._checkboxDescriptionButtonRef}
-        aria-label={'More Info'}
-        className="msla-button msla-checkbox-description-icon-button"
-        title={'More Info'}
-        onClick={this._handleCheckboxDescriptionButtonClick}>
-        <Icon className="msla-checkbox-description-icon" iconName="Info" />
-      </button>
-    );
-  }
-
-  private _handleChange: React.MouseEventHandler<HTMLInputElement> = () => {
-    const checked = !this.state.checked;
-
-    this.setState({
-      checked,
-    });
-
-    const { onChange } = this.props;
+    const { onChange } = props;
     if (onChange) {
       onChange(checked);
     }
   };
 
-  private _handleCheckboxDescriptionButtonClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+  const handleCheckboxDescriptionButtonClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault();
-
-    this.setState({
-      checkboxDescriptionExpanded: !this.state.checkboxDescriptionExpanded,
-    });
+    setCheckboxDescriptionExpanded(!checkboxDescriptionExpanded);
   };
 
-  private _handleCheckboxDescriptionDismiss = (): void => {
-    this.setState({
-      checkboxDescriptionExpanded: false,
-    });
-  };
-}
+  const { ariaLabel, className, id, text, disabled } = props;
+
+  const moreInfoMessage = intl.formatMessage({
+    id: 'ui_checkbox_moreinfo',
+    defaultMessage: 'More Info',
+  });
+  return (
+    <div className={css(className, 'msla-checkbox')}>
+      <FabricCheckbox
+        ariaLabel={ariaLabel}
+        componentRef={(e) => (checkboxRef.current = e as any)}
+        checked={checked}
+        className="msla-checkbox-label"
+        id={id}
+        label={text}
+        styles={checkboxStyles}
+        disabled={disabled}
+        onChange={handleChange as any}
+      />
+      {props.descriptionText ? (
+        <button
+          ref={(ref) => (checkboxDescriptionButtonRef.current = ref)}
+          aria-label={moreInfoMessage}
+          className="msla-button msla-checkbox-description-icon-button"
+          title={moreInfoMessage}
+          onClick={handleCheckboxDescriptionButtonClick}>
+          <Icon className="msla-checkbox-description-icon" iconName="Info" />
+        </button>
+      ) : null}
+      {checkboxDescriptionExpanded ? (
+        <Callout
+          ariaLabel={props.descriptionText}
+          className="msla-checkbox-description-callout"
+          directionalHint={DirectionalHint.rightCenter}
+          gapSpace={0}
+          setInitialFocus={true}
+          styles={calloutContentStyles}
+          target={checkboxDescriptionButtonRef.current}
+          onDismiss={handleCheckboxDescriptionDismiss}>
+          <div data-is-focusable={true} role="dialog" tabIndex={0}>
+            {props.descriptionText}
+          </div>
+        </Callout>
+      ) : null}
+    </div>
+  );
+};
