a826b6d73d569ddabbb2ac88be98512e5b7e0774
Travis Harris <travisharris@microsoft.com>
2021-09-20 14:04:10 -0700

    Fix One Loc Build

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Checkbox } from '@designer/ui/checkbox';
 import { ThemeProvider } from '@fluentui/react';
 import { AzureThemeLight } from '../azure-themes';
-import { AnnouncedMatches } from '@designer/ui/announcedmatches';
 import { IntlProvider } from 'react-intl';
 const theme = AzureThemeLight;
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <IntlProvider locale="en">
-        <Checkbox descriptionText="Hello World" text="Hello World" />
+        <Checkbox descriptionText="Hello World" text="Hello Tony!" />
       </IntlProvider>
     </ThemeProvider>
   );
