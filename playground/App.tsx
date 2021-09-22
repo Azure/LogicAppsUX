5524f44829b75c2bc753474945ab1c33fc5d44cc
Travis Harris <travisharris@microsoft.com>
2021-09-21 21:20:19 -0700

    trying something

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { DesignerProvider } from '@designer/core/DesignerProvider';
 import { BJSWorkflowProvider } from '@designer/core';
+import { ReactFlowFromWorkflow } from './playground-entries/react-flow-from-workflow';
 
 function App() {
   return (
     <DesignerProvider locale="en">
       <BJSWorkflowProvider workflow={{} as any}>
-        <div>Hello World</div>
+        <ReactFlowFromWorkflow />
       </BJSWorkflowProvider>
     </DesignerProvider>
   );
