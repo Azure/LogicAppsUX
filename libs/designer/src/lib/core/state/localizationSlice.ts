4ccfaf7ba4ccf9f8c9778249169c3a3e1b72864d
Travis Harris <travisharris@Traviss-MacBook-Pro.local>
2021-10-07 01:04:33 -0700

    Add rest of loc langs

--- libs/designer/src/lib/core/state/localizationSlice.ts
+++ libs/designer/src/lib/core/state/localizationSlice.ts
@@ -2,7 +2,23 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { MessageFormatElement } from 'react-intl';
 import fr from '../../Localize/compiled-lang/strings.fr.json';
 import en from '../../Localize/compiled-lang/strings.json';
-
+import enxa from '../../Localize/compiled-lang/strings.en-XA.json';
+import cs from '../../Localize/compiled-lang/strings.cs.json';
+import de from '../../Localize/compiled-lang/strings.de.json';
+import es from '../../Localize/compiled-lang/strings.es.json';
+import hu from '../../Localize/compiled-lang/strings.hu.json';
+import it from '../../Localize/compiled-lang/strings.it.json';
+import ja from '../../Localize/compiled-lang/strings.ja.json';
+import ko from '../../Localize/compiled-lang/strings.ko.json';
+import nl from '../../Localize/compiled-lang/strings.nl.json';
+import pl from '../../Localize/compiled-lang/strings.pl.json';
+import ptbr from '../../Localize/compiled-lang/strings.pt-BR.json';
+import ptpt from '../../Localize/compiled-lang/strings.pt-PT.json';
+import ru from '../../Localize/compiled-lang/strings.ru.json';
+import sv from '../../Localize/compiled-lang/strings.sv.json';
+import tr from '../../Localize/compiled-lang/strings.tr.json';
+import zhhans from '../../Localize/compiled-lang/strings.zh-Hans.json';
+import zhhant from '../../Localize/compiled-lang/strings.zh-Hant.json';
 export interface LocalizationState {
   locale: string;
   defaultLocale: string;
@@ -11,44 +27,42 @@ export interface LocalizationState {
 
 const loadLocaleData = async (locale: string) => {
   switch (locale.split('-')[0].toLowerCase()) {
+    case 'nl':
+      return nl;
+    case 'pl':
+      return pl;
+    case 'pt':
+      if (locale === 'pt-BR') return ptbr;
+      else return ptpt;
+    case 'ru':
+      return ru;
+    case 'sv':
+      return sv;
+    case 'tr':
+      return tr;
+    case 'zh':
+      if (locale === 'zh-Hans') return zhhans;
+      else return zhhant;
     case 'fr':
       return fr;
-    // case 'en':
-    //   if (locale === 'en-XA')
-    //     return import('../../Localize/compiled-lang/strings.en-XA.json');
-    //   else return import('../../Localize/compiled-lang/strings.json');
-    // case 'cs':
-    //   return import('../../Localize/compiled-lang/strings.cs.json');
-    // case 'de':
-    //   return import('../../Localize/compiled-lang/strings.de.json');
-    // case 'es':
-    //   return import('../../Localize/compiled-lang/strings.es.json');
-    // case 'hu':
-    //   return import('../../Localize/compiled-lang/strings.hu.json');
-    // case 'it':
-    //   return import('../../Localize/compiled-lang/strings.it.json');
-    // case 'ja':
-    //   return import('../../Localize/compiled-lang/strings.ja.json');
-    // case 'ko':
-    //   return import('../../Localize/compiled-lang/strings.ko.json');
-    // case 'nl':
-    //   return import('../../Localize/compiled-lang/strings.nl.json');
-    // case 'pl':
-    //   return import('../../Localize/compiled-lang/strings.pl.json');
-    // case 'pt':
-    //   if (locale === 'pt-BR')
-    //     return import('../../Localize/compiled-lang/strings.pt-BR.json');
-    //   else return import('../../Localize/compiled-lang/strings.pt-PT.json');
-    // case 'ru':
-    //   return import('../../Localize/compiled-lang/strings.ru.json');
-    // case 'sv':
-    //   return import('../../Localize/compiled-lang/strings.sv.json');
-    // case 'tr':
-    //   return import('../../Localize/compiled-lang/strings.tr.json');
-    // case 'zh':
-    //   if (locale === 'zh-Hans')
-    //     return import('../../Localize/compiled-lang/strings.zh-Hans.json');
-    //   else return import('../../Localize/compiled-lang/strings.zh-Hant.json');
+    case 'en':
+      if (locale === 'en-XA') return enxa;
+      else return en;
+    case 'cs':
+      return cs;
+    case 'de':
+      return de;
+    case 'es':
+      return es;
+    case 'hu':
+      return hu;
+    case 'it':
+      return it;
+    case 'ja':
+      return ja;
+    case 'ko':
+      return ko;
+
     default:
       return en;
   }
