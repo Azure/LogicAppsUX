f8575f0cc7c36d9bc3499681a57850590095852a
Travis Harris <travisharris@Traviss-MacBook-Pro-2.local>
2021-10-09 07:17:26 -0700

    Branding changes

---
+++ libs/designer/.storybook/mytheme.js
@@ -0,0 +1,10 @@
+// .storybook/YourTheme.js
+
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Microsoft Azure Logic Apps Designer',
+    brandUrl: 'https://azure.microsoft.com/en-us/services/logic-apps/',
+    brandImage: 'https://logos-world.net/wp-content/uploads/2021/05/Azure-Logo.png',
+});
