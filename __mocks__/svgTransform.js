37d7c2d1dc0a685bfc087c5c5205513021c98b48
Travis Harris <travisharris@Traviss-MacBook-Pro-2.local>
2021-10-13 16:15:21 -0400

    Put jest SVG back

--- __mocks__/svgTransform.js
+++ __mocks__/svgTransform.js
@@ -4,7 +4,7 @@
 const path = require('path');
 module.exports = {
   process(src, filename) {
-    return src;
+    return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
   },
   // getCacheKey() {
   //     // The output is always the same.
