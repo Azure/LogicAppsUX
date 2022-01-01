c9ad716df84943332a9a6410aec1ddb38961e52c
Travis Harris <me@travis.dev>
2021-12-31 20:21:31 -0800

    Upgrade to latest version of NX

--- libs/designer/.storybook/main.js
+++ libs/designer/.storybook/main.js
@@ -3,6 +3,7 @@ const rootMain = require('../../../.storybook/main');
 module.exports = {
   ...rootMain,
 
+  core: { ...rootMain.core, builder: 'webpack5' },
   stories: [...rootMain.stories, '../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [...rootMain.addons, '@nrwl/react/plugins/storybook'],
   webpackFinal: async (config, { configType }) => {
