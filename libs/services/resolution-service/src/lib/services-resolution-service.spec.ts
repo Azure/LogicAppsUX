bd03a670c4ee70a8b17caef81834720ae8e0ee1a
Krrish Mittal 🕺 ⚽️ <krmitta@microsoft.com>
2022-01-19 11:40:32 -0800

    delete boilerplate code

--- libs/services/resolution-service/src/lib/services-resolution-service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { servicesResolutionService } from './services-resolution-service';
-
-describe('servicesResolutionService', () => {
-  it('should work', () => {
-    expect(servicesResolutionService()).toEqual('services-resolution-service');
-  });
-});
