ead7f423f21625ef2761108d5e957cc6dc02f3ea
Krrish Mittal 🕺 ⚽️ <krmitta@microsoft.com>
2022-01-19 15:22:19 -0800

    helpers added

---
+++ libs/services/resolution-service/src/lib/common/exceptions/expression.ts
@@ -0,0 +1,8 @@
+export enum ExpressionExceptionCode {
+    UNRECOGNIZED_EXPRESSION = 'UnrecognizedExpression',
+    EMPTY_VALUE = 'EmptyValue',
+    LIMIT_EXCEEDED = 'LimitExceeded',
+    STRING_LITERAL_NOT_TERMINATED = 'StringLiteralNotTerminated',
+    TOKEN_NOT_FOUND = 'TokenNotFound',
+    UNEXPECTED_CHARACTER = 'UnexpectedCharacter',
+}
