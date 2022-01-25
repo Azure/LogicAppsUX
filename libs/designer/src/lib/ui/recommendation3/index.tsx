7b2ffbcc19a2eda4a75fdcb8b201ad748f9233ed
Travis Harris <travisharris@Traviss-MBP.localdomain>
2022-01-25 10:09:19 -0800

    Remove Recommendation 3 component until it can be revisited

--- libs/designer/src/lib/ui/recommendation3/index.tsx
+++
@@ -1,6 +0,0 @@
-export * from './badge';
-export * from './categorypivot';
-export * from './documentationitem';
-export * from './errormessage';
-export * from './expandconnectorsbutton';
-export * from './uservoice';
