1fedb64bd91174f0753ff342f11c428617064100
Eric-B-Wu <95886809+Eric-B-Wu@users.noreply.github.com>
2022-01-31 11:55:41 -0500

    Added cypress test (#89)
    * Added cypress test
    
    * formatting
    
    * added msla to classname

--- apps/designer-e2e/src/integration/MicrosoftLogicAppsDesigner/MicrosoftLogicAppsDesigner.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('designer: MicrosoftLogicAppsDesigner component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=components-actionbuttonv2--standard'));
-
-  it('should render the component', () => {
-    cy.get('button').invoke('attr', 'aria-label').should('eq', "I'm an action button");
-  });
-});
