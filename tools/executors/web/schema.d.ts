116d912b706c825f5d2dfbc077a568fd88d8b2a5
Travis Harris <hartra344@users.noreply.github.com>
2022-02-04 11:50:08 -0800

    Move to using a custom library package process (#113)
    * Add custom executor for lib build
    
    * revertLoc
    
    Co-authored-by: Travis Harris <travisharris@Traviss-MBP.localdomain>

---
+++ tools/executors/web/schema.d.ts
@@ -0,0 +1,23 @@
+type Compiler = 'babel' | 'swc';
+
+export interface Globals {
+  moduleId: string;
+  global: string;
+}
+
+export interface WebRollupOptions {
+  outputPath: string;
+  tsConfig: string;
+  project: string;
+  entryFile: string;
+  extractCss?: boolean;
+  globals?: Globals[];
+  external?: string[];
+  rollupConfig?: string | string[];
+  watch?: boolean;
+  assets?: any[];
+  updateBuildableProjectDepsInPackageJson?: boolean;
+  buildableProjectDepsInPackageJsonType?: 'dependencies' | 'peerDependencies';
+  umdName?: string;
+  deleteOutputPath?: boolean;
+}
