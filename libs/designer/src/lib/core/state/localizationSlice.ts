8a494ecb34f254d4e4f3db71147eda2f3af64beb
Travis Harris <travisharris@Traviss-MBP.localdomain>
2022-02-04 01:32:54 -0800

    revertLoc

--- libs/designer/src/lib/core/state/localizationSlice.ts
+++ libs/designer/src/lib/core/state/localizationSlice.ts
@@ -1,24 +1,24 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { MessageFormatElement } from 'react-intl';
-// import fr from '../../../../../../Localize/compiled-lang/strings.fr.json';
+import fr from '../../../../../../Localize/compiled-lang/strings.fr.json';
 import en from '../../../../../../Localize/compiled-lang/strings.json';
-// import enxa from '../../../../../../Localize/compiled-lang/strings.en-XA.json';
-// import cs from '../../../../../../Localize/compiled-lang/strings.cs.json';
-// import de from '../../../../../../Localize/compiled-lang/strings.de.json';
-// import es from '../../../../../../Localize/compiled-lang/strings.es.json';
-// import hu from '../../../../../../Localize/compiled-lang/strings.hu.json';
-// import it from '../../../../../../Localize/compiled-lang/strings.it.json';
-// import ja from '../../../../../../Localize/compiled-lang/strings.ja.json';
-// import ko from '../../../../../../Localize/compiled-lang/strings.ko.json';
-// import nl from '../../../../../../Localize/compiled-lang/strings.nl.json';
-// import pl from '../../../../../../Localize/compiled-lang/strings.pl.json';
-// import ptbr from '../../../../../../Localize/compiled-lang/strings.pt-BR.json';
-// import ptpt from '../../../../../../Localize/compiled-lang/strings.pt-PT.json';
-// import ru from '../../../../../../Localize/compiled-lang/strings.ru.json';
-// import sv from '../../../../../../Localize/compiled-lang/strings.sv.json';
-// import tr from '../../../../../../Localize/compiled-lang/strings.tr.json';
-// import zhhans from '../../../../../../Localize/compiled-lang/strings.zh-Hans.json';
-// import zhhant from '../../../../../../Localize/compiled-lang/strings.zh-Hant.json';
+import enxa from '../../../../../../Localize/compiled-lang/strings.en-XA.json';
+import cs from '../../../../../../Localize/compiled-lang/strings.cs.json';
+import de from '../../../../../../Localize/compiled-lang/strings.de.json';
+import es from '../../../../../../Localize/compiled-lang/strings.es.json';
+import hu from '../../../../../../Localize/compiled-lang/strings.hu.json';
+import it from '../../../../../../Localize/compiled-lang/strings.it.json';
+import ja from '../../../../../../Localize/compiled-lang/strings.ja.json';
+import ko from '../../../../../../Localize/compiled-lang/strings.ko.json';
+import nl from '../../../../../../Localize/compiled-lang/strings.nl.json';
+import pl from '../../../../../../Localize/compiled-lang/strings.pl.json';
+import ptbr from '../../../../../../Localize/compiled-lang/strings.pt-BR.json';
+import ptpt from '../../../../../../Localize/compiled-lang/strings.pt-PT.json';
+import ru from '../../../../../../Localize/compiled-lang/strings.ru.json';
+import sv from '../../../../../../Localize/compiled-lang/strings.sv.json';
+import tr from '../../../../../../Localize/compiled-lang/strings.tr.json';
+import zhhans from '../../../../../../Localize/compiled-lang/strings.zh-Hans.json';
+import zhhant from '../../../../../../Localize/compiled-lang/strings.zh-Hant.json';
 export interface LocalizationState {
   locale: string;
   defaultLocale: string;
@@ -27,41 +27,41 @@ export interface LocalizationState {
 
 const loadLocaleData = async (locale: string) => {
   switch (locale.split('-')[0].toLowerCase()) {
-    // case 'nl':
-    //   return nl;
-    // case 'pl':
-    //   return pl;
-    // case 'pt':
-    //   if (locale === 'pt-BR') return ptbr;
-    //   else return ptpt;
-    // case 'ru':
-    //   return ru;
-    // case 'sv':
-    //   return sv;
-    // case 'tr':
-    //   return tr;
-    // case 'zh':
-    //   if (locale === 'zh-Hans') return zhhans;
-    //   else return zhhant;
-    // case 'fr':
-    //   return fr;
-    // case 'en':
-    //   if (locale === 'en-XA') return enxa;
-    //   else return en;
-    // case 'cs':
-    //   return cs;
-    // case 'de':
-    //   return de;
-    // case 'es':
-    //   return es;
-    // case 'hu':
-    //   return hu;
-    // case 'it':
-    //   return it;
-    // case 'ja':
-    //   return ja;
-    // case 'ko':
-    //   return ko;
+    case 'nl':
+      return nl;
+    case 'pl':
+      return pl;
+    case 'pt':
+      if (locale === 'pt-BR') return ptbr;
+      else return ptpt;
+    case 'ru':
+      return ru;
+    case 'sv':
+      return sv;
+    case 'tr':
+      return tr;
+    case 'zh':
+      if (locale === 'zh-Hans') return zhhans;
+      else return zhhant;
+    case 'fr':
+      return fr;
+    case 'en':
+      if (locale === 'en-XA') return enxa;
+      else return en;
+    case 'cs':
+      return cs;
+    case 'de':
+      return de;
+    case 'es':
+      return es;
+    case 'hu':
+      return hu;
+    case 'it':
+      return it;
+    case 'ja':
+      return ja;
+    case 'ko':
+      return ko;
 
     default:
       return en;
