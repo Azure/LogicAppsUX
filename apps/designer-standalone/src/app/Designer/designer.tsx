3fbe6006b6c3f5c80c600de9fa8cbee0c9e137db
Travis Harris <hartra344@users.noreply.github.com>
2022-02-06 14:25:32 -0800

    Simplify the standalone designer a bit and make it easier to load hardcoded workflow files for graph testing (#134)
    * Simplify the standalone designer a bit and make it easier to load hardcoded workflow files for graph testing
    
    * Move checkbox to top of form
    
    Co-authored-by: Travis Harris <travisharris@Traviss-MBP.localdomain>

--- apps/designer-standalone/src/app/Designer/designer.tsx
+++ apps/designer-standalone/src/app/Designer/designer.tsx
@@ -1,21 +1,19 @@
 import { DesignerProvider, BJSWorkflowProvider, Designer } from '@microsoft/logic-apps-designer';
+import { useSelector } from 'react-redux';
 import { SettingsBox } from '../../components/settings_box';
+import { RootState } from '../../state/store';
 
-export interface DesignerWrapperProps {
-  workflow: LogicAppsV2.WorkflowDefinition;
-  setResourceId: (res: string) => void;
-  setToken: (res: string) => void;
-  token?: string | null;
-  resourceId?: string | null;
-}
-export const DesignerWrapper = ({ workflow, setResourceId, setToken, resourceId, token }: DesignerWrapperProps) => {
+export const DesignerWrapper = () => {
+  const workflow = useSelector((state: RootState) => state.workflowLoader.workflowDefinition);
   return (
     <>
-      <SettingsBox setResourceId={setResourceId} setToken={setToken} resourceId={resourceId} token={token} />
+      <SettingsBox />
       <DesignerProvider locale="en-US">
-        <BJSWorkflowProvider workflow={workflow}>
-          <Designer></Designer>
-        </BJSWorkflowProvider>
+        {workflow ? (
+          <BJSWorkflowProvider workflow={workflow}>
+            <Designer></Designer>
+          </BJSWorkflowProvider>
+        ) : null}
       </DesignerProvider>
     </>
   );
