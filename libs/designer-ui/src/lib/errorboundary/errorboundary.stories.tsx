01fd1876ebaccda3533abe1f77846dc95a0cc074
Joe Chung <joechung@microsoft.com>
2022-02-06 14:55:51 -0500

    Add controls to error boundary story (#133)

--- libs/designer-ui/src/lib/errorboundary/errorboundary.stories.tsx
+++ libs/designer-ui/src/lib/errorboundary/errorboundary.stories.tsx
@@ -1,34 +1,27 @@
-// Button.stories.ts | Button.stories.tsx
-
-import React from 'react';
+// errorboundary.stories.js|jsx|ts|tsx
 
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ErrorBoundary, ErrorBoundaryProps } from './index';
 
-import { ErrorBoundary } from './index';
 export default {
   component: ErrorBoundary,
   title: 'Components/ErrorBoundary',
 } as ComponentMeta<typeof ErrorBoundary>;
 
-const Fallback = () => {
-  return <div>Fallback rendered when error occurs</div>;
-};
+const Fallback = () => <div>Fallback rendered when error occurs</div>;
 const ThrowError = () => {
   throw new Error("I'm an error");
 };
 
-export const Standard: ComponentStory<typeof ErrorBoundary> = () => {
-  return (
-    <ErrorBoundary>
-      <ThrowError />
-    </ErrorBoundary>
-  );
-};
+const Template: ComponentStory<typeof ErrorBoundary> = (args: ErrorBoundaryProps) => (
+  <ErrorBoundary {...args}>
+    <ThrowError />
+  </ErrorBoundary>
+);
 
-export const WithAFallback: ComponentStory<typeof ErrorBoundary> = () => {
-  return (
-    <ErrorBoundary fallback={<Fallback />}>
-      <ThrowError />
-    </ErrorBoundary>
-  );
+export const Standard = Template.bind({});
+
+export const WithFallback = Template.bind({});
+WithFallback.args = {
+  fallback: <Fallback />,
 };
