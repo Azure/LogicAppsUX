6906bca07df643179ced9186b0a43d5fd3c20462
Travis Harris <hartra344@users.noreply.github.com>
2022-02-06 14:25:32 -0800

    Simplify the standalone designer a bit and make it easier to load hardcoded workflow files for graph testing (#134)
    * Simplify the standalone designer a bit and make it easier to load hardcoded workflow files for graph testing
    
    * Move checkbox to top of form
    
    Co-authored-by: Travis Harris <travisharris@Traviss-MBP.localdomain>

--- apps/designer-standalone/src/components/settings_box.tsx
+++ apps/designer-standalone/src/components/settings_box.tsx
@@ -3,12 +3,7 @@ import { css } from '@fluentui/utilities';
 import { Login } from '../app/Login/login';
 import styles from './settings_box.module.less';
 
-export const SettingsBox = (props: {
-  setResourceId: (res: string) => void;
-  setToken: (res: string) => void;
-  token?: string | null;
-  resourceId?: string | null;
-}) => {
+export const SettingsBox = () => {
   const [active, toggleActive] = useBoolean(false);
   const [showLoadSettings, toggleLoadSettings] = useBoolean(true);
   const cs = css(styles.toybox, active && styles.active);
@@ -23,11 +18,7 @@ export const SettingsBox = (props: {
         <h4 onClick={toggleLoadSettings.toggle}>
           <span className={css(showLoadSettings && styles.openIcon)}>▼</span> Workflow Load Settings
         </h4>
-        <div className={styles.content}>
-          {showLoadSettings && (
-            <Login setResourceId={props.setResourceId} setToken={props.setToken} resourceId={props.resourceId} token={props.token}></Login>
-          )}
-        </div>
+        <div className={styles.content}>{showLoadSettings && <Login></Login>}</div>
       </div>
     </div>
   );
