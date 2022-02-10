8761c3a12cb98d83cbd52ec7cc69fac94b40677b
Joe Chung <joechung@microsoft.com>
2022-02-10 00:53:03 -0500

    Fix status pill bugs (#149)

--- libs/designer-ui/src/lib/monitoring/statuspill/images/waiting.tsx
+++ libs/designer-ui/src/lib/monitoring/statuspill/images/waiting.tsx
@@ -9,7 +9,7 @@ export function Waiting({ isInverted }: WaitingProps): JSX.Element {
   const pathFill = isInverted ? '#979593' : '#979593';
 
   return (
-    <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="24" height="24" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g filter="url(#filter0_d)">
         <circle cx="16" cy="16" r="14" fill={circleFill} />
       </g>
