88d1cfd5bccc66b609e7c35fc36fc5c048b77caa
Joe Chung <joechung@microsoft.com>
2022-02-13 14:55:22 -0500

    refactor: Update card components (#214)

---
+++ libs/designer-ui/src/lib/card/utils.ts
@@ -0,0 +1,44 @@
+import { IButtonStyles } from '@fluentui/react';
+import { hexToRgbA } from '@microsoft-logic-apps/utils';
+import Constants from '../constants';
+
+export function getCardStyle(brandColor?: string): React.CSSProperties {
+  return {
+    borderLeft: `4px solid ${getBrandColorRgbA(brandColor, /* opacity */ 1)}`,
+    borderRadius: '2px',
+  };
+}
+
+export function getCardButtonsStyle(themeColor: string): IButtonStyles {
+  return {
+    icon: {
+      color: themeColor,
+      width: 15,
+      height: 15,
+    },
+    flexContainer: {
+      width: 15,
+      height: 15,
+    },
+    root: {
+      marginTop: 11,
+      padding: 0,
+      margin: 0,
+      width: 15,
+      marginLeft: 12,
+      marginRight: 10,
+    },
+  };
+}
+
+export function getHeaderStyle(brandColor?: string): React.CSSProperties | undefined {
+  return brandColor ? { backgroundColor: getBrandColorRgbA(brandColor, /* opacity */ 1) } : undefined;
+}
+
+function getBrandColorRgbA(brandColor = Constants.DEFAULT_BRAND_COLOR, opacity = Constants.HEADER_AND_TOKEN_OPACITY): string {
+  try {
+    return hexToRgbA(brandColor, opacity);
+  } catch {
+    return hexToRgbA(Constants.DEFAULT_BRAND_COLOR, opacity);
+  }
+}
