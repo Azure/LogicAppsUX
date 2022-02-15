875047e2f4f0c5f063193bcd233d7a138d0ff36c
Travis Harris <travisharris@Traviss-MBP.localdomain>
2022-02-15 14:21:20 -0800

    Add full localization code to the intl library

--- libs/designer/src/lib/core/DesignerProvider.tsx
+++ libs/designer/src/lib/core/DesignerProvider.tsx
@@ -1,11 +1,10 @@
 import { ThemeProvider, Theme } from '@fluentui/react';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { IntlProvider } from '@microsoft-logic-apps/intl';
 import { AzureThemeLight } from '@fluentui/azure-themes/lib/azure/AzureThemeLight';
-import { RootState, store } from './store';
-import { Provider, useDispatch, useSelector } from 'react-redux';
+import { store } from './store';
+import { Provider } from 'react-redux';
 import { ProviderWrappedContext } from './ProviderWrappedContext';
-import { loadLocaleMessages } from './state/localizationSlice';
 
 export interface DesignerProviderProps {
   theme?: Theme;
@@ -14,19 +13,13 @@ export interface DesignerProviderProps {
 }
 
 const DesignerProviderInner = ({ theme = AzureThemeLight, locale = 'en', children }: DesignerProviderProps) => {
-  const i18n = useSelector((state: RootState) => state.localization);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadLocaleMessages(locale));
-  }, [dispatch, locale]);
   return (
     <ProviderWrappedContext.Provider value={true}>
       <ThemeProvider theme={theme} className="msla-theme-provider">
         <Provider store={store}>
           <IntlProvider
-            locale={i18n.locale}
-            defaultLocale={i18n.defaultLocale}
-            messages={i18n.messages}
+            locale={locale}
+            defaultLocale={locale}
             onError={(err) => {
               if (err.code === 'MISSING_TRANSLATION') {
                 return;
