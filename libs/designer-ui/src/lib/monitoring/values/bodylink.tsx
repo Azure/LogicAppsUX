244b9cc35e18043ec0eddd6f512a9124ebb403b4
Joe Chung <joechung@microsoft.com>
2022-02-23 14:52:58 -0500

    fix: Fix accessibility of monitoring value components (#275)

--- libs/designer-ui/src/lib/monitoring/values/bodylink.tsx
+++ libs/designer-ui/src/lib/monitoring/values/bodylink.tsx
@@ -1,21 +1,26 @@
 import { Link } from '@fluentui/react';
-import { useId } from '@fluentui/react-hooks';
 import { useIntl } from 'react-intl';
 import { RawValue } from './raw';
 import type { ValueProps } from './types';
 import { isContentLink } from './utils';
 
 export const BodyLinkValue: React.FC<ValueProps> = (props) => {
-  const id = useId('msla-body-link');
+  const { displayName, value, visible = true } = props;
   const intl = useIntl();
   const Resources = {
+    DOWNLOAD_PROMPT_ARIA_LABEL: intl.formatMessage(
+      {
+        defaultMessage: "Alt/Option + click to download '{displayName}'",
+        description: 'ARIA label text for the download link',
+      },
+      { displayName }
+    ),
     DOWNLOAD_PROMPT_MESSAGE: intl.formatMessage({
       defaultMessage: 'Download (Alt/Option + click)',
       description: 'Link text for the prompt to download large inputs or outputs',
     }),
   };
 
-  const { displayName, value, visible = true } = props;
   if (!visible) {
     return null;
   } else if (!isContentLink(value)) {
@@ -24,11 +29,9 @@ export const BodyLinkValue: React.FC<ValueProps> = (props) => {
 
   return (
     <section className="msla-trace-value-label">
-      <label className="msla-trace-value-display-name" id={id}>
-        {displayName}
-      </label>
+      <label className="msla-trace-value-display-name">{displayName}</label>
       <div className="msla-trace-value-text">
-        <Link aria-labelledby={id} href={value.uri} rel="noopener" target="_blank">
+        <Link aria-label={Resources.DOWNLOAD_PROMPT_ARIA_LABEL} href={value.uri} rel="noopener" target="_blank">
           {Resources.DOWNLOAD_PROMPT_MESSAGE}
         </Link>
       </div>
