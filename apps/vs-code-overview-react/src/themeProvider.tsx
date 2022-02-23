e3fc695b4fef4e505ab4c2fd43eb3a84d6ac4da9
Travis Harris <hartra344@users.noreply.github.com>
2022-02-22 17:43:13 -0800

    fix: Fix issue where overview will initially be light theme even in dark mode (#267)
    * fix: Fix issue where overview will initially be light theme even in dark mode
    
    * Small Fix

--- apps/vs-code-overview-react/src/themeProvider.tsx
+++ apps/vs-code-overview-react/src/themeProvider.tsx
@@ -2,17 +2,21 @@ import { useEffect, useMemo, useState } from 'react';
 import { AzureThemeDark, AzureThemeLight } from '@fluentui/azure-themes';
 import { ThemeProvider as FluentThemeProvider } from '@fluentui/react';
 
+const getTheme = (element: HTMLElement): 'light' | 'dark' => {
+  const { classList } = element;
+  const isInverted = classList.contains('vscode-dark');
+  const theme = isInverted ? 'dark' : 'light';
+  return theme;
+};
 export const ThemeProvider: React.FC = ({ children }) => {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+  const [theme, setTheme] = useState<'light' | 'dark'>(getTheme(document.body));
 
   const observer = useMemo(
     () =>
       new MutationObserver((mutationRecord) => {
         for (const mutation of mutationRecord) {
           if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-            const { classList } = mutation.target as HTMLElement;
-            const isInverted = classList.contains('vscode-dark');
-            const newTheme = isInverted ? 'dark' : 'light';
+            const newTheme = getTheme(mutation.target as HTMLElement);
             if (theme !== newTheme) {
               setTheme(newTheme);
             }
