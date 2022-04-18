6cc717e98671228c0368551df317a76921107074
Andrew Fowose <andrewfowose@Andrews-MacBook-Pro.local>
2022-04-18 16:34:53 -0400

    adding setting types to module

--- libs/designer/src/lib/ui/panel/panelTabs/settingsTab.tsx
+++ libs/designer/src/lib/ui/panel/panelTabs/settingsTab.tsx
@@ -1,16 +1,14 @@
-import { SettingsSection } from "@microsoft/designer-ui";
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { ReactiveToggle } from '../../../../../../designer-ui/src/lib/settings/settingsection/settingreactiveinput';
+import { SettingsSection, ReactiveToggle } from '@microsoft/designer-ui';
 
 export const SettingsTab = () => {
-    const settingSectionProps = {
-        id: 'textFieldandToggle',
-        title:'Reactive Toggle',
-        expanded:false,
-        textFieldValue:'',
-        renderContent:ReactiveToggle,
-        isInverted:false,
-        isReadOnly:false
-    }
-    return <SettingsSection {...settingSectionProps}/>;
+  const settingSectionProps = {
+    id: 'textFieldandToggle',
+    title: 'Reactive Toggle',
+    expanded: false,
+    textFieldValue: '',
+    renderContent: ReactiveToggle,
+    isInverted: false,
+    isReadOnly: false,
+  };
+  return <SettingsSection {...settingSectionProps} />;
 };
