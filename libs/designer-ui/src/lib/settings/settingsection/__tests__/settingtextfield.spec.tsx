a1a6bfed040ddd37051a8ac25c3e72562f83d40f
Andrew Fowose <usafowose@gmail.com>
2022-04-20 17:09:59 -0400

    adding test assertion

--- libs/designer-ui/src/lib/settings/settingsection/__tests__/settingtextfield.spec.tsx
+++ libs/designer-ui/src/lib/settings/settingsection/__tests__/settingtextfield.spec.tsx
@@ -5,19 +5,20 @@ import renderer from 'react-test-renderer';
 import * as ReactShallowRenderer from 'react-test-renderer/shallow';
 
 describe('ui/settings/settingtextfield', () => {
-    let minimal: SettingTextFieldProps, shallow: ReactShallowRenderer.ShallowRenderer;
+  let minimal: SettingTextFieldProps, shallow: ReactShallowRenderer.ShallowRenderer;
 
-    beforeEach(() => {
-        minimal = { id: 'testId', isReadOnly: false, label: 'label text', value: 'sample value' };
-        shallow = ReactShallowRenderer.createRenderer();
-    });
+  beforeEach(() => {
+    minimal = { id: 'testId', isReadOnly: false, label: 'label text', value: 'sample value' };
+    shallow = ReactShallowRenderer.createRenderer();
+  });
 
-    afterEach(() => {
-        shallow.unmount();
-    });
+  afterEach(() => {
+    shallow.unmount();
+  });
 
-    it('should construct', () => {
-        renderer.create(<SettingTextField {...minimal} />);
-        const textField = shallow.getRenderOutput();
-    })
+  it('should construct', () => {
+    renderer.create(<SettingTextField {...minimal} />);
+    const textField = shallow.getRenderOutput();
+    expect(textField).toMatchSnapshot();
+  });
 });
