458ccf2e01adfaa901613c67df7a1274338faa04
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2022-04-20 18:30:32 -0400

    Settings_Unit_Tests (#368)
    * exporting interface for testing
    
    * adding test assertion
    
    * removing unused import
    
    * adding tests for parent setting section component

---
+++ libs/designer-ui/src/lib/settings/settingsection/__tests__/settingtoggle.spec.tsx
@@ -0,0 +1,22 @@
+import type { SettingToggleProps} from '../settingtoggle';
+import { RenderToggleSetting } from '../settingtoggle';
+import * as React from 'react';
+import * as ReactShallowRenderer from 'react-test-renderer/shallow';
+
+describe('ui/settings/settingtoggle', () => {
+    let minimal: SettingToggleProps, renderer: ReactShallowRenderer.ShallowRenderer;
+
+    beforeEach(() => {
+        minimal = { isReadOnly: false, checked: false };
+        renderer = ReactShallowRenderer.createRenderer();
+    });
+
+    afterEach(() => {
+        renderer.unmount();
+    });
+
+    it('should construct', () => {
+        const settingToggle = renderer.render(<RenderToggleSetting {...minimal} />);
+        expect(settingToggle).toMatchSnapshot();
+    });
+});
