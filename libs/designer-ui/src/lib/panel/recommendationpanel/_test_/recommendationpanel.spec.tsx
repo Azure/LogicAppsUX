cc6999ceafd1ae27d732f29fe1ef4f0e0f1f3f86
Danielle Cogburn <dacogburn@microsoft.com>
2022-04-22 11:13:25 -0700

    added recommendation test file

---
+++ libs/designer-ui/src/lib/panel/recommendationpanel/_test_/recommendationpanel.spec.tsx
@@ -0,0 +1,28 @@
+import type { RecommendationPanelProps } from './../recommendationpanel';
+import { RecommendationPanel } from './../recommendationpanel';
+import { ConnectorsMock } from '@microsoft-logic-apps/utils';
+import renderer from 'react-test-renderer';
+import * as ReactShallowRenderer from 'react-test-renderer/shallow';
+
+describe('recommendation panel', () => {
+  let shallow: ReactShallowRenderer.ShallowRenderer;
+  const searchMock = jest.fn();
+  const props: RecommendationPanelProps = {
+    placeholder: 'search',
+    onSearch: searchMock,
+    toggleCollapse: jest.fn(),
+    operationSearchResults: [],
+    connectorBrowse: [ConnectorsMock[0]],
+    isCollapsed: false,
+    width: '500px',
+  };
+
+  beforeEach(() => {
+    shallow = ReactShallowRenderer.createRenderer();
+  });
+
+  it('renders browse view when there are no search results', () => {
+    const component = shallow.render(<RecommendationPanel {...props}></RecommendationPanel>);
+    expect(component).toMatchSnapshot();
+  });
+});
