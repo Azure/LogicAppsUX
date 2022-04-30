8bda00a163c42ea6bfb8b158f382368752def10c
Eric <wue@microsoft.com>
2022-04-30 10:07:11 -0400

    added stories for other languages

--- libs/designer-ui/src/lib/editor/editor.stories.tsx
+++ libs/designer-ui/src/lib/editor/editor.stories.tsx
@@ -6,9 +6,28 @@ export default {
   component: CustomEditor,
   title: 'Components/Editor',
 } as ComponentMeta<typeof CustomEditor>;
-export const Standard: ComponentStory<typeof CustomEditor> = (args: EditorProps) => <CustomEditor {...args} />;
+const Template: ComponentStory<typeof CustomEditor> = (args: EditorProps) => <CustomEditor {...args} />;
 
-Standard.args = {
+export const templateExpressionLanguage = Template.bind({});
+templateExpressionLanguage.args = {
   language: EditorLanguage.templateExpressionLanguage,
-  value: 'test startOfDay() addMinutes()',
+  value: "true false\naddMinutes()\n12345\n'string'",
+};
+
+export const javascript = Template.bind({});
+javascript.args = {
+  language: EditorLanguage.javascript,
+  value: "// First line\nfunction hello() {\n\talert('Hello world!');\n}\n// Last line",
+};
+
+export const JSON = Template.bind({});
+JSON.args = {
+  language: EditorLanguage.json,
+  value: '{\n"test": true,\n"test2" : \n\t{\n\t\t"object" : "value"\n\t}\n}',
+};
+
+export const XML = Template.bind({});
+XML.args = {
+  language: EditorLanguage.xml,
+  value: "<note> \n\t<to>\n\t\t<from>\n\t\t\t<body> Don't Forget To Bring the Shampoo </body>\n\t\t</from>\n\t</to>\n</note>",
 };
