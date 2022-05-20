b5fb4b57956cbb0b5995909ca879175bdec8d340
Jennifer Lee <t-jennlee@microsoft.com>
2022-05-20 11:17:59 -0700

    set up basic parser tests with jest

---
+++ libs/data-mapper/src/lib/parser/__test__/parser.spec.ts
@@ -0,0 +1,21 @@
+import { convertJsonToMapCode } from '../utils/converters';
+
+describe('utils/converters', () => {
+  describe('convertJsonToMapCode', () => {
+    it('Most basic json to mapcode', () => {
+      // const input = "";
+      // const output = "";
+
+      // expect(convertJsonToMapCode(input)).toEqual(output);
+
+      expect(1).toEqual(1);
+    });
+
+    it('Most basic json to mapcode2', () => {
+      const input = '';
+      const output = '';
+
+      expect(convertJsonToMapCode(input)).toEqual(output);
+    });
+  });
+});
--- libs/data-mapper/src/lib/parser/utils/converters.ts
+++ libs/data-mapper/src/lib/parser/utils/converters.ts
@@ -1,3 +1,24 @@
-export function convertJsonToYamlLike(json: string): string {
+import MapLexer from '../definition/MapLexer';
+import MapParser from '../definition/MapParser';
+import MapParserVisitor from '../definition/MapParserVisitor';
+import antlr from 'antlr4';
+
+export function convertJsonToMapCode(json: string): string {
   return json;
+}
+
+export function tokenize(text: string) {
+  const input = new antlr.InputStream(text);
+  const lexer = new MapLexer(input);
+  const tokens = new antlr.CommonTokenStream(lexer);
+  const parser = new MapParser(tokens);
+
+  // const tokenStream = new TokenRewriteStream(lexer);
+
+  const mainContext = parser.main();
+
+  const visitor = new MapParserVisitor();
+  visitor.visitMain(mainContext);
+
+  return parser;
 }
