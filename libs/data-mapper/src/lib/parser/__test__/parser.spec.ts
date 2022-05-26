cecd8ca5b5323761928e0e2be7bfe203a4670b5a
Elaina Lee <t-jennlee@microsoft.com>
2022-05-26 11:30:52 -0700

    added test cases for removeNodeKey function

--- libs/data-mapper/src/lib/parser/__test__/parser.spec.ts
+++ libs/data-mapper/src/lib/parser/__test__/parser.spec.ts
@@ -8,7 +8,7 @@ import {
 } from '../__mocks__';
 import { customerOrdersJsonMock } from '../__mocks__/json/customerOrders';
 import { customerOrdersMapcodeMock } from '../__mocks__/mapcode/customerOrders';
-import { jsonToMapcode } from '../jsonToMapcodeParser';
+import { jsonToMapcode, removeNodeKey } from '../jsonToMapcodeParser';
 
 describe('jsonToMapcodeParser', () => {
   describe('convertJsonToMapCode', () => {
@@ -26,6 +26,40 @@ describe('jsonToMapcodeParser', () => {
 
     it('Test CBR Input', () => {
       expect(jsonToMapcode(CBRInputRecordJsonMock)).toEqual(CBRInputRecordMapcodeMock);
+    });
+  });
+
+  describe('removeNodeKey', () => {
+    it('Test where nodeKey and loopSource are equivalent', () => {
+      expect(removeNodeKey('/abcd/cd/', 'bcd', 'bcd')).toEqual('/acd/');
+    });
+
+    it('Test where nodeKey and loopSource are empty', () => {
+      expect(removeNodeKey('/abcd/cd/', '', '')).toEqual('abcdcd');
+    });
+
+    it('Test where only the nodeKey is empty', () => {
+      expect(removeNodeKey('/abcd/cd/', '', 'bcd')).toEqual('acd');
+    });
+
+    it('Test where only the loopSource is empty', () => {
+      expect(removeNodeKey('/abcd/cd/', 'bcd', '')).toEqual('/acd/');
+    });
+
+    it('Test where the input contains multiple nodeKey', () => {
+      expect(removeNodeKey('/abcd/bcd/cd/', 'bcd', '')).toEqual('/acd/');
+    });
+
+    it('Test where the input contains multiple nodeKey without / gaps', () => {
+      expect(removeNodeKey('/abcd/bcdbcd/cd/', 'bcd', 'fg')).toEqual('/abcdcd/');
+    });
+
+    it('Test where the input contains multiple loopSource', () => {
+      expect(removeNodeKey('/abcd/bcd', 'fg', 'bcd')).toEqual('/a/');
+    });
+
+    it('Test where the input contains multiple loopSource without / gaps', () => {
+      expect(removeNodeKey('/abcd/bcdbcd/cd/', 'fg', 'bcd')).toEqual('/a//cd/');
     });
   });
 });
