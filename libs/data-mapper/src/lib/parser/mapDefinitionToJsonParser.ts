25e8ca311b3c6df7692c0d9e7ac09125f42389b6
Elaina Lee <t-jennlee@microsoft.com>
2022-05-27 09:52:23 -0700

    added skeleton

--- libs/data-mapper/src/lib/parser/mapDefinitionToJsonParser.ts
+++ libs/data-mapper/src/lib/parser/mapDefinitionToJsonParser.ts
@@ -1,3 +1,5 @@
+import type { Node } from './types';
+
 export function mapDefinitionToJson(inputMapDefinition: string): string {
   const mapcodeLines: string[] = inputMapDefinition.split('\n');
 
@@ -9,4 +11,40 @@ export function mapDefinitionToJson(inputMapDefinition: string): string {
   }
 
   return '';
+}
+
+// function mapDefinitionToNode()
+
+function lineToNode(inputLine: string): Node | null {
+  const inputLineSplited = inputLine.split(': ');
+
+  // 1) TARGETNODEKEY
+
+  // FOR LOOPSOURCE OR CONDITION
+  if (inputLineSplited[0].startsWith('for')) {
+    //TODO: forloop. make loopSource and targetNodeKey then recursion - actually recursion in another if
+  } else if (inputLineSplited[0].startsWith('if')) {
+    //TODO: forloop. make condition and targetNodeKey then recursion - actually recursion in another if.
+  }
+
+  // FOR TARGETNODEKEY - everylevel has targetNodeKey if not for or if.
+  // TODO
+
+  // 2) VALUE OR CHILDREN
+
+  // FOR CHILDREN OR VALUE - if children, recursion; if targetValue, end of recursion.
+  if (inputLineSplited.length == 1) {
+    // need below
+    return null;
+  } else {
+    // end of the node
+    const node: Node = {
+      targetNodeKey: inputLineSplited[0],
+      targetValue: {
+        value: inputLineSplited[1],
+      },
+    };
+
+    return node;
+  }
 }
