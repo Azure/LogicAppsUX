d6085b8f192d9915c6663021e78248ebea02acf2
Elaina Lee <t-jennlee@microsoft.com>
2022-05-27 11:36:14 -0700

    changed monacoeditor to peek

--- libs/data-mapper/src/lib/parser/components/SampleDataDisplayer.tsx
+++ libs/data-mapper/src/lib/parser/components/SampleDataDisplayer.tsx
@@ -1,40 +1,24 @@
-import { MonacoEditor, EditorLanguage } from '@microsoft/designer-ui';
-import { useEffect, useState } from 'react';
+import { Peek } from '@microsoft/designer-ui';
+import { IntlProvider } from 'react-intl';
 
 export interface SampleDataDisplayerProps {
   data: string;
   onOKClick?(): void;
 }
 
-export const SampleDataDisplayer = ({ data, onOKClick }: SampleDataDisplayerProps) => {
-  const options = {
-    fontSize: 13,
-    readOnly: true,
-    scrollBeyondLastLine: false,
-    wordWrap: 'on',
-  };
-
-  const [editorStyle, setEditorStyle] = useState(getEditorStyle(data));
-
-  useEffect(() => {
-    setEditorStyle(getEditorStyle(data));
-  }, [data]);
-
+export const SampleDataDisplayer = ({ data, onOKClick }: SampleDataDisplayerProps, { locale = 'en' }) => {
   return (
-    <div>
-      Displaying the Data
-      <MonacoEditor
-        defaultValue={data}
-        fontSize={options.fontSize}
-        readOnly={options.readOnly}
-        language={EditorLanguage.json}
-        height={editorStyle}
-      />
-    </div>
+    <IntlProvider
+      locale={locale}
+      defaultLocale={locale}
+      onError={(err) => {
+        if (err.code === 'MISSING_TRANSLATION') {
+          return;
+        }
+        throw err;
+      }}
+    >
+      <Peek input={data} />
+    </IntlProvider>
   );
 };
-
-// Monaco should be at least 3 rows high (19*3 px) but no more than 20 rows high (19*20 px).
-function getEditorStyle(input = ''): number {
-  return Math.min(Math.max(input.split('\n').length * 19, 57), 380);
-}
