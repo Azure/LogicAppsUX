ed0b49d6da4bfa6a025195133aefbc56407dd00c
Travis Harris <travisharris@microsoft.com>
2022-06-03 09:49:45 -0700

    Stash

--- libs/designer/src/lib/ui/panel/panelTabs/parametersTab/index.tsx
+++ libs/designer/src/lib/ui/panel/panelTabs/parametersTab/index.tsx
@@ -1,12 +1,16 @@
 import constants from '../../../../common/constants';
+import { ProviderWrappedContext } from '../../../../core';
 import type { RootState } from '../../../../core/store';
-import type { PanelTab } from '@microsoft/designer-ui';
+import type { PanelTab, SettingSectionProps } from '@microsoft/designer-ui';
+import { SettingsSection } from '@microsoft/designer-ui';
+import { useContext } from 'react';
 import { useSelector } from 'react-redux';
 
 export const ParametersTab = () => {
   // TODO: Retrieve logic from a redux store?
   const selectedNode = useSelector((state: RootState) => state.panel.selectedNode);
   const parameters = useSelector((state: RootState) => state.operations.inputParameters[selectedNode]);
+  const { readOnly } = useContext(ProviderWrappedContext) ?? {};
   if (parameters?.isLoading) {
     return <div>Loading</div>;
   }
@@ -14,16 +18,26 @@ export const ParametersTab = () => {
   return (
     <>
       {Object.keys(parameters?.parameterGroups ?? {}).map((key) => {
+        const settingSectionProps: SettingSectionProps = {
+          id: 'defaultID',
+          title: 'Default',
+          expanded: false,
+          settings: parameters.parameterGroups[key]?.parameters.map((param) => {
+            return {
+              settingType: 'SettingTextField',
+              settingProp: {
+                readOnly,
+                id: param.id,
+                label: param.label,
+                value: param.value,
+              },
+            };
+          }),
+        };
         return (
           <>
             {key === 'default' ? null : <h3>{key}</h3>}
-            {parameters.parameterGroups[key]?.parameters?.map((x) => {
-              return (
-                <>
-                  <div>{x.label}</div> <pre>{JSON.stringify(x.value, null, 2)}</pre>
-                </>
-              );
-            })}
+            <SettingsSection {...settingSectionProps} />
           </>
         );
       })}
