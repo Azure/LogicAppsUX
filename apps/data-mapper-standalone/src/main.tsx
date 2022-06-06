7d97a189ada36d4f03d95c9bc2b487f0ca9634c6
Elaina Lee <t-jennlee@microsoft.com>
2022-06-06 14:29:09 -0700

    brought back original main

--- apps/data-mapper-standalone/src/main.tsx
+++ apps/data-mapper-standalone/src/main.tsx
@@ -1,9 +1,7 @@
 import '../../../libs/data-mapper/src/lib/styles.less';
 import { DataMapperStandaloneDesigner } from './app/DataMapperStandaloneDesigner';
-import { ParserViewInApp } from './components/ParserView';
 import { store } from './state/Store';
 import { initializeIcons } from '@fluentui/react';
-import { ParserInputFormat } from '@microsoft/logic-apps-data-mapper';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -13,8 +11,7 @@ initializeIcons();
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
-      <ParserViewInApp />
-      {/* <DataMapperStandaloneDesigner /> */}
+      <DataMapperStandaloneDesigner />
     </Provider>
   </StrictMode>,
   document.getElementById('root')
