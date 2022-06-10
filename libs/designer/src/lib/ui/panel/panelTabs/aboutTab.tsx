129b4c814ccdfd9dbbb2b78741a60340a2ffa17e
preetriti1 <48265693+preetriti1@users.noreply.github.com>
2022-06-10 16:07:39 -0700

    #14696623 - Add data from store to About tab in panel (#483)
    Co-authored-by: Priti Sambandam <psamband@microsoft.com>

--- libs/designer/src/lib/ui/panel/panelTabs/aboutTab.tsx
+++ libs/designer/src/lib/ui/panel/panelTabs/aboutTab.tsx
@@ -1,19 +1,36 @@
 import constants from '../../../common/constants';
+import {
+  useConnectorDescription,
+  useConnectorDocumentation,
+  useConnectorEnvironmentBadge,
+  useConnectorName,
+  useConnectorStatusBadge,
+  useOperationInfo,
+} from '../../../core/state/selectors/actionMetadataSelector';
+import type { RootState } from '../../../core/store';
 import type { PanelTab } from '@microsoft/designer-ui';
 import { About } from '@microsoft/designer-ui';
+import { useSelector } from 'react-redux';
 
 export const AboutTab = () => {
-  // TODO: Retrieve logic from a redux store?
-  const descriptionDocumentation = { url: 'www.microsoft.com', description: 'more info' };
+  const nodeId = useSelector((state: RootState) => state.panel.selectedNode);
+  const operationInfo = useOperationInfo(nodeId);
+  const displayName = useConnectorName(operationInfo);
+  const description = useConnectorDescription(operationInfo);
+  const documentation = useConnectorDocumentation(operationInfo);
+  const environmentBadge = useConnectorEnvironmentBadge(operationInfo);
+  const statusBadge = useConnectorStatusBadge(operationInfo);
+
   const headerIcons = [
-    { title: 'Tag1', badgeText: 'test' },
-    { title: 'Tag2', badgeText: 'more' },
+    ...(environmentBadge ? [{ badgeText: environmentBadge.name, title: environmentBadge.description }] : []),
+    ...(statusBadge ? [{ badgeText: statusBadge.name, title: statusBadge.description }] : []),
   ];
+
   return (
     <About
-      connectorDisplayName="Node Name"
-      description="description "
-      descriptionDocumentation={descriptionDocumentation}
+      connectorDisplayName={displayName}
+      description={description}
+      descriptionDocumentation={documentation}
       headerIcons={headerIcons}
     />
   );
@@ -22,7 +39,7 @@ export const AboutTab = () => {
 export const aboutTab: PanelTab = {
   title: 'About',
   name: constants.PANEL_TAB_NAMES.ABOUT,
-  description: 'Request History',
+  description: 'About Tab',
   enabled: true,
   content: <AboutTab />,
   order: 0,
