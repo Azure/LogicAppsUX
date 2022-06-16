c792ae1ce35a648751ad7789bc76f3e4ce2a5b94
Reid Fortier <refortie@microsoft.com>
2022-06-16 11:33:33 -0700

    Fix storybook

--- libs/data-mapper/src/lib/components/breadcrumb/EditorBreadcrumb.stories.tsx
+++ libs/data-mapper/src/lib/components/breadcrumb/EditorBreadcrumb.stories.tsx
@@ -1,42 +1,41 @@
 import type { UpdateBreadcrumbAction } from '../../core/state/BreadcrumbSlice';
-import breadcrumbReducer, { updateBreadcrumbForSchema } from '../../core/state/BreadcrumbSlice';
+import { updateBreadcrumbForSchema } from '../../core/state/BreadcrumbSlice';
+import { store } from '../../core/state/Store';
 import type { Schema } from '../../models/Schema';
 import { convertSchemaToSchemaExtended } from '../../models/Schema';
 import { simpleMockSchema } from '../../models/__mocks__';
 import { EditorBreadcrumb } from './EditorBreadcrumb';
-import { configureStore } from '@reduxjs/toolkit';
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Provider } from 'react-redux';
 
-const MockStore = ({ children }) => (
-  <Provider
-    store={configureStore({
-      reducer: {
-        breadcrumb: breadcrumbReducer,
-      },
-    })}
-  >
-    {children}
-  </Provider>
-);
+const MockStore = ({ mockState, children }) => {
+  store.dispatch(updateBreadcrumbForSchema(mockState));
+
+  return <Provider store={store}>{children}</Provider>;
+};
 
 export default {
   component: EditorBreadcrumb,
   title: 'Data Mapper/Breadcrumb',
 } as ComponentMeta<typeof EditorBreadcrumb>;
-export const Standard: ComponentStory<typeof EditorBreadcrumb> = () => {
-  const schema: Schema = JSON.parse(JSON.stringify(simpleMockSchema));
-  const extendedSchema = convertSchemaToSchemaExtended(schema);
 
-  const stateUpdate: UpdateBreadcrumbAction = {
-    schema: extendedSchema,
-    currentNode: extendedSchema.schemaTreeRoot.children[0],
-  };
-  updateBreadcrumbForSchema(stateUpdate);
+const Template: ComponentStory<typeof EditorBreadcrumb> = () => {
+  console.log(store.getState().breadcrumb.breadcrumbItems);
 
-  return (
-    <MockStore>
-      <EditorBreadcrumb />
-    </MockStore>
-  );
+  return <EditorBreadcrumb />;
 };
+
+export const Standard = Template.bind({});
+Standard.decorators = [
+  (story) => {
+    const schema: Schema = JSON.parse(JSON.stringify(simpleMockSchema));
+    const extendedSchema = convertSchemaToSchemaExtended(schema);
+
+    const stateUpdate: UpdateBreadcrumbAction = {
+      schema: extendedSchema,
+      currentNode: extendedSchema.schemaTreeRoot.children[0],
+    };
+
+    return <MockStore mockState={stateUpdate}>{story()}</MockStore>;
+  },
+];
--- libs/data-mapper/src/lib/components/breadcrumb/EditorBreadcrumb.tsx
+++ libs/data-mapper/src/lib/components/breadcrumb/EditorBreadcrumb.tsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 const maxBreadcrumbItems = 3;
 
 export const EditorBreadcrumb = (): JSX.Element => {
-  const breadcrumbItems = useSelector((state: RootState) => state.breadCrumb.breadcrumbItems);
+  const breadcrumbItems = useSelector((state: RootState) => state.breadcrumb.breadcrumbItems);
 
   return <Breadcrumb items={breadcrumbItems} maxDisplayedItems={maxBreadcrumbItems} />;
 };
