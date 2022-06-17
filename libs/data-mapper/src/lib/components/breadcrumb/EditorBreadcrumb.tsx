1ca5b7fe50846edeb35f5b1111cd31f8600e9a78
Reid Fortier <refortie@microsoft.com>
2022-06-17 10:39:37 -1000

    Add upwards traversal using the breadcrumb
    Also remove unnecessary slices and using a different pattern

--- libs/data-mapper/src/lib/components/breadcrumb/EditorBreadcrumb.tsx
+++ libs/data-mapper/src/lib/components/breadcrumb/EditorBreadcrumb.tsx
@@ -1,35 +1,37 @@
-import type { RootState } from '../../core/state/Store';
-import type { SchemaExtended, SchemaNodeExtended } from '../../models/Schema';
+import { setCurrentOutputNode } from '../../core/state/SchemaSlice';
+import type { AppDispatch, RootState } from '../../core/state/Store';
+import type { PathItem, SchemaExtended, SchemaNodeExtended } from '../../models/Schema';
+import type { IBreadcrumbItem } from '@fluentui/react';
 import { Breadcrumb } from '@fluentui/react';
-import { createSelector } from '@reduxjs/toolkit';
-import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const maxBreadcrumbItems = 3;
 const overflowIndex = 1;
 
-const outputSchema = (state: RootState) => state.schema.outputSchema;
-const currentOutputNode = (state: RootState) => state.schema.currentOutputNode;
-//const dispatch = (state: RootState) => state.
-export const breadCrumbItemSelector = createSelector([outputSchema, currentOutputNode], (outputSchema, currentOutputNode) => {
-  if (outputSchema) {
-    return convertToBreadcrumbItems(outputSchema, currentOutputNode);
-  }
-
-  return [];
-});
-
 export const EditorBreadcrumb = (): JSX.Element => {
-  const breadcrumbItems = useSelector(breadCrumbItemSelector);
+  const dispatch = useDispatch<AppDispatch>();
+  const outputSchema = useSelector((state: RootState) => state.schema.outputSchema);
+  const currentOutputNode = useSelector((state: RootState) => state.schema.currentOutputNode);
+
+  const breadcrumbItems = useMemo<IBreadcrumbItem[]>(() => {
+    if (outputSchema) {
+      return convertToBreadcrumbItems(dispatch, outputSchema, currentOutputNode);
+    }
+
+    return [];
+  }, [dispatch, outputSchema, currentOutputNode]);
+
   return <Breadcrumb items={breadcrumbItems} maxDisplayedItems={maxBreadcrumbItems} overflowIndex={overflowIndex} />;
 };
 
-const convertToBreadcrumbItems = (schema: SchemaExtended, currentNode?: SchemaNodeExtended) => {
+const convertToBreadcrumbItems = (dispatch: AppDispatch, schema: SchemaExtended, currentNode?: SchemaNodeExtended) => {
   const rootItem = {
     key: schema.name,
     text: schema.name,
     // TODO (14748905): Click root to view map overview, not top node
     onClick: () => {
-      //dispatch(setCurrentOutputNode(schema.schemaTreeRoot));
+      dispatch(setCurrentOutputNode(schema.schemaTreeRoot));
     },
   };
 
@@ -38,14 +40,27 @@ const convertToBreadcrumbItems = (schema: SchemaExtended, currentNode?: SchemaNo
   if (currentNode?.pathToRoot) {
     currentNode.pathToRoot.forEach((pathItem) => {
       breadcrumbItems.push({
-        key: pathItem.name,
+        key: pathItem.key,
         text: pathItem.name,
         onClick: () => {
-          // dispatch(setCurrentOutputNode(currentNode));
+          const destinationNode = findChildNode(schema.schemaTreeRoot, [...currentNode.pathToRoot]);
+          dispatch(setCurrentOutputNode(destinationNode));
         },
       });
     });
   }
 
   return breadcrumbItems;
+};
+
+const findChildNode = (schemaNode: SchemaNodeExtended, pathItems: PathItem[]): SchemaNodeExtended => {
+  if (pathItems.length > 1) {
+    const nextPathItem = pathItems.shift();
+    const nextChild = schemaNode.children.find((child) => child.key === nextPathItem?.key);
+    if (nextChild) {
+      return findChildNode(nextChild, pathItems);
+    }
+  }
+
+  return schemaNode;
 };
