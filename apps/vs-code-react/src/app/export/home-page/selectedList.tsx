8eaa6b37d9120c7defaa9a3a46466cfd7579d944
Carlos Castro Trejo <ccastrotrejo@microsoft.com>
2022-06-23 14:08:49 -0700

    Update selected list styles

--- apps/vs-code-react/src/app/export/home-page/selectedList.tsx
+++ apps/vs-code-react/src/app/export/home-page/selectedList.tsx
@@ -1,28 +1,33 @@
+import type { WorkflowsList } from '../../../run-service';
 import { Text, IconButton } from '@fluentui/react';
 import type { IIconProps } from '@fluentui/react';
 
 export const SelectedList: React.FC<any> = ({ selectedWorkflows }) => {
   const emojiIcon: IIconProps = { iconName: 'Cancel' };
 
-  const renderWorkflows = selectedWorkflows.map((workflow: any) => {
+  const renderWorkflows = selectedWorkflows.map((workflow: WorkflowsList) => {
     return (
-      <div key={workflow.id} className="msla-export-overview-panel-selected-list-item">
+      <div key={workflow.key} className="msla-export-overview-panel-selected-list-item">
         <IconButton iconProps={emojiIcon} aria-label="cancel" />
-        <Text variant="large" nowrap block>
-          {workflow.name}
+        <Text variant="large" nowrap block className="msla-export-overview-panel-selected-list-item-text">
+          {workflow.name + ' '}
         </Text>
-        <Text variant="mediumPlus" nowrap block className="msla-export-overview-panel-selected-list-item-subtext">
-          ({workflow.resourceGroup})
-        </Text>
+        <div className="msla-export-overview-panel-selected-list-item-subtext subtext-color">
+          (
+          <Text variant="medium" nowrap block className="subtext-color">
+            {workflow.resourceGroup}
+          </Text>
+          )
+        </div>
       </div>
     );
   });
   return (
-    <div className="msla-export-overview-panel-selected-list">
-      <Text variant="xLarge" nowrap block>
+    <div className="msla-export-overview-panel-selected">
+      <Text variant="xLarge" nowrap block className="msla-export-overview-panel-selected-title">
         Selected Apps
       </Text>
-      {renderWorkflows}
+      <div className="msla-export-overview-panel-selected-list">{renderWorkflows}</div>
     </div>
   );
 };
