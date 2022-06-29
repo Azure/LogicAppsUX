ad648f6342c48b3f0f5d4eef6ec7df4673edd427
refortie <37600290+refortie@users.noreply.github.com>
2022-06-29 11:10:07 -0700

    [DataMapper] Button container (#547)
    * Add in new button container component
    
    * Add story
    
    * Rebase

---
+++ libs/data-mapper/src/lib/components/buttonContainer/ButtonContainer.tsx
@@ -0,0 +1,34 @@
+import type { IButtonProps } from '@fluentui/react';
+import { IconButton, Stack, StackItem } from '@fluentui/react';
+
+export interface ButtonContainerProps {
+  xPos: string;
+  yPos: string;
+  buttons: IButtonProps[];
+  horizontal: boolean;
+}
+
+export const ButtonContainer: React.FC<ButtonContainerProps> = ({ buttons, horizontal, xPos, yPos }: ButtonContainerProps) => {
+  const stackItems = buttons.map((buttonProps, index) => {
+    return (
+      <StackItem key={index}>
+        <IconButton {...buttonProps} style={{ color: '#000', background: '#fff' }} />
+      </StackItem>
+    );
+  });
+
+  const stackStyle: React.CSSProperties = {
+    position: 'absolute',
+    zIndex: 10,
+    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.14), 0px 0px 2px rgba(0, 0, 0, 0.12)',
+    borderRadius: '4px',
+    top: yPos,
+    left: xPos,
+  };
+
+  return (
+    <Stack horizontal={horizontal} style={stackStyle}>
+      {stackItems}
+    </Stack>
+  );
+};
