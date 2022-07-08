f3c48c1bcc461d726c004365ea4005acaf82ed6a
Carlos Castro Trejo <ccastrotrejo@microsoft.com>
2022-07-08 14:29:57 -0700

    Add groupedList component

---
+++ apps/vs-code-react/src/app/export/validation/helper.ts
@@ -0,0 +1,6 @@
+export const getValidationColumns = () => {
+  return [
+    { key: 'action', name: 'action', fieldName: 'action', minWidth: 170, maxWidth: 250, isResizable: true },
+    { key: 'status', name: 'status', fieldName: 'status', minWidth: 170, maxWidth: 250, isResizable: true },
+  ];
+};
--- apps/vs-code-react/src/app/export/validation/validation.tsx
+++ apps/vs-code-react/src/app/export/validation/validation.tsx
@@ -1,3 +1,26 @@
+import { getValidationColumns } from './helper';
+import { DetailsRow, GroupedList } from '@fluentui/react';
+import { SelectionMode } from '@fluentui/react';
+import type { IGroup } from '@fluentui/react';
+
 export const Validation: React.FC = () => {
-  return <div className="msla-export-validation"></div>;
+  const onRenderCell = (nestingDepth?: number, item?: any, itemIndex?: number, group?: IGroup): React.ReactNode => {
+    return item && typeof itemIndex === 'number' && itemIndex > -1 ? (
+      <DetailsRow
+        columns={getValidationColumns()}
+        groupNestingDepth={nestingDepth}
+        item={item}
+        itemIndex={itemIndex}
+        selectionMode={SelectionMode.none}
+        compact={true}
+        group={group}
+      />
+    ) : null;
+  };
+
+  return (
+    <div className="msla-export-validation">
+      <GroupedList items={[]} onRenderCell={onRenderCell} selectionMode={SelectionMode.none} compact={true} />
+    </div>
+  );
 };
