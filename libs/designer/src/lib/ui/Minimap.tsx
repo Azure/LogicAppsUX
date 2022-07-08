b34e9a84fc504cead505a5c7a27a134a51d6e5c3
Riley Evans <rileyevans@microsoft.com>
2022-07-08 10:23:15 -0500

    Fixed merge issues

--- libs/designer/src/lib/ui/Minimap.tsx
+++ libs/designer/src/lib/ui/Minimap.tsx
@@ -1,19 +1,21 @@
-import { useMinimap } from '../core/state/designerView/designerViewSelectors';
+import type { WorkflowNodeType } from '../core/parsers/models/workflowNode';
+import { useShowMinimap } from '../core/state/designerView/designerViewSelectors';
 import { MiniMap } from 'react-flow-renderer';
 
 const Minimap = () => {
-  const showMinimap = useMinimap();
+  const showMinimap = useShowMinimap();
   if (!showMinimap) return null;
 
-  const nodeColors: Record<string, any> = {
-    TEST_NODE: { fill: '#ECECEC', stroke: '#A19F9D' },
+  const nodeColors: Record<WorkflowNodeType, any> = {
+    OPERATION_NODE: { fill: '#ECECEC', stroke: '#A19F9D' },
     GRAPH_NODE: { fill: '#FFF', stroke: '#A19F9D' },
-    SCOPE_NODE: { fill: '#1f85ff', stroke: '#1f85ff' },
-    SUBGRAPH_NODE: { fill: '#486991', stroke: '#486991' },
+    SUBGRAPH_NODE: { fill: '#FFF', stroke: '#A19F9D' },
+    SCOPE_CARD_NODE: { fill: '#1f85ff', stroke: '#1f85ff' },
+    SUBGRAPH_CARD_NODE: { fill: '#486991', stroke: '#486991' },
     HIDDEN_NODE: { fill: '#00000000', stroke: '#00000000' },
   };
-  const nodeColor = (node: any) => nodeColors[node.type].fill;
-  const nodeStrokeColor = (node: any) => nodeColors[node.type].stroke;
+  const nodeColor = (node: any) => nodeColors[node.type as WorkflowNodeType].fill;
+  const nodeStrokeColor = (node: any) => nodeColors[node.type as WorkflowNodeType].stroke;
 
   return <MiniMap nodeColor={nodeColor} nodeStrokeColor={nodeStrokeColor} nodeStrokeWidth={3} nodeBorderRadius={0} />;
 };
