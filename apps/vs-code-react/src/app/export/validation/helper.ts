2017b5db7906a52eea33b818dcf52652e2d548b9
Carlos Castro Trejo <ccastrotrejo@microsoft.com>
2022-07-12 15:41:44 -0700

    Update logic to get groups in validation list

--- apps/vs-code-react/src/app/export/validation/helper.ts
+++ apps/vs-code-react/src/app/export/validation/helper.ts
@@ -14,27 +14,33 @@ const getActionDetails = (itemsSchema: any, actionName: string) => {
   return errors;
 };
 
-const getValidationGroup = (workflowSchema: any, groupName: string) => {
-  const isCollapsed = workflowSchema.validationState && workflowSchema.validationState === 'Succeeded';
+const getValidationGroup = (workflowSchema: any, groupName: string, level: number, startIndex: number, count: number, children: any[]) => {
+  const isCollapsed = (workflowSchema?.validationState && workflowSchema?.validationState === 'Succeeded') ?? false;
 
   return {
-    children: [],
+    children,
     isCollapsed: isCollapsed,
     key: groupName,
-    level: 0,
+    level,
+    count,
     name: groupName,
-    startIndex: 0,
+    startIndex,
   };
 };
 
 const getItemsValidation = (itemsSchema: any) => {
   const items: any = [];
+  const validationGroups: any = [];
+
   const workflowsId = Object.keys(itemsSchema);
 
   workflowsId.forEach((workflowId) => {
     const item = itemsSchema[workflowId];
     const actionName = item.displayName ?? workflowId;
     const actionStatus = item.validationState;
+    const startIndex = validationGroups.length > 0 ? validationGroups[validationGroups.length - 1].startIndex + 1 : 0;
+
+    validationGroups.push(getValidationGroup(item, actionName, 2, startIndex, 1, []));
 
     if (actionStatus !== 'Succeeded' && item?.details) {
       items.push(...getActionDetails(item, actionName));
@@ -43,25 +49,47 @@ const getItemsValidation = (itemsSchema: any) => {
     }
   });
 
-  return items;
+  return { items, validationGroups };
 };
 
 export const parseValidationData = (validationData: any) => {
   const workflowsState = validationData?.properties?.workflows ?? {};
-  const validationGroups: any = [];
+  const validationGroupsGlobal: any = [];
   const validationItems: any = [];
 
   const workflowsId = Object.keys(workflowsState);
 
   workflowsId.forEach((workflowId) => {
     const workflowSchema = workflowsState[workflowId];
-    const workflowGroup = getValidationGroup(workflowSchema, workflowId);
+    let count = 0;
+    const children: any[] = [];
 
-    validationItems.push(...getItemsValidation(workflowSchema.workflowOperations));
-    validationItems.push(...getItemsValidation(workflowSchema.connections));
-    validationItems.push(...getItemsValidation(workflowSchema.parameters));
-    validationGroups.push(workflowGroup);
+    Object.keys(workflowSchema).forEach((workflowSchemaKey, index) => {
+      const { items = [], validationGroups = [] } = index === 0 ? {} : getItemsValidation(workflowSchema[workflowSchemaKey]);
+      count += items.length;
+
+      if (index !== 0) {
+        const innerStart = validationGroupsGlobal.length > 0 ? validationGroupsGlobal[validationGroupsGlobal.length - 1].startIndex : 0;
+
+        const innerValidationGroup = getValidationGroup(
+          workflowSchema[workflowSchemaKey],
+          workflowSchemaKey,
+          1,
+          innerStart,
+          validationGroups.length,
+          validationGroups
+        );
+        children.push(innerValidationGroup);
+      }
+
+      validationItems.push(...items);
+    });
+
+    const startIndex =
+      validationGroupsGlobal.length > 0 ? validationGroupsGlobal[validationGroupsGlobal.length - 1].startIndex + count - 1 : 0;
+    const workflowGroup = getValidationGroup(workflowSchema, workflowId, 0, startIndex, count, children);
+    validationGroupsGlobal.push(workflowGroup);
   });
 
-  return { validationItems, validationGroups };
+  return { validationItems, validationGroups: validationGroupsGlobal };
 };
