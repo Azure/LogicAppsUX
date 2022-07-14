986f7cba1454083fb2037c340bf3b6092b360015
Eric <wue@microsoft.com>
2022-07-14 14:15:21 -0400

    combobox updates

--- libs/designer-ui/src/lib/combobox/combobox.stories.tsx
+++ libs/designer-ui/src/lib/combobox/combobox.stories.tsx
@@ -1,13 +1,28 @@
-// import { Combobox } from './';
-// import type { ComboBoxProps } from './';
-// import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import type { Segment } from '../editor/base';
+import { Combobox } from './';
+import type { ComboboxProps } from './';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
 
-// export default {
-//   component: Combobox,
-//   title: 'Components/Editor/Array',
-// } as ComponentMeta<typeof Combobox>;
-// const Template: ComponentStory<typeof Combobox> = (args: ComboBoxProps) => <Combobox {...args} />;
+export default {
+  component: Combobox,
+  title: 'Components/Editor/Combobox',
+} as ComponentMeta<typeof Combobox>;
+const Template: ComponentStory<typeof Combobox> = (args: ComboboxProps) => <Combobox {...args} />;
 
-// export const Standard = Template.bind({});
-// Standard.args = {
-// };
+export const Standard = Template.bind({});
+
+Standard.args = {
+  options: [
+    { value: 'GET', key: 'GET', disabled: false },
+    { value: 'PUT', key: 'PUT', disabled: false },
+    { value: 'POST', key: 'POST', disabled: false },
+    { value: 'PATCH', key: 'PATCH', disabled: false },
+    { value: 'DELETE', key: 'DELETE', disabled: false },
+  ],
+  placeholderText: 'Method is Required',
+  label: 'Method',
+  selectedKey: 'PUT',
+  setSelectedKey: (key: Segment[]) => {
+    console.log(key);
+  },
+};
--- libs/designer-ui/src/lib/combobox/index.tsx
+++ libs/designer-ui/src/lib/combobox/index.tsx
@@ -1,18 +1,195 @@
-// import constants from '../constants';
-// import type { Segment } from '../editor/base';
-// import { useState } from 'react';
+import { ValueSegmentType } from '../editor';
+import type { Segment } from '../editor/base';
+import { BaseEditor } from '../editor/base';
+import { Label } from '../label';
+import { CustomValue } from './plugins/CustomValue';
+// import { CustomValue } from './plugins/CustomValue';
+import type {
+  IButtonStyles,
+  IComboBox,
+  IComboBoxOption,
+  IComboBoxOptionStyles,
+  IComboBoxStyles,
+  IIconProps,
+  ITooltipHostStyles,
+} from '@fluentui/react';
+import { IconButton, TooltipHost, SelectableOptionMenuItemType, ComboBox } from '@fluentui/react';
+import { getIntl } from '@microsoft-logic-apps/intl';
+import { useRef, useState, useCallback, useEffect } from 'react';
+import { useIntl } from 'react-intl';
 
-// export interface ComboBoxProps {
-//   customValue: Segment[] | null;
-// }
+const comboboxStyles: Partial<IComboBoxStyles> = {
+  divider: {
+    height: '2px',
+  },
+};
 
-// export const Combobox: React.FC<ComboBoxProps> = ({ customValue }: ComboBoxProps): JSX.Element => {
-//   const [expandDropdown, setExpandDropdown] = useState(false);
-//   const [customVal, setCustomVal] = useState<Segment[] | null>(customValue);
-//   let className = expandDropdown ? 'msla-combobox' : 'msla combobox msla-combobox-collapsed';
-//   if (!customVal) {
-//     className += ` ${constants.TOKEN_PICKER_CONTROL_CLASSES.DISMISSIBLE}`;
-//   }
+const customValueStyles: Partial<IComboBoxOptionStyles> = {
+  label: {
+    color: 'blue',
+  },
+};
 
-//   return <div className={className}></div>;
-// };
+const clearIcon: IIconProps = { iconName: 'Cancel' };
+const calloutProps = { gapSpace: 0 };
+const hostStyles: Partial<ITooltipHostStyles> = { root: { display: 'inline-block', position: 'absolute', right: '18px', top: '35px' } };
+const buttonStyles: Partial<IButtonStyles> = { root: { height: '22px', width: '30px' } };
+
+export interface ComboboxItem {
+  disabled?: boolean;
+  key: string;
+  value: string;
+  displayValue?: string;
+  data?: string;
+}
+export interface ComboboxProps {
+  options: ComboboxItem[];
+  customValue: Segment[] | null;
+  placeholderText?: string;
+  label?: string;
+  allowFreeForm?: boolean;
+  selectedKey?: string | number;
+  required?: boolean;
+  setSelectedKey?: (key: Segment[]) => void;
+}
+export const Combobox = ({
+  customValue,
+  options,
+  placeholderText,
+  label,
+  allowFreeForm = true,
+  required = true,
+  selectedKey,
+  setSelectedKey,
+}: ComboboxProps): JSX.Element => {
+  const intl = useIntl();
+  const comboBoxRef = useRef<IComboBox>(null);
+  const [customVal, setCustomVal] = useState<Segment[] | null>(customValue);
+  const [comboboxOptions, setComboBoxOptions] = useState<IComboBoxOption[]>(getOptions(options));
+
+  useEffect(() => {
+    if (setSelectedKey && customVal) {
+      setSelectedKey(customVal);
+    }
+  }, [customVal, setSelectedKey]);
+
+  const toggleExpand = useCallback(() => {
+    comboBoxRef.current?.focus(true);
+    comboBoxRef.current?.dismissMenu();
+  }, []);
+
+  const updateOptions = (value?: string): void => {
+    if (value !== undefined) {
+      comboBoxRef.current?.focus(true);
+      const newOptions = options.filter((option) => new RegExp(value.replace(/\\/g, '').toLowerCase()).test(option.value.toLowerCase()));
+      if (newOptions.length === 0) {
+        const noValuesLabel = intl.formatMessage({
+          defaultMessage: 'No values matching your search',
+          description: 'Label for when no values match search value',
+        });
+        newOptions.push({ key: 'header', value: noValuesLabel, disabled: true });
+      }
+      newOptions.push({ key: 'divider', value: '-' });
+      if (options.filter((option) => option.value === value).length === 0 && value !== '') {
+        const customValueLabel = intl.formatMessage(
+          {
+            defaultMessage: 'Use "{value}" as a custom value',
+            description: 'Label for button to allow user to create custom value in combobox from current input',
+          },
+          { value: value }
+        );
+        newOptions.push({ key: value, value: customValueLabel, disabled: false, data: 'customrender' });
+      }
+      setComboBoxOptions(getOptions(newOptions));
+    }
+  };
+
+  const onRenderOption = (item?: IComboBoxOption) => {
+    switch (item?.data) {
+      case 'customrender':
+        return <span className="msla-combobox-custom-option">{item?.text}</span>;
+      default:
+        return <span className="msla-combobox-option">{item?.text}</span>;
+    }
+  };
+
+  const handleCustomOptions = (option?: IComboBoxOption): void => {
+    if (option?.data === 'customrender') {
+      setCustomVal([{ type: ValueSegmentType.LITERAL, value: option.key === 'customValue' ? '' : option.key.toString() }]);
+    } else {
+      if (setSelectedKey && option?.key) {
+        setSelectedKey([{ type: ValueSegmentType.LITERAL, value: option.key.toString() }]);
+      }
+    }
+  };
+
+  const clearEditor = intl.formatMessage({
+    defaultMessage: 'Clear custom value',
+    description: 'Label for button to clear the editor',
+  });
+
+  const handleClearClick = () => {
+    setCustomVal(null);
+    updateOptions('');
+  };
+
+  return (
+    <div className="msla-combobox-container">
+      {label ? <Label className="msla-combobox-label" text={label} isRequiredField={required} /> : null}
+      {customVal ? (
+        <div className="msla-combobox-editor-container">
+          <BaseEditor
+            className="msla-combobox-editor"
+            placeholder={placeholderText}
+            BasePlugins={{ tokens: true, clearEditor: true }}
+            initialValue={customVal}
+          >
+            <CustomValue setCustomVal={setCustomVal} />
+            <TooltipHost content={clearEditor} calloutProps={calloutProps} styles={hostStyles} setAriaDescribedBy={false}>
+              <IconButton styles={buttonStyles} iconProps={clearIcon} aria-label={clearEditor} onClick={() => handleClearClick()} />
+            </TooltipHost>
+          </BaseEditor>
+        </div>
+      ) : (
+        <ComboBox
+          className="msla-combobox"
+          defaultSelectedKey={selectedKey}
+          componentRef={comboBoxRef}
+          useComboBoxAsMenuWidth
+          allowFreeform={allowFreeForm}
+          autoComplete="off"
+          placeholder={placeholderText}
+          options={comboboxOptions}
+          onInputValueChange={updateOptions}
+          onClick={toggleExpand}
+          onRenderOption={onRenderOption}
+          styles={comboboxStyles}
+          onItemClick={(_, o) => handleCustomOptions(o)}
+        />
+      )}
+    </div>
+  );
+};
+const getOptions = (options: ComboboxItem[]): IComboBoxOption[] => {
+  const intl = getIntl();
+
+  const customValueLabel = intl.formatMessage({
+    defaultMessage: 'Enter custom value',
+    description: 'Label for button to allow user to create custom value in combobox',
+  });
+
+  return [
+    ...options.map((option: ComboboxItem) => {
+      const { key, displayValue, value, disabled, data } = option;
+      switch (key) {
+        case 'divider':
+          return { key: key, text: value, itemType: SelectableOptionMenuItemType.Divider, disabled: disabled, data: data };
+        case 'header':
+          return { key: key, text: value, itemType: SelectableOptionMenuItemType.Header, data: data, disabed: disabled };
+        default:
+          return { key: key, text: displayValue ?? value, disabled: disabled, data: data };
+      }
+    }),
+    { key: 'customValue', text: customValueLabel, styles: customValueStyles, data: 'customrender' },
+  ];
+};
