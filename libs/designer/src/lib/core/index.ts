74786f0689cbd7dad1f90dd12ba03b36d538e632
Travis Harris <travisharris@microsoft.com>
2022-07-20 00:44:56 -0700

    Export some state parameters

--- libs/designer/src/lib/core/index.ts
+++ libs/designer/src/lib/core/index.ts
@@ -1,3 +1,5 @@
 export * from './DesignerProvider';
 export * from './BJSWorkflowProvider';
 export * from './ProviderWrappedContext';
+export type { RootState, AppDispatch } from './store';
+export { discardAllChanges } from './state/workflow/workflowSlice';
