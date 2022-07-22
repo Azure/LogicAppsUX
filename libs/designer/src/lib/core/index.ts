bb49386432f36beb8efec9024dba512f25f413a7
Travis Harris <travisharris@microsoft.com>
2022-07-22 15:15:37 -0700

    Export serializer

--- libs/designer/src/lib/core/index.ts
+++ libs/designer/src/lib/core/index.ts
@@ -4,3 +4,4 @@ export * from './ProviderWrappedContext';
 export type { RootState, AppDispatch } from './store';
 export { store } from './store';
 export { discardAllChanges } from './state/workflow/workflowSlice';
+export { serializeWorkflow } from './actions/bjsworkflow/serializer';
