5266ea3545ae8303f95c818463c1a3b4f21f5a53
Riley Evans <rileyevans@microsoft.com>
2022-07-29 12:58:48 -0500

    useThrottledEffect bugfix

--- libs/designer/src/lib/core/graphlayout/elklayout.ts
+++ libs/designer/src/lib/core/graphlayout/elklayout.ts
@@ -5,7 +5,7 @@ import { getRootWorkflowGraphForLayout } from '../state/workflow/workflowSelecto
 import { LogEntryLevel, LoggerService } from '@microsoft-logic-apps/designer-client-services';
 import type { ElkExtendedEdge, ElkNode } from 'elkjs/lib/elk.bundled';
 import ELK from 'elkjs/lib/elk.bundled';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import type { Edge, Node } from 'react-flow-renderer';
 import { useSelector } from 'react-redux';
 
@@ -182,14 +182,17 @@ export const exportForTesting = {
 export const useThrottledEffect = (effect: () => void, deps: any[], delay: number) => {
   const timestamp = useRef(Date.now());
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const callback = useCallback(effect, deps); // Our trace was making this loop infinitely if we depend on the effect
+
   useEffect(() => {
     const timeoutFunc = () => {
       if (Date.now() - timestamp.current >= delay) {
-        effect();
+        callback();
         timestamp.current = Date.now();
       }
     };
     const handler = setTimeout(timeoutFunc, delay - (Date.now() - timestamp.current));
     return () => clearTimeout(handler);
-  }, [effect, ...deps, delay]);
+  }, [callback, delay]);
 };
