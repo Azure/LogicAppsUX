3c64a4dd5d5f9009aa189ce75ffde57777b93eea
Carlos Castro Trejo <ccastrotrejo@microsoft.com>
2022-08-01 13:29:01 -0700

    Add buttons and textfield

--- apps/vs-code-react/src/app/export/summary/newResourceGroup.tsx
+++ apps/vs-code-react/src/app/export/summary/newResourceGroup.tsx
@@ -1,4 +1,4 @@
-import { Callout, Link, Text } from '@fluentui/react';
+import { Callout, Link, PrimaryButton, Text, TextField } from '@fluentui/react';
 import { useBoolean } from '@fluentui/react-hooks';
 import { useIntl } from 'react-intl';
 
@@ -15,16 +15,33 @@ export const NewResourceGroup: React.FC = () => {
       defaultMessage: 'A resource group is a container that holds related resources for an Azure solution.',
       description: 'Deploy managed connections warning text',
     }),
+    OK: intl.formatMessage({
+      defaultMessage: 'OK',
+      description: 'OK button',
+    }),
+    CANCEL: intl.formatMessage({
+      defaultMessage: 'Cancel',
+      description: 'Cancel button',
+    }),
+    NAME: intl.formatMessage({
+      defaultMessage: 'Name',
+      description: 'Name button',
+    }),
   };
 
   return (
     <>
-      <Link onClick={toggleIsCalloutVisible}>{intlText.CREATE_NEW}</Link>
+      <Link className="msla-export-summary-connections-new-resource" onClick={toggleIsCalloutVisible}>
+        {intlText.CREATE_NEW}
+      </Link>
       {isCalloutVisible && (
-        <Callout role="dialog" gapSpace={0} onDismiss={toggleIsCalloutVisible}>
+        <Callout role="dialog" gapSpace={8} target={'.msla-export-summary-connections-new-resource'} onDismiss={toggleIsCalloutVisible}>
           <Text variant="mediumPlus" block>
             {intlText.RESOURCE_GROUP_DESCRIPTION}
           </Text>
+          <TextField required label={intlText.NAME} />
+          <PrimaryButton className="msla-export-navigation-panel-button" text={intlText.OK} ariaLabel={intlText.OK} />
+          <PrimaryButton className="msla-export-navigation-panel-button" text={intlText.CANCEL} ariaLabel={intlText.CANCEL} />
         </Callout>
       )}
     </>
