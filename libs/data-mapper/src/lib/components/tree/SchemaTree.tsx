1dffd52e2debe72fb8a91e6b6286de4c8e34da8a
refortie <37600290+refortie@users.noreply.github.com>
2022-08-08 16:11:03 -0700

    [Data Mapper] Add/Remove input nodes from the canvas (#690)
    * Start to style the input schema tool box
    
    * Fix circular dependency
    
    * Input node toggling
    
    * Fix icons
    
    * Fix storybook
    
    * Add tooltip for icon
    
    * Fix button role
    
    * Rework entire tree item to fix WCAG issues
    
    * Fix ButtonContainer.stories.tsx
    
    * Last fix for button stories

--- libs/data-mapper/src/lib/components/tree/SchemaTree.tsx
+++ libs/data-mapper/src/lib/components/tree/SchemaTree.tsx
@@ -1,7 +1,9 @@
-import type { SchemaExtended, SchemaNodeExtended } from '../../models';
+import type { SchemaExtended, SchemaNodeDataType, SchemaNodeExtended } from '../../models';
+import { icon16ForSchemaNodeType } from './SchemaTree.Utils';
+import { bundleIcon, CheckmarkCircle16Filled, Circle16Regular } from '@fluentui/react-icons';
 import { fluentTreeItem, fluentTreeView, provideFluentDesignSystem } from '@fluentui/web-components';
 import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 const { wrap } = provideReactWrapper(React, provideFluentDesignSystem());
 export const FastTreeView = wrap(fluentTreeView());
@@ -9,20 +11,33 @@ export const FastTreeItem = wrap(fluentTreeItem());
 
 export interface SchemaTreeProps {
   schema: SchemaExtended;
+  currentlySelectedNodes: SchemaNodeExtended[];
   onLeafNodeClick: (schemaNode: SchemaNodeExtended) => void;
 }
 
-export const SchemaTree: React.FC<SchemaTreeProps> = ({ schema, onLeafNodeClick }: SchemaTreeProps) => {
-  return <FastTreeView>{convertToFastTreeItem(schema.schemaTreeRoot, onLeafNodeClick)}</FastTreeView>;
+export const SchemaTree: React.FC<SchemaTreeProps> = ({ schema, currentlySelectedNodes, onLeafNodeClick }: SchemaTreeProps) => {
+  const treeItems = useMemo<JSX.Element[]>(() => {
+    return convertToFastTreeItem(schema.schemaTreeRoot, currentlySelectedNodes, onLeafNodeClick);
+  }, [schema, currentlySelectedNodes, onLeafNodeClick]);
+
+  return <FastTreeView>{treeItems}</FastTreeView>;
 };
 
-const convertToFastTreeItem = (node: SchemaNodeExtended, onLeafNodeClick: (schemaNode: SchemaNodeExtended) => void) => {
+const convertToFastTreeItem = (
+  node: SchemaNodeExtended,
+  currentlySelectedNodes: SchemaNodeExtended[],
+  onLeafNodeClick: (schemaNode: SchemaNodeExtended) => void
+) => {
   return node.children.map((childNode) => {
+    const isNodeSelected = !!currentlySelectedNodes.find((currentlySelectedNode) => currentlySelectedNode.key === childNode.key);
     if (childNode.schemaNodeDataType === 'ComplexType' || childNode.schemaNodeDataType === 'None') {
       return (
+        // TODO onclick for object level adding
         <FastTreeItem key={childNode.key}>
-          {childNode.name}
-          {convertToFastTreeItem(childNode, onLeafNodeClick)}
+          <TreeItemContent nodeType={childNode.schemaNodeDataType} filled={isNodeSelected}>
+            {childNode.name}
+          </TreeItemContent>
+          {convertToFastTreeItem(childNode, currentlySelectedNodes, onLeafNodeClick)}
         </FastTreeItem>
       );
     } else {
@@ -33,9 +48,33 @@ const convertToFastTreeItem = (node: SchemaNodeExtended, onLeafNodeClick: (schem
             onLeafNodeClick(childNode);
           }}
         >
-          {childNode.name}
+          <TreeItemContent nodeType={childNode.schemaNodeDataType} filled={isNodeSelected}>
+            {childNode.name}
+          </TreeItemContent>
         </FastTreeItem>
       );
     }
   });
+};
+
+export interface SchemaNodeTreeItemContentProps {
+  nodeType: SchemaNodeDataType;
+  filled: boolean;
+}
+
+const TreeItemContent: React.FC<SchemaNodeTreeItemContentProps> = ({ nodeType, filled, children }) => {
+  const BundledTypeIcon = icon16ForSchemaNodeType(nodeType);
+  const BundledAddIcon = bundleIcon(CheckmarkCircle16Filled, Circle16Regular);
+
+  return (
+    <>
+      <span style={{ display: 'flex', marginRight: '4px' }} slot="start">
+        <BundledTypeIcon style={{ verticalAlign: 'middle' }} filled={filled} />
+      </span>
+      <span style={{ marginRight: '8px', width: '100%' }}>{children}</span>
+      <span style={{ display: 'flex', marginRight: '4px' }} slot="end">
+        <BundledAddIcon style={{ verticalAlign: 'middle' }} filled={filled} />
+      </span>
+    </>
+  );
 };
