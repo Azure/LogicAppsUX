890e1da37f727d284ba88fa4f8a0eaf75c804349
Riley Evans <rileyevans@microsoft.com>
2022-08-09 15:18:50 -0500

    Fixed build errors

--- libs/designer/src/lib/ui/panel/recommendation/operationGroupDetailView.tsx
+++ libs/designer/src/lib/ui/panel/recommendation/operationGroupDetailView.tsx
@@ -1,25 +1,15 @@
-import Constants from '../../../common/constants';
+import { initializeOperationDetails } from '../../../core/actions/bjsworkflow/add';
 import { isBuiltInConnector } from '../../../core/actions/bjsworkflow/connections';
-import { getInputParametersFromManifest, getOutputParametersFromManifest } from '../../../core/actions/bjsworkflow/initialize';
-import type { NodeDataWithManifest } from '../../../core/actions/bjsworkflow/operationdeserializer';
-import { getOperationSettings } from '../../../core/actions/bjsworkflow/settings';
 import type { AddNodePayload } from '../../../core/parsers/addNodeToWorkflow';
-import type { WorkflowNode } from '../../../core/parsers/models/workflowNode';
 import { getConnectionsForConnector } from '../../../core/queries/connections';
 import { getOperationManifest } from '../../../core/queries/operation';
 import { changeConnectionMapping } from '../../../core/state/connection/connectionSlice';
 import type { AddNodeOperationPayload } from '../../../core/state/operation/operationMetadataSlice';
-import { initializeNodes, initializeOperationInfo } from '../../../core/state/operation/operationMetadataSlice';
+import { initializeOperationInfo } from '../../../core/state/operation/operationMetadataSlice';
 import { selectOperationGroupId, switchToOperationPanel } from '../../../core/state/panel/panelSlice';
-import type { NodeTokens, VariableDeclaration } from '../../../core/state/tokensSlice';
-import { initializeTokensAndVariables } from '../../../core/state/tokensSlice';
 import { addNode } from '../../../core/state/workflow/workflowSlice';
 import type { RootState } from '../../../core/store';
-import { getTokenNodeIds, getBuiltInTokens, convertOutputsToTokens } from '../../../core/utils/tokens';
-import { setVariableMetadata, getVariableDeclarations } from '../../../core/utils/variables';
-import { OperationManifestService } from '@microsoft-logic-apps/designer-client-services';
-import type { DiscoveryOperation, DiscoveryResultTypes, OperationApi, OperationInfo } from '@microsoft-logic-apps/utils';
-import { equals } from '@microsoft-logic-apps/utils';
+import type { DiscoveryOperation, DiscoveryResultTypes, OperationApi } from '@microsoft-logic-apps/utils';
 import type { OperationActionData } from '@microsoft/designer-ui';
 import { OperationGroupDetailsPage } from '@microsoft/designer-ui';
 import type { Dispatch } from '@reduxjs/toolkit';
@@ -101,69 +91,4 @@ export const OperationGroupDetailView = (props: OperationGroupDetailViewProps) =
       onClickBack={onClickBack}
     />
   );
-};
-
-const initializeOperationDetails = async (
-  nodeId: string,
-  operationInfo: OperationInfo,
-  operationType: string,
-  operationKind: string,
-  rootState: RootState,
-  dispatch: Dispatch
-): Promise<void> => {
-  const operationManifestService = OperationManifestService();
-  if (operationManifestService.isSupported(operationType)) {
-    const manifest = await getOperationManifest(operationInfo);
-
-    const nodeInputs = getInputParametersFromManifest(nodeId, manifest);
-    const nodeOutputs = getOutputParametersFromManifest(nodeId, manifest);
-    const settings = getOperationSettings(false /* isTrigger */, operationType, operationKind, manifest);
-
-    dispatch(initializeNodes([{ id: nodeId, nodeInputs, nodeOutputs, settings }]));
-
-    // TODO (Danielle) - Please comment out the below part when state has updated graph and nodesMetadata.
-    // We need the graph and nodesMetadata updated with the newly added node for token dependencies to be calculated.
-    // addTokensAndVariables(nodeId, operationType, { id: nodeId, nodeInputs, nodeOutputs, settings, manifest }, rootState, dispatch);
-  } else {
-    // swagger case here
-  }
-};
-
-export const addTokensAndVariables = (
-  nodeId: string,
-  operationType: string,
-  nodeData: NodeDataWithManifest,
-  rootState: RootState,
-  dispatch: Dispatch
-): void => {
-  const {
-    workflow: { graph, nodesMetadata, operations },
-  } = rootState;
-  const { nodeInputs, nodeOutputs, settings, manifest } = nodeData;
-  const nodeMap = Object.keys(operations).reduce((actionNodes: Record<string, string>, id: string) => ({ ...actionNodes, [id]: id }), {
-    [nodeId]: nodeId,
-  });
-  const upstreamNodeIds = getTokenNodeIds(nodeId, graph as WorkflowNode, nodesMetadata, { [nodeId]: nodeData }, nodeMap);
-  const tokensAndVariables = {
-    outputTokens: {
-      [nodeId]: { tokens: [], upstreamNodeIds } as NodeTokens,
-    },
-    variables: {} as Record<string, VariableDeclaration[]>,
-  };
-
-  tokensAndVariables.outputTokens[nodeId].tokens.push(...getBuiltInTokens(manifest));
-  tokensAndVariables.outputTokens[nodeId].tokens.push(
-    ...convertOutputsToTokens(nodeId, operationType, nodeOutputs.outputs ?? {}, manifest, settings)
-  );
-
-  if (equals(operationType, Constants.NODE.TYPE.INITIALIZE_VARIABLE)) {
-    setVariableMetadata(manifest.properties.iconUri, manifest.properties.brandColor);
-
-    const variables = getVariableDeclarations(nodeInputs);
-    if (variables.length) {
-      tokensAndVariables.variables[nodeId] = variables;
-    }
-  }
-
-  dispatch(initializeTokensAndVariables(tokensAndVariables));
 };
