b276c40370b2d7df9bf2d5572bf1eaf1d2b812ff
Travis Harris <travisharris@microsoft.com>
2022-08-10 12:02:21 -0700

    Label Case labels for run after

--- libs/designer/src/lib/ui/settings/sections/runafter.tsx
+++ libs/designer/src/lib/ui/settings/sections/runafter.tsx
@@ -6,6 +6,7 @@ import { addEdge, updateRunAfter } from '../../../core/state/workflow/workflowSl
 import type { SettingSectionProps } from '../settingsection';
 import { SettingsSection } from '../settingsection';
 import type { RunAfterActionDetailsProps } from './runafterconfiguration';
+import { labelCase } from '@microsoft-logic-apps/utils';
 import { useIntl } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -47,7 +48,7 @@ export const RunAfter = ({ runAfter, readOnly = false, expanded, onHeaderClick, 
         id: id,
         isDeleteVisible: true,
         readOnly: readOnly,
-        title: id,
+        title: labelCase(id),
         statuses: value,
         onStatusChange: (status, checked) => {
           handleStatusChange(id, status, checked);
