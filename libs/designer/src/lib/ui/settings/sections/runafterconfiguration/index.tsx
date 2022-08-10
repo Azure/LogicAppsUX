44a462f859285a842bfe6e6ab4765fb8d9f1cf5f
Travis Harris <hartra344@users.noreply.github.com>
2022-08-10 12:16:19 -0700

    Update Run After to use redux state as source of truth nad have abity to update (#712)
    * Update Run After to use redux state as source of truth nad have ability to update
    
    * Label Case labels for run after
    
    * Use id instead of title for icon

--- libs/designer/src/lib/ui/settings/sections/runafterconfiguration/index.tsx
+++ libs/designer/src/lib/ui/settings/sections/runafterconfiguration/index.tsx
@@ -1,19 +1,23 @@
 import type { RunAfterActionDetailsProps } from './runafteractiondetails';
 import { RunAfterActionDetails } from './runafteractiondetails';
+import { PrimaryButton } from '@fluentui/react';
 
 export * from './runafteractiondetails';
 
 export interface RunAfterProps {
   items: RunAfterActionDetailsProps[];
   readOnly?: boolean;
+  onEdgeAddition: (parent: string) => void;
 }
 
-export const RunAfter = ({ items, readOnly = false }: RunAfterProps) => {
+export const RunAfter = ({ items, onEdgeAddition, readOnly = false }: RunAfterProps) => {
   return (
     <div>
       {items.map((item, key) => {
         return <RunAfterActionDetails {...item} key={key} readOnly={readOnly} />;
       })}
+      {/*This is tempoarary way to test edge addition, adding full support for choosing a predicessor will be in next PR(or the one after deletion)*/}
+      <PrimaryButton text="TEMPORARY ADD EDGE" onClick={() => onEdgeAddition('manual')} />
     </div>
   );
 };
