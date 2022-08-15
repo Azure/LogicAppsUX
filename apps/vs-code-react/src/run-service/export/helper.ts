e0d2e2c95f4e02617592a014aa5cfe79ebf060cf
Carlos Emiliano Castro Trejo <102700317+ccastrotrejo@users.noreply.github.com>
2022-08-15 14:24:50 -0700

    Remove geo restriction (#736)

--- apps/vs-code-react/src/run-service/export/helper.ts
+++ apps/vs-code-react/src/run-service/export/helper.ts
@@ -6,7 +6,7 @@ export const getWorkflowsUri = (subscriptionId: string, iseId: string) => {
 
 export const getExportUri = (selectedSubscription: string, location: string, isValidation: boolean) => {
   const exportStep = isValidation ? 'Validate' : '';
-  return `https://brazilus.management.azure.com/subscriptions/${selectedSubscription}/providers/Microsoft.Logic/locations/${location}/${exportStep}WorkflowExport?api-version=2022-09-01-preview`;
+  return `https://management.azure.com/subscriptions/${selectedSubscription}/providers/Microsoft.Logic/locations/${location}/${exportStep}WorkflowExport?api-version=2022-09-01-preview`;
 };
 
 export const getValidationPayload = (selectedWorkflows: Array<WorkflowsList>) => {
