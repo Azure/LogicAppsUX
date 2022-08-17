635ef04a31eb7ee11a34e73e52713da5d61bff2b
Nicolas Layne <49288482+NicL9923@users.noreply.github.com>
2022-08-17 11:08:06 -0500

    [Data Mapper | VSCode Ext] Foundation (#746)
    * Initial vs-code-data-mapper boilerplate extension + layout
    
    * Figuring out message passing to/from the webview (Data Mapper React component)
    
    * Remove extra comma
    
    * Working towards passing messages and state between VS Code and Data Mapper
    
    * Spelling fix
    
    * Create class to manage webview panel
    
    * Add method to send messages to webview (for readabilitys sake)
    
    * Added separate cmds for input/output schema loading, and tinkering w/ how to best receive schema and pass to components
    
    * Read in input schema/output schema from files and pass to webview (data mapper)
    
    Co-authored-by: Nicolas Layne <nicolaslayne@microsoft.com>

--- apps/vs-code-data-mapper-react/src/main.tsx
+++ apps/vs-code-data-mapper-react/src/main.tsx
@@ -1,14 +1,20 @@
+import { WebViewMsgHandler } from './WebViewMsgHandler';
 import { App } from './app/app';
+import { store } from './state/Store';
 import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <StrictMode>
     <FluentProvider theme={teamsLightTheme}>
-      <App />
+      <Provider store={store}>
+        <WebViewMsgHandler>
+          <App />
+        </WebViewMsgHandler>
+      </Provider>
     </FluentProvider>
-    ,
   </StrictMode>,
   document.getElementById('root')
 );
