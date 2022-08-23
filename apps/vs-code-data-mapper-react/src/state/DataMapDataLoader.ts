f3f17f0b3edb5689a9fc2be010fea33b3bc2179d
Nicolas Layne <nicolaslayne@microsoft.com>
2022-08-23 09:51:51 -0500

    Clean up state

--- apps/vs-code-data-mapper-react/src/state/DataMapDataLoader.ts
+++ apps/vs-code-data-mapper-react/src/state/DataMapDataLoader.ts
@@ -5,29 +5,22 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 export interface DataMapLoadingState {
   armToken?: string;
-  resourcePath?: string;
   loadingMethod: 'file' | 'arm';
   dataMap?: DataMap;
 }
 
 const initialState: DataMapLoadingState = {
   loadingMethod: 'file',
-  resourcePath: '',
 };
 
+// TODO: Data Map handling (file and ARM)
 export const loadDataMap = createAsyncThunk('loadDataMap', async (_: void, thunkAPI) => {
   const currentState: RootState = thunkAPI.getState() as RootState;
 
-  // TODO ARM loading
   if (currentState.dataMapDataLoader.loadingMethod === 'arm') {
-    return null;
+    return undefined;
   } else {
-    try {
-      const dataMap = await import(`../../../../__mocks__/dataMaps/${currentState.dataMapDataLoader.resourcePath}`);
-      return dataMap;
-    } catch {
-      return null;
-    }
+    return undefined;
   }
 });
 
@@ -37,9 +30,6 @@ export const dataMapDataLoaderSlice = createSlice({
   reducers: {
     changeArmToken: (state, action: PayloadAction<string>) => {
       state.armToken = action.payload;
-    },
-    changeResourcePath: (state, action: PayloadAction<string>) => {
-      state.resourcePath = action.payload;
     },
     changeLoadingMethod: (state, action: PayloadAction<'file' | 'arm'>) => {
       state.loadingMethod = action.payload;
@@ -51,7 +41,6 @@ export const dataMapDataLoaderSlice = createSlice({
     });
 
     builder.addCase(loadDataMap.rejected, (state) => {
-      // TODO change to null for error handling case
       state.dataMap = undefined;
     });
   },
--- apps/vs-code-data-mapper-react/src/state/SchemaDataLoader.ts
+++ apps/vs-code-data-mapper-react/src/state/SchemaDataLoader.ts
@@ -1,13 +1,9 @@
-//import type { RootState } from './Store';
 import type { Schema } from '@microsoft/logic-apps-data-mapper';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
 
 export interface SchemaLoadingState {
   armToken?: string;
-  inputResourcePath?: string;
-  outputResourcePath?: string;
-  availableResourcesPaths?: string[];
   loadingMethod: 'file' | 'arm';
   inputSchema?: Schema;
   outputSchema?: Schema;
@@ -16,28 +12,7 @@ export interface SchemaLoadingState {
 
 const initialState: SchemaLoadingState = {
   loadingMethod: 'file',
-  inputResourcePath: '',
-  outputResourcePath: '',
 };
-
-/*
-export const loadAvailableSchemas = createAsyncThunk('schema/loadAvailableSchemas', async (_: void, thunkAPI) => {
-  const currentState: RootState = thunkAPI.getState() as RootState;
-  const availableSchemaPaths = currentState.schemaDataLoader.availableResourcesPaths;
-
-  // TODO ARM loading
-  if (currentState.schemaDataLoader.loadingMethod === 'arm') {
-    return undefined;
-  } else {
-    if (availableSchemaPaths) {
-      const loadedSchemas = availableSchemaPaths.map(async (schemaPath) => loadSchemaFromMock(schemaPath));
-      return (await Promise.all(loadedSchemas)).filter((loadedSchema) => loadedSchema) as Schema[];
-    }
-  }
-
-  return undefined;
-});
-*/
 
 export const schemaDataLoaderSlice = createSlice({
   name: 'schema',
@@ -45,15 +20,6 @@ export const schemaDataLoaderSlice = createSlice({
   reducers: {
     changeArmToken: (state, action: PayloadAction<string>) => {
       state.armToken = action.payload;
-    },
-    changeInputResourcePath: (state, action: PayloadAction<string>) => {
-      state.inputResourcePath = action.payload;
-    },
-    changeOutputResourcePath: (state, action: PayloadAction<string>) => {
-      state.outputResourcePath = action.payload;
-    },
-    changeAvailableResourcesPath: (state, action: PayloadAction<string[]>) => {
-      state.availableResourcesPaths = action.payload;
     },
     changeLoadingMethod: (state, action: PayloadAction<'file' | 'arm'>) => {
       state.loadingMethod = action.payload;
@@ -76,33 +42,5 @@ export const schemaDataLoaderSlice = createSlice({
         state.availableSchemas = [...state.availableSchemas, action.payload];
       }
     },
-  } /*
-  extraReducers: (builder) => {
-    builder.addCase(loadInputSchema.fulfilled, (state, action) => {
-      state.inputSchema = action.payload;
-    });
-
-    builder.addCase(loadInputSchema.rejected, (state) => {
-      // TODO change to null for error handling case
-      state.inputSchema = undefined;
-    });
-
-    builder.addCase(loadOutputSchema.fulfilled, (state, action) => {
-      state.outputSchema = action.payload;
-    });
-
-    builder.addCase(loadOutputSchema.rejected, (state) => {
-      // TODO change to null for error handling case
-      state.outputSchema = undefined;
-    });
-
-    builder.addCase(loadAvailableSchemas.fulfilled, (state, action) => {
-      state.availableSchemas = action.payload;
-    });
-
-    builder.addCase(loadAvailableSchemas.rejected, (state) => {
-      // TODO change to null for error handling case
-      state.availableSchemas = undefined;
-    });
-  },*/,
+  },
 });
