b665ae87eb9a55cdd7fa2ad68f668a1930ef53ca
Nicolas Layne <nicolaslayne@microsoft.com>
2022-08-24 13:17:50 -0500

    Fix storybook for properties pane

--- libs/data-mapper/src/lib/components/propertiesPane/PropertiesPane.stories.tsx
+++ libs/data-mapper/src/lib/components/propertiesPane/PropertiesPane.stories.tsx
@@ -8,13 +8,25 @@ const exInputNode = { type: NodeType.Input };
 const exOutputNode = { type: NodeType.Output };
 const exExpressionNode = { type: NodeType.Expression };
 
+const nodeOptions = { noneSelected: undefined, exInputNode, exOutputNode, exExpressionNode };
+
 export default {
   component: PropertiesPane,
   title: 'Data Mapper/PropertiesPane',
   argTypes: {
     currentNode: {
-      options: [undefined, exInputNode, exOutputNode, exExpressionNode],
-      control: { type: 'select' },
+      defaultValue: nodeOptions[0],
+      options: nodeOptions,
+      mappings: nodeOptions,
+      control: {
+        type: 'select',
+        labels: {
+          noneSelected: 'No node selected',
+          exInputNode: 'Input node',
+          exOutputNode: 'Output node',
+          exExpressionNode: 'Expression node',
+        },
+      },
     },
   },
 } as ComponentMeta<typeof PropertiesPane>;
