8f9990bdb8de64e741f8b4fac50aeaaae1c1b7ad
Nicolas Layne <49288482+NicL9923@users.noreply.github.com>
2022-08-25 16:48:37 -0500

    [DM | VSCExt] Use current VSC theme (light/dark/high-contrast) to set DM's theme (#793)
    * Remove testing msg, and add expected schema file extension .xsd
    
    * Pass theme to Data Mapper based on current VS Code theme
    
    Co-authored-by: Nicolas Layne <nicolaslayne@microsoft.com>

--- apps/vs-code-data-mapper/src/main.ts
+++ apps/vs-code-data-mapper/src/main.ts
@@ -1,14 +1,13 @@
 import { registerCommands } from './commands/commands';
 import type { ExtensionContext } from 'vscode';
-import { commands, window } from 'vscode';
+import { commands } from 'vscode';
 
 export function activate(context: ExtensionContext) {
-  window.showInformationMessage('Data Mapper extension has loaded!'); // TESTING ITEM
-
   // Set supported file extensions for context menu detection
   commands.executeCommand('setContext', 'dataMapperExtension.supportedFileExts', [
     '.xslt', // Data Maps
-    '.json', // Schemas
+    '.xsd', // Schemas
+    '.json',
     '.xml',
   ]);
 
