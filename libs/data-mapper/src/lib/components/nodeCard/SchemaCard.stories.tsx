aa278157c408f4663854ff2991e5e26b10cf019a
DanielleCogs <99753874+DanielleCogs@users.noreply.github.com>
2022-08-25 11:33:35 -0700

    added back handle and fixed storybook (#789)

--- libs/data-mapper/src/lib/components/nodeCard/SchemaCard.stories.tsx
+++ libs/data-mapper/src/lib/components/nodeCard/SchemaCard.stories.tsx
@@ -3,6 +3,7 @@ import type { SchemaCardWrapperProps } from './SchemaCard';
 import { SchemaCardWrapper } from './SchemaCard';
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
+import { ReactFlowProvider } from 'react-flow-renderer';
 
 export default {
   component: SchemaCardWrapper,
@@ -11,13 +12,15 @@ export default {
 
 export const Standard: ComponentStory<typeof SchemaCardWrapper> = (args: SchemaCardWrapperProps) => (
   <div style={{ padding: '10px' }}>
-    <SchemaCardWrapper {...args} />
+    <ReactFlowProvider>
+      <SchemaCardWrapper {...args} />
+    </ReactFlowProvider>
   </div>
 );
 Standard.args = {
   label: 'label',
   schemaType: SchemaTypes.Input,
-  displayHandle: true,
+  displayHandle: false,
   isLeaf: false,
   onClick: () => console.log('Schema card clicked'),
   disabled: false,
