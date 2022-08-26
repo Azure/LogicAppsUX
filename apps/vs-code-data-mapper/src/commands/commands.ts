97ce643912abe4ca69a46736c83e425bb8f9dcb1
Nicolas Layne <nicolaslayne@microsoft.com>
2022-08-26 10:46:24 -0500

    Cmds to create/load YAML data map files, and specific-file-extension-support for cmds

--- apps/vs-code-data-mapper/src/commands/commands.ts
+++ apps/vs-code-data-mapper/src/commands/commands.ts
@@ -1,10 +1,14 @@
 import DataMapperPanel from '../DataMapperPanel';
 import { promises as fs } from 'fs';
-import { commands, window } from 'vscode';
+import { commands, workspace } from 'vscode';
 import type { ExtensionContext, Uri } from 'vscode';
+
+import path = require('path');
+import yaml = require('js-yaml');
 
 export const registerCommands = (context: ExtensionContext) => {
   context.subscriptions.push(commands.registerCommand('dataMapperExtension.start', () => startCmd(context)));
+  context.subscriptions.push(commands.registerCommand('dataMapperExtension.createDataMapFile', createDataMapFileCmd));
   context.subscriptions.push(commands.registerCommand('dataMapperExtension.loadInputSchemaFile', loadInputSchemaFileCmd));
   context.subscriptions.push(commands.registerCommand('dataMapperExtension.loadOutputSchemaFile', loadOutputSchemaFileCmd));
   context.subscriptions.push(commands.registerCommand('dataMapperExtension.loadDataMapFile', loadDataMapFileCmd));
@@ -14,20 +18,32 @@ const startCmd = async (context: ExtensionContext) => {
   DataMapperPanel.createOrShow(context);
 };
 
+const createDataMapFileCmd = async () => {
+  const newDataMapTemplate = yaml.dump({
+    srcSchemaName: 'sourceSchemaName',
+    dstSchemaName: 'destinationSchemaName',
+    mappings: {
+      targetNodeKey: 'targetNodeKey',
+    },
+  });
+
+  const filePath = path.join(workspace.workspaceFolders[0].uri.fsPath, 'maps', 'NewDataMap.yml');
+  fs.writeFile(filePath, newDataMapTemplate, 'utf8');
+
+  DataMapperPanel.currentPanel.sendMsgToWebview({ command: 'loadDataMap', data: newDataMapTemplate });
+};
+
 const loadInputSchemaFileCmd = async (uri: Uri) => {
-  const inputSchemaJson = JSON.parse(await fs.readFile(uri.fsPath, 'utf-8'));
-  DataMapperPanel.currentPanel.sendMsgToWebview({ command: 'loadInputSchema', data: inputSchemaJson });
+  const inputSchema = JSON.parse(await fs.readFile(uri.fsPath, 'utf-8'));
+  DataMapperPanel.currentPanel.sendMsgToWebview({ command: 'loadInputSchema', data: inputSchema });
 };
 
 const loadOutputSchemaFileCmd = async (uri: Uri) => {
-  const outputSchemaJson = JSON.parse(await fs.readFile(uri.fsPath, 'utf-8'));
-  DataMapperPanel.currentPanel.sendMsgToWebview({ command: 'loadOutputSchema', data: outputSchemaJson });
+  const outputSchema = JSON.parse(await fs.readFile(uri.fsPath, 'utf-8'));
+  DataMapperPanel.currentPanel.sendMsgToWebview({ command: 'loadOutputSchema', data: outputSchema });
 };
 
 const loadDataMapFileCmd = async (uri: Uri) => {
-  const dataMapJson = JSON.parse(await fs.readFile(uri.fsPath, 'utf-8'));
-  window.showInformationMessage(JSON.stringify(dataMapJson));
-  // DataMapperPanel.currentPanel.sendMsgToWebview({ command: 'loadDataMap', data: TODO - datamap })
-
-  window.showInformationMessage("Hey, you can't do that yet! (loading data maps)"); // TESTING ITEM
+  const dataMap = JSON.parse(await fs.readFile(uri.fsPath, 'utf-8'));
+  DataMapperPanel.currentPanel.sendMsgToWebview({ command: 'loadDataMap', data: dataMap });
 };
--- apps/vs-code-data-mapper/src/main.ts
+++ apps/vs-code-data-mapper/src/main.ts
@@ -4,12 +4,13 @@ import { commands } from 'vscode';
 
 export function activate(context: ExtensionContext) {
   // Set supported file extensions for context menu detection
-  commands.executeCommand('setContext', 'dataMapperExtension.supportedFileExts', [
-    '.xslt', // Data Maps
-    '.xsd', // Schemas
-    '.json',
-    '.xml',
-  ]);
+  const supportedDataMapFileExts = ['.xslt', '.yml']; // TODO: Probably remove .xslt, I don't believe Data Mapper is expected to read that in (yet at least)
+  commands.executeCommand('setContext', 'dataMapperExtension.supportedDataMapFileExts', supportedDataMapFileExts);
+
+  const supportedSchemaFileExts = ['.xsd', '.json']; // JSON for TESTING expected returned values/schema-json from backend
+  commands.executeCommand('setContext', 'dataMapperExtension.supportedSchemaFileExts', supportedSchemaFileExts);
+
+  commands.executeCommand('setContext', 'dataMapperExtension.supportedFileExts', [...supportedDataMapFileExts, ...supportedSchemaFileExts]);
 
   registerCommands(context);
 }
