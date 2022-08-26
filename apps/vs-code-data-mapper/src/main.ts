fad5a232306321f69cb07ec28bc2903ed9b7053d
Nicolas Layne <nicolaslayne@microsoft.com>
2022-08-26 12:13:53 -0500

    Support only .yml for loading data maps

--- apps/vs-code-data-mapper/src/main.ts
+++ apps/vs-code-data-mapper/src/main.ts
@@ -4,7 +4,7 @@ import { commands } from 'vscode';
 
 export function activate(context: ExtensionContext) {
   // Set supported file extensions for context menu detection
-  const supportedDataMapFileExts = ['.xslt', '.yml']; // TODO: Probably remove .xslt, I don't believe Data Mapper is expected to read that in (yet at least)
+  const supportedDataMapFileExts = ['.yml'];
   commands.executeCommand('setContext', 'dataMapperExtension.supportedDataMapFileExts', supportedDataMapFileExts);
 
   const supportedSchemaFileExts = ['.xsd', '.json']; // JSON for TESTING expected returned values/schema-json from backend
