b4b5413793fdca28a093e506c217aec5f4df6b96
danielle <dacogbur@microsoft.com>
2022-09-01 06:21:24 -0700

    styling improvements for schema tree

--- libs/data-mapper/src/lib/components/tree/SchemaTree.tsx
+++ libs/data-mapper/src/lib/components/tree/SchemaTree.tsx
@@ -1,14 +1,8 @@
 import type { SchemaExtended, SchemaNodeDataType, SchemaNodeExtended } from '../../models';
 import { icon16ForSchemaNodeType } from '../../utils/Icon.Utils';
-import { tokens } from '@fluentui/react-components';
-import { bundleIcon, CheckmarkCircle16Filled, Circle16Regular } from '@fluentui/react-icons';
-import {
-  fluentTreeItem,
-  fluentTreeView,
-  neutralFillStealthRest,
-  provideFluentDesignSystem,
-  treeItemStyles,
-} from '@fluentui/web-components';
+import { Caption1, tokens } from '@fluentui/react-components';
+import { bundleIcon, CheckmarkCircle16Filled, Circle16Regular, ChevronRight16Regular, ChevronRight16Filled } from '@fluentui/react-icons';
+import { fluentTreeItem, fluentTreeView, provideFluentDesignSystem, treeItemStyles } from '@fluentui/web-components';
 import { css } from '@microsoft/fast-element';
 import type { FoundationElementDefinition, OverrideFoundationElementDefinition, TreeItemOptions } from '@microsoft/fast-foundation';
 import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
@@ -21,7 +15,19 @@ import React, { useMemo } from 'react';
 // :host([selected])::after {
 //   visibility: hidden;
 // }
+// :host(:not([disabled])).positioning-region:hover {
+//   background: purple; ${tokens.colorNeutralBackground1Hover};
+// }
 // `;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const ChevronIcon = bundleIcon(ChevronRight16Regular, ChevronRight16Filled);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const options2: TreeItemOptions = {
+  expandCollapseGlyph: `<ChevronIcon></ChevronIcon>`,
+  baseName: 'tree-item',
+};
 
 const overrides: OverrideFoundationElementDefinition<FoundationElementDefinition> = {
   styles: (ctx, def) => {
@@ -31,16 +37,23 @@ const overrides: OverrideFoundationElementDefinition<FoundationElementDefinition
       ${baseStyles}
       .positioning-region {
         border-radius: ${tokens.borderRadiusMedium};
-        color: orange;
+        background-color: ${tokens.colorNeutralBackground1};
+
+        height: 28px;
+        padding: 4px 6px 4px 6px;
       }
-      :host {
-        border-radius: ${tokens.borderRadiusMedium};
+      .content-region {
+        height: 16px;
+        color: ${tokens.colorNeutralForeground1};
+      }
+      :host(:not([disabled])).positioning-region:hover {
+        background: ${tokens.colorNeutralBackground1Hover};
       }
       :host([selected])::after {
         visibility: hidden;
       }
       :host(:not([disabled])[selected]) .positioning-region {
-        background: ${neutralFillStealthRest};
+        background: ${tokens.colorBrandBackground2};
       }
     `;
     return mergedStyles; //css`${baseStyles.behaviors}`
@@ -72,12 +85,13 @@ const convertToFastTreeItem = (
 ) => {
   return node.children.map((childNode) => {
     const isNodeSelected = !!currentlySelectedNodes.find((currentlySelectedNode) => currentlySelectedNode.key === childNode.key);
+    const nameText = <Caption1>{childNode.name}</Caption1>;
     if (childNode.schemaNodeDataType === 'ComplexType' || childNode.schemaNodeDataType === 'None') {
       return (
         // TODO onclick for object level adding
         <FastTreeItem key={childNode.key}>
           <TreeItemContent nodeType={childNode.schemaNodeDataType} filled={isNodeSelected}>
-            {childNode.name}
+            {nameText}
           </TreeItemContent>
           {convertToFastTreeItem(childNode, currentlySelectedNodes, onLeafNodeClick)}
         </FastTreeItem>
@@ -91,7 +105,7 @@ const convertToFastTreeItem = (
           }}
         >
           <TreeItemContent nodeType={childNode.schemaNodeDataType} filled={isNodeSelected}>
-            {childNode.name}
+            {nameText}
           </TreeItemContent>
         </FastTreeItem>
       );
