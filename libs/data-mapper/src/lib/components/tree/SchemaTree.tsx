c6dd769fd53232080f1edc51bfd2fa92842408d7
danielle <dacogbur@microsoft.com>
2022-09-01 12:30:53 -0600

    added chevron and other styling

--- libs/data-mapper/src/lib/components/tree/SchemaTree.tsx
+++ libs/data-mapper/src/lib/components/tree/SchemaTree.tsx
@@ -1,13 +1,48 @@
 import type { SchemaExtended, SchemaNodeDataType, SchemaNodeExtended } from '../../models';
 import { icon16ForSchemaNodeType } from '../../utils/Icon.Utils';
-import { Caption1, tokens } from '@fluentui/react-components';
+import { Caption1, makeStyles, tokens } from '@fluentui/react-components';
 import { bundleIcon, CheckmarkCircle16Filled, Circle16Regular, ChevronRight16Regular, ChevronRight16Filled } from '@fluentui/react-icons';
 import { fluentTreeItem, fluentTreeView, provideFluentDesignSystem, treeItemStyles } from '@fluentui/web-components';
 import { css } from '@microsoft/fast-element';
-import type { FoundationElementDefinition, OverrideFoundationElementDefinition, TreeItemOptions } from '@microsoft/fast-foundation';
+import type { OverrideFoundationElementDefinition, TreeItemOptions } from '@microsoft/fast-foundation';
 import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
 import React, { useMemo } from 'react';
+import { renderToString } from 'react-dom/server';
 
+// const styles = makeStyles(
+//   {
+//     icon: {
+//       '&:hover': {
+//         backgroundColor: 'inherit'
+//       }
+//     }
+//   }
+// )
+
+const { wrap } = provideReactWrapper(React, provideFluentDesignSystem());
+const FastTreeView = wrap(fluentTreeView());
+
+const tryingStyles = makeStyles({
+  '.positioning-region': {
+    //bor: tokens.borderRadiusMedium,
+    backgroundColor: tokens.colorBrandShadowAmbient, //tokens.colorNeutralBackground1
+    //height: 28px;
+    //padding: 4px 6px 4px 6px;
+  },
+  // .content-region {
+  //   height: 16px;
+  //   color: ${tokens.colorNeutralForeground1};
+  // }
+  // :host(:not([disabled])).positioning-region:hover {
+  //   background: ${tokens.colorNeutralBackground1Hover};
+  // }
+  // :host([selected])::after {
+  //   visibility: hidden;
+  // }
+  // :host(:not([disabled])[selected]) .positioning-region {
+  //   background: ${tokens.colorBrandBackground2};
+  // }
+});
 // const css2 = `
 // :host {
 //   border-radius: ${tokens.borderRadiusMedium};
@@ -20,49 +55,8 @@ import React, { useMemo } from 'react';
 // }
 // `;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const ChevronIcon = bundleIcon(ChevronRight16Regular, ChevronRight16Filled);
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const options2: TreeItemOptions = {
-  expandCollapseGlyph: `<ChevronIcon></ChevronIcon>`,
-  baseName: 'tree-item',
-};
-
-const overrides: OverrideFoundationElementDefinition<FoundationElementDefinition> = {
-  styles: (ctx, def) => {
-    const baseStyles = treeItemStyles(ctx, def as TreeItemOptions);
-    console.log(baseStyles);
-    const mergedStyles = css`
-      ${baseStyles}
-      .positioning-region {
-        border-radius: ${tokens.borderRadiusMedium};
-        background-color: ${tokens.colorNeutralBackground1};
-
-        height: 28px;
-        padding: 4px 6px 4px 6px;
-      }
-      .content-region {
-        height: 16px;
-        color: ${tokens.colorNeutralForeground1};
-      }
-      :host(:not([disabled])).positioning-region:hover {
-        background: ${tokens.colorNeutralBackground1Hover};
-      }
-      :host([selected])::after {
-        visibility: hidden;
-      }
-      :host(:not([disabled])[selected]) .positioning-region {
-        background: ${tokens.colorBrandBackground2};
-      }
-    `;
-    return mergedStyles; //css`${baseStyles.behaviors}`
-  },
-};
-
-const { wrap } = provideReactWrapper(React, provideFluentDesignSystem());
-export const FastTreeView = wrap(fluentTreeView());
-export const FastTreeItem = wrap(fluentTreeItem(overrides));
+// ${tryingStyles()['.positioning-region']}
+console.log(tryingStyles);
 
 export interface SchemaTreeProps {
   schema: SchemaExtended;
@@ -78,11 +72,28 @@ export const SchemaTree: React.FC<SchemaTreeProps> = ({ schema, currentlySelecte
   return <FastTreeView>{treeItems}</FastTreeView>;
 };
 
+const ChevronIcon = bundleIcon(ChevronRight16Filled, ChevronRight16Regular);
+
 const convertToFastTreeItem = (
   node: SchemaNodeExtended,
   currentlySelectedNodes: SchemaNodeExtended[],
   onLeafNodeClick: (schemaNode: SchemaNodeExtended) => void
 ) => {
+  //const treeItemStyles = useStyles();
+  const overrides: OverrideFoundationElementDefinition<TreeItemOptions> = {
+    expandCollapseGlyph: renderToString(<ChevronIcon filled={false} />),
+    baseName: 'tree-item',
+    styles: (ctx, def) => {
+      const baseStyles = treeItemStyles(ctx, def as TreeItemOptions);
+      console.log(baseStyles);
+      const mergedStyles = css`
+        ${baseStyles}
+      `;
+      return mergedStyles; //css`${baseStyles.behaviors}`
+    },
+  };
+  const FastTreeItem = wrap(fluentTreeItem(overrides));
+
   return node.children.map((childNode) => {
     const isNodeSelected = !!currentlySelectedNodes.find((currentlySelectedNode) => currentlySelectedNode.key === childNode.key);
     const nameText = <Caption1>{childNode.name}</Caption1>;
