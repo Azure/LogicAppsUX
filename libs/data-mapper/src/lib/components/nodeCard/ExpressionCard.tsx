873daf933ca3ac0be4571f28af0b7ddc2ae0fe86
refortie <37600290+refortie@users.noreply.github.com>
2022-09-09 13:43:15 -0700

    [Data Mapper] Allow expression cards to be added to the map (#854)
    * Enhance model wip
    
    * Expressions Cards on the canvas

--- libs/data-mapper/src/lib/components/nodeCard/ExpressionCard.tsx
+++ libs/data-mapper/src/lib/components/nodeCard/ExpressionCard.tsx
@@ -1,4 +1,6 @@
 import type { ExpressionGroupBranding } from '../../constants/ExpressionConstants';
+import { store } from '../../core/state/Store';
+import type { ExpressionInput } from '../../models/Expression';
 import { iconUriForIconImageName } from '../../utils/Icon.Utils';
 import type { CardProps } from './NodeCard';
 import { getStylesForSharedState } from './NodeCard';
@@ -15,13 +17,15 @@ import {
   Tooltip,
 } from '@fluentui/react-components';
 import type { FunctionComponent } from 'react';
-import type { NodeProps } from 'react-flow-renderer';
+import type { Connection as ReactFlowConnection, NodeProps } from 'react-flow-renderer';
+import { Handle, Position } from 'react-flow-renderer';
 
 export type ExpressionCardProps = {
   expressionName: string;
+  numberOfInputs: number;
+  inputs: ExpressionInput[];
   iconFileName?: string;
   expressionBranding: ExpressionGroupBranding;
-  onClick: () => void;
 } & CardProps;
 
 const useStyles = makeStyles({
@@ -79,13 +83,41 @@ const useStyles = makeStyles({
   }),
 });
 
+const handleStyle: React.CSSProperties = { zIndex: 5, width: '10px', height: '10px' };
+
+const isValidConnection = (connection: ReactFlowConnection, inputs: ExpressionInput[]): boolean => {
+  const flattenedInputSchema = store.getState().dataMap.curDataMapOperation.flattenedInputSchema;
+
+  if (connection.source && connection.target && flattenedInputSchema) {
+    const inputNode = flattenedInputSchema[connection.source];
+
+    // For now just allow all expression to expression
+    // TODO validate express to expression connections
+    return (
+      !inputNode ||
+      inputs.some((input) => input.acceptableInputTypes.some((acceptableType) => acceptableType === inputNode.schemaNodeDataType))
+    );
+  }
+
+  return false;
+};
+
 export const ExpressionCard: FunctionComponent<NodeProps<ExpressionCardProps>> = (props: NodeProps<ExpressionCardProps>) => {
-  const { onClick, expressionName, disabled, error, expressionBranding, iconFileName } = props.data;
+  const { expressionName, numberOfInputs, inputs, disabled, error, expressionBranding, iconFileName, displayHandle, onClick } = props.data;
   const classes = useStyles();
   const mergedClasses = mergeClasses(getStylesForSharedState().root, classes.root);
 
   return (
     <div className={classes.container}>
+      {displayHandle && numberOfInputs !== 0 ? (
+        <Handle
+          type={'target'}
+          position={Position.Left}
+          style={handleStyle}
+          isValidConnection={(connection) => isValidConnection(connection, inputs)}
+        />
+      ) : null}
+
       {error && <PresenceBadge size="extra-small" status="busy" className={classes.badge}></PresenceBadge>}
       <Tooltip
         content={{
@@ -102,6 +134,7 @@ export const ExpressionCard: FunctionComponent<NodeProps<ExpressionCardProps>> =
           )}
         </Button>
       </Tooltip>
+      {displayHandle ? <Handle type={'source'} position={Position.Right} style={handleStyle} /> : null}
     </div>
   );
 };
