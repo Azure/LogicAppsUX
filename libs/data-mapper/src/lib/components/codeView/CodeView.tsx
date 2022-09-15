e4a28a58253d45200a6bfdaf1a88d6fc8a151b93
Nicolas Layne <49288482+NicL9923@users.noreply.github.com>
2022-09-15 16:53:50 -0500

    [Data Mapper] Code View component (#892)
    * Initial Code View component
    
    * Properly integrate CodeView to central canvas view
    
    * Fix view not being hidden by properties pane
    
    * Change prop pane
    
    * Add temp border style around code editor
    
    * Slight style adjustments
    
    Co-authored-by: Nicolas Layne <nicolaslayne@microsoft.com>

---
+++ libs/data-mapper/src/lib/components/codeView/CodeView.tsx
@@ -0,0 +1,63 @@
+import { Stack } from '@fluentui/react';
+import { Button, makeStyles, shorthands, Text, typographyStyles } from '@fluentui/react-components';
+import { Code20Regular, Dismiss20Regular } from '@fluentui/react-icons';
+import { EditorLanguage, MonacoEditor } from '@microsoft/designer-ui';
+import { useIntl } from 'react-intl';
+
+const useStyles = makeStyles({
+  containerStyle: {
+    height: '100%',
+    ...shorthands.overflow('hidden'),
+    width: '25%',
+    ...shorthands.padding('12px'),
+    boxSizing: 'border-box',
+  },
+  titleTextStyle: {
+    ...typographyStyles.body1Strong,
+  },
+  editorStyle: {
+    ...shorthands.border('1px', 'solid', '#313131'),
+    ...shorthands.borderRadius('3px'),
+    height: '500px',
+  },
+});
+
+export interface CodeViewProps {
+  dataMapDefinition: string;
+  isCodeViewOpen: boolean;
+  setIsCodeViewOpen: (isOpen: boolean) => void;
+}
+
+export const CodeView = ({ dataMapDefinition, isCodeViewOpen, setIsCodeViewOpen }: CodeViewProps) => {
+  const intl = useIntl();
+  const styles = useStyles();
+
+  const codeViewLoc = intl.formatMessage({
+    defaultMessage: 'Code view',
+    description: 'Code view title',
+  });
+
+  return (
+    <div className={styles.containerStyle} style={{ height: '100%', display: isCodeViewOpen ? 'block' : 'none' }}>
+      <Stack horizontal verticalAlign="center" style={{ justifyContent: 'space-between', marginBottom: '12px', marginTop: '4px' }}>
+        <Stack horizontal verticalAlign="center">
+          <Code20Regular />
+          <Text className={styles.titleTextStyle} style={{ marginLeft: '8px' }}>
+            {codeViewLoc}
+          </Text>
+        </Stack>
+
+        <Button icon={<Dismiss20Regular />} appearance="subtle" onClick={() => setIsCodeViewOpen(false)} />
+      </Stack>
+
+      <MonacoEditor
+        language={EditorLanguage.templateExpressionLanguage}
+        value={dataMapDefinition}
+        lineNumbers="on"
+        scrollbar={{ horizontal: 'auto', vertical: 'auto' }}
+        className={styles.editorStyle}
+        readOnly
+      />
+    </div>
+  );
+};
