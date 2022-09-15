3fe2b405d46409fb1e263a431620b864f40395f0
Eric <wue@microsoft.com>
2022-09-15 15:40:49 -0400

    some formatting changes

--- libs/designer/src/lib/ui/panel/panelTabs/scratchTab.tsx
+++ libs/designer/src/lib/ui/panel/panelTabs/scratchTab.tsx
@@ -51,16 +51,10 @@ export const ScratchTab = () => {
           initialValue={[
             {
               type: ValueSegmentType.LITERAL,
-              value: `var rows = prompt("How many rows for your multiplication table?");
-var cols = prompt("How many columns for your multiplication table?");
-if (rows == "" || rows == null)
-  rows = 10;
-if (cols == "" || cols == null)
-  cols = 10;
-createTable(rows, cols);
+              value: `
 function createTable(rows, cols) {
   var j = 1;
-  var output = "<table border='1' width='500' cellspacing='0'cellpadding='5'>";
+  var output = "<table>";
   for (i = 1; i <= rows; i++) {
     output = output + "<tr>";
     while (j <= cols) {
