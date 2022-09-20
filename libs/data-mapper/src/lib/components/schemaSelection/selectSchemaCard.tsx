340c4138a08aaf1ea4ac902de1cfaf1193a3a726
Nicolas Layne <nicolaslayne@microsoft.com>
2022-09-20 13:22:42 -0500

    Specific styling touch-ups

--- libs/data-mapper/src/lib/components/schemaSelection/selectSchemaCard.tsx
+++ libs/data-mapper/src/lib/components/schemaSelection/selectSchemaCard.tsx
@@ -1,27 +1,25 @@
 import { SchemaTypes } from '../../models';
 import CardOnHover from './card_onHover.svg';
 import CardOnRest from './card_onRest.svg';
-import { Image } from '@fluentui/react';
-import { makeStyles, shorthands, tokens } from '@fluentui/react-components';
+import { Image, Stack } from '@fluentui/react';
+import { makeStyles, shorthands, tokens, typographyStyles } from '@fluentui/react-components';
 import { useState } from 'react';
 import { useIntl } from 'react-intl';
 
 const useStyles = makeStyles({
   schemaSelectCard: {
-    width: '240px',
-    height: '280px',
-    backgroundColor: 'white',
+    width: '200px',
+    height: '260px',
+    backgroundColor: tokens.colorNeutralBackground1,
+    color: tokens.colorNeutralForeground1,
     ...shorthands.borderRadius(tokens.borderRadiusMedium),
-    ...shorthands.border('2px', 'solid', 'white'),
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: '16px',
+    ...shorthands.border(tokens.strokeWidthThick, 'solid', tokens.colorNeutralBackground1),
+    ...typographyStyles.body1Strong,
     cursor: 'pointer',
     ...shorthands.margin('auto'),
-    ...shorthands.padding('auto'),
     ':hover': {
-      backgroundColor: '#deecf9',
-      ...shorthands.borderColor('#0078d4'),
+      backgroundColor: tokens.colorBrandBackground2,
+      ...shorthands.border(tokens.strokeWidthThick, 'solid', tokens.colorBrandStroke1),
     },
   },
 });
@@ -30,8 +28,6 @@ export interface SelectSchemaCardProps {
   schemaType: SchemaTypes;
   onClick: () => void;
 }
-
-const svgStyle = { paddingLeft: 70, paddingRight: 70, paddingTop: 64, paddingBottom: 28 };
 
 export const SelectSchemaCard = ({ schemaType, onClick }: SelectSchemaCardProps) => {
   const intl = useIntl();
@@ -59,14 +55,16 @@ export const SelectSchemaCard = ({ schemaType, onClick }: SelectSchemaCardProps)
   }
 
   return (
-    <div
+    <Stack
+      verticalAlign="center"
+      horizontalAlign="center"
       className={styles.schemaSelectCard}
       onClick={onClick}
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
     >
-      <Image src={isHovering ? CardOnHover : CardOnRest} style={svgStyle} alt="Empty schema card svg" />
+      <Image src={isHovering ? CardOnHover : CardOnRest} alt="Empty schema card svg" style={{ paddingBottom: '28px' }} />
       {selectSchemaMsg}
-    </div>
+    </Stack>
   );
 };
