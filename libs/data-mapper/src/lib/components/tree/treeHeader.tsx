ecb3cfa7186649c00a2527ac7a33d821cd24bcb8
Nicolas Layne <nicolaslayne@microsoft.com>
2022-09-28 11:11:25 -0500

    Output pane integration

--- libs/data-mapper/src/lib/components/tree/treeHeader.tsx
+++ libs/data-mapper/src/lib/components/tree/treeHeader.tsx
@@ -1,15 +1,32 @@
 import type { ISearchBoxStyleProps, ISearchBoxStyles, IStyleFunctionOrObject } from '@fluentui/react';
 import { SearchBox } from '@fluentui/react';
 import { Body1, makeStyles, tokens, typographyStyles } from '@fluentui/react-components';
-import React from 'react';
+import { useIntl } from 'react-intl';
 
-interface TreeHeaderProps {
-  title: string;
-  onSearch: (searchTerm: string) => void;
-  onClear: () => void;
-}
+const searchBoxStyles: IStyleFunctionOrObject<ISearchBoxStyleProps, ISearchBoxStyles> = {
+  root: {
+    '.ms-SearchBox': {
+      borderColor: tokens.colorNeutralStroke1,
+      borderBottom: `${tokens.strokeWidthThin} solid ${tokens.colorNeutralStrokeAccessible}`,
+    },
+    '::after': {
+      borderRadius: tokens.borderRadiusMedium,
+      borderTop: `${tokens.strokeWidthThin} solid ${tokens.colorNeutralStroke1}`,
+      borderLeft: `${tokens.strokeWidthThin} solid ${tokens.colorNeutralStroke1}`,
+      borderRight: `${tokens.strokeWidthThin} solid ${tokens.colorNeutralStroke1}`,
+    },
+    ':hover': {
+      borderColor: tokens.colorNeutralStroke1Hover,
+      borderBottomColor: tokens.colorNeutralStrokeAccessible,
+    },
+    borderColor: tokens.colorNeutralStroke1,
+    borderBottomColor: tokens.colorNeutralStrokeAccessible,
+    borderRadius: tokens.borderRadiusMedium,
+    marginBottom: '6px',
+  },
+};
 
-const titleStyle = makeStyles({
+const useStyles = makeStyles({
   header: {
     ...typographyStyles.body1Strong,
     paddingBottom: '10px',
@@ -17,35 +34,25 @@ const titleStyle = makeStyles({
   },
 });
 
-export const TreeHeader: React.FC<TreeHeaderProps> = (props) => {
-  const style2 = titleStyle();
-  const searchBoxStyles: IStyleFunctionOrObject<ISearchBoxStyleProps, ISearchBoxStyles> = {
-    root: {
-      '.ms-SearchBox': {
-        borderColor: tokens.colorNeutralStroke1,
-        borderBottom: `${tokens.strokeWidthThin} solid ${tokens.colorNeutralStrokeAccessible}`,
-      },
-      '::after': {
-        borderRadius: tokens.borderRadiusMedium,
-        borderTop: `${tokens.strokeWidthThin} solid ${tokens.colorNeutralStroke1}`,
-        borderLeft: `${tokens.strokeWidthThin} solid ${tokens.colorNeutralStroke1}`,
-        borderRight: `${tokens.strokeWidthThin} solid ${tokens.colorNeutralStroke1}`,
-      },
-      ':hover': {
-        borderColor: tokens.colorNeutralStroke1Hover,
-        borderBottomColor: tokens.colorNeutralStrokeAccessible,
-      },
-      borderColor: tokens.colorNeutralStroke1,
-      borderBottomColor: tokens.colorNeutralStrokeAccessible,
-      borderRadius: tokens.borderRadiusMedium,
-      marginBottom: '6px',
-    },
-  };
+interface TreeHeaderProps {
+  title?: string;
+  onSearch: (searchTerm: string) => void;
+  onClear: () => void;
+}
+
+export const TreeHeader = ({ title, onSearch, onClear }: TreeHeaderProps) => {
+  const intl = useIntl();
+  const styles = useStyles();
+
+  const searchLoc = intl.formatMessage({
+    defaultMessage: 'Search',
+    description: 'Search',
+  });
 
   return (
     <span>
-      <Body1 className={style2.header}>{props.title}</Body1>
-      <SearchBox onSearch={props.onSearch} onClear={props.onClear} styles={searchBoxStyles} placeholder="Search"></SearchBox>
+      {title && <Body1 className={styles.header}>{title}</Body1>}
+      <SearchBox onSearch={onSearch} onClear={onClear} styles={searchBoxStyles} placeholder={searchLoc}></SearchBox>
     </span>
   );
 };
