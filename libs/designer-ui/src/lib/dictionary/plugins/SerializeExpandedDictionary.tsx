8c62070e857358ae053fb1090a0ade06a5052a25
Eric Wu <95886809+Eric-B-Wu@users.noreply.github.com>
2022-09-29 09:29:47 -0400

    Table Editor (#975)
    * dynamic ref for tokenpicker button
    
    * temporary checkin
    
    * fixed issue of deleting item in dictionary editor
    
    * fixed token picker button width adjusting
    
    * changes based on comments
    
    * fix dictionary type
    
    * make default dictionary type

--- libs/designer-ui/src/lib/dictionary/plugins/SerializeExpandedDictionary.tsx
+++ libs/designer-ui/src/lib/dictionary/plugins/SerializeExpandedDictionary.tsx
@@ -1,10 +1,12 @@
 import type { DictionaryEditorItemProps } from '..';
 import type { ValueSegment } from '../../editor';
 import { serializeEditorState } from '../../editor/base/utils/editorToSegement';
+import { parseSegments } from '../../editor/base/utils/parsesegments';
 import { ExpandedDictionaryEditorType } from '../expandeddictionary';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
-import type { EditorState } from 'lexical';
+import { $setSelection, CLEAR_EDITOR_COMMAND, type EditorState } from 'lexical';
+import { useEffect, useState } from 'react';
 
 interface SerializeExpandedDictionaryProps {
   items: DictionaryEditorItemProps[];
@@ -16,17 +18,40 @@ interface SerializeExpandedDictionaryProps {
 
 export const SerializeExpandedDictionary = ({ items, initialItem, index, type, setItems }: SerializeExpandedDictionaryProps) => {
   const [editor] = useLexicalComposerContext();
+  const [isDelete, setIsDelete] = useState(false);
+  const [itemLength, setItemLength] = useState(0);
+
+  useEffect(() => {
+    if (itemLength > items.length) {
+      editor.dispatchCommand(CLEAR_EDITOR_COMMAND, undefined);
+      editor.update(() => {
+        if (type === ExpandedDictionaryEditorType.KEY) {
+          parseSegments(items[index].key, true);
+        } else {
+          parseSegments(items[index].value, true);
+        }
+        $setSelection(null);
+      });
+    }
+    setItemLength(items.length);
+    setIsDelete(true);
+  }, [editor, index, itemLength, items, type]);
 
   const onChange = (editorState: EditorState) => {
     const newValue = serializeEditorState(editorState);
     if (notEqual(initialItem, newValue)) {
       const newItems = JSON.parse(JSON.stringify(items));
+
       if (type === ExpandedDictionaryEditorType.KEY) {
         newItems[index].key = newValue;
       } else {
         newItems[index].value = newValue;
       }
-      setItems(newItems);
+      if (!isDelete) {
+        setItems(newItems);
+      } else {
+        setIsDelete(false);
+      }
       editor.focus();
     }
   };
