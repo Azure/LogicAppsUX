191d0a7ad2bab91d07a0d93074b909c8be3d1f4d
Travis Harris <hartra344@users.noreply.github.com>
2022-09-30 12:38:30 -0700

    Add serialization E2E test and add assertion to dnd e2e test (#985)
    * Add serialization E2E test and add assertion to dnd e2e test
    
    * Fix foreach deserialization/serialization
    
    * Change text

--- libs/designer/src/lib/index.tsx
+++ libs/designer/src/lib/index.tsx
@@ -1,3 +1,17 @@
+import * as Models from './common/models/workflow';
+import { serializeWorkflow as serializeBJSWorkflow } from './core/actions/bjsworkflow/serializer';
+import * as Core from './core/index';
+import * as UI from './ui/index';
+
+if (process.env.NODE_ENV === 'development') {
+  if (process.env)
+    (window as any).DesignerModule = {
+      UI,
+      Core,
+      Models,
+      serializeBJSWorkflow,
+    };
+}
 export * from './ui/index';
 export * from './core/index';
 export * from './common/models/workflow';
