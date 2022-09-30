4ac9579d36410f9270289e2390a017a4f43be977
Nicolas Layne <49288482+NicL9923@users.noreply.github.com>
2022-09-30 16:53:55 -0500

    [DM | Target Schema Pane] Status icons (#989)
    * Hooked up status icons for leaf nodes
    
    * Have parents track their childrens toggled status - status icons working for parents now as a result
    
    * Major refactor of SchemaTreeItem - everything still works but fast styles -> not properly setting
    
    * Resolve ambiguous prop type
    
    * Restore fast style object to its original state for more debugging!
    
    * Renaming for clarity
    
    * Mostly working recursion for target schema item statuses, and some style changes
    
    * Resolve FAST styles!
    
    * Fix issue with recursion returning wrong value
    
    * Remove console log
    
    * Refactor giant recursive blob
    
    Co-authored-by: Nicolas Layne <nicolaslayne@microsoft.com>

--- libs/data-mapper/src/lib/components/targetSchemaPane/TargetSchemaPane.tsx
+++ libs/data-mapper/src/lib/components/targetSchemaPane/TargetSchemaPane.tsx
@@ -1,10 +1,12 @@
 import { setCurrentTargetNode } from '../../core/state/DataMapSlice';
 import type { AppDispatch, RootState } from '../../core/state/Store';
-import type { SchemaNodeExtended } from '../../models';
-import { TargetSchemaTree } from '../tree/TargetTree';
+import { NormalizedDataType, SchemaNodeDataType, type SchemaNodeExtended } from '../../models';
+import { SchemaTree } from '../tree/SchemaTree';
+import { ItemToggledState, type NodeToggledStateDictionary } from '../tree/SchemaTreeItem';
 import { Stack } from '@fluentui/react';
 import { Button, makeStyles, shorthands, Text, tokens, typographyStyles } from '@fluentui/react-components';
 import { ChevronDoubleRight20Regular, ChevronDoubleLeft20Regular } from '@fluentui/react-icons';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useIntl } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -35,6 +37,10 @@ export const TargetSchemaPane = ({ isExpanded, setIsExpanded }: TargetSchemaPane
 
   const dispatch = useDispatch<AppDispatch>();
   const targetSchema = useSelector((state: RootState) => state.dataMap.curDataMapOperation.targetSchema);
+  const targetSchemaDictionary = useSelector((state: RootState) => state.dataMap.curDataMapOperation.flattenedTargetSchema);
+  const connectionDictionary = useSelector((state: RootState) => state.dataMap.curDataMapOperation.dataMapConnections);
+
+  const [toggledStatesDictionary, setToggledStatesDictionary] = useState<NodeToggledStateDictionary | undefined>({});
 
   const targetSchemaLoc = intl.formatMessage({
     defaultMessage: 'Target schema',
@@ -44,6 +50,80 @@ export const TargetSchemaPane = ({ isExpanded, setIsExpanded }: TargetSchemaPane
   const handleItemClick = (schemaNode: SchemaNodeExtended) => {
     dispatch(setCurrentTargetNode({ schemaNode: schemaNode, resetSelectedSourceNodes: true }));
   };
+
+  // For MVP - only checks for a connection, not its validity
+  const targetNodesWithConnections = useMemo(() => {
+    const nodesWithConnections: { [key: string]: true } = {};
+
+    Object.entries(connectionDictionary).forEach(([_key, value]) => {
+      if (value.reactFlowDestination in targetSchemaDictionary) {
+        nodesWithConnections[value.destination] = true; // targetSchemaDictionary[value.reactFlowDestination]
+      }
+    });
+
+    return nodesWithConnections;
+  }, [connectionDictionary, targetSchemaDictionary]);
+
+  /*eslint no-param-reassign: ["error", { "props": false }]*/
+  const handleObjectParentToggledState = useCallback(
+    (stateDict: NodeToggledStateDictionary, nodeKey: string, nodeChildrenToggledAmt: number, nodeChildrenAmt: number) => {
+      if (nodeChildrenToggledAmt === nodeChildrenAmt) {
+        stateDict[nodeKey] = ItemToggledState.Completed;
+        return 1;
+      } else if (nodeChildrenToggledAmt === 0) {
+        stateDict[nodeKey] = ItemToggledState.NotStarted;
+      } else {
+        stateDict[nodeKey] = ItemToggledState.InProgress;
+      }
+
+      return 0;
+    },
+    []
+  );
+
+  const handleNodeWithValue = useCallback(
+    (stateDict: NodeToggledStateDictionary, nodeKey: string) => {
+      if (nodeKey in targetNodesWithConnections) {
+        stateDict[nodeKey] = ItemToggledState.Completed;
+        return 1;
+      } else {
+        stateDict[nodeKey] = ItemToggledState.NotStarted;
+      }
+
+      return 0;
+    },
+    [targetNodesWithConnections]
+  );
+
+  const checkNodeStatuses = useCallback(
+    (schemaNode: any, stateDict: NodeToggledStateDictionary) => {
+      let numChildrenToggled = 0;
+
+      schemaNode.children.forEach((child: any) => {
+        numChildrenToggled += checkNodeStatuses(child, stateDict);
+      });
+
+      // TODO: Sync w/ any type/expected-functionality updates
+      if (schemaNode.schemaNodeDataType === SchemaNodeDataType.None || schemaNode.normalizedDataType === NormalizedDataType.ComplexType) {
+        // Is object parent
+        return handleObjectParentToggledState(stateDict, schemaNode.key, numChildrenToggled, schemaNode.children.length);
+      } else {
+        // Is node that can have value/connection (*could still have children, but its toggled state will be based off itself instead of them)
+        return handleNodeWithValue(stateDict, schemaNode.key);
+      }
+    },
+    [handleObjectParentToggledState, handleNodeWithValue]
+  );
+
+  useEffect(() => {
+    if (!targetSchema || !connectionDictionary) {
+      setToggledStatesDictionary(undefined);
+    } else {
+      const newToggledStatesDictionary: NodeToggledStateDictionary = {};
+      checkNodeStatuses(targetSchema.schemaTreeRoot, newToggledStatesDictionary);
+      setToggledStatesDictionary(newToggledStatesDictionary);
+    }
+  }, [checkNodeStatuses, connectionDictionary, targetSchema, targetNodesWithConnections]);
 
   return (
     <div className={styles.outputPane} style={{ display: 'flex', flexDirection: 'column', flex: '0 1 1px' }}>
@@ -75,7 +155,12 @@ export const TargetSchemaPane = ({ isExpanded, setIsExpanded }: TargetSchemaPane
 
       {isExpanded && targetSchema && (
         <div style={{ margin: 8, marginLeft: 40, width: 290, flex: '1 1 1px', overflowY: 'auto' }}>
-          <TargetSchemaTree schema={targetSchema} currentlySelectedNodes={[]} onNodeClick={handleItemClick} />
+          <SchemaTree
+            schema={targetSchema}
+            toggledStatesDictionary={toggledStatesDictionary}
+            onNodeClick={handleItemClick}
+            isTargetSchema
+          />
         </div>
       )}
     </div>
--- libs/data-mapper/src/lib/components/tree/SchemaTree.stories.tsx
+++ libs/data-mapper/src/lib/components/tree/SchemaTree.stories.tsx
@@ -15,7 +15,6 @@ const mockSchema = convertSchemaToSchemaExtended(JSON.parse(JSON.stringify(simpl
 export const Standard: ComponentStory<typeof SchemaTree> = (args: SchemaTreeProps) => <SchemaTree {...args} />;
 Standard.args = {
   schema: mockSchema,
-  currentlySelectedNodes: [],
-  visibleConnectedNodes: [],
+  toggledNodes: [],
   onNodeClick: (_schemaNode) => console.log('Clicked'),
 };
