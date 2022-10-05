d037f527375f9c1ff53ab28c4936456244d6fba1
Nicolas Layne <nicolaslayne@microsoft.com>
2022-10-05 11:46:39 -0500

    Work out size and shadow style kinks

--- libs/data-mapper/src/lib/components/edge/ConnectionEdge.tsx
+++ libs/data-mapper/src/lib/components/edge/ConnectionEdge.tsx
@@ -5,7 +5,9 @@ import { useIntl } from 'react-intl';
 import { BaseEdge, getSmoothStepPath } from 'reactflow';
 import type { EdgeProps } from 'reactflow';
 
-const addFunctionBtnSize = 32;
+const addFunctionBtnSize = 24;
+const parentPadding = 4;
+const parentTotalSize = addFunctionBtnSize + parentPadding * 2;
 
 const getLineColor = (isSelected: boolean, isHovered: boolean) => {
   if (isSelected) {
@@ -15,16 +17,26 @@ const getLineColor = (isSelected: boolean, isHovered: boolean) => {
   }
 };
 
+const btnStyles = {
+  height: addFunctionBtnSize,
+  minHeight: addFunctionBtnSize,
+  maxHeight: addFunctionBtnSize,
+  width: addFunctionBtnSize,
+  minWidth: addFunctionBtnSize,
+  maxWidth: addFunctionBtnSize,
+  padding: 0,
+};
+
 const useStyles = makeStyles({
   addFnBtn: {
     color: tokens.colorCompoundBrandForeground1,
     backgroundColor: tokens.colorNeutralBackground1,
-    //boxShadow: tokens.shadow4,
+    boxShadow: tokens.shadow2,
 
     ':hover': {
       color: tokens.colorCompoundBrandForeground1Hover,
       backgroundColor: tokens.colorNeutralBackground1Hover,
-      //boxShadow: tokens.shadow8,
+      boxShadow: tokens.shadow4,
     },
   },
   addFnPlaceholder: {
@@ -69,10 +81,14 @@ export const ConnectionEdge = (props: EdgeProps) => {
       />
 
       <foreignObject
-        width={addFunctionBtnSize}
-        height={addFunctionBtnSize}
-        x={labelX - addFunctionBtnSize / 2}
-        y={labelY - addFunctionBtnSize / 2}
+        width={parentTotalSize}
+        height={parentTotalSize}
+        x={labelX - parentTotalSize / 2}
+        y={labelY - parentTotalSize / 2}
+        style={{
+          borderRadius: tokens.borderRadiusCircular,
+          padding: parentPadding,
+        }}
       >
         {data?.isHovered && (
           <Tooltip relationship="label" content={insertFnLoc}>
@@ -81,12 +97,13 @@ export const ConnectionEdge = (props: EdgeProps) => {
               icon={<Add20Filled style={{ height: 16, width: 16 }} />}
               onClick={onAddFunctionClick}
               className={styles.addFnBtn}
+              style={btnStyles}
             />
           </Tooltip>
         )}
 
         {false && ( // TODO: Hook up this condition (actually adding Fn)
-          <Button shape="circular" icon={<Add20Filled />} className={styles.addFnPlaceholder} disabled />
+          <Button shape="circular" icon={<Add20Filled />} className={styles.addFnPlaceholder} style={btnStyles} disabled />
         )}
       </foreignObject>
     </>
