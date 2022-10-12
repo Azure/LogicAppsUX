a1e8b43feb162e47660c2569dcc05b70ed336a07
Nicolas Layne <nicolaslayne@microsoft.com>
2022-10-12 11:09:27 -0500

    More InputDropdown foundational work

--- libs/data-mapper/src/lib/components/inputDropdown/InputDropdown.tsx
+++ libs/data-mapper/src/lib/components/inputDropdown/InputDropdown.tsx
@@ -3,9 +3,15 @@ import { makeConnection } from '../../core/state/DataMapSlice';
 import type { AppDispatch, RootState } from '../../core/state/Store';
 import { NodeType } from '../../models/SelectedNode';
 import type { SelectedNode } from '../../models/SelectedNode';
-import { Dropdown } from '@fluentui/react';
+import { Dropdown, SelectableOptionMenuItemType, TextField } from '@fluentui/react';
 import type { IDropdownOption } from '@fluentui/react';
+import { useDebouncedCallback } from '@react-hookz/web';
+import { useEffect, useMemo, useState } from 'react';
+import { useIntl } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
+
+const customValueOptionKey = 'customValue';
+const customValueDebounceDelay = 300;
 
 export type InputOptions = {
   [key: string]: {
@@ -32,18 +38,50 @@ export interface InputDropdownProps {
 export const InputDropdown = (props: InputDropdownProps) => {
   const { currentNode, typeMatchedOptions, inputValue, inputIndex, dropdownStyle, label, placeholder } = props;
   const dispatch = useDispatch<AppDispatch>();
+  const intl = useIntl();
 
   const sourceSchemaDictionary = useSelector((state: RootState) => state.dataMap.curDataMapOperation.flattenedSourceSchema);
   const functionNodeDictionary = useSelector((state: RootState) => state.dataMap.curDataMapOperation.currentFunctionNodes);
   const targetSchemaDictionary = useSelector((state: RootState) => state.dataMap.curDataMapOperation.flattenedTargetSchema);
 
-  const validateAndCreateConnection = (option?: IDropdownOption<InputOptionData>) => {
-    if (!option?.data) {
+  const [isCustomValue, setIsCustomValue] = useState(false);
+
+  const customValueOptionLoc = intl.formatMessage({
+    defaultMessage: 'Enter custom value',
+    description: 'Label for dropdown option to enter custom value',
+  });
+
+  const onRenderOption = (item?: IDropdownOption) => {
+    switch (item?.key) {
+      case customValueOptionKey:
+        return <span style={{ color: 'rgb(0, 120, 212)' }}>{item?.text}</span>;
+      default:
+        return <span>{item?.text}</span>;
+    }
+  };
+
+  const onSelectOption = (option?: IDropdownOption<InputOptionData>) => {
+    if (!option) {
       return;
     }
 
     // Don't do anything if same value
     if (option.key === inputValue) {
+      return;
+    }
+
+    if (option.key === customValueOptionKey) {
+      // NOTE: useEffect below will handle setting isCustomValue
+      // TODO: update connection input value to be ''
+    } else {
+      // Any other selected option will be a node
+      validateAndCreateConnection(option);
+    }
+  };
+
+  const validateAndCreateConnection = (option: IDropdownOption<InputOptionData>) => {
+    if (!option.data) {
+      console.error('InputDropdown called to create connection with node without necessary data');
       return;
     }
 
@@ -78,14 +116,70 @@ export const InputDropdown = (props: InputDropdownProps) => {
     );
   };
 
+  const onChangeCustomValue = useDebouncedCallback(
+    (newValue?: string) => {
+      if (!newValue) {
+        return;
+      }
+
+      // TODO: update specific connection input's value
+    },
+    [],
+    customValueDebounceDelay
+  );
+
+  // TODO: onClearCustomValue -> set that input to undefined (should have some shared functionality w/ validateAnd...)
+
+  useEffect(() => {
+    // Check if inputValue is defined, and if it's a node reference or a custom value
+    if (inputValue) {
+      const srcSchemaNode = sourceSchemaDictionary[`${sourcePrefix}${inputValue}`];
+      const functionNode = functionNodeDictionary[inputValue];
+
+      setIsCustomValue(!srcSchemaNode && !functionNode);
+    } else {
+      setIsCustomValue(false);
+    }
+  }, [inputValue, sourceSchemaDictionary, functionNodeDictionary]);
+
+  const modifiedOptions = useMemo(() => {
+    if (!typeMatchedOptions) {
+      return [];
+    }
+
+    const newModifiedOptions = [...typeMatchedOptions];
+
+    // Divider
+    newModifiedOptions.push({
+      key: 'divider',
+      text: '',
+      itemType: SelectableOptionMenuItemType.Divider,
+    });
+
+    // Custom value option
+    newModifiedOptions.push({
+      key: customValueOptionKey,
+      text: customValueOptionLoc,
+    });
+
+    return newModifiedOptions;
+  }, [typeMatchedOptions, customValueOptionLoc]);
+
   return (
-    <Dropdown
-      options={typeMatchedOptions ?? []}
-      selectedKey={inputValue}
-      onChange={(_e, option) => validateAndCreateConnection(option)}
-      label={label}
-      placeholder={placeholder}
-      style={dropdownStyle}
-    />
+    <>
+      {!isCustomValue ? (
+        <Dropdown
+          options={modifiedOptions}
+          selectedKey={inputValue}
+          onChange={(_e, option) => onSelectOption(option)}
+          label={label}
+          placeholder={placeholder}
+          style={dropdownStyle}
+          onRenderOption={onRenderOption}
+        />
+      ) : (
+        <TextField value={inputValue} onChange={(_e, newValue) => onChangeCustomValue(newValue)} label={label} placeholder={placeholder} />
+      )}
+    </>
   );
 };
