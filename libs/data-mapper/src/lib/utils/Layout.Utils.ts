d02a2fcc663b65eea0276a1dfadee6f292e7f732
Nicolas Layne <nicolaslayne@microsoft.com>
2022-10-13 16:30:54 -0500

    Update settings identified to get desired layout

--- libs/data-mapper/src/lib/utils/Layout.Utils.ts
+++ libs/data-mapper/src/lib/utils/Layout.Utils.ts
@@ -4,25 +4,21 @@ import type { ElkExtendedEdge, ElkNode } from 'elkjs/lib/elk.bundled';
 const elk = new ELK();
 
 const defaultLayoutOptions: Record<string, string> = {
-    'elk.algorithm': 'org.eclipse.elk.layered',
-    'elk.direction': 'RIGHT',
-    'elk.alignment': 'LEFT',
-    'elk.layered.layering.strategy': 'INTERACTIVE',
-    'elk.edge.type': 'DIRECTED',
-    'elk.layered.unnecessaryBendpoints': 'false',
-    'elk.hierarchyHandling': 'INCLUDE_CHILDREN',
-    'elk.layered.nodePlacement.strategy': 'BRANDES_KOEPF',
-    'elk.layered.nodePlacement.bk.fixedAlignment': 'BALANCED',
-    'elk.layered.spacing.edgeEdgeBetweenLayers': '0',
-    'elk.layered.spacing.edgeNodeBetweenLayers': '64',
-    'elk.layered.spacing.nodeNodeBetweenLayers': '64',
-    'elk.padding': '[top=16,left=16,bottom=16,right=16]',
-    // 'elk.layered.crossingMinimization.semiInteractive': 'true',
-    'elk.layered.crossingMinimization.forceNodeModelOrder': 'false',
-    'elk.layered.considerModelOrder.strategy': 'NODES_AND_EDGES',
+    'nodePlacement.strategy': 'BRANDES_KOEPF',
+    'algorithm': 'layered',
+    'crossingMinimization.semiInteractive': 'true',
+    'spacing.edgeNodeBetweenLayers': '32.0',
+    'partitioning.activate': 'true',
+    'hierarchyHandling': 'INCLUDE_CHILDREN',
+    'layering.strategy': 'INTERACTIVE',
+    'spacing.nodeNodeBetweenLayers': '32.0',
+    'crossingMinimization.forceNodeModelOrder': 'true',
+    'considerModelOrder.strategy': 'NODES_AND_EDGES',
+    'alignment': 'LEFT',
+    'elk.direction': 'RIGHT'
 };
 
-const convertDataMapGraphToElkGraph = () => {};
+const convertDataMapNodesToElkGraph = () => {};
 
 const applyElkLayout = async (graph: ElkNode) => {
     return elk.layout(graph, {
