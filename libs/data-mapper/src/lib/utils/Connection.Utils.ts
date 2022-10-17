56a99da39549c3703d124a12f72dd2f24aa91b1a
Nicolas Layne <49288482+NicL9923@users.noreply.github.com>
2022-10-17 15:43:50 -0500

    [DM | PropPane] Finalize target schema node inputs & generate map definitions for custom values (#1075)
    * Flesh out target schema node inputs (prop pane)
    
    * Remove console log
    
    * Still show custom value dropdown option if no other possible options
    
    * Add validity check for custom values to generate map definition
    
    * Remove console log
    
    * Re-add hyphen
    
    Co-authored-by: Nicolas Layne <nicolaslayne@microsoft.com>

--- libs/data-mapper/src/lib/utils/Connection.Utils.ts
+++ libs/data-mapper/src/lib/utils/Connection.Utils.ts
@@ -196,12 +196,16 @@ export const nodeHasSourceNodeEventually = (currentConnection: Connection, conne
 
   // Put 0 input, content enricher functions in the node bucket
   const flattenedInputs = flattenInputs(currentConnection.inputs);
+  const customValueInputs = flattenedInputs.filter(isCustomValue);
   const definedNonCustomValueInputs: ConnectionUnit[] = flattenedInputs.filter(isConnectionUnit);
   const functionInputs = definedNonCustomValueInputs.filter((input) => isFunctionData(input.node) && input.node.maxNumberOfInputs !== 0);
   const nodeInputs = definedNonCustomValueInputs.filter((input) => isSchemaNodeExtended(input.node) || input.node.maxNumberOfInputs === 0);
 
   // All the sources are input nodes
   if (nodeInputs.length === flattenedInputs.length) {
+    return true;
+  } else if (customValueInputs.length === flattenedInputs.length) {
+    // All inputs are custom values
     return true;
   } else {
     // Still have traversing to do
