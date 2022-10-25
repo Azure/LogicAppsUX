f96d02ec8e4284a44b7b6e40b284b9d3cba730a6
Nicolas Layne <nicolaslayne@microsoft.com>
2022-10-25 10:02:58 -0500

    Prevent infinite re-rendering on MapOverview useLayout hook

--- libs/data-mapper/src/lib/components/mapOverview/ReactFlowSchemaOverview.tsx
+++ libs/data-mapper/src/lib/components/mapOverview/ReactFlowSchemaOverview.tsx
@@ -9,12 +9,33 @@ const reactFlowStyle = {
   height: '100%',
 };
 
+// Using this to prevent infinite re-rendering
+const placeholderData = {
+  currentlySelectedSourceNodes: [],
+  connectedSourceNodes: [],
+  allSourceNodes: {},
+  addedFunctionNodes: {},
+  functionConnectionUnits: [],
+  //currentTargetNode, - This is the one thing that has an actual value (schema.schemaTreeRoot)
+  connections: {},
+  selectedItemKey: undefined,
+};
+
 interface ReactFlowSchemaOverviewProps {
   schema: SchemaExtended;
 }
 
 export const ReactFlowSchemaOverview = ({ schema }: ReactFlowSchemaOverviewProps) => {
-  const [reactFlowNodes, _reactFlowEdges] = useLayout([], [], {}, {}, [], schema.schemaTreeRoot, {}, undefined);
+  const [reactFlowNodes, _reactFlowEdges] = useLayout(
+    placeholderData.currentlySelectedSourceNodes,
+    placeholderData.connectedSourceNodes,
+    placeholderData.allSourceNodes,
+    placeholderData.addedFunctionNodes,
+    placeholderData.functionConnectionUnits,
+    schema.schemaTreeRoot,
+    placeholderData.connections,
+    placeholderData.selectedItemKey
+  );
 
   return (
     <ReactFlow
