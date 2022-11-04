0516b6f95a113e3822fe9488f8af5f84c570e0ee
preetriti1 <48265693+preetriti1@users.noreply.github.com>
2022-11-04 15:11:14 -0700

    Fixing the MSI auth in authentication editor (#1262)
    Co-authored-by: Priti Sambandam <psamband@microsoft.com>

--- libs/designer-ui/src/lib/authentication/MSIAuth/MSIAuth.tsx
+++ libs/designer-ui/src/lib/authentication/MSIAuth/MSIAuth.tsx
@@ -9,6 +9,7 @@ import type { ManagedIdentity } from '@microsoft-logic-apps/utils';
 import { ResourceIdentityType, equals } from '@microsoft-logic-apps/utils';
 import type { Dispatch, SetStateAction } from 'react';
 import { useState } from 'react';
+import type { IntlShape } from 'react-intl';
 import { useIntl } from 'react-intl';
 
 interface MSIAuthenticationProps {
@@ -27,8 +28,9 @@ export const MSIAuthentication = ({
   setCurrentProps,
 }: MSIAuthenticationProps): JSX.Element => {
   const intl = useIntl();
-  const [errorMessage, setErrorMessage] = useState('');
-  const [managedIdentityDropdownOptions, setManagedIdentityDropdownOptions] = useState<IDropdownOption[]>([]);
+  const { options, errorMessage: error } = getManagedIdentityData(identity, msiProps.msiIdentity, intl);
+  const [errorMessage] = useState(error);
+  const [managedIdentityDropdownOptions] = useState<IDropdownOption[]>(options);
 
   const updateMsiAudience = (newState: ChangeState) => {
     setCurrentProps((prevState: AuthProps) => ({
@@ -36,19 +38,6 @@ export const MSIAuthentication = ({
       msi: { ...prevState.msi, msiAudience: newState.value },
     }));
   };
-
-  const invalidUserAssignedManagedIdentity = intl.formatMessage({
-    defaultMessage: 'The entered identity is not associated with this Logic App.',
-    description: 'error message for invalid user',
-  });
-  const systemAssignedManagedIdentity = intl.formatMessage({
-    defaultMessage: 'System-assigned managed identity',
-    description: 'Text for dropdown of system-assigned managed identity',
-  });
-  const userIdentityNotSupported = intl.formatMessage({
-    defaultMessage: 'User identity is not supported when Logic App has system assigned managed identity enabled.',
-    description: 'error message for unspported identity',
-  });
 
   const MSIAuthLabel = intl.formatMessage({
     defaultMessage: 'Managed identity',
@@ -59,49 +48,7 @@ export const MSIAuthentication = ({
     description: 'Placehodler text for dropdown',
   });
 
-  const selectedManagedIdentityKey = msiProps.msiIdentity;
-  const { msiAudience } = msiProps;
-
-  if (identity?.type) {
-    const userAssignedIdentities = containsUserAssignedIdentities(identity) ? getUserAssignedIdentities(identity) : undefined;
-    if (
-      equals(identity.type, ResourceIdentityType.USER_ASSIGNED) ||
-      equals(identity.type, ResourceIdentityType.SYSTEM_ASSIGNED_USER_ASSIGNED)
-    ) {
-      if (userAssignedIdentities) {
-        setManagedIdentityDropdownOptions(userAssignedIdentities);
-      }
-      if (selectedManagedIdentityKey && userAssignedIdentities?.find((userIdentity) => userIdentity.key === selectedManagedIdentityKey)) {
-        setManagedIdentityDropdownOptions([
-          ...managedIdentityDropdownOptions,
-          { key: selectedManagedIdentityKey, text: getIdentityDisplayName(selectedManagedIdentityKey) },
-        ]);
-        setErrorMessage(invalidUserAssignedManagedIdentity);
-      }
-    }
-    if (
-      equals(identity.type, ResourceIdentityType.SYSTEM_ASSIGNED) ||
-      equals(identity.type, ResourceIdentityType.SYSTEM_ASSIGNED_USER_ASSIGNED)
-    ) {
-      setManagedIdentityDropdownOptions([
-        ...managedIdentityDropdownOptions,
-        {
-          key: ResourceIdentityType.SYSTEM_ASSIGNED,
-          text: systemAssignedManagedIdentity,
-        },
-      ]);
-      if (selectedManagedIdentityKey && equals(identity.type, ResourceIdentityType.SYSTEM_ASSIGNED)) {
-        setManagedIdentityDropdownOptions([
-          ...managedIdentityDropdownOptions,
-          {
-            key: selectedManagedIdentityKey,
-            text: getIdentityDisplayName(selectedManagedIdentityKey),
-          },
-        ]);
-        setErrorMessage(userIdentityNotSupported);
-      }
-    }
-  }
+  const { msiAudience, msiIdentity } = msiProps;
 
   return (
     <div className="msla-authentication-editor-MSI-container">
@@ -111,7 +58,7 @@ export const MSIAuthentication = ({
             dropdownLabel={MSIAuthLabel}
             dropdownPlaceholder={MSIAuthPlaceholder}
             errorMessage={errorMessage}
-            selectedKey={selectedManagedIdentityKey ?? ResourceIdentityType.SYSTEM_ASSIGNED}
+            selectedKey={msiIdentity ?? ResourceIdentityType.SYSTEM_ASSIGNED}
             options={managedIdentityDropdownOptions}
             onChange={onManagedIdentityChange}
           />
@@ -134,9 +81,60 @@ export const MSIAuthentication = ({
   );
 };
 
-function getUserAssignedIdentities(identity: ManagedIdentity): IDropdownOption[] {
+const getManagedIdentityData = (
+  identity: ManagedIdentity | undefined,
+  selectedManagedIdentityKey: string | undefined,
+  intl: IntlShape
+): { options: IDropdownOption<any>[]; errorMessage?: string } => {
+  const identityOptions: IDropdownOption<any>[] = [];
+  let errorMessage: string | undefined;
+
+  const invalidUserAssignedManagedIdentity = intl.formatMessage({
+    defaultMessage: 'The entered identity is not associated with this Logic App.',
+    description: 'error message for invalid user',
+  });
+  const systemAssignedManagedIdentity = intl.formatMessage({
+    defaultMessage: 'System-assigned managed identity',
+    description: 'Text for dropdown of system-assigned managed identity',
+  });
+  const userIdentityNotSupported = intl.formatMessage({
+    defaultMessage: 'User identity is not supported when Logic App has system assigned managed identity enabled.',
+    description: 'error message for unspported identity',
+  });
+  const userAssignedIdentities = containsUserAssignedIdentities(identity) ? getUserAssignedIdentities(identity) : undefined;
+  if (identity?.type) {
+    if (
+      equals(identity.type, ResourceIdentityType.USER_ASSIGNED) ||
+      equals(identity.type, ResourceIdentityType.SYSTEM_ASSIGNED_USER_ASSIGNED)
+    ) {
+      if (userAssignedIdentities) {
+        identityOptions.push(...userAssignedIdentities);
+      }
+
+      if (selectedManagedIdentityKey && userAssignedIdentities?.find((userIdentity) => userIdentity.key === selectedManagedIdentityKey)) {
+        identityOptions.push({ key: selectedManagedIdentityKey, text: getIdentityDisplayName(selectedManagedIdentityKey) });
+        errorMessage = invalidUserAssignedManagedIdentity;
+      }
+    }
+    if (
+      equals(identity.type, ResourceIdentityType.SYSTEM_ASSIGNED) ||
+      equals(identity.type, ResourceIdentityType.SYSTEM_ASSIGNED_USER_ASSIGNED)
+    ) {
+      identityOptions.push({ key: ResourceIdentityType.SYSTEM_ASSIGNED, text: systemAssignedManagedIdentity });
+
+      if (selectedManagedIdentityKey && equals(identity.type, ResourceIdentityType.SYSTEM_ASSIGNED)) {
+        identityOptions.push({ key: selectedManagedIdentityKey, text: getIdentityDisplayName(selectedManagedIdentityKey) });
+        errorMessage = userIdentityNotSupported;
+      }
+    }
+  }
+
+  return { options: identityOptions, errorMessage };
+};
+
+const getUserAssignedIdentities = (identity: ManagedIdentity | undefined): IDropdownOption[] => {
   const options: IDropdownOption[] = [];
-  if (identity.userAssignedIdentities) {
+  if (identity?.userAssignedIdentities) {
     for (const userAssignedIdentity of Object.keys(identity.userAssignedIdentities)) {
       options.push({
         key: userAssignedIdentity,
@@ -145,8 +143,8 @@ function getUserAssignedIdentities(identity: ManagedIdentity): IDropdownOption[]
     }
   }
   return options;
-}
+};
 
-function getIdentityDisplayName(msiIdentity: string): string {
+const getIdentityDisplayName = (msiIdentity: string): string => {
   return msiIdentity.split('/').slice(-1)[0];
-}
+};
