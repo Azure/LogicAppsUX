86208f7f18369a56448de759b3a485503075507c
Nicolas Layne <nicolaslayne@microsoft.com>
2022-11-04 11:47:36 -0500

    Test for functionInputHasInputs

--- libs/data-mapper/src/lib/utils/__test__/FunctionUtils.spec.ts
+++ libs/data-mapper/src/lib/utils/__test__/FunctionUtils.spec.ts
@@ -1,4 +1,6 @@
-import { getFunctionOutputValue } from '../Function.Utils';
+import type { ConnectionDictionary } from '../../models/Connection';
+import type { FunctionData } from '../../models/Function';
+import { functionInputHasInputs, getFunctionOutputValue } from '../Function.Utils';
 
 describe('utils/Functions', () => {
   describe('getFunctionOutputValue', () => {
@@ -11,6 +13,31 @@ describe('utils/Functions', () => {
 
     it('Test correct output value with arguments', () => {
       expect(getFunctionOutputValue(inputArgs, functionName)).toEqual(`${functionName}(${inputArgs[0]}, ${inputArgs[1]}, ${inputArgs[2]})`);
+    });
+  });
+
+  describe('functionInputHasInputs', () => {
+    const mockReactFlowKey = 'testFunctionInputKey';
+    const mockConnections: ConnectionDictionary = {
+      [mockReactFlowKey]: {
+        self: {
+          reactFlowKey: mockReactFlowKey,
+          node: {} as FunctionData,
+        },
+        inputs: {
+          0: [],
+        },
+        outputs: [],
+      },
+    };
+
+    it('Test function input that doesnt have inputs', () => {
+      expect(functionInputHasInputs(mockReactFlowKey, mockConnections)).toEqual(false);
+    });
+
+    it('Test function input that has inputs', () => {
+      mockConnections[mockReactFlowKey].inputs[0] = ['testInput'];
+      expect(functionInputHasInputs(mockReactFlowKey, mockConnections)).toEqual(true);
     });
   });
 });
