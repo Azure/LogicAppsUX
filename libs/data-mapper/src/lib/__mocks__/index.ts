03ddd70427d5604ebca4c4b81bf069283c9d3aaa
refortie <37600290+refortie@users.noreply.github.com>
2022-11-08 10:03:09 -0800

    [Data Mapper] Loop schemas (#1274)
    Loop schemas

--- libs/data-mapper/src/lib/__mocks__/index.ts
+++ libs/data-mapper/src/lib/__mocks__/index.ts
@@ -1,2 +1,4 @@
 export { default as sourceMockSchema } from './SourceMockSchema.json';
 export { default as targetMockSchema } from './TargetMockSchema.json';
+export { default as layeredLoopSourceMockSchema } from './LayeredLoopSourceMockSchema.json';
+export { default as layeredLoopTargetMockSchema } from './LayeredLoopTargetMockSchema.json';
