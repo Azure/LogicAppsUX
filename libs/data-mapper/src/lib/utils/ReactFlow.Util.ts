20e0839f11da95418458328ee0a7e01cbeeb099f
Nicolas Layne <nicolaslayne@microsoft.com>
2022-11-09 05:47:18 -0600

    Alter overview to work based off of the schema root as opposed to the schemaTreeRoot node to be a little more sensible

--- libs/data-mapper/src/lib/utils/ReactFlow.Util.ts
+++ libs/data-mapper/src/lib/utils/ReactFlow.Util.ts
@@ -299,7 +299,7 @@ export const useOverviewLayout = (
         schemaNode: parentSchemaNode,
         schemaType,
         displayHandle: false,
-        displayChevron: false,
+        displayChevron: schemaType === SchemaType.Target && !!shouldTargetSchemaDisplayChevrons,
         isLeaf: false,
         isChild: false,
         disabled: false,
