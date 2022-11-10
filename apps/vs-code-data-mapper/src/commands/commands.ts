898096c7409942bd406c500318629939abbce186
Nicolas Layne <49288482+NicL9923@users.noreply.github.com>
2022-11-10 15:29:42 -0600

    [Data Mapper] (VSIX) Telemetry (#1294)
    * Install Azure/AppInsights telemetry packages for VSIX/Data Mapper React app, and add aiKey to VSIX package.json
    
    * Manage extension context through DataMapperExt static class, and implement azext telemetry wrappers
    
    * Add telemetry/error handling wrappers around key DM VSIX events
    
    Co-authored-by: Nicolas Layne <nicolaslayne@microsoft.com>

--- apps/vs-code-data-mapper/src/commands/commands.ts
+++ apps/vs-code-data-mapper/src/commands/commands.ts
@@ -1,25 +1,27 @@
 import DataMapperExt from '../DataMapperExt';
 import { startBackendRuntime } from '../FxWorkflowRuntime';
 import { schemasPath } from '../extensionConfig';
+import { callWithTelemetryAndErrorHandlingSync, registerCommand } from '@microsoft/vscode-azext-utils';
+import type { IActionContext } from '@microsoft/vscode-azext-utils';
 import { promises as fs, existsSync as fileExists } from 'fs';
 import * as yaml from 'js-yaml';
 import * as path from 'path';
-import { commands, window, workspace } from 'vscode';
-import type { ExtensionContext, Uri } from 'vscode';
+import { window, workspace } from 'vscode';
+import type { Uri } from 'vscode';
 
-export const registerCommands = (context: ExtensionContext) => {
-  context.subscriptions.push(commands.registerCommand('azureDataMapper.openDataMapper', () => openDataMapperCmd(context)));
-  context.subscriptions.push(commands.registerCommand('azureDataMapper.createNewDataMap', () => createNewDataMapCmd(context)));
-  context.subscriptions.push(commands.registerCommand('azureDataMapper.loadDataMapFile', (uri: Uri) => loadDataMapFileCmd(uri, context)));
+export const registerCommands = () => {
+  registerCommand('azureDataMapper.openDataMapper', () => openDataMapperCmd());
+  registerCommand('azureDataMapper.createNewDataMap', () => createNewDataMapCmd());
+  registerCommand('azureDataMapper.loadDataMapFile', (context: IActionContext, uri: Uri) => loadDataMapFileCmd(uri));
 };
 
-const openDataMapperCmd = async (context: ExtensionContext) => {
+const openDataMapperCmd = async () => {
   await startBackendRuntime(DataMapperExt.getWorkspaceFolderFsPath());
 
-  DataMapperExt.createOrShow(context);
+  DataMapperExt.createOrShow();
 };
 
-const createNewDataMapCmd = (context: ExtensionContext) => {
+const createNewDataMapCmd = () => {
   // TODO: Data map name validation
   window.showInputBox({ prompt: 'Data Map name: ' }).then(async (newDatamapName) => {
     if (!newDatamapName) {
@@ -28,13 +30,13 @@ const createNewDataMapCmd = (context: ExtensionContext) => {
 
     DataMapperExt.currentDataMapName = newDatamapName;
 
-    await openDataMapperCmd(context);
+    await openDataMapperCmd();
 
     DataMapperExt.currentPanel.sendMsgToWebview({ command: 'loadNewDataMap', data: {} });
   });
 };
 
-const loadDataMapFileCmd = async (uri: Uri, context: ExtensionContext) => {
+const loadDataMapFileCmd = async (uri: Uri) => {
   const mapDefinition = yaml.load(await fs.readFile(uri.fsPath, 'utf-8')) as {
     $sourceSchema: string;
     $targetSchema: string;
@@ -47,29 +49,31 @@ const loadDataMapFileCmd = async (uri: Uri, context: ExtensionContext) => {
   const tgtSchemaPath = path.join(schemasFolder, mapDefinition.$targetSchema);
 
   const attemptToResolveMissingSchemaFile = async (schemaName: string, schemaPath: string): Promise<boolean> => {
-    const findSchemaFileButton = 'Find schema file';
-    const clickedButton = await window.showErrorMessage(
-      `Error loading map definition: ${schemaName} was not found in the Schemas folder!`,
-      findSchemaFileButton
-    );
+    return callWithTelemetryAndErrorHandlingSync('azureDataMapper.attemptToResolveMissingSchemaFile', async (_context: IActionContext) => {
+      const findSchemaFileButton = 'Find schema file';
+      const clickedButton = await window.showErrorMessage(
+        `Error loading map definition: ${schemaName} was not found in the Schemas folder!`,
+        findSchemaFileButton
+      );
 
-    if (clickedButton && clickedButton === findSchemaFileButton) {
-      const fileUris = await window.showOpenDialog({
-        canSelectMany: false,
-        canSelectFiles: true,
-        canSelectFolders: false,
-        filters: { 'XML Schema Definition': ['xsd'] },
-      });
+      if (clickedButton && clickedButton === findSchemaFileButton) {
+        const fileUris = await window.showOpenDialog({
+          canSelectMany: false,
+          canSelectFiles: true,
+          canSelectFolders: false,
+          filters: { 'XML Schema Definition': ['xsd'] },
+        });
 
-      if (fileUris && fileUris.length > 0) {
-        // Copy the schema file they selected to the Schemas folder (can safely continue map definition loading)
-        await fs.copyFile(fileUris[0].fsPath, schemaPath);
-        return true;
+        if (fileUris && fileUris.length > 0) {
+          // Copy the schema file they selected to the Schemas folder (can safely continue map definition loading)
+          await fs.copyFile(fileUris[0].fsPath, schemaPath);
+          return true;
+        }
       }
-    }
 
-    // If user doesn't select a file, or doesn't click the above action, just return (cancel loading the MapDef)
-    return false;
+      // If user doesn't select a file, or doesn't click the above action, just return (cancel loading the MapDef)
+      return false;
+    });
   };
 
   // If schema file doesn't exist, prompt to find/select it
@@ -93,7 +97,7 @@ const loadDataMapFileCmd = async (uri: Uri, context: ExtensionContext) => {
 
   DataMapperExt.currentDataMapName = path.basename(uri.fsPath, path.extname(uri.fsPath)); // Gets filename w/o ext
 
-  await openDataMapperCmd(context);
+  await openDataMapperCmd();
 
   DataMapperExt.currentPanel.sendMsgToWebview({
     command: 'loadDataMap',
