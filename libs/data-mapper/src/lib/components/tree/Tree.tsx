475495e6dfd20faa3f94cdba95611bd279233993
Nicolas Layne <49288482+NicL9923@users.noreply.github.com>
2022-11-15 14:16:12 -0600

    [DM | Tree] Fix chevron color/filled styles + move indicator management to Tree (#1308)
    Fix chevron color/filled styles + move indicator management to Tree
    
    Co-authored-by: Nicolas Layne <nicolaslayne@microsoft.com>

--- libs/data-mapper/src/lib/components/tree/Tree.tsx
+++ libs/data-mapper/src/lib/components/tree/Tree.tsx
@@ -1,5 +1,5 @@
 import TreeBranch from './TreeBranch';
-import { makeStyles, mergeClasses, shorthands } from '@fluentui/react-components';
+import { makeStyles, mergeClasses, shorthands, tokens } from '@fluentui/react-components';
 import type { ReactNode } from 'react';
 
 export interface ITreeNode<T> {
@@ -16,6 +16,18 @@ export const useTreeStyles = makeStyles({
   nodeContainer: {
     width: '100%',
   },
+  indicator: {
+    height: '16px',
+    width: '2px',
+    ...shorthands.borderRadius(tokens.borderRadiusSmall),
+    backgroundColor: tokens.colorBrandForeground1,
+  },
+  chevron: {
+    color: `${tokens.colorNeutralForeground3} !important`,
+    ':hover :focus :active': {
+      color: tokens.colorNeutralForeground3,
+    },
+  },
 });
 
 export interface CoreTreeProps<T> {
@@ -24,6 +36,7 @@ export interface CoreTreeProps<T> {
   nodeContainerStyle?: (node: ITreeNode<T>) => React.CSSProperties;
   childPadding?: number; // 0 will also not render hidden chevrons (meaning the space is recouped) - used in FxList
   onClickItem?: (node: ITreeNode<T>) => void;
+  shouldShowIndicator?: (node: ITreeNode<T>) => boolean;
   parentItemClickShouldExpand?: boolean;
 }
 
