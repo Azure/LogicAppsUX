25b6941aa0859690d81f167cb480e70d205e8ea9
ccastrotrejo <ccastrotrejo@microsoft.com>
2022-11-14 16:26:18 -0800

    Initialize icons

--- apps/vs-code-designer-react/src/main.tsx
+++ apps/vs-code-designer-react/src/main.tsx
@@ -1,9 +1,12 @@
 import { App } from './app/app';
 import { store } from './state/Store';
 import { WebViewCommunication } from './webviewCommunication';
+import { initializeIcons } from '@fluentui/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+
+initializeIcons();
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!;
