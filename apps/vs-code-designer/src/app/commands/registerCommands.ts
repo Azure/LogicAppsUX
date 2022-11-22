cee4ae5c8976daaa9b2fc491124b96a727f61e55
ccastrotrejo <ccastrotrejo@microsoft.com>
2022-11-21 16:15:27 -0800

    Implement LoadMore command

--- apps/vs-code-designer/src/app/commands/registerCommands.ts
+++ apps/vs-code-designer/src/app/commands/registerCommands.ts
@@ -3,9 +3,15 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import { extensionCommand } from '../../constants';
+import { ext } from '../../extensionVariables';
 import { openDesigner } from './workflows/openDesigner/openDesigner';
 import { registerCommand } from '@microsoft/vscode-azext-utils';
+import type { AzExtTreeItem, IActionContext } from '@microsoft/vscode-azext-utils';
 
 export function registerCommands(): void {
   registerCommand(extensionCommand.openDesigner, openDesigner);
+  registerCommand(
+    extensionCommand.loadMore,
+    async (context: IActionContext, node: AzExtTreeItem) => await ext.tree.loadMore(node, context)
+  );
 }
