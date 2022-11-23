e1ea08681929cde77ab38acb45948b31b27c418e
ccastrotrejo <ccastrotrejo@microsoft.com>
2022-11-23 14:45:33 -0800

    Update theme to enum in libs

--- apps/vs-code-designer/src/app/utils/tree/assets.ts
+++ apps/vs-code-designer/src/app/utils/tree/assets.ts
@@ -1,4 +1,5 @@
 import { ext } from '../../../extensionVariables';
+import { Theme } from '@microsoft-logic-apps/utils';
 import type { TreeItemIconPath } from '@microsoft/vscode-azext-utils';
 import * as path from 'path';
 
@@ -8,8 +9,8 @@ export function getIconPath(iconName: string): TreeItemIconPath {
 
 export function getThemedIconPath(iconName: string): TreeItemIconPath {
   return {
-    light: path.join(getResourcesPath(), 'light', `${iconName}.svg`),
-    dark: path.join(getResourcesPath(), 'dark', `${iconName}.svg`),
+    light: path.join(getResourcesPath(), Theme.Light, `${iconName}.svg`),
+    dark: path.join(getResourcesPath(), Theme.Dark, `${iconName}.svg`),
   };
 }
 
