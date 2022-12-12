7bf10ddaa1bb2f216e499dd6b5512544bbd9c077
Nicolas Layne <49288482+NicL9923@users.noreply.github.com>
2022-12-12 15:49:32 -0600

    [DM | Utils] Combine connection input setter methods + other minor refactors (#1417)
    * Combine connection input setter methods
    
    * Update DataMapSlice and MapDefinition tests (NOT expected results - just methods used)
    
    * Add Doc comments to primary connection utils
    
    * Add back in commented code w/ TODO to un-deprecate
    
    * Finish un-delete
    
    * Update setConnectionInputValue Util test suites to prefix its scenarios with the method name
    
    * Wrap common test suites
    
    * Explicitly skip broken test
    
    * Rename flag to findInputSlot
    
    Co-authored-by: Nicolas Layne <nicolaslayne@microsoft.com>

--- libs/data-mapper/src/lib/utils/Connection.Utils.ts
+++ libs/data-mapper/src/lib/utils/Connection.Utils.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { sourcePrefix, targetPrefix } from '../constants/ReactFlowConstants';
-import type { DataMapOperationState, UpdateConnectionInputAction } from '../core/state/DataMapSlice';
+import type { DataMapOperationState, SetConnectionInputAction } from '../core/state/DataMapSlice';
 import type { SchemaNodeExtended } from '../models';
 import { NormalizedDataType, SchemaNodeDataType, SchemaNodeProperty, SchemaType } from '../models';
 import type { Connection, ConnectionDictionary, ConnectionUnit, InputConnection, InputConnectionDictionary } from '../models/Connection';
@@ -10,8 +10,15 @@ import { addReactFlowPrefix, addTargetReactFlowPrefix } from './ReactFlow.Util';
 import { isSchemaNodeExtended } from './Schema.Utils';
 import type { WritableDraft } from 'immer/dist/internal';
 
-// NOTE: This method should be the gateway for anything getting into dataMapConnections
-// - meaning all default inputs/etc can safely be managed in this singular spot
+/**
+ * Creates a connection entry in the connections dictionary if it doesn't already exist.
+ * This is the gateway for any entries made into dataMapConnections - meaning all default
+ * inputs/etc can safely be managed in this singular spot.
+ *
+ * @param connections The connections dictionary to add the entry to
+ * @param node The node to create the connection entry for
+ * @param reactFlowKey The reactFlowKey of the node
+ */
 export const createConnectionEntryIfNeeded = (
   connections: ConnectionDictionary,
   node: SchemaNodeExtended | FunctionData,
@@ -40,152 +47,137 @@ export const createConnectionEntryIfNeeded = (
   }
 };
 
-// Primarily used for drawn connections
-export const addNodeToConnections = (
+/**
+ * Creates or updates a connection entry's input value.
+ * This is the gateway for *setting ANY input values* in dataMapConnections - including
+ * both those that are deserialized, handle-drawn, or InputDropdown-set.
+ *
+ * @param connections The connections dictionary to add the entry to
+ * @param targetNode The node to create the connection entry for
+ * @param targetNodeReactFlowKey The reactFlowKey of `targetNode`
+ * @param inputIndex The index of the input to set the value for (only needed if NOT `findInputSlot`)
+ * @param value The value to set the input to (InputConnection, or `null` to delete an unbounded input)
+ * @param findInputSlot Flag to find an available input spot if `true`, or use inputIndex if `false`/`undefined` (used for drawn and deserialized connections)
+ */
+export const setConnectionInputValue = (
   connections: ConnectionDictionary,
-  sourceNode: SchemaNodeExtended | FunctionData,
-  sourceReactFlowKey: string,
-  self: SchemaNodeExtended | FunctionData,
-  selfReactFlowKey: string
+  { targetNode, targetNodeReactFlowKey, inputIndex, value, findInputSlot }: SetConnectionInputAction
 ) => {
-  if (sourceNode && self) {
-    createConnectionEntryIfNeeded(connections, self, selfReactFlowKey);
-    const currentConnectionInputs = connections[selfReactFlowKey].inputs;
-    const newInputValue = { node: sourceNode, reactFlowKey: sourceReactFlowKey };
-
-    // Schema nodes can only ever have 1 input as long as it is not repeating
-    if (isSchemaNodeExtended(self)) {
-      if (self.nodeProperties.includes(SchemaNodeProperty.Repeating)) {
-        currentConnectionInputs[0].push(newInputValue);
-      } else {
-        currentConnectionInputs[0] = [newInputValue];
-      }
-    } else {
-      // If the destination has unlimited inputs, all should go on the first input
-      if (self.maxNumberOfInputs === -1) {
-        // Check if an undefined input field exists first (created through PropPane)
-        const indexOfFirstOpenInput = currentConnectionInputs[0].findIndex((inputCon) => !inputCon);
-
-        if (indexOfFirstOpenInput >= 0) {
-          currentConnectionInputs[0][indexOfFirstOpenInput] = newInputValue;
-        } else {
-          // Otherwise we can safely just append its value to the end
-          currentConnectionInputs[0].push(newInputValue);
-        }
-      } else {
-        // Add input to first available and type-matched place (handle & PropPane validation should guarantee there's at least one)
-        let added = false;
-        Object.entries(currentConnectionInputs).forEach(([key, value], idx) => {
-          if (
-            !added &&
-            value.length < 1 &&
-            self.inputs[idx].allowedTypes.some(
-              (type) =>
-                type === NormalizedDataType.Any ||
-                (isSchemaNodeExtended(sourceNode) ? type === sourceNode.normalizedDataType : type === sourceNode.outputValueType)
-            )
-          ) {
-            currentConnectionInputs[key].push(newInputValue);
-            added = true;
-          }
-        });
-      }
-    }
-
-    createConnectionEntryIfNeeded(connections, sourceNode, sourceReactFlowKey);
-    connections[sourceReactFlowKey].outputs.push({ node: self, reactFlowKey: selfReactFlowKey });
-    connections[sourceReactFlowKey].outputs = connections[sourceReactFlowKey].outputs.filter(onlyUniqueConnections);
+  if (!findInputSlot && inputIndex === undefined) {
+    console.error('Invalid Connection Input Op: inputIndex was not provided for a non-handle-drawn/deserialized connection');
+    return;
   }
-};
 
-export const addCustomValueToConnections = (
-  connections: ConnectionDictionary,
-  customValue: string,
-  destination: SchemaNodeExtended | FunctionData,
-  destinationReactFlowKey: string
-) => {
-  createConnectionEntryIfNeeded(connections, destination, destinationReactFlowKey);
-  const currentConnectionInputs = connections[destinationReactFlowKey].inputs;
-
-  // Schema nodes can only ever have 1 input as long as it is not repeating
-  if (isSchemaNodeExtended(destination)) {
-    if (destination.nodeProperties.includes(SchemaNodeProperty.Repeating)) {
-      currentConnectionInputs[0].push(customValue);
-    } else {
-      currentConnectionInputs[0] = [customValue];
-    }
-  } else {
-    // If the destination has unlimited inputs, all should go on the first input
-    if (destination.maxNumberOfInputs === -1) {
-      // Check if an undefined input field exists first (created through PropPane)
-      const indexOfFirstOpenInput = currentConnectionInputs[0].findIndex((inputCon) => !inputCon);
-
-      if (indexOfFirstOpenInput >= 0) {
-        currentConnectionInputs[0][indexOfFirstOpenInput] = customValue;
-      } else {
-        // Otherwise we can safely just append its value to the end
-        currentConnectionInputs[0].push(customValue);
-      }
-    } else {
-      // Add input to first available that has a custom value allowed
-      let added = false;
-      Object.entries(currentConnectionInputs).forEach(([key, value], idx) => {
-        if (!added && value.length < 1 && destination.inputs[idx].allowCustomInput) {
-          currentConnectionInputs[key].push(customValue);
-          added = true;
-        }
-      });
-    }
+  if (findInputSlot && (value === undefined || value === null)) {
+    console.error('Invalid Connection Input Op: value is undefined or null for a handle-drawn/deserialized connection');
+    return;
   }
-};
 
-// Primarily used by PropPane InputDropdowns
-export const updateConnectionInputValue = (
-  connections: ConnectionDictionary,
-  { targetNode, targetNodeReactFlowKey, inputIndex, value, isUnboundedInput }: UpdateConnectionInputAction
-) => {
-  // Verify if old value was a ConnectionUnit, and if so, remove it from source's outputs[]
+  if (findInputSlot && inputIndex !== undefined) {
+    console.warn('Invalid Connection Input Op: inputIndex was provided for a handle-drawn/deserialized connection');
+  }
+
   let connection = connections[targetNodeReactFlowKey];
-  if (connection?.inputs) {
-    let inputConnection: InputConnection = undefined;
 
-    if (isUnboundedInput) {
-      inputConnection = connection.inputs[0][inputIndex];
-    } else if (connection.inputs[inputIndex].length > 0) {
-      inputConnection = connection.inputs[inputIndex][0];
-    }
+  let isFunctionUnboundedInputOrRepeatingSchemaNode = false;
 
-    if (inputConnection && isConnectionUnit(inputConnection)) {
-      connections[inputConnection.reactFlowKey].outputs = connections[inputConnection.reactFlowKey].outputs.filter(
-        (output) => output.reactFlowKey !== targetNodeReactFlowKey
-      );
+  if (isSchemaNodeExtended(targetNode) && targetNode.nodeProperties.includes(SchemaNodeProperty.Repeating)) {
+    isFunctionUnboundedInputOrRepeatingSchemaNode = true;
+  } else if (isFunctionData(targetNode) && targetNode.maxNumberOfInputs === -1) {
+    isFunctionUnboundedInputOrRepeatingSchemaNode = true;
+  }
+
+  if (!findInputSlot && inputIndex !== undefined) {
+    // Verify if we're updating an old value that's a ConnectionUnit, and if so, remove it from source's outputs[]
+    if (connection?.inputs) {
+      let inputConnection: InputConnection = undefined;
+
+      if (isFunctionUnboundedInputOrRepeatingSchemaNode) {
+        inputConnection = connection.inputs[0][inputIndex];
+      } else if (connection.inputs[inputIndex].length > 0) {
+        inputConnection = connection.inputs[inputIndex][0];
+      }
+
+      if (inputConnection && isConnectionUnit(inputConnection)) {
+        connections[inputConnection.reactFlowKey].outputs = connections[inputConnection.reactFlowKey].outputs.filter(
+          (output) => output.reactFlowKey !== targetNodeReactFlowKey
+        );
+      }
     }
   }
 
   createConnectionEntryIfNeeded(connections, targetNode, targetNodeReactFlowKey);
   connection = connections[targetNodeReactFlowKey];
 
+  let confirmedInputIndex = inputIndex ?? 0;
+
+  if (findInputSlot && value !== undefined && value !== null) {
+    // Schema nodes can only ever have 1 input as long as it is not repeating
+    if (isSchemaNodeExtended(targetNode)) {
+      if (targetNode.nodeProperties.includes(SchemaNodeProperty.Repeating)) {
+        confirmedInputIndex = -1;
+      }
+    } else {
+      // If the destination has unlimited inputs, all should go on the first input
+      if (isFunctionUnboundedInputOrRepeatingSchemaNode) {
+        // Check if an undefined input field exists first (created through PropPane)
+        // - otherwise we can safely just append its value to the end
+        const indexOfFirstOpenInput = connection.inputs[0].findIndex((inputCon) => !inputCon);
+        confirmedInputIndex = indexOfFirstOpenInput >= 0 ? indexOfFirstOpenInput : -1;
+      } else {
+        if (isConnectionUnit(value)) {
+          // Add input to first available and type-matched place (handle & PropPane validation should guarantee there's at least one)
+          confirmedInputIndex = Object.values(connection.inputs).findIndex(
+            (inputCon, idx) =>
+              inputCon.length < 1 &&
+              targetNode.inputs[idx].allowedTypes.some(
+                (type) =>
+                  type === NormalizedDataType.Any ||
+                  (isSchemaNodeExtended(value.node) ? type === value.node.normalizedDataType : type === value.node.outputValueType)
+              )
+          );
+        } else if (isCustomValue(value)) {
+          // Add input to first available that allows custom values
+          confirmedInputIndex = Object.values(connection.inputs).findIndex(
+            (inputCon, idx) => inputCon.length < 1 && targetNode.inputs[idx].allowCustomInput
+          );
+        }
+      }
+    }
+  }
+
   // null is signal to delete unbounded input value
   if (value === null) {
-    if (isUnboundedInput) {
+    if (isFunctionUnboundedInputOrRepeatingSchemaNode) {
       const newUnboundedInputValues = connection.inputs[0];
-      newUnboundedInputValues.splice(inputIndex, 1);
+      newUnboundedInputValues.splice(confirmedInputIndex, 1);
       connection.inputs[0] = newUnboundedInputValues;
     } else {
       console.error('Invalid Connection Input Op: null was provided for non-unbounded-input value');
     }
   } else if (value === undefined) {
-    // NOTE: Explicit undefined check to account for empty custom values ('')
-    if (isUnboundedInput) {
-      connection.inputs[0][inputIndex] = undefined;
+    // Explicit undefined check to handle empty custom values ('') in the next block
+    if (isFunctionUnboundedInputOrRepeatingSchemaNode) {
+      connection.inputs[0][confirmedInputIndex] = undefined;
     } else {
-      connection.inputs[inputIndex] = [];
+      connection.inputs[confirmedInputIndex] = [];
     }
   } else {
-    if (isUnboundedInput) {
-      connection.inputs[0][inputIndex] = value;
+    // Set the value (ConnectionUnit or custom value)
+    if (isFunctionUnboundedInputOrRepeatingSchemaNode) {
+      if (confirmedInputIndex === -1) {
+        // Repeating schema node
+        connection.inputs[0].push(value);
+      } else {
+        // Function unbounded input
+        connection.inputs[0][confirmedInputIndex] = value;
+      }
     } else {
-      connection.inputs[inputIndex][0] = value;
+      if (confirmedInputIndex !== -1) {
+        connection.inputs[confirmedInputIndex][0] = value;
+      } else {
+        connection.inputs[0].push(value);
+      }
     }
 
     // Only need to update/add value to source's outputs[] if it's a ConnectionUnit
@@ -197,6 +189,7 @@ export const updateConnectionInputValue = (
 
       createConnectionEntryIfNeeded(connections, value.node, value.reactFlowKey);
       connections[value.reactFlowKey].outputs.push(tgtConUnit);
+      connections[value.reactFlowKey].outputs = connections[value.reactFlowKey].outputs.filter(onlyUniqueConnections);
     }
   }
 };
