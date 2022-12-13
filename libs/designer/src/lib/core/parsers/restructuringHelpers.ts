52b8fcf4080715d3dfb6a8ac6cf50d53fec78c04
Riley Evans <rllyy97@gmail.com>
2022-12-13 09:26:21 -0500

    labugfix: Root Node Run-After Bugfix (#1421)
    * Fixed bug
    
    * Fixed minor test
    
    Co-authored-by: Riley Evans <rileyevans@WIN-Q8OPD5QU6IF.redmond.corp.microsoft.com>

--- libs/designer/src/lib/core/parsers/restructuringHelpers.ts
+++ libs/designer/src/lib/core/parsers/restructuringHelpers.ts
@@ -55,6 +55,7 @@ export const reassignEdgeSources = (
 
   // Remove would-be duplicate edges
   const targetEdges = graph.edges?.filter((edge) => edge.source === oldSourceId) ?? [];
+  if (targetEdges.length === 0) return;
   targetEdges.forEach((tEdge) => {
     if (graph.edges?.some((aEdge) => aEdge.source === newSourceId && aEdge.target === tEdge.target)) {
       removeEdge(state, oldSourceId, tEdge.target, graph);
@@ -112,10 +113,9 @@ const moveRunAfterSource = (
 
 export const applyIsRootNode = (state: WorkflowState, rootNodeId: string, graph: WorkflowNode, metadata: NodesMetadata) => {
   graph.edges?.forEach((edge) => {
-    if (edge.source === rootNodeId)
-      if (metadata?.[edge.target]) {
-        delete metadata[edge.target].isRoot;
-        (state.operations[edge.target] as LogicAppsV2.ActionDefinition).runAfter = {};
-      }
+    if (edge.source === rootNodeId && metadata?.[edge.target]) {
+      delete metadata[edge.target].isRoot;
+      (state.operations[edge.target] as LogicAppsV2.ActionDefinition).runAfter = {};
+    }
   });
 };
