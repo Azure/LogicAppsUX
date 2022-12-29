70be909c6bd3f0fa6f249814b2b2dbc126fb72e1
Reid Fortier <refortie@microsoft.com>
2022-12-29 14:26:02 -0800

    De-dup logic

--- libs/data-mapper/src/lib/mapDefinitions/MapDefinitionDeserializer.ts
+++ libs/data-mapper/src/lib/mapDefinitions/MapDefinitionDeserializer.ts
@@ -53,74 +53,158 @@ const parseDefinitionToConnection = (
   functions: FunctionData[]
 ) => {
   if (typeof sourceNodeObject === 'string') {
-    const sourceEndOfFunction = sourceNodeObject.indexOf('(');
-    const amendedSourceKey = targetKey.includes(mapNodeParams.for) ? getSourceValueFromLoop(sourceNodeObject, targetKey) : sourceNodeObject;
-
-    const sourceNode =
-      sourceEndOfFunction > -1
-        ? findFunctionForFunctionName(amendedSourceKey.substring(0, sourceEndOfFunction), functions)
-        : findNodeForKey(amendedSourceKey, sourceSchema.schemaTreeRoot);
-
-    let sourceKey: string;
-    let functionAlreadyCreated = false;
-    if (sourceNode && isFunctionData(sourceNode)) {
-      if (createdNodes[amendedSourceKey]) {
-        functionAlreadyCreated = true;
-        sourceKey = createdNodes[amendedSourceKey];
-      } else {
-        sourceKey = createReactFlowFunctionKey(sourceNode);
-      }
-    } else {
-      sourceKey = `${sourcePrefix}${amendedSourceKey}`;
-    }
-    createdNodes[amendedSourceKey] = sourceKey;
-
-    const destinationNode = getDestinationNode(targetKey, functions, targetSchema.schemaTreeRoot);
-    const destinationKey = getDestinationKey(targetKey, destinationNode);
-
-    if (targetKey.includes(mapNodeParams.for)) {
-      // if has $for, add parent connection
-      if (sourceNode && destinationNode) {
-        addParentConnectionForRepeatingElements(destinationNode, sourceNode, sourceSchemaFlattened, targetSchemaFlattened, connections);
-      }
-    }
-
-    if (destinationNode) {
-      setConnectionInputValue(connections, {
-        targetNode: destinationNode,
-        targetNodeReactFlowKey: destinationKey,
-        findInputSlot: true,
-        value: sourceNode
-          ? {
-              reactFlowKey: sourceKey,
-              node: sourceNode,
-            }
-          : amendedSourceKey,
-      });
-    }
-
-    // Need to extract and create connections for nested functions
-    if (sourceEndOfFunction > -1 && !functionAlreadyCreated) {
-      const childFunctions = splitKeyIntoChildren(sourceNodeObject);
-
-      childFunctions.forEach((childFunction) => {
-        parseDefinitionToConnection(
-          childFunction,
-          sourceKey,
-          connections,
-          createdNodes,
-          sourceSchema,
-          sourceSchemaFlattened,
-          targetSchema,
-          targetSchemaFlattened,
-          functions
-        );
-      });
-    }
-
+    createConnections(
+      sourceNodeObject,
+      targetKey,
+      connections,
+      createdNodes,
+      sourceSchema,
+      sourceSchemaFlattened,
+      targetSchema,
+      targetSchemaFlattened,
+      functions
+    );
     return;
   }
 
+  callChildObjects(
+    sourceNodeObject,
+    targetKey,
+    connections,
+    createdNodes,
+    sourceSchema,
+    sourceSchemaFlattened,
+    targetSchema,
+    targetSchemaFlattened,
+    functions
+  );
+};
+
+const parseDefinitionToConditionalConnection = (
+  sourceNodeObject: any,
+  sourceNodeObjectAsString: string,
+  targetKey: string,
+  connections: ConnectionDictionary,
+  createdNodes: { [completeFunction: string]: string },
+  sourceSchema: SchemaExtended,
+  sourceSchemaFlattened: SchemaNodeDictionary,
+  targetSchema: SchemaExtended,
+  targetSchemaFlattened: SchemaNodeDictionary,
+  functions: FunctionData[]
+) => {
+  createConnections(
+    sourceNodeObjectAsString,
+    targetKey,
+    connections,
+    createdNodes,
+    sourceSchema,
+    sourceSchemaFlattened,
+    targetSchema,
+    targetSchemaFlattened,
+    functions
+  );
+
+  callChildObjects(
+    sourceNodeObject,
+    targetKey,
+    connections,
+    createdNodes,
+    sourceSchema,
+    sourceSchemaFlattened,
+    targetSchema,
+    targetSchemaFlattened,
+    functions
+  );
+};
+
+const createConnections = (
+  sourceNodeString: string,
+  targetKey: string,
+  connections: ConnectionDictionary,
+  createdNodes: { [completeFunction: string]: string },
+  sourceSchema: SchemaExtended,
+  sourceSchemaFlattened: SchemaNodeDictionary,
+  targetSchema: SchemaExtended,
+  targetSchemaFlattened: SchemaNodeDictionary,
+  functions: FunctionData[]
+) => {
+  const sourceEndOfFunction = sourceNodeString.indexOf('(');
+  const amendedSourceKey = targetKey.includes(mapNodeParams.for) ? getSourceValueFromLoop(sourceNodeString, targetKey) : sourceNodeString;
+
+  const sourceNode =
+    sourceEndOfFunction > -1
+      ? findFunctionForFunctionName(amendedSourceKey.substring(0, sourceEndOfFunction), functions)
+      : findNodeForKey(amendedSourceKey, sourceSchema.schemaTreeRoot);
+
+  let sourceKey: string;
+  let functionAlreadyCreated = false;
+  if (sourceNode && isFunctionData(sourceNode)) {
+    if (createdNodes[amendedSourceKey]) {
+      functionAlreadyCreated = true;
+      sourceKey = createdNodes[amendedSourceKey];
+    } else {
+      sourceKey = createReactFlowFunctionKey(sourceNode);
+    }
+  } else {
+    sourceKey = `${sourcePrefix}${amendedSourceKey}`;
+  }
+  createdNodes[amendedSourceKey] = sourceKey;
+
+  const destinationNode = getDestinationNode(targetKey, functions, targetSchema.schemaTreeRoot);
+  const destinationKey = getDestinationKey(targetKey, destinationNode);
+
+  if (targetKey.includes(mapNodeParams.for)) {
+    // if has $for, add parent connection
+    if (sourceNode && destinationNode) {
+      addParentConnectionForRepeatingElements(destinationNode, sourceNode, sourceSchemaFlattened, targetSchemaFlattened, connections);
+    }
+  }
+
+  if (destinationNode) {
+    setConnectionInputValue(connections, {
+      targetNode: destinationNode,
+      targetNodeReactFlowKey: destinationKey,
+      findInputSlot: true,
+      value: sourceNode
+        ? {
+            reactFlowKey: sourceKey,
+            node: sourceNode,
+          }
+        : amendedSourceKey,
+    });
+  }
+
+  // Need to extract and create connections for nested functions
+  if (sourceEndOfFunction > -1 && !functionAlreadyCreated) {
+    const childFunctions = splitKeyIntoChildren(sourceNodeString);
+
+    childFunctions.forEach((childFunction) => {
+      parseDefinitionToConnection(
+        childFunction,
+        sourceKey,
+        connections,
+        createdNodes,
+        sourceSchema,
+        sourceSchemaFlattened,
+        targetSchema,
+        targetSchemaFlattened,
+        functions
+      );
+    });
+  }
+};
+
+const callChildObjects = (
+  sourceNodeObject: string | object | any,
+  targetKey: string,
+  connections: ConnectionDictionary,
+  createdNodes: { [completeFunction: string]: string },
+  sourceSchema: SchemaExtended,
+  sourceSchemaFlattened: SchemaNodeDictionary,
+  targetSchema: SchemaExtended,
+  targetSchemaFlattened: SchemaNodeDictionary,
+  functions: FunctionData[]
+) => {
   const childEntries = Object.entries(sourceNodeObject);
   childEntries.forEach((childEntry) => {
     let childTargetKey = targetKey;
@@ -130,19 +214,6 @@ const parseDefinitionToConnection = (
     }
 
     if (childEntry[0].startsWith(mapNodeParams.if)) {
-      /*
-      parseDefinitionToConnection(
-        childEntry[1],
-        targetKey,
-        connections,
-        createdNodes,
-        sourceSchema,
-        sourceSchemaFlattened,
-        targetSchema,
-        targetSchemaFlattened,
-        functions
-      );*/
-
       parseDefinitionToConditionalConnection(
         sourceNodeObject[childEntry[0]],
         childEntry[0],
@@ -168,99 +239,5 @@ const parseDefinitionToConnection = (
         functions
       );
     }
-  });
-};
-
-const parseDefinitionToConditionalConnection = (
-  sourceNodeObject: any,
-  sourceNodeObjectAsString: string,
-  targetKey: string,
-  connections: ConnectionDictionary,
-  createdNodes: { [completeFunction: string]: string },
-  sourceSchema: SchemaExtended,
-  sourceSchemaFlattened: SchemaNodeDictionary,
-  targetSchema: SchemaExtended,
-  targetSchemaFlattened: SchemaNodeDictionary,
-  functions: FunctionData[]
-) => {
-  const sourceEndOfFunction = sourceNodeObjectAsString.indexOf('(');
-  const amendedSourceKey = targetKey.includes(mapNodeParams.for)
-    ? getSourceValueFromLoop(sourceNodeObjectAsString, targetKey)
-    : sourceNodeObjectAsString;
-
-  const sourceNode =
-    sourceEndOfFunction > -1
-      ? findFunctionForFunctionName(amendedSourceKey.substring(0, sourceEndOfFunction), functions)
-      : findNodeForKey(amendedSourceKey, sourceSchema.schemaTreeRoot);
-
-  let sourceKey: string;
-  let functionAlreadyCreated = false;
-  if (sourceNode && isFunctionData(sourceNode)) {
-    if (createdNodes[amendedSourceKey]) {
-      functionAlreadyCreated = true;
-      sourceKey = createdNodes[amendedSourceKey];
-    } else {
-      sourceKey = createReactFlowFunctionKey(sourceNode);
-    }
-  } else {
-    sourceKey = `${sourcePrefix}${amendedSourceKey}`;
-  }
-  createdNodes[amendedSourceKey] = sourceKey;
-
-  const destinationNode = getDestinationNode(targetKey, functions, targetSchema.schemaTreeRoot);
-  const destinationKey = getDestinationKey(targetKey, destinationNode);
-
-  if (destinationNode) {
-    setConnectionInputValue(connections, {
-      targetNode: destinationNode,
-      targetNodeReactFlowKey: destinationKey,
-      findInputSlot: true,
-      value: sourceNode
-        ? {
-            reactFlowKey: sourceKey,
-            node: sourceNode,
-          }
-        : amendedSourceKey,
-    });
-  }
-
-  // Need to extract and create connections for nested functions
-  if (sourceEndOfFunction > -1 && !functionAlreadyCreated) {
-    const childFunctions = splitKeyIntoChildren(sourceNodeObjectAsString);
-
-    childFunctions.forEach((childFunction) => {
-      parseDefinitionToConnection(
-        childFunction,
-        sourceKey,
-        connections,
-        createdNodes,
-        sourceSchema,
-        sourceSchemaFlattened,
-        targetSchema,
-        targetSchemaFlattened,
-        functions
-      );
-    });
-  }
-
-  const childEntries = Object.entries(sourceNodeObject);
-  childEntries.forEach((childEntry) => {
-    let childTargetKey = targetKey;
-    if (childEntry[0] !== mapNodeParams.value) {
-      const trimmedChildKey = childEntry[0].startsWith('$@') ? childEntry[0].substring(1) : childEntry[0];
-      childTargetKey = `${targetKey}/${trimmedChildKey}`;
-    }
-
-    parseDefinitionToConnection(
-      childEntry[1],
-      childTargetKey,
-      connections,
-      createdNodes,
-      sourceSchema,
-      sourceSchemaFlattened,
-      targetSchema,
-      targetSchemaFlattened,
-      functions
-    );
   });
 };
