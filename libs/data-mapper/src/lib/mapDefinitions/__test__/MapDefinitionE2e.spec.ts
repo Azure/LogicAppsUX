fc6afc2844a137e06e40943d2c8464c50a28710a
Nicolas Layne <nicolaslayne@microsoft.com>
2023-01-04 16:57:40 -0600

    Remove unnecessary describe

--- libs/data-mapper/src/lib/mapDefinitions/__test__/MapDefinitionE2e.spec.ts
+++ libs/data-mapper/src/lib/mapDefinitions/__test__/MapDefinitionE2e.spec.ts
@@ -7,7 +7,7 @@ import { convertFromMapDefinition } from '../MapDefinitionDeserializer';
 import { convertToMapDefinition } from '../MapDefinitionSerializer';
 import * as yaml from 'js-yaml';
 
-describe('mapDefinitions/MapDefinitionE2e', () => {
+describe.skip('mapDefinitions/MapDefinitionE2e', () => {
   const mockSourceSchema: SchemaExtended = convertSchemaToSchemaExtended(sourceMockSchema);
   const mockTargetSchema: SchemaExtended = convertSchemaToSchemaExtended(targetMockSchema);
   const mockTranscriptMapDefinition: MapDefinitionEntry = yaml.load(transcriptMockMapDefinitionString) as MapDefinitionEntry;
@@ -20,23 +20,21 @@ describe('mapDefinitions/MapDefinitionE2e', () => {
   );
 
   // TODO: Un-skip once all deserialization scenarios are supported/working
-  describe.skip('Deserializing then re-serializing produces the same map definition', () => {
-    // Compare YAML as parsed objects to disregard any comments
-    const reserializedMapDefinitionString: string = convertToMapDefinition(
-      deserializedConnectionDictionary,
-      mockSourceSchema,
-      mockTargetSchema
-    );
-    const reserializedMapDefinition: MapDefinitionEntry = yaml.load(reserializedMapDefinitionString) as MapDefinitionEntry;
+  // Compare YAML as parsed objects to disregard any comments
+  const reserializedMapDefinitionString: string = convertToMapDefinition(
+    deserializedConnectionDictionary,
+    mockSourceSchema,
+    mockTargetSchema
+  );
+  const reserializedMapDefinition: MapDefinitionEntry = yaml.load(reserializedMapDefinitionString) as MapDefinitionEntry;
 
-    it('Current map definition data was able to be serialized', () => {
-      // NOTE: Will be undefined if string === '', which happens when there's an issue w/ serialization
-      expect(reserializedMapDefinition).toBeDefined();
-    });
+  it('Map definition data was able to be serialized', () => {
+    // NOTE: Will be undefined if string === '', which happens when there's an issue w/ serialization
+    expect(reserializedMapDefinition).toBeDefined();
+  });
 
-    it('Unmodified / re-serialized map definition reproduces exact same initial map definition', () => {
-      // NOTE: toEqual will log the specific properties/values that differ
-      expect(reserializedMapDefinition).toEqual(mockTranscriptMapDefinition);
-    });
+  it('Deserializing then re-serializing produces the same map definition', () => {
+    // NOTE: toEqual will log the specific properties/values that differ
+    expect(reserializedMapDefinition).toEqual(mockTranscriptMapDefinition);
   });
 });
