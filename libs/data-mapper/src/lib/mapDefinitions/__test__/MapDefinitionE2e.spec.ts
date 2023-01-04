72b09ad109613aca8a220a0fcb5ddc548595ea77
Nicolas Layne <nicolaslayne@microsoft.com>
2023-01-04 13:20:14 -0600

    Set up test categories based on ones in map definition

--- libs/data-mapper/src/lib/mapDefinitions/__test__/MapDefinitionE2e.spec.ts
+++ libs/data-mapper/src/lib/mapDefinitions/__test__/MapDefinitionE2e.spec.ts
@@ -1,7 +1,7 @@
 import { sourceMockSchema, targetMockSchema, transcriptMockMapDefinitionString } from '../../__mocks__';
 import { functionMock } from '../../models';
 import type { MapDefinitionEntry, SchemaExtended } from '../../models';
-import type { ConnectionDictionary } from '../../models/Connection';
+import type { ConnectionDictionary, ConnectionUnit } from '../../models/Connection';
 import { convertSchemaToSchemaExtended } from '../../utils/Schema.Utils';
 import { convertFromMapDefinition } from '../MapDefinitionDeserializer';
 import { convertToMapDefinition } from '../MapDefinitionSerializer';
@@ -19,18 +19,90 @@ describe('mapDefinitions/MapDefinitionE2e', () => {
     functionMock
   );
 
+  // TODO: remove
+  console.dir(Object.entries(deserializedConnectionDictionary).slice(3, 8), { depth: null });
+
   describe('Initial deserialization', () => {
-    it('TODO: Each individual deserialization case', () => {
+    it('Direct translation (passthrough)', () => {
+      expect(
+        (deserializedConnectionDictionary['target-/ns0:Root/DirectTranslation/Employee/ID']?.inputs[0][0] as ConnectionUnit).reactFlowKey
+      ).toBe('source-/ns0:Root/DirectTranslation/EmployeeID');
+      expect(
+        (deserializedConnectionDictionary['target-/ns0:Root/DirectTranslation/Employee/Name']?.inputs[0][0] as ConnectionUnit).reactFlowKey
+      ).toBe('source-/ns0:Root/DirectTranslation/EmployeeName');
+    });
+
+    it('Data translation (attributes)', () => {
+      expect(
+        (deserializedConnectionDictionary['target-/ns0:Root/DataTranslation/EmployeeName/@RegularFulltime']?.inputs[0][0] as ConnectionUnit)
+          .reactFlowKey
+      ).toBe('source-/ns0:Root/DataTranslation/Employee/EmploymentStatus');
+
+      expect(
+        (
+          deserializedConnectionDictionary['target-/ns0:Root/DataTranslation/EmployeeName']?.inputs[0][0] as ConnectionUnit
+        ).reactFlowKey.includes('Concat')
+      ).toBe(true);
+      // TODO: Ensure the Concat node's inputs match what the map definition specifies
+    });
+
+    it('Content enrichment', () => {
+      expect(
+        (
+          deserializedConnectionDictionary['target-/ns0:Root/ContentEnrich/DateOfDemo']?.inputs[0][0] as ConnectionUnit
+        ).reactFlowKey.includes('CurrentDate')
+      ).toBe(true);
+    });
+
+    it('Cumulative expression (loops w/ functions)', () => {
+      expect(true).toBeTruthy(); // TODO
+    });
+
+    it('Conditional mapping', () => {
+      expect(true).toBeTruthy(); // TODO
+    });
+
+    it('Looping', () => {
+      expect(true).toBeTruthy(); // TODO
+    });
+
+    it('Conditional looping', () => {
+      expect(true).toBeTruthy(); // TODO
+    });
+
+    // TODO: WI #16700911
+    it.skip('Looping w/ index', () => {
+      expect(true).toBeTruthy();
+    });
+
+    // TODO: WI #16700908 - this scenario technically occurs within that ^ category
+    // from the mapDef, so may need to do some rearranging/re-labeling
+
+    // TODO: Figure out what this category is testing for (or if it's just demo'ing a different real-world scenario)
+    it('Name value transforms (???)', () => {
+      expect(true).toBeTruthy();
+    });
+
+    // TODO: WI #16701021
+    it.skip('Dot value-access', () => {
       expect(true).toBeTruthy();
     });
   });
 
-  // Compare YAML as parsed objects to disregard any comments
-  const reserializedMapDefinition: MapDefinitionEntry = yaml.load(
-    convertToMapDefinition(deserializedConnectionDictionary, mockSourceSchema, mockTargetSchema)
-  ) as MapDefinitionEntry;
-
   describe('Re-serialization', () => {
+    // Compare YAML as parsed objects to disregard any comments
+    const reserializedMapDefinitionString: string = convertToMapDefinition(
+      deserializedConnectionDictionary,
+      mockSourceSchema,
+      mockTargetSchema
+    );
+    const reserializedMapDefinition: MapDefinitionEntry = yaml.load(reserializedMapDefinitionString) as MapDefinitionEntry;
+
+    it('Current map definition data was able to be serialized', () => {
+      // NOTE: Will be undefined if string === '', which happens when there's an issue w/ serialization
+      expect(reserializedMapDefinition).toBeDefined();
+    });
+
     it('Unmodified / re-serialized map definition reproduces exact same initial map definition', () => {
       // NOTE: toEqual will log the specific properties/values that differ
       expect(reserializedMapDefinition).toEqual(mockTranscriptMapDefinition);
