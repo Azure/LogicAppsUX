dd8d51b15a2c7b1193b7cb4ee10e6b03e65cd6a6
Nicolas Layne <nicolaslayne@microsoft.com>
2023-01-05 08:58:42 -0600

    Remove unneeded export

--- libs/data-mapper/src/lib/__mocks__/index.ts
+++ libs/data-mapper/src/lib/__mocks__/index.ts
@@ -4,4 +4,3 @@ export { default as layeredLoopSourceMockSchema } from './LayeredLoopSourceMockS
 export { default as layeredLoopTargetMockSchema } from './LayeredLoopTargetMockSchema.json';
 export { default as simpleLoopSource } from './SimpleLoopSource.json';
 export { default as simpleLoopTarget } from './SimpleLoopTarget.json';
-export * from './TranscriptMockMapDefinition';
