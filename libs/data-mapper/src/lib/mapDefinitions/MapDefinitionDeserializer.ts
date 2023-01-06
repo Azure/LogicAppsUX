414345da872f35d4cc592211cdab4558f732afed
refortie <37600290+refortie@users.noreply.github.com>
2023-01-06 12:46:52 -0800

    [Data Mapper] Deserialization property conditionals (#1477)
    * Working property level conditional deserialization
    
    * Skip conditional object
    
    * Update name of variable

--- libs/data-mapper/src/lib/mapDefinitions/MapDefinitionDeserializer.ts
+++ libs/data-mapper/src/lib/mapDefinitions/MapDefinitionDeserializer.ts
@@ -1,15 +1,16 @@
 /* eslint-disable no-param-reassign */
 import { mapNodeParams, reservedMapDefinitionKeysArray } from '../constants/MapDefinitionConstants';
-import { sourcePrefix } from '../constants/ReactFlowConstants';
+import { sourcePrefix, targetPrefix } from '../constants/ReactFlowConstants';
 import { addParentConnectionForRepeatingElements } from '../core/state/DataMapSlice';
 import type { FunctionData, MapDefinitionEntry, SchemaExtended, SchemaNodeDictionary } from '../models';
 import { SchemaType } from '../models';
 import type { ConnectionDictionary } from '../models/Connection';
 import { setConnectionInputValue } from '../utils/Connection.Utils';
-import { getDestinationKey, getDestinationNode, getSourceValueFromLoop, splitKeyIntoChildren } from '../utils/DataMap.Utils';
+import { getDestinationNode, getSourceValueFromLoop, splitKeyIntoChildren } from '../utils/DataMap.Utils';
 import { findFunctionForFunctionName, isFunctionData } from '../utils/Function.Utils';
 import { createReactFlowFunctionKey } from '../utils/ReactFlow.Util';
 import { findNodeForKey, flattenSchemaIntoDictionary } from '../utils/Schema.Utils';
+import { isAGuid } from '@microsoft/utils-logic-apps';
 
 export const convertFromMapDefinition = (
   mapDefinition: MapDefinitionEntry,
@@ -128,7 +129,7 @@ const callChildObjects = (
   targetSchemaFlattened: SchemaNodeDictionary,
   functions: FunctionData[]
 ) => {
-  const childEntries = Object.entries(sourceNodeObject);
+  const childEntries = Object.entries<MapDefinitionEntry>(sourceNodeObject);
   childEntries.forEach((childEntry) => {
     let childTargetKey = targetKey;
     if (childEntry[0] !== mapNodeParams.value) {
@@ -137,10 +138,25 @@ const callChildObjects = (
     }
 
     if (childEntry[0].startsWith(mapNodeParams.if)) {
+      Object.values(childEntry[1]).forEach((childSubValue) => {
+        parseDefinitionToConnection(
+          childSubValue,
+          childEntry[0],
+          connections,
+          createdNodes,
+          sourceSchema,
+          sourceSchemaFlattened,
+          targetSchema,
+          targetSchemaFlattened,
+          functions
+        );
+      });
+
+      const childSubKey = Object.keys(childEntry[1])[0];
       parseDefinitionToConditionalConnection(
         sourceNodeObject[childEntry[0]],
         childEntry[0],
-        targetKey,
+        `${targetKey}/${childSubKey}`,
         connections,
         createdNodes,
         sourceSchema,
@@ -185,10 +201,10 @@ const createConnections = (
       : findNodeForKey(amendedSourceKey, sourceSchema.schemaTreeRoot);
 
   let sourceKey: string;
-  let functionAlreadyCreated = false;
+  let sourceFunctionAlreadyCreated = false;
   if (sourceNode && isFunctionData(sourceNode)) {
     if (createdNodes[amendedSourceKey]) {
-      functionAlreadyCreated = true;
+      sourceFunctionAlreadyCreated = true;
       sourceKey = createdNodes[amendedSourceKey];
     } else {
       sourceKey = createReactFlowFunctionKey(sourceNode);
@@ -199,7 +215,20 @@ const createConnections = (
   createdNodes[amendedSourceKey] = sourceKey;
 
   const destinationNode = getDestinationNode(targetKey, functions, targetSchema.schemaTreeRoot);
-  const destinationKey = getDestinationKey(targetKey, destinationNode);
+
+  let destinationKey: string;
+  if (destinationNode && isFunctionData(destinationNode)) {
+    if (createdNodes[targetKey]) {
+      destinationKey = createdNodes[targetKey];
+    } else if (isAGuid(targetKey.substring(targetKey.length - 36))) {
+      destinationKey = targetKey;
+    } else {
+      destinationKey = createReactFlowFunctionKey(destinationNode);
+      createdNodes[targetKey] = destinationKey;
+    }
+  } else {
+    destinationKey = `${targetPrefix}${destinationNode?.key}`;
+  }
 
   if (targetKey.includes(mapNodeParams.for)) {
     // if has $for, add parent connection
@@ -209,6 +238,23 @@ const createConnections = (
   }
 
   if (destinationNode) {
+    if (targetKey.startsWith(mapNodeParams.if)) {
+      // We need to make sure we create the contents of the conditional as well and attach it as an input
+      const trimmedTargetKey = targetKey.substring(mapNodeParams.if.length + 1, targetKey.length - 1);
+
+      createConnections(
+        trimmedTargetKey,
+        destinationKey,
+        connections,
+        createdNodes,
+        sourceSchema,
+        sourceSchemaFlattened,
+        targetSchema,
+        targetSchemaFlattened,
+        functions
+      );
+    }
+
     setConnectionInputValue(connections, {
       targetNode: destinationNode,
       targetNodeReactFlowKey: destinationKey,
@@ -223,7 +269,7 @@ const createConnections = (
   }
 
   // Need to extract and create connections for nested functions
-  if (sourceEndOfFunction > -1 && !functionAlreadyCreated) {
+  if (sourceEndOfFunction > -1 && !sourceFunctionAlreadyCreated) {
     const childFunctions = splitKeyIntoChildren(sourceNodeString);
 
     childFunctions.forEach((childFunction) => {
