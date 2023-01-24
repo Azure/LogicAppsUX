a4eeb2b8eaf15e5ba8a0cb03ba503b707a9ed63f
Carlos Emiliano Castro Trejo <102700317+ccastrotrejo@users.noreply.github.com>
2023-01-24 12:21:01 -0600

    VSFeat: Add use sql storage command (#1553)
    * Register and add logic of command
    
    * Add command to package.json

---
+++ apps/vs-code-designer/src/app/commands/workflows/useSQLStorage.ts
@@ -0,0 +1,36 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { sqlStorageConnectionStringKey } from '../../../constants';
+import { localize } from '../../../localize';
+import { addOrUpdateLocalAppSettings } from '../../utils/appSettings/localSettings';
+import { getFunctionProjectRoot } from '../../utils/codeless/connection';
+import { getLocalSettingsFile } from '../appSettings/getLocalSettingsFile';
+import { validateSQLConnectionString } from '../deploy/storageAccountSteps/SQLStringNameStep';
+import type { IActionContext } from '@microsoft/vscode-azext-utils';
+import * as vscode from 'vscode';
+
+export async function useSQLStorage(context: IActionContext) {
+  const sqlConnectionString = await context.ui.showInputBox({
+    placeHolder: localize('sqlConnectionStringPlaceholder', 'SQL connection string'),
+    prompt: localize('sqlConnectionStringPrompt', 'Provide your SQL connection string'),
+    validateInput: async (connectionString: string): Promise<string | undefined> => await validateSQLConnectionString(connectionString),
+  });
+
+  const message: string = localize('selectLocalSettings', 'Select your local settings file.');
+  const localSettingsFile: string = await getLocalSettingsFile(context, message);
+  const projectPath = await getFunctionProjectRoot(context, localSettingsFile);
+
+  if (!projectPath) {
+    throw new Error(localize('FunctionRootFolderError', 'Unable to determine logic app project root folder.'));
+  }
+
+  const valuesToUpdateInSettings: Record<string, string> = {};
+  valuesToUpdateInSettings[sqlStorageConnectionStringKey] = sqlConnectionString;
+
+  await addOrUpdateLocalAppSettings(context, projectPath, valuesToUpdateInSettings);
+  await vscode.window.showInformationMessage(
+    localize('logicapp.sqlstorageupdate', 'The logic app project settings are updated to use SQL storage.')
+  );
+}
