b8bd022bed679142ef64eefdfd610d5afa0496c7
Carlos Emiliano Castro Trejo <102700317+ccastrotrejo@users.noreply.github.com>
2023-01-24 16:23:25 -0600

    VSFeat: Add configure web hook for connections command (#1555)
    * Add command to package.json
    
    * Add configure webhook logic and steps
    
    * Update httpmethods in export logic app

---
+++ apps/vs-code-designer/src/app/commands/workflows/configureWebhookRedirectEndpoint/configureWebhookRedirectEndpoint.ts
@@ -0,0 +1,33 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { localSettingsFileName, webhookRedirectHostUri } from '../../../../constants';
+import { getLocalSettingsJson } from '../../../utils/appSettings/localSettings';
+import { tryGetFunctionProjectRoot } from '../../../utils/verifyIsProject';
+import { getContainingWorkspace, getWorkspaceFolder } from '../../../utils/workspace';
+import { createWebhookWizard } from './webhookWizard';
+import type { IActionContext } from '@microsoft/vscode-azext-utils';
+import { nonNullValue } from '@microsoft/vscode-azext-utils';
+import type { ILocalSettingsJson } from '@microsoft/vscode-extension';
+import * as path from 'path';
+import type { Uri, WorkspaceFolder } from 'vscode';
+
+export async function configureWebhookRedirectEndpoint(context: IActionContext, data: Uri): Promise<void> {
+  let workspaceFolder: WorkspaceFolder;
+
+  if (!data?.fsPath) {
+    workspaceFolder = await getWorkspaceFolder(context);
+  } else {
+    workspaceFolder = nonNullValue(getContainingWorkspace(data.fsPath), 'workspaceFolder');
+  }
+
+  const workspacePath = workspaceFolder.uri.fsPath;
+  const projectPath = (await tryGetFunctionProjectRoot(context, workspacePath)) || workspacePath;
+  const localSettings: ILocalSettingsJson = await getLocalSettingsJson(context, path.join(projectPath, localSettingsFileName));
+  const redirectEndpoint: string = localSettings.Values[webhookRedirectHostUri] || '';
+  const wizard = createWebhookWizard({ ...context, redirectEndpoint }, projectPath);
+
+  await wizard.prompt();
+  await wizard.execute();
+}
--- apps/vs-code-designer/src/constants.ts
+++ apps/vs-code-designer/src/constants.ts
@@ -107,6 +107,7 @@ export enum extensionCommand {
   toggleAppSettingVisibility = 'logicAppsExtension.toggleAppSettingVisibility',
   useSQLStorage = 'logicAppsExtension.useSQLStorage',
   switchDebugMode = 'logicAppsExtension.switchDebugMode',
+  configureWebhookRedirectEndpoint = 'logicAppsExtension.configureWebhookRedirectEndpoint',
 }
 
 // Context
