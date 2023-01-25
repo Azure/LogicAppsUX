6cc878a34937784ebceee9ffad4a9f91e262aaa9
Carlos Emiliano Castro Trejo <102700317+ccastrotrejo@users.noreply.github.com>
2023-01-25 12:30:11 -0600

    VSFeat: Add connect to Github, view commit in Github and disconnect from repo commands (#1558)
    * Add commands to package.json
    
    * Add connect to github command
    
    * Add disconnect repo logic
    
    * Add view commit in github command
    
    * Fix refresh when connecting from azure tab

--- apps/vs-code-designer/src/app/tree/slotsTree/SlotTreeItem.ts
+++ apps/vs-code-designer/src/app/tree/slotsTree/SlotTreeItem.ts
@@ -2,9 +2,11 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import type { ProductionSlotTreeItem } from './ProductionSlotTreeItem';
 import { SlotTreeItemBase } from './SlotTreeItemBase';
 import type { SlotsTreeItem } from './SlotsTreeItem';
 import type { ParsedSite } from '@microsoft/vscode-azext-azureappservice';
+import type { AzExtParentTreeItem } from '@microsoft/vscode-azext-utils';
 
 export class SlotTreeItem extends SlotTreeItemBase {
   public static contextValue = 'azLogicAppsSlot';
@@ -18,4 +20,8 @@ export class SlotTreeItem extends SlotTreeItemBase {
   public get label(): string {
     return this.site.slotName;
   }
+}
+
+export function isSlotTreeItem(treeItem: SlotTreeItem | ProductionSlotTreeItem | AzExtParentTreeItem): treeItem is SlotTreeItem {
+  return !!(treeItem as SlotTreeItem).site;
 }
