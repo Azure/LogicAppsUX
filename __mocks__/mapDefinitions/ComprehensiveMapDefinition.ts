5efa2e6ae75a130652b5265a20c07600f6c83d25
Nicolas Layne <nicolaslayne@microsoft.com>
2023-01-27 11:34:03 -0600

    Prefix certain source schema nodes for easier debugging + update mapDef to match update keys

--- __mocks__/mapDefinitions/ComprehensiveMapDefinition.ts
+++ __mocks__/mapDefinitions/ComprehensiveMapDefinition.ts
@@ -10,55 +10,55 @@ $sourceNamespaces:
 $targetNamespaces:
   ns0: http://tempuri.org/ComprehensiveTarget.xsd
   xs: http://www.w3.org/2001/XMLSchema
-ns0:SchemaRoot:
+ns0:TargetSchemaRoot:
   DirectTranslation:
-    FullName: /ns0:SchemaRoot/DirectTranslation/FullName
-    NumCorgis: /ns0:SchemaRoot/DirectTranslation/NumCorgis
-    $@IsTheNewGuy: /ns0:SchemaRoot/DirectTranslation/@IsTheNewGuy
+    FullName: /ns0:SourceSchemaRoot/DirectTranslation/SourceFullName
+    NumCorgis: /ns0:SourceSchemaRoot/DirectTranslation/SourceNumCorgis
+    $@IsTheNewGuy: /ns0:SourceSchemaRoot/DirectTranslation/@SourceIsTheNewGuy
   ContentEnrichment:
     Timestamp: current-date()
   Transformations:
     FullName: >-
-      concat(/ns0:SchemaRoot/Transformations/FirstName, " ",
-      /ns0:SchemaRoot/Transformations/LastName)
+      concat(/ns0:SourceSchemaRoot/Transformations/FirstName, " ",
+      /ns0:SourceSchemaRoot/Transformations/LastName)
     NumCoffees: >-
-      multiply(/ns0:SchemaRoot/Transformations/AssignedTasks,
-      /ns0:SchemaRoot/Transformations/SleepDeprivationLevel)
+      multiply(/ns0:SourceSchemaRoot/Transformations/AssignedTasks,
+      /ns0:SourceSchemaRoot/Transformations/SleepDeprivationLevel)
   CustomValues:
     SuperSecretKey: "password"
     MOTD: concat("Welcome! Today's date is ", string(current-date()))
   Conditionals:
-    $if(is-null(/ns0:SchemaRoot/Conditionals/Object)):
+    $if(is-null(/ns0:SourceSchemaRoot/Conditionals/SourceObject)):
       Object:
-        $if(is-null(/ns0:SchemaRoot/Conditionals/Object/Property)):
-          Property: /ns0:SchemaRoot/Conditionals/Object/Property
+        $if(is-null(/ns0:SourceSchemaRoot/Conditionals/SourceObject/SourceProperty)):
+          Property: /ns0:SourceSchemaRoot/Conditionals/SourceObject/SourceProperty
   Looping:
     OneToOne:
-      $for(/ns0:SchemaRoot/Looping/OneToOne/Simple):
+      $for(/ns0:SourceSchemaRoot/Looping/OneToOne/Simple):
         Simple:
-          Direct: Direct
-          FunctionChain: substring(lower-case(FunctionChain), 0, 5)
-      $for(/ns0:SchemaRoot/Looping/OneToOne/RelativePaths):
+          Direct: SourceDirect
+          FunctionChain: substring(lower-case(SourceFunctionChain), 0, 5)
+      $for(/ns0:SourceSchemaRoot/Looping/OneToOne/RelativePaths):
         RelativePaths:
           Object(DotAccess): .
           Property: Property
           $@Attribute: ./@Attribute
     ManyToOne:
       $for(/ns0:SourceSchemaRoot/Looping/ManyToOne/Simple):
-        $for(SimpleChild):
-          $for(SimpleChildChild):
+        $for(SourceSimpleChild):
+          $for(SourceSimpleChildChild):
             Simple:
-              Direct: Direct
-              FunctionChain: lower-case(FunctionChain)
+              Direct: SourceDirect
+              FunctionChain: lower-case(SourceFunctionChain)
     ManyToMany:
       $for(/ns0:SourceSchemaRoot/Looping/ManyToMany/Simple):
         Simple:
-          $for(SimpleChild):
+          $for(SourceSimpleChild):
             SimpleChild:
-              $for(SimpleChildChild):
+              $for(SourceSimpleChildChild):
                 SimpleChildChild:
-                  Direct: Direct
-                  FunctionChain: lower-case(/ns0:SourceSchemaRoot/Looping/ManyToMany/Simple/SimpleChild/FunctionChain)
+                  Direct: SourceDirect
+                  FunctionChain: lower-case(SourceFunctionChain)
     LoopReduce:
-      BestItemName: /ns0:SchemaRoot/Looping/LoopReduce/ItemsList[3]/ItemName
+      BestItemName: /ns0:SourceSchemaRoot/Looping/LoopReduce/ItemsList[3]/ItemName
 `;
