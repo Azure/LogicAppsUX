b98ee454a28286a48938f1e1b83045cfd4a576fc
refortie <37600290+refortie@users.noreply.github.com>
2023-02-16 11:27:40 -0800

    [Data Mapper] Major reworks to ReactFlow.utils.ts (#1647)
    * Whole Map view initial work
    * Added WholeMapOverview - tentative name
    * Reworked how widths are calculated, should hopefully be sturdier and simpler to follow
    * Fixed bug in minimap node sizes
    * Refactored react flow node creation to go back to using 1 method for both source and target
    
    * Clean up unused params
    
    * Fix handles
    
    * Additional fixes
    
    * Fix maps
    
    * Fix for missing target status in overview

--- libs/data-mapper/src/lib/components/wholeView/WholeView.tsx
+++ libs/data-mapper/src/lib/components/wholeView/WholeView.tsx
@@ -1,14 +1,9 @@
 import { reactFlowFitViewOptions, ReactFlowNodeType } from '../../constants/ReactFlowConstants';
 import type { RootState } from '../../core/state/Store';
 import { SchemaType } from '../../models/';
-import { useOverviewLayout } from '../../utils/ReactFlow.Util';
+import { useWholeViewLayout } from '../../utils/ReactFlow.Util';
 import { SchemaCard } from '../nodeCard/SchemaCard';
 import { SchemaNameBadge } from '../schemaSelection/SchemaNameBadge';
-import { SelectSchemaCard } from '../schemaSelection/SelectSchemaCard';
-import type { TargetNodesWithConnectionsDictionary } from '../targetSchemaPane/TargetSchemaPane';
-import { checkNodeStatuses } from '../targetSchemaPane/TargetSchemaPane';
-import type { NodeToggledStateDictionary } from '../tree/TargetSchemaTreeItem';
-import { Stack } from '@fluentui/react';
 import { makeStyles, shorthands, tokens } from '@fluentui/react-components';
 import { useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
@@ -30,47 +25,27 @@ const useStyles = makeStyles({
   },
 });
 
-export const MapOverview = () => {
+export const WholeMapOverview = () => {
   const styles = useStyles();
 
   return (
     <div className={styles.mapOverviewStyles}>
       <ReactFlowProvider>
-        <OverviewReactFlowWrapper />
+        <WholeOverviewReactFlowWrapper />
       </ReactFlowProvider>
     </div>
   );
 };
 
-const OverviewReactFlowWrapper = () => {
+const WholeOverviewReactFlowWrapper = () => {
   const { fitView } = useReactFlow();
 
   const sourceSchema = useSelector((state: RootState) => state.dataMap.curDataMapOperation.sourceSchema);
   const targetSchema = useSelector((state: RootState) => state.dataMap.curDataMapOperation.targetSchema);
-  const connectionDictionary = useSelector((state: RootState) => state.dataMap.curDataMapOperation.dataMapConnections);
-  const targetSchemaDictionary = useSelector((state: RootState) => state.dataMap.curDataMapOperation.flattenedTargetSchema);
+  // TODO will be used - const connectionDictionary = useSelector((state: RootState) => state.dataMap.curDataMapOperation.dataMapConnections);
+  // const targetSchemaDictionary = useSelector((state: RootState) => state.dataMap.curDataMapOperation.flattenedTargetSchema);
 
-  const targetSchemaStates = useMemo<NodeToggledStateDictionary>(() => {
-    if (!targetSchema) {
-      return {};
-    }
-
-    // Find target schema nodes with connections
-    const tgtSchemaNodesWithConnections: TargetNodesWithConnectionsDictionary = {};
-    Object.values(connectionDictionary).forEach((connection) => {
-      if (connection.self.reactFlowKey in targetSchemaDictionary && connection.inputs[0] && connection.inputs[0].length > 0) {
-        tgtSchemaNodesWithConnections[connection.self.node.key] = true;
-      }
-    });
-
-    // Recursively traverse the schema tree to calculate connected statuses from the leaf nodes up
-    const newToggledStatesDictionary: NodeToggledStateDictionary = {};
-    checkNodeStatuses(targetSchema.schemaTreeRoot, newToggledStatesDictionary, tgtSchemaNodesWithConnections);
-
-    return newToggledStatesDictionary;
-  }, [targetSchema, connectionDictionary, targetSchemaDictionary]);
-
-  const reactFlowNodes = useOverviewLayout(sourceSchema?.schemaTreeRoot, targetSchema?.schemaTreeRoot, targetSchemaStates);
+  const reactFlowNodes = useWholeViewLayout(sourceSchema?.schemaTreeRoot, targetSchema?.schemaTreeRoot);
 
   // Fit the canvas view any time a schema changes
   useEffect(() => {
@@ -107,12 +82,6 @@ const OverviewReactFlowWrapper = () => {
       fitViewOptions={reactFlowFitViewOptions}
       fitView
     >
-      <Stack horizontal horizontalAlign="space-around" verticalAlign="center" style={{ height: '100%' }}>
-        <SelectSchemaCard schemaType={SchemaType.Source} style={{ visibility: !sourceSchema ? 'visible' : 'hidden' }} />
-
-        <SelectSchemaCard schemaType={SchemaType.Target} style={{ visibility: !targetSchema ? 'visible' : 'hidden' }} />
-      </Stack>
-
       {sourceSchema && <SchemaNameBadge schemaName={sourceSchema.name} schemaTreeRootXPos={srcSchemaTreeRootXPos} />}
       {targetSchema && <SchemaNameBadge schemaName={targetSchema.name} schemaTreeRootXPos={tgtSchemaTreeRootXPos} />}
     </ReactFlow>
