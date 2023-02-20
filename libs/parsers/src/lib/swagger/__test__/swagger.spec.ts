7b99876604bb0f8a8965532a35adaddf0ac47cce
SauravMishra459 <119877492+SauravMishra459@users.noreply.github.com>
2023-02-21 03:40:54 +0530

    Callback Url Support in Header (#1662)

--- libs/parsers/src/lib/swagger/__test__/swagger.spec.ts
+++ libs/parsers/src/lib/swagger/__test__/swagger.spec.ts
@@ -6,4 +6,36 @@ describe('Swagger tests', () => {
     const result = await SwaggerParser.parse(Outlook as OpenAPIV2.Document);
     expect(result).toBeDefined();
   });
+
+  it(`should allow callback Url in header`, async () => {
+    const callback = {
+      swagger: '2.0',
+      info: {
+        version: '1.0',
+        title: 'CallBackInHeader',
+      },
+      paths: {
+        '/trigger': {
+          put: {
+            operationId: 'Trigger',
+            parameters: [
+              {
+                name: 'callBackUrl',
+                in: 'header',
+                type: 'string',
+                required: true,
+                'x-ms-notification-url': true,
+              },
+            ],
+            responses: {},
+          },
+        },
+      },
+    };
+    const parser = new SwaggerParser(callback as OpenAPIV2.Document);
+    const parameters = parser.getInputParameters('Trigger').byName;
+    expect(Object.keys(parameters).length).toBe(1);
+    expect(parameters['callBackUrl']['in']).toBe('header');
+    expect(parameters['callBackUrl']['isNotificationUrl']).toBe(true);
+  });
 });
