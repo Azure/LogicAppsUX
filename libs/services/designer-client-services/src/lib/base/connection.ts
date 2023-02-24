663a27f163e0b612093c8d83b55ff2f0f04040bb
Riley Evans <rllyy97@gmail.com>
2023-02-24 10:45:42 -0600

    Fixed some issues with connections for custom connectors

--- libs/services/designer-client-services/src/lib/base/connection.ts
+++ libs/services/designer-client-services/src/lib/base/connection.ts
@@ -285,6 +285,25 @@ export abstract class BaseConnectionService implements IConnectionService {
   }
 
   protected async getConnectionsForConnector(connectorId: string): Promise<Connection[]> {
+    // Right now the `managedApiName` filter property does not work on custom connections, so we need to filter them manually
+    if (equals(connectorId.split('/').at(-2), 'customApis')) {
+      const {
+        apiHubServiceDetails: { location, apiVersion },
+        httpClient,
+      } = this.options;
+      const response = await httpClient.get<ConnectionsResponse>({
+        uri: `${this._subscriptionResourceGroupWebUrl}/connections`,
+        queryParameters: {
+          'api-version': apiVersion,
+          $filter: `Location eq '${location}' and Kind eq '${this._vVersion}'`,
+        },
+      });
+      const filteredConnections = response.value.filter((connection) => {
+        return equals(connection.properties.api.id, connectorId);
+      });
+      return filteredConnections;
+    }
+
     if (isArmResourceId(connectorId)) {
       const {
         apiHubServiceDetails: { location, apiVersion },
@@ -410,8 +429,6 @@ export abstract class BaseConnectionService implements IConnectionService {
   }
 
   async fetchFunctionApps(): Promise<any> {
-    console.log('functionAppsResponse', this.getFunctionAppsRequestPath());
-
     const functionAppsResponse = await this.options.httpClient.get<any>({
       uri: this.getFunctionAppsRequestPath(),
       queryParameters: { 'api-version': this.options.apiVersion },
