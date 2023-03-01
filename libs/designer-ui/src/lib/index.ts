a6ee7ea7fcfcd6c0abf198b51761beb60da2027e
preetriti1 <48265693+preetriti1@users.noreply.github.com>
2023-02-28 23:32:09 -0800

    lafeat: Adding support for recurrence editor (#1687)
    * lafeat: Adding Recurrence editor control
    
    * Fixing styling and value update code
    
    ---------
    
    Co-authored-by: Priti Sambandam <psamband@microsoft.com>

--- libs/designer-ui/src/lib/index.ts
+++ libs/designer-ui/src/lib/index.ts
@@ -59,6 +59,7 @@ export * from './panel/panelcontainer';
 export * from './panel/panelUtil';
 export * from './panel/panelheader/panelheader';
 export * from './panel/recommendationpanel';
+export * from './recurrence';
 export * from './scratch';
 // export * from './picker';
 // export * from './rulescontrol';
