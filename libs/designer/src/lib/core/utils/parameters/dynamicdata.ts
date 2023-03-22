78c7530bf8d30dd65f47034c8e48202a31ee14c3
Riley Evans <rllyy97@gmail.com>
2023-03-22 18:31:16 -0500

    Added extra specification to include the /

--- libs/designer/src/lib/core/utils/parameters/dynamicdata.ts
+++ libs/designer/src/lib/core/utils/parameters/dynamicdata.ts
@@ -583,6 +583,6 @@ function getSwaggerTypeFromVariableType(variableType: string): string | undefine
 }
 
 function isOpenApiKeyPath(arr: string[]): boolean {
-  for (let i = 0; i < arr.length - 1; i++) if (!arr[i + 1].startsWith(arr[i])) return false;
+  for (let i = 0; i < arr.length - 1; i++) if (!arr[i + 1].startsWith(`${arr[i]}/`)) return false;
   return true;
 }
