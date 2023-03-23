f050a6eb938daf5cf15931f6568fd791bd967fb0
Eric <erickum@microsoft.com>
2023-03-23 10:04:00 -0700

    Export `useNodeDisplayName` hook for host services

--- libs/designer/src/lib/core/index.ts
+++ libs/designer/src/lib/core/index.ts
@@ -5,6 +5,7 @@ export { getReactQueryClient } from './ReactQueryProvider';
 export type { RootState, AppDispatch } from './store';
 export { store } from './store';
 export { discardAllChanges } from './state/workflow/workflowSlice';
+export { useNodeDisplayName } from './state/workflow/workflowSelectors';
 export { serializeWorkflow } from './actions/bjsworkflow/serializer';
 export { clearPanel, switchToWorkflowParameters } from './state/panel/panelSlice';
 export { useSelectedNodeId } from './state/panel/panelSelectors';
