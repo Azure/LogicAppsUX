45bba68142cba0c53e4497d733c97ff70faca9b9
Riley Evans <rllyy97@gmail.com>
2023-04-11 11:56:41 -0500

    laFix: Azure Blob connector - Auth options fix (#1973)
    * Fixed issues
    
    * Tiny change

--- libs/designer-ui/src/lib/createConnection/universalConnectionParameter.tsx
+++ libs/designer-ui/src/lib/createConnection/universalConnectionParameter.tsx
@@ -57,14 +57,14 @@ export const UniversalConnectionParameter = (props: ConnectionParameterProps) =>
 
   // Dropdown Parameter
   else if ((constraints?.allowedValues?.length ?? 0) > 0) {
+    const selectedKey = constraints?.allowedValues?.findIndex((_value) => _value.text === value);
+    if (selectedKey === -1) setValue(constraints?.allowedValues?.[0].text);
     inputComponent = (
       <Dropdown
         id={`connection-param-${parameterKey}`}
         className="connection-parameter-input"
-        selectedKey={constraints?.allowedValues?.findIndex((_value) => _value.text === value)}
-        onChange={(e: any, newVal?: IDropdownOption) => {
-          setValue(newVal?.text);
-        }}
+        selectedKey={selectedKey}
+        onChange={(e: any, newVal?: IDropdownOption) => setValue(newVal?.text)}
         disabled={isLoading}
         ariaLabel={description}
         placeholder={description}
