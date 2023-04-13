c1a6301f914719076c32ab08c29ae416ac30d757
Reid Fortier <refortie@microsoft.com>
2023-04-13 14:32:56 -0700

    Try wrapping test

--- apps/data-mapper-e2e/src/general/GeneralTranscript.e2e.spec.ts
+++ apps/data-mapper-e2e/src/general/GeneralTranscript.e2e.spec.ts
@@ -1,30 +1,32 @@
 import { baseUrl } from '../utils';
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-test.skip('Data Mapper - General Transcript E2E', async ({ page }) => {
-  await page.goto(baseUrl);
+test.describe('Data Mapper - General Transcript E2E', () => {
+  test.skip('Make connections', async ({ page }) => {
+    await page.goto(baseUrl);
 
-  await page.getByText('Select a map definition').click();
+    await page.getByText('Select a map definition').click();
 
-  await page.getByRole('option', { name: 'Transcript' }).click();
+    await page.getByRole('option', { name: 'Transcript' }).click();
 
-  await page.getByTestId('rf__node-target-/ns0:Root/DirectTranslation').getByRole('button').nth(1).click();
+    await page.getByTestId('rf__node-target-/ns0:Root/DirectTranslation').getByRole('button').nth(1).click();
 
-  await page.getByTestId('rf__node-target-/ns0:Root/DirectTranslation/Employee').getByRole('button').nth(1).click();
+    await page.getByTestId('rf__node-target-/ns0:Root/DirectTranslation/Employee').getByRole('button').nth(1).click();
 
-  await page.getByTestId('rf__node-target-/ns0:Root/DirectTranslation/Employee/ID').getByRole('button').click();
+    await page.getByTestId('rf__node-target-/ns0:Root/DirectTranslation/Employee/ID').getByRole('button').click();
 
-  expect(page.getByTestId('inputDropdown-dropdown-0').locator('span:has-text("EmployeeID")')).toBeDefined();
-  await page.getByTestId('inputDropdown-dropdown-0').locator('span:has-text("EmployeeID")').click();
+    expect(page.getByTestId('inputDropdown-dropdown-0').locator('span:has-text("EmployeeID")')).toBeDefined();
+    await page.getByTestId('inputDropdown-dropdown-0').locator('span:has-text("EmployeeID")').click();
 
-  await page.getByRole('option', { name: 'Enter custom value' }).click();
+    await page.getByRole('option', { name: 'Enter custom value' }).click();
 
-  await page.getByTestId('inputDropdown-textField-0').click();
+    await page.getByTestId('inputDropdown-textField-0').click();
 
-  await page.getByTestId('inputDropdown-textField-0').fill('Hello');
+    await page.getByTestId('inputDropdown-textField-0').fill('Hello');
 
-  await page.getByRole('button', { name: 'Show code' }).click();
+    await page.getByRole('button', { name: 'Show code' }).click();
 
-  // TODO: Uncomment once entire Transcript map definition deserializes properly
-  //await expect(page.getByText('EmployeeName, string(Hello)')).toBeDefined();
+    // TODO: Uncomment once entire Transcript map definition deserializes properly
+    //await expect(page.getByText('EmployeeName, string(Hello)')).toBeDefined();
+  });
 });
