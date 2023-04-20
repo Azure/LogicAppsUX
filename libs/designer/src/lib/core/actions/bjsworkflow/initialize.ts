d1a3e4b9334a34591a8d2ce0342d3c9fbed6c5e4
ek68794998 <1350074+ek68794998@users.noreply.github.com>
2023-04-20 16:48:39 -0700

    Fix missing initializer

--- libs/designer/src/lib/core/actions/bjsworkflow/initialize.ts
+++ libs/designer/src/lib/core/actions/bjsworkflow/initialize.ts
@@ -347,6 +347,7 @@ export const updateCallbackUrl = async (rootState: RootState, dispatch: Dispatch
   if (updatedParameter) {
     dispatch(
       updateNodeParameters({
+        isUserAction: false,
         nodeId: trigger,
         parameters: [{ groupId: ParameterGroupKeys.DEFAULT, parameterId: updatedParameter.id, propertiesToUpdate: updatedParameter }],
       })
