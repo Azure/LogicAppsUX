84ff2de1e54468c318ef5b828d9f14f3da00adb4
preetriti1 <48265693+preetriti1@users.noreply.github.com>
2023-05-02 14:45:49 -0700

    fix(designer): Fixing the operation setting for async pattern in http manifest (#2207)
    Co-authored-by: Priti Sambandam <psamband@microsoft.com>

--- libs/services/designer-client-services/src/lib/base/manifests/http.ts
+++ libs/services/designer-client-services/src/lib/base/manifests/http.ts
@@ -150,7 +150,11 @@ export const httpManifest = {
       },
       operationOptions: {
         scopes: [SettingScope.Action],
-        options: [OperationOptions.Asynchronous, OperationOptions.DisableAutomaticDecompression, OperationOptions.SuppressWorkflowHeaders],
+        options: [
+          OperationOptions.DisableAsyncPattern,
+          OperationOptions.DisableAutomaticDecompression,
+          OperationOptions.SuppressWorkflowHeaders,
+        ],
       },
       paging: {
         scopes: [SettingScope.Action],
@@ -290,7 +294,11 @@ export const httpWithSwaggerManifest = {
       },
       operationOptions: {
         scopes: [SettingScope.Action],
-        options: [OperationOptions.Asynchronous, OperationOptions.DisableAutomaticDecompression, OperationOptions.SuppressWorkflowHeaders],
+        options: [
+          OperationOptions.DisableAsyncPattern,
+          OperationOptions.DisableAutomaticDecompression,
+          OperationOptions.SuppressWorkflowHeaders,
+        ],
       },
       requestOptions: {
         scopes: [SettingScope.Action],
