6298bc68c442dd60abd5f3270bfb8a87b5d2e151
Riley Evans <rllyy97@gmail.com>
2023-05-04 19:43:17 -0500

    Added WP dirty state

--- libs/designer/src/lib/core/index.ts
+++ libs/designer/src/lib/core/index.ts
@@ -9,6 +9,7 @@ export type { NodeInputs } from './state/operation/operationMetadataSlice';
 export { useOperationsInputParameters } from './state/operation/operationSelector';
 export { discardAllChanges, setFocusNode, setIsGraphLocked, setIsWorkflowDirty } from './state/workflow/workflowSlice';
 export { useIsWorkflowDirty, useNodeDisplayName } from './state/workflow/workflowSelectors';
+export { useIsDesignerDirty, resetDesignerDirtyState } from './state/global';
 export { serializeWorkflow } from './actions/bjsworkflow/serializer';
 export { changePanelNode, clearPanel, switchToWorkflowParameters } from './state/panel/panelSlice';
 export { useSelectedNodeId } from './state/panel/panelSelectors';
