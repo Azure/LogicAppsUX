367f7b2bd58d1fde5b169dd5be87500e7d83faa1
Jaime E Reyes Salazar <jaimereyesmx@gmail.com>
2023-05-08 14:08:30 -0700

    fix(designer): adding unit tests for respecting workflow dimensions logic (#2278)
    * designer(fix): pass the right parameters to respect node dimensions
    
    * fix(designer): adding unit tests for respecting workflow dimensions logic
    
    ---------
    
    Co-authored-by: Jaime Reyes <jasalaza@microsoft.com>

--- libs/designer/src/lib/core/parsers/ParseReduxAction.ts
+++ libs/designer/src/lib/core/parsers/ParseReduxAction.ts
@@ -56,7 +56,7 @@ export const initializeGraphState = createAsyncThunk<
   throw new Error('Invalid Workflow Spec');
 });
 
-function updateChildrenDimensions(currentChildren: WorkflowNode[], previousChildren: WorkflowNode[]) {
+export function updateChildrenDimensions(currentChildren: WorkflowNode[], previousChildren: WorkflowNode[]) {
   for (const node of currentChildren) {
     const previousNode = previousChildren.find((item) => item.id === node.id);
     if (previousNode?.height && previousNode?.width) {
@@ -67,7 +67,7 @@ function updateChildrenDimensions(currentChildren: WorkflowNode[], previousChild
   }
 }
 
-function flattenWorkflowNodes(nodes: WorkflowNode[]): WorkflowNode[] {
+export function flattenWorkflowNodes(nodes: WorkflowNode[]): WorkflowNode[] {
   const result: WorkflowNode[] = [];
 
   for (const node of nodes) {
