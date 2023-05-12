c6a8a3feb239d2803d9cc5eb65d2a2644e9bb914
Sami Karakra <samikarakra@microsoft.com>
2023-05-12 13:16:21 -0700

    removed print statements

--- apps/vs-code-designer/src/app/commands/createNewProject/createProjectSteps/FolderListStep.ts
+++ apps/vs-code-designer/src/app/commands/createNewProject/createProjectSteps/FolderListStep.ts
@@ -8,21 +8,17 @@ export class FolderListStep extends AzureWizardPromptStep<IProjectWizardContext>
   public hideStepCount = true;
 
   public static setProjectPath(context: Partial<IProjectWizardContext>, projectPath: string): void {
-    console.log('Before setting project path:', context.projectPath);
     context.projectPath = projectPath;
     context.workspaceFolder = getContainingWorkspace(projectPath);
     context.workspacePath = (context.workspaceFolder && context.workspaceFolder.uri.fsPath) || projectPath;
     if (context.workspaceFolder) {
       context.openBehavior = OpenBehavior.alreadyOpen;
     }
-    console.log('After setting project path:', context.projectPath);
   }
 
   public async prompt(context: IProjectWizardContext): Promise<void> {
     const placeHolder: string = localize('selectNewProjectFolder', 'Select the folder that will contain your workflow project');
-    console.log('Before setting project path in prompt:', context.projectPath);
     FolderListStep.setProjectPath(context, await selectWorkspaceFolder(context, placeHolder));
-    console.log('After setting project path in prompt:', context.projectPath);
   }
 
   public shouldPrompt(context: IProjectWizardContext): boolean {
