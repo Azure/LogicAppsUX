4b8582879943bb12f71c6b149823889d891622ed
e.k <1350074+ek68794998@users.noreply.github.com>
2023-06-12 15:59:13 -0700

    feat(designer): Add 'search' function for conditional param dropdown (#2718)
    * Add 'search' function for conditional param dropdown
    
    * Remove unused import
    
    * Run check-in scripts
    
    * Refactor conditional parameter dropdown + tests
    
    * Run hooks
    
    * Fix import order
    
    * Rename parameters dropdown
    
    * Refactor searchable dropdown into UI component
    
    * Run hooks
    
    * Remove superfluous component
    
    * Fix a few more references to parameters

---
+++ libs/designer-ui/src/lib/searchabledropdown/index.tsx
@@ -0,0 +1,82 @@
+import type { IDropdownProps } from '@fluentui/react';
+import { SearchBox, DropdownMenuItemType, Dropdown } from '@fluentui/react';
+import type { FC } from 'react';
+import { useState } from 'react';
+import { useIntl } from 'react-intl';
+
+export interface SearchableDropdownProps {
+  dropdownProps: Pick<IDropdownProps, 'options'> & Partial<Omit<IDropdownProps, 'onChange' | 'onDismiss' | 'onRenderItem'>>;
+  onItemSelectionChanged: (id: string, isSelected: boolean) => void;
+  searchPlaceholderText?: string;
+  showSearchItemThreshold?: number;
+}
+
+export const SearchableDropdown: FC<SearchableDropdownProps> = ({
+  dropdownProps,
+  onItemSelectionChanged,
+  searchPlaceholderText,
+  showSearchItemThreshold: showFilterItemThreshold,
+}): JSX.Element => {
+  const showFilterInputItemThreshold = showFilterItemThreshold ?? 4;
+  const headerKey = 'FilterHeader';
+
+  const intl = useIntl();
+
+  const [conditionalVisibilityTempArray, setConditionalVisibilityTempArray] = useState<string[]>([]);
+  const [filterText, setFilterText] = useState('');
+
+  const searchOperation =
+    searchPlaceholderText ??
+    intl.formatMessage({
+      defaultMessage: 'Search',
+      description: 'Default placeholder for search box that searches dropdown options',
+    });
+
+  const options = dropdownProps.options.filter((option) => option.text.toLowerCase().includes(filterText.toLowerCase()));
+
+  if (dropdownProps.options.length >= showFilterInputItemThreshold) {
+    options.unshift(
+      { key: headerKey, text: '', itemType: DropdownMenuItemType.Header },
+      { key: 'FilterDivider', text: '-', itemType: DropdownMenuItemType.Divider }
+    );
+  }
+
+  return (
+    <Dropdown
+      {...dropdownProps}
+      className="msla-searchable-dropdown"
+      options={options}
+      selectedKeys={conditionalVisibilityTempArray}
+      onChange={(_e: any, item: any) => {
+        if (item?.key) {
+          setConditionalVisibilityTempArray(
+            conditionalVisibilityTempArray.includes(item.key)
+              ? conditionalVisibilityTempArray.filter((key) => key !== item.key)
+              : [...conditionalVisibilityTempArray, item.key]
+          );
+        }
+      }}
+      onDismiss={() => {
+        conditionalVisibilityTempArray.forEach((parameterId) => {
+          onItemSelectionChanged(parameterId, true);
+        });
+        setConditionalVisibilityTempArray([]);
+        setFilterText('');
+      }}
+      onRenderItem={(item, defaultRenderer) => {
+        if (item?.key === headerKey) {
+          return (
+            <SearchBox
+              autoFocus={true}
+              className="msla-searchable-dropdown-search"
+              onChange={(e, newValue) => setFilterText(newValue ?? '')}
+              placeholder={searchOperation}
+            />
+          );
+        }
+
+        return defaultRenderer?.(item) ?? null;
+      }}
+    />
+  );
+};
