e8b67cee35d723a40b4087a2e046b6f6c2a66f5a
e.k <1350074+ek68794998@users.noreply.github.com>
2023-06-19 16:29:25 -0700

    fix(designer): Prevent panel from closing if `intl` is updated (#2780)
    * Fix designer provider to use `useCallback` so as to not update `intl`

--- libs/designer/src/lib/core/DesignerProvider.tsx
+++ libs/designer/src/lib/core/DesignerProvider.tsx
@@ -7,8 +7,9 @@ import { AzureThemeDark } from '@fluentui/azure-themes/lib/azure/AzureThemeDark'
 import { AzureThemeLight } from '@fluentui/azure-themes/lib/azure/AzureThemeLight';
 import { ThemeProvider } from '@fluentui/react';
 import { FluentProvider, webDarkTheme, webLightTheme } from '@fluentui/react-components';
+import type { OnErrorFn as OnIntlErrorFn } from '@formatjs/intl';
 import { IntlProvider } from '@microsoft/intl-logic-apps';
-import React, { useEffect, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import { Provider as ReduxProvider, useDispatch } from 'react-redux';
 
 export interface DesignerProviderProps {
@@ -39,6 +40,12 @@ export const DesignerProvider = ({ locale = 'en', options, children }: DesignerP
   const azTheme = !isDarkMode ? AzureThemeLight : AzureThemeDark;
   const webTheme = !isDarkMode ? webLightTheme : webDarkTheme;
   const themeName = useMemo(() => (!isDarkMode ? 'light' : 'dark'), [isDarkMode]);
+  const onError = useCallback<OnIntlErrorFn>((err) => {
+    if (err.code === 'MISSING_TRANSLATION') {
+      return;
+    }
+    throw err;
+  }, []);
 
   return (
     <ReduxProvider store={store}>
@@ -48,16 +55,7 @@ export const DesignerProvider = ({ locale = 'en', options, children }: DesignerP
             <FluentProvider theme={webTheme}>
               <div data-color-scheme={themeName} className={`msla-theme-${themeName}`} style={{ height: '100vh', overflow: 'hidden' }}>
                 <ReactQueryProvider>
-                  <IntlProvider
-                    locale={locale}
-                    defaultLocale={locale}
-                    onError={(err) => {
-                      if (err.code === 'MISSING_TRANSLATION') {
-                        return;
-                      }
-                      throw err;
-                    }}
-                  >
+                  <IntlProvider locale={locale} defaultLocale={locale} onError={onError}>
                     {children}
                   </IntlProvider>
                 </ReactQueryProvider>
