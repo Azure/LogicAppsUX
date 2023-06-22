52741e2415b5245b9b25bc72dcd855261b688d53
Riley Evans <rllyy97@gmail.com>
2023-06-22 12:28:49 -0500

    fix(Designer): Removed duplicate service calls (#2806)
    * Fixed first issue of duplicate manifest requests
    
    * Removed duplicate query provider
    
    * Fixed double cache issue
    
    * Fixed tests
    
    * Adjusted some vscode code
    
    * Reverted some vscode changes

--- apps/designer-standalone/src/app/DesignerShell/designer.tsx
+++ apps/designer-standalone/src/app/DesignerShell/designer.tsx
@@ -3,38 +3,20 @@ import { useIsConsumption, useIsLocal, useResourcePath } from '../../state/workf
 import LogicAppsDesignerStandard from '../AzureLogicAppsDesigner/laDesigner';
 import LogicAppsDesignerConsumption from '../AzureLogicAppsDesigner/laDesignerConsumption';
 import { LocalDesigner } from '../LocalDesigner/localDesigner';
-import { ReactQueryProvider } from '@microsoft/logic-apps-designer';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { getReactQueryClient } from '@microsoft/logic-apps-designer';
+import { QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchInterval: false,
-      refetchIntervalInBackground: false,
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      refetchOnMount: false,
-    },
-  },
-});
+const standaloneQueryClient = getReactQueryClient();
 
-const InnerDesigner = () => {
+export const DesignerWrapper = () => {
   const resourcePath = useResourcePath();
   const isLocal = useIsLocal();
   const isConsumption = useIsConsumption();
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={standaloneQueryClient}>
       <SettingsBox />
       {isLocal ? <LocalDesigner /> : resourcePath ? isConsumption ? <LogicAppsDesignerConsumption /> : <LogicAppsDesignerStandard /> : null}
     </QueryClientProvider>
-  );
-};
-
-export const DesignerWrapper = () => {
-  return (
-    <ReactQueryProvider>
-      <InnerDesigner />
-    </ReactQueryProvider>
   );
 };
