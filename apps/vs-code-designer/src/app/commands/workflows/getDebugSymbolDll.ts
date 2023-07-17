e403aaf1a384983ae0d22f552bcf31e4192f0901
ccastrotrejo <ccastrotrejo@microsoft.com>
2023-07-17 10:50:43 -0700

    Add commmand to download extension bundle

--- apps/vs-code-designer/src/app/commands/workflows/getDebugSymbolDll.ts
+++ apps/vs-code-designer/src/app/commands/workflows/getDebugSymbolDll.ts
@@ -5,6 +5,7 @@
 import { debugSymbolDll } from '../../../constants';
 import { ext } from '../../../extensionVariables';
 import { localize } from '../../../localize';
+import { getWorkspaceSetting } from '../../utils/vsCodeConfig/settings';
 import * as cp from 'child_process';
 import * as fse from 'fs-extra';
 import * as path from 'path';
@@ -34,7 +35,8 @@ export async function getDebugSymbolDll(): Promise<string> {
  * @returns {string} Extension bundle folder path.
  */
 async function getExtensionBundleFolder(): Promise<string> {
-  const command = 'func GetExtensionBundlePath';
+  const funcBinariesLocation = getWorkspaceSetting<string>('funcCoreToolsPath');
+  const command = `${funcBinariesLocation} GetExtensionBundlePath`;
   const outputChannel = ext.outputChannel;
 
   if (outputChannel) {
