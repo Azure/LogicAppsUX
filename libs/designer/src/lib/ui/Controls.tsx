269973e0ef6a78426c31cea1eed45adb2167ed1d
Travis Harris <hartra344@users.noreply.github.com>
2023-07-24 17:59:27 -0700

    feat(designer): Better surface workflow search as a button (#3025)

--- libs/designer/src/lib/ui/Controls.tsx
+++ libs/designer/src/lib/ui/Controls.tsx
@@ -1,5 +1,6 @@
 import { useShowMinimap } from '../core/state/designerView/designerViewSelectors';
 import { toggleMinimap } from '../core/state/designerView/designerViewSlice';
+import { switchToNodeSearchPanel } from '../core/state/panel/panelSlice';
 import { Icon, useTheme } from '@fluentui/react';
 import { useIntl } from 'react-intl';
 import { useDispatch } from 'react-redux';
@@ -15,15 +16,27 @@ const CustomControls = () => {
     dispatch(toggleMinimap());
   };
 
+  const searchToggleClick = () => {
+    dispatch(switchToNodeSearchPanel());
+  };
+
   const minimapAria = intl.formatMessage({
     defaultMessage: 'Toggle Minimap',
     description: 'Turn the minimap on or off',
+  });
+
+  const searchAria = intl.formatMessage({
+    defaultMessage: 'Search Workflow Actions',
+    description: 'Aria label for a button that opens a search panel to search the actions in the users workflow',
   });
 
   const iconStyles = { root: { color: showMinimap ? '#1F85FF' : isInverted ? '#FFFFFF' : '#000000' } };
 
   return (
     <Controls showInteractive={false}>
+      <ControlButton aria-label={searchAria} title={searchAria} onClick={searchToggleClick}>
+        <Icon iconName={'Search'} styles={iconStyles} />
+      </ControlButton>
       <ControlButton aria-label={minimapAria} title={minimapAria} onClick={minimapToggleClick}>
         <Icon iconName={'Nav2DMapView'} styles={iconStyles} />
       </ControlButton>
