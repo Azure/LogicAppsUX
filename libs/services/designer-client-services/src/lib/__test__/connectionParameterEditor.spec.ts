0702175652c063bab7ab52bc2d9ed309b527aeab
Cédric Golmard <92312794+cdgolmar-msft@users.noreply.github.com>
2023-08-02 01:10:02 -0700

    feat(designer): Add IConnectionParameterEditorService to support customization of connections fields (#3038)
    * feat(designer): Add IConnectionParameterEditorService to support customization of connections fields
    
    * feat(designer) factorize ConnectionParameterRow for reuse
    
    * feat(designer): add tests for ConnectionParameterRow

---
+++ libs/services/designer-client-services/src/lib/__test__/connectionParameterEditor.spec.ts
@@ -0,0 +1,25 @@
+import {
+  ConnectionParameterEditorService,
+  InitConnectionParameterEditorService,
+  type IConnectionParameterInfo,
+} from '../connectionParameterEditor';
+
+describe('lib/designer-client-services/connectionParameterEditor', () => {
+  it('should call "getConnectionParameterEditor" with initialized service instance', () => {
+    const service = {
+      getConnectionParameterEditor: jest.fn(),
+    };
+
+    expect(ConnectionParameterEditorService()).toBeUndefined();
+
+    InitConnectionParameterEditorService(service);
+
+    const parameter: IConnectionParameterInfo = {
+      connectorId: 'connectorId',
+      parameterKey: 'parameterKey',
+    };
+    ConnectionParameterEditorService()?.getConnectionParameterEditor(parameter);
+
+    expect(service.getConnectionParameterEditor).toHaveBeenCalledWith(parameter);
+  });
+});
