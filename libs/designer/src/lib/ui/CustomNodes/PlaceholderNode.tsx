f29b4f1fb225ddf0fbf1e6a12546f7f71de4620b
Carlos Emiliano Castro Trejo <102700317+ccastrotrejo@users.noreply.github.com>
2023-08-22 09:08:10 -0700

    feat(designer): Add no actions node for is read only mode (#3173)
    * Add no actions node component
    
    * Update styles
    
    * Reduce css and tags

--- libs/designer/src/lib/ui/CustomNodes/PlaceholderNode.tsx
+++ libs/designer/src/lib/ui/CustomNodes/PlaceholderNode.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import type { AppDispatch } from '../../core';
+import { useReadOnly } from '../../core/state/designerOptions/designerOptionsSelectors';
 import { useIsNodeSelected } from '../../core/state/panel/panelSelectors';
 import { expandDiscoveryPanel } from '../../core/state/panel/panelSlice';
-import { AddActionCard, ADD_CARD_TYPE } from '@microsoft/designer-ui';
+import { AddActionCard, ADD_CARD_TYPE, NoActionCard } from '@microsoft/designer-ui';
 import { guid } from '@microsoft/utils-logic-apps';
 import { memo, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
@@ -13,6 +14,7 @@ const PlaceholderNode = ({ targetPosition = Position.Top, sourcePosition = Posit
   const dispatch = useDispatch<AppDispatch>();
 
   const selected = useIsNodeSelected(id);
+  const isReadOnly = useReadOnly();
 
   const openAddNodePanel = useCallback(() => {
     const newId = guid();
@@ -20,10 +22,11 @@ const PlaceholderNode = ({ targetPosition = Position.Top, sourcePosition = Posit
     dispatch(expandDiscoveryPanel({ nodeId: newId, relationshipIds, addingTrigger: true }));
   }, [dispatch]);
 
+  console.log('charlie', isReadOnly);
   return (
     <div>
       <Handle className="node-handle top" type="target" position={targetPosition} isConnectable={false} />
-      <AddActionCard addCardType={ADD_CARD_TYPE.TRIGGER} onClick={openAddNodePanel} selected={selected} />
+      {isReadOnly ? <NoActionCard /> : <AddActionCard addCardType={ADD_CARD_TYPE.TRIGGER} onClick={openAddNodePanel} selected={selected} />}
       <Handle className="node-handle bottom" type="source" position={sourcePosition} isConnectable={false} />
     </div>
   );
