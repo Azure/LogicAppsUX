ea74ca51eba2eb3f1b326bc74e3a0365181edf1d
Carlos Emiliano Castro Trejo <102700317+ccastrotrejo@users.noreply.github.com>
2023-08-29 11:33:36 -0700

    fix(designer): Fix search service operations (#3212)
    Fix search service

--- libs/designer/src/lib/ui/panel/recommendation/searchView.tsx
+++ libs/designer/src/lib/ui/panel/recommendation/searchView.tsx
@@ -32,8 +32,13 @@ export const SearchView: React.FC<SearchViewProps> = (props) => {
 
   useDebouncedEffect(
     () => {
-      const searchOperations = SearchService().searchOperations ?? new DefaultSearchOperationsService(allOperations).searchOperations;
-      searchOperations(searchTerm, filters['actionType'], filters['runtime']).then((results) => {
+      const searchService = SearchService && SearchService().searchOperations;
+
+      const searchResultsPromise = searchService
+        ? searchService(searchTerm, filters['actionType'], filters['runtime'])
+        : new DefaultSearchOperationsService(allOperations).searchOperations(searchTerm, filters['actionType'], filters['runtime']);
+
+      searchResultsPromise.then((results) => {
         setSearchResults(results);
         setIsLoadingSearchResults(false);
       });
