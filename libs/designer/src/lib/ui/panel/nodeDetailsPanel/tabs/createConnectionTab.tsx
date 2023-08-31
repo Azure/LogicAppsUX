1459e0a07275e0d519f5b3f7f93520cfbac5a4b6
Riley Evans <rllyy97@gmail.com>
2023-08-31 10:22:05 -0500

    fix(Designer): OAuth - Passed redirect url value from oauth setting in connection parameters (#3224)
    Passed redirect url value from oauth setting

--- libs/designer/src/lib/ui/panel/nodeDetailsPanel/tabs/createConnectionTab.tsx
+++ libs/designer/src/lib/ui/panel/nodeDetailsPanel/tabs/createConnectionTab.tsx
@@ -138,6 +138,19 @@ const CreateConnectionTab = () => {
           outputParameterValues = { ...outputParameterValues, ...assistedParams };
         }
 
+        // If oauth, find the oauth parameter and assign the redirect url
+        if (isOAuthConnection && selectedParameterSet) {
+          const oAuthParameter = Object.entries(selectedParameterSet?.parameters).find(
+            ([_, parameter]) => !!parameter?.oAuthSettings?.redirectUrl
+          );
+          if (oAuthParameter) {
+            const oAuthParameterKey = oAuthParameter?.[0];
+            const oAuthParameterObj = oAuthParameter?.[1];
+            const redirectUrl = oAuthParameterObj?.oAuthSettings?.redirectUrl;
+            outputParameterValues[oAuthParameterKey] = redirectUrl;
+          }
+        }
+
         const connectionParameterSetValues: ConnectionParameterSetValues = {
           name: selectedParameterSet?.name ?? '',
           values: Object.keys(outputParameterValues).reduce((acc: any, key) => {
