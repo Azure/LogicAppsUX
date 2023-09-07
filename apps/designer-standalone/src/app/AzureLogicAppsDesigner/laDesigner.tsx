5670ec85973a9e8373147331a099539079928d2c
ccastrotrejo <ccastrotrejo@microsoft.com>
2023-09-07 14:08:24 -0700

    Resolve connection references in standalone

--- apps/designer-standalone/src/app/AzureLogicAppsDesigner/laDesigner.tsx
+++ apps/designer-standalone/src/app/AzureLogicAppsDesigner/laDesigner.tsx
@@ -68,8 +68,7 @@ const DesignerEditor = () => {
   const { data: tenantId } = useCurrentTenantId();
   const [designerID, setDesignerID] = React.useState(guid());
   const [workflow, setWorkflow] = React.useState(data?.properties.files[Artifact.WorkflowFile]);
-  const connectionsData = data?.properties.files[Artifact.ConnectionsFile] ?? {};
-  const connectionReferences = WorkflowUtility.convertConnectionsDataToReferences(connectionsData);
+  const originalConnectionsData = data?.properties.files[Artifact.ConnectionsFile] ?? {};
   const parameters = data?.properties.files[Artifact.ParametersFile] ?? {};
   const queryClient = getReactQueryClient();
 
@@ -83,6 +82,16 @@ const DesignerEditor = () => {
     }
   };
   const { data: runInstanceData } = useRunInstanceStandard(workflowName, onRunInstanceSuccess, appId, runId);
+
+  const connectionsData = React.useMemo(
+    () =>
+      WorkflowUtility.resolveConnectionsReferences(
+        JSON.stringify(clone(originalConnectionsData ?? {})),
+        parameters,
+        settingsData?.properties ?? {}
+      ),
+    [originalConnectionsData, parameters, settingsData?.properties]
+  );
 
   const addConnectionData = async (connectionAndSetting: ConnectionAndAppSetting): Promise<void> => {
     addConnectionInJson(connectionAndSetting, connectionsData ?? {});
@@ -114,6 +123,8 @@ const DesignerEditor = () => {
 
     return undefined;
   };
+
+  const connectionReferences = WorkflowUtility.convertConnectionsDataToReferences(connectionsData);
 
   const discardAllChanges = () => {
     setDesignerID(guid());
@@ -156,7 +167,6 @@ const DesignerEditor = () => {
   }
 
   const originalSettings: Record<string, string> = { ...(settingsData?.properties ?? {}) };
-  const originalConnectionsData: ConnectionsData = clone(connectionsData ?? {});
   const originalParametersData: ParametersData = clone(parameters ?? {});
 
   if (isError || settingsIsError) {
