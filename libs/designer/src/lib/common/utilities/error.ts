3a8467597c79a8f8e2fe5c695d13b3f6b91823a8
Carlos Emiliano Castro Trejo <102700317+ccastrotrejo@users.noreply.github.com>
2023-09-14 09:00:09 -0700

    fix(designer): Catch scenario when error object has an error object (#3313)
    Catch scenario when error object has an error object

--- libs/designer/src/lib/common/utilities/error.ts
+++ libs/designer/src/lib/common/utilities/error.ts
@@ -9,15 +9,20 @@ export interface ErrorProps {
   message?: string;
 }
 
+export interface ErrorRun {
+  code: string;
+  message: string;
+}
+
 /**
  * Translates a monitoring view `errors` prop into designer error props.
- * @arg {{ code: string; message: string }} errorRun - Operation error run object.
+ * @arg {{ error: ErrorRun} | ErrorRun  | undefined} errorRun - Operation error run object.
  * @arg {string} statusRun - The operation status, e.g., Failed, Succeeded.
  * @arg {string} codeRun - The operation code.
  * @return {ErrorProps | undefined}
  */
 export function getMonitoringError(
-  errorRun: { code: string; message: string } | undefined,
+  errorRun: { error: ErrorRun } | ErrorRun | undefined,
   statusRun: string | undefined,
   codeRun: string | undefined
 ): ErrorProps {
@@ -30,7 +35,7 @@ export function getMonitoringError(
     };
   }
 
-  const { code, message } = errorRun ?? { code: codeRun, message: '' };
+  const { code, message } = errorRun ? ('error' in errorRun ? errorRun.error : errorRun) : { code: codeRun, message: '' };
 
   let errorLevel: MessageBarType;
   if (statusRun === constants.FLOW_STATUS.SKIPPED) {
