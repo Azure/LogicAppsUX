c305ee84f6047014c6662cacbcf9fc4d805fc1b3
Eric-B-Wu <wue@microsoft.com>
2023-10-06 13:23:07 -0400

    update

--- libs/designer-ui/src/lib/modaldialog/index.tsx
+++ libs/designer-ui/src/lib/modaldialog/index.tsx
@@ -19,11 +19,12 @@ export interface ModalDialogProps {
 const modalProps: IModalProps = {
   className: 'msla-modal-dialog',
 };
-const dialogContentProps: IDialogContentProps = {
-  type: DialogType.close,
-};
 
 export const ModalDialog = ({ confirmText, isOpen, title, children, getStyles, onConfirm, onDismiss }: ModalDialogProps): JSX.Element => {
+  const dialogContentProps: IDialogContentProps = {
+    type: DialogType.close,
+    title,
+  };
   const handleConfirm = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
     onConfirm(e);
   };
@@ -35,14 +36,7 @@ export const ModalDialog = ({ confirmText, isOpen, title, children, getStyles, o
     e.stopPropagation();
   };
   return (
-    <Dialog
-      dialogContentProps={dialogContentProps}
-      hidden={!isOpen}
-      modalProps={modalProps}
-      styles={getStyles}
-      title={title}
-      onDismiss={handleDismiss}
-    >
+    <Dialog dialogContentProps={dialogContentProps} hidden={!isOpen} modalProps={modalProps} styles={getStyles} onDismiss={handleDismiss}>
       <div className="msla-modal-content" onClick={handleModalBodyClick}>
         <div className="msla-modal-body">{children}</div>
       </div>
