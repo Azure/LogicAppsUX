33aea13d9bc2fe5dc596750e3f073341c0c60bb0
DanielleCogs <99753874+DanielleCogs@users.noreply.github.com>
2023-10-12 23:21:19 +0200

    feat(data-mapper): create unique ID for function and load position (#3432)
    * created unique identifier for functions metadata
    
    * able to save and load positions
    
    * pr prep
    
    * more PR cleanup

--- libs/data-mapper/src/lib/mapDefinitions/MapMetadataSerializer.ts
+++ libs/data-mapper/src/lib/mapDefinitions/MapMetadataSerializer.ts
@@ -1,17 +1,18 @@
 import type { FunctionDictionary } from '../models';
 import type { ConnectionDictionary } from '../models/Connection';
-import type { FunctionMetadata, MapMetadata } from '../models/MapMetadata';
-import { isFunctionData } from '../utils/Function.Utils';
+import type { ConnectionAndOrder, FunctionMetadata, MapMetadata } from '../models/MapMetadata';
 
 export const generateMapMetadata = (functionDictionary: FunctionDictionary, connections: ConnectionDictionary): MapMetadata => {
   const functionMetadata: FunctionMetadata[] = [];
 
   Object.entries(functionDictionary).forEach(([functionKey, functionValue]) => {
+    const connectionIdMetadata = generateFunctionConnectionMetadata(functionKey, connections);
     functionMetadata.push({
       reactFlowGuid: functionKey,
       functionKey: functionValue.functionData.key,
       positions: functionValue.functionData.positions || [],
-      connections: generateFunctionConnectionMetadata(functionKey, connections),
+      connections: connectionIdMetadata,
+      connectionShorthand: convertConnectionShorthandToId(connectionIdMetadata),
     });
   });
 
@@ -20,16 +21,42 @@ export const generateMapMetadata = (functionDictionary: FunctionDictionary, conn
   };
 };
 
-export const generateFunctionConnectionMetadata = (connectionKey: string, connections: ConnectionDictionary): string[] => {
+export const convertConnectionShorthandToId = (connectionArr: ConnectionAndOrder[]): string => {
+  let ans = '';
+  connectionArr.forEach((obj) => {
+    ans = ans + `${obj.inputOrder}-${obj.name},`;
+  });
+  return ans;
+};
+
+export const generateFunctionConnectionMetadata = (connectionKey: string, connections: ConnectionDictionary): ConnectionAndOrder[] => {
   const connection = connections[connectionKey];
 
   if (connection) {
-    const isFunctionNode = isFunctionData(connection.self.node);
-
-    const results: string[] = [isFunctionNode ? connectionKey : connection.self.node.key];
-    connection.outputs.forEach((output) => {
-      results.push(generateFunctionConnectionMetadata(output.reactFlowKey, connections).join());
-    });
+    const results: ConnectionAndOrder[] = [];
+    if (connection.outputs[0] === undefined) {
+      return results;
+    }
+    const firstOutputKey = connection.outputs[0].reactFlowKey;
+    let index = 0;
+    const firstOutputObj = connections[firstOutputKey];
+    const outputsInput = firstOutputObj.inputs;
+    while (outputsInput[index.toString()]) {
+      const possibleMatchingInput = outputsInput[index.toString()][0];
+      if (possibleMatchingInput && typeof possibleMatchingInput !== 'string' && possibleMatchingInput.reactFlowKey === connectionKey) {
+        const connAndOrder: ConnectionAndOrder = {
+          name: firstOutputKey,
+          inputOrder: index,
+        };
+        if ('functionName' in firstOutputObj.self.node) {
+          connAndOrder.name = firstOutputObj.self.node.functionName;
+        }
+        results.push(connAndOrder);
+        results.push(...generateFunctionConnectionMetadata(firstOutputKey, connections));
+        break;
+      }
+      index++;
+    }
 
     return results;
   }
