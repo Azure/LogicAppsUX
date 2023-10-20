f89380db38431b27d3d4322d8c1752566a6f9121
Taylor Brazelton (from Dev Box) <tbrazelton@microsoft.com>
2023-10-20 15:24:38 -0700

    Fix to sort items alphabetically. ASC.

--- libs/designer-ui/src/lib/combobox/index.tsx
+++ libs/designer-ui/src/lib/combobox/index.tsx
@@ -116,6 +116,9 @@ export const Combobox = ({
         ? [errorOption]
         : options.filter((option) => new RegExp(searchValue.replace(/\\/g, '').toLowerCase()).test(option.displayName.toLowerCase()));
 
+      // Sort newOptions array alphabetically based on the `key` property.
+      newOptions.sort((currentItem, nextItem) => currentItem.displayName.localeCompare(nextItem.displayName));
+
       if (newOptions.length === 0) {
         const noValuesLabel = intl.formatMessage({
           defaultMessage: 'No values match your search.',
