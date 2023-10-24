469d23c34b22c10ff13702ec3079ff7f04ef13c5
Alan Kai <12243528+YogiJogiFresh@users.noreply.github.com>
2023-10-24 17:16:00 -0400

    Rename to timeOutErrorOperation

--- apps/vs-code-designer/src/app/utils/timeout.ts
+++ apps/vs-code-designer/src/app/utils/timeout.ts
@@ -2,15 +2,24 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { delay } from './delay';
-
 export async function timeout(asyncFunc: (...params: any[]) => Promise<void>, timeoutMs: number, ...params: any[]): Promise<void> {
   try {
     const asyncOperation = asyncFunc(...params);
-    await Promise.race([asyncOperation, delay(timeoutMs)]);
+    await Promise.race([asyncOperation, timeOutErrorOperation(timeoutMs)]);
 
     return await asyncOperation;
   } catch (error) {
     throw new Error(`${asyncFunc.name} timed out after ${timeoutMs} ms.`);
   }
+}
+
+/**
+ * Sets a timeout and throws an error if timeout.
+ */
+async function timeOutErrorOperation(ms: number): Promise<void> {
+  return await new Promise<void>((_, reject) => {
+    setTimeout(() => {
+      reject(new Error());
+    }, ms);
+  });
 }
