7258393f8103c2fed4c9f96a638e97233c24ae6b
samikay101 <samikarakra@microsoft.com>
2023-10-26 09:51:23 -0700

    added logging to file management class

--- apps/vs-code-designer/src/app/commands/generateDeploymentScripts/iacGestureHelperFunctions.ts
+++ apps/vs-code-designer/src/app/commands/generateDeploymentScripts/iacGestureHelperFunctions.ts
@@ -2,6 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import { ext } from '../../../extensionVariables';
 import * as vscode from 'vscode';
 
 export class FileManagement {
@@ -11,17 +12,24 @@ export class FileManagement {
    */
   public static addFolderToWorkspace(folderPath: string): void {
     try {
+      ext.outputChannel.appendLog(`Adding folder to workspace: ${folderPath}`);
+
       const uri = vscode.Uri.file(folderPath);
       const existingFolders = vscode.workspace.workspaceFolders || [];
-
-      // Check if the folder is already in the workspace
       const isAlreadyInWorkspace = existingFolders.some((folder) => folder.uri.fsPath === folderPath);
 
-      // Add the folder to the workspace if it's not already there
       if (!isAlreadyInWorkspace) {
-        vscode.workspace.updateWorkspaceFolders(0, null, { uri });
+        const result = vscode.workspace.updateWorkspaceFolders(0, null, { uri });
+        if (result) {
+          ext.outputChannel.appendLog(`Folder added successfully: ${folderPath}`);
+        } else {
+          ext.outputChannel.appendLog(`Failed to add folder to workspace (updateWorkspaceFolders returned false): ${folderPath}`);
+        }
+      } else {
+        ext.outputChannel.appendLog(`Folder is already in the workspace: ${folderPath}`);
       }
     } catch (error) {
+      ext.outputChannel.appendLog(`Error in addFolderToWorkspace: ${error}`);
       vscode.window.showErrorMessage('Failed to add folder to workspace: ' + error.message);
     }
   }
@@ -30,25 +38,28 @@ export class FileManagement {
    * Converts a directory to a valid multi root workspace.
    * @param targetDirectory - The directory to be converted.
    */
+
   public static convertToValidWorkspace(targetDirectory: string): void {
     try {
+      ext.outputChannel.appendLog(`Converting directory to valid workspace: ${targetDirectory}`);
       const workspaceFolders = vscode.workspace.workspaceFolders;
       const folderPaths = workspaceFolders?.map((folder) => folder.uri.fsPath) || [];
 
-      // Check if the target directory is already a workspace folder
       if (folderPaths.includes(targetDirectory)) {
+        ext.outputChannel.appendLog(`Directory is already a workspace folder: ${targetDirectory}`);
         return;
       }
 
-      // Add the target directory as the root workspace folder
       folderPaths.unshift(targetDirectory);
-
-      // Update the workspace folders with the new configuration
       const added = vscode.workspace.updateWorkspaceFolders(0, null, ...folderPaths.map((path) => ({ uri: vscode.Uri.file(path) })));
+
       if (!added) {
         throw new Error(workspaceFolders ? 'Failed to add folder to workspace' : 'Failed to create workspace');
+      } else {
+        ext.outputChannel.appendLog(`Workspace folders updated successfully with new directory: ${targetDirectory}`);
       }
     } catch (error) {
+      ext.outputChannel.appendLog(`Error in convertToValidWorkspace: ${error}`);
       vscode.window.showErrorMessage('Failed to convert to valid workspace: ' + error.message);
     }
   }
