21f759f95dafe0a6082f60a862d895231d138345
Valentina Vallalta <vvallalta@hmc.edu>
2023-10-30 11:53:37 -0700

    adding new text

--- libs/designer-ui/src/lib/chatbot/components/assistantGreeting.tsx
+++ libs/designer-ui/src/lib/chatbot/components/assistantGreeting.tsx
@@ -16,17 +16,57 @@ export const AssistantGreeting = ({ item }: { item: AssistantGreetingItem }) => 
       defaultMessage: 'Welcome back! If you want me to change your flow, just say what you want. For example:',
       description: 'Chatbot greeting message from existing flow',
     }),
+    saveYourFlow: intl.formatMessage({
+      defaultMessage: 'Check the flow’s actions to see if any parameters need to be set. Don’t forget to save when you’re done!',
+      description: 'Chatbot suggestion to user to check if parameters need to be set in the workflow actions and to save',
+    }),
+    Heading1: intl.formatMessage({
+      defaultMessage: 'Welcome to Copilot in Azure Logic Apps!',
+      description: 'Chatbot greeting message',
+    }),
+    SubHeading1: intl.formatMessage({
+      defaultMessage: ' Copilot can help you with tasks such as the following:',
+      description: 'Chatbot introduction message to suggest what it can help with',
+    }),
+    suggestedAction1: intl.formatMessage({
+      defaultMessage: 'Learn about workflows, the way they work, and how to create them.',
+      description: 'Chatbot suggestion to user to learn about how their workflow works',
+    }),
+    suggestedAction2: intl.formatMessage({
+      defaultMessage: 'Get details about Azure Logic Apps platform’s capabilities and connectors.',
+      description: 'Chatbot suggestion to user to get help on Logic Apps platform capabilities as well as connectors',
+    }),
+    Outro1: intl.formatMessage({
+      defaultMessage: 'Copilot is designed only to provide help and doesn’t support workflow creation or editing.',
+      description: 'Chatbot disclaimer message that Copilot can only provide help and not modify workflows',
+    }),
+    Heading2: intl.formatMessage({
+      defaultMessage: 'Some things you can say:',
+      description: 'Chatbot disclaimer message that Copilot can only provide help and not modify workflows',
+    }),
     suggestedPromptItem1: intl.formatMessage({
-      defaultMessage: 'Add an action that sends an email',
-      description: 'Chatbot suggested input for user',
+      defaultMessage: 'Describe this workflow',
+      description: 'Chatbot disclaimer message that Copilot can only provide help and not modify workflows',
     }),
     suggestedPromptItem2: intl.formatMessage({
-      defaultMessage: 'Explain what an action does',
-      description: 'Chatbot suggested input for user',
+      defaultMessage: 'How to receive files from SFTP server',
+      description: 'Chatbot disclaimer message that Copilot can only provide help and not modify workflows',
     }),
     suggestedPromptItem3: intl.formatMessage({
-      defaultMessage: 'Add a condition',
-      description: 'Chatbot suggested input prompt for user',
+      defaultMessage: 'How to call an external endpoint',
+      description: 'Chatbot disclaimer message that Copilot can only provide help and not modify workflows',
+    }),
+    suggestedPromptItem4: intl.formatMessage({
+      defaultMessage: 'What is the concurrency setting of this workflow',
+      description: 'Chatbot disclaimer message that Copilot can only provide help and not modify workflows',
+    }),
+    FirstDisclaimer3: intl.formatMessage({
+      defaultMessage: 'Copilot doesn’t edit or change your workflows.',
+      description: 'Chatbot disclaimer message that Copilot does not modify workflows.',
+    }),
+    SecondDisclaimer3: intl.formatMessage({
+      defaultMessage: 'Copilot doesn’t store or use your data.',
+      description: 'Chatbot disclaimer message that Copilot does not gather data',
     }),
     saveYourFlow: intl.formatMessage({
       defaultMessage: 'Check the flow’s actions to see if any parameters need to be set. Don’t forget to save when you’re done!',
