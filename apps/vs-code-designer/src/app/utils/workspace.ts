d2d177f368957cf7846f3c4a2877da5b3d8ba1d8
Elaina-Lee <144840522+Elaina-Lee@users.noreply.github.com>
2023-11-08 11:52:16 -0800

    fix(Data Mapper): Fixed Data Mapper Build Error (#3646)
    fixed the build error

--- apps/vs-code-designer/src/app/utils/workspace.ts
+++ apps/vs-code-designer/src/app/utils/workspace.ts
@@ -10,7 +10,6 @@ import { tryGetFunctionProjectRoot } from './verifyIsProject';
 import { isNullOrUndefined, isString } from '@microsoft/utils-logic-apps';
 import { UserCancelledError } from '@microsoft/vscode-azext-utils';
 import type { IActionContext, IAzureQuickPickItem } from '@microsoft/vscode-azext-utils';
-//TODO: revisit this import again (globby)
 import * as globby from 'globby';
 import * as path from 'path';
 import * as vscode from 'vscode';
