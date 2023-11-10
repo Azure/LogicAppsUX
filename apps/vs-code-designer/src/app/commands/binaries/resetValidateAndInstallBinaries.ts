5223c9aa3f2e9f18f63d4b74d85f457babfc52a9
ccastrotrejo <ccastrotrejo@microsoft.com>
2023-11-10 12:18:33 -0800

    Move install binaries function

---
+++ apps/vs-code-designer/src/app/commands/binaries/resetValidateAndInstallBinaries.ts
@@ -0,0 +1,9 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import type { IActionContext } from '@microsoft/vscode-azext-utils';
+
+export const resetValidateAndInstallBinaries = async (activateContext: IActionContext) => {
+  console.log(activateContext);
+};
