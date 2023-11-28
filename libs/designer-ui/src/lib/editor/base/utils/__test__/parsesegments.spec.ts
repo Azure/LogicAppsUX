47988d4f465b499ffb2891b738207d5bfb3b0464
ek <1350074+ek68794998@users.noreply.github.com>
2023-11-27 17:13:21 -0800

    Fix neglected test case

--- libs/designer-ui/src/lib/editor/base/utils/__test__/parsesegments.spec.ts
+++ libs/designer-ui/src/lib/editor/base/utils/__test__/parsesegments.spec.ts
@@ -6,7 +6,10 @@ describe('lib/editor/base/utils/parseSegments', () => {
       ['plain text', 'plain text'],
       [`$[concat(...),concat('&lt;'),#AD008C]$`, `$[concat(...),concat('%26lt;'),#AD008C]$`],
       [`text$[concat(...),concat('&lt;'),#AD008C]$text`, `text$[concat(...),concat('%26lt;'),#AD008C]$text`],
-      [`$[replace(...),replace(replace(replace('abc','&lt;','<'),'&gt;','>'),'&quot;','"') ,#AD008C]$`, `$[replace(...),replace(replace(replace('abc','%26lt;','<'),'%26gt;','>'),'%26quot;','"') ,#AD008C]$`]
+      [
+        `$[replace(...),replace(replace(replace('abc','&lt;','<'),'&gt;','>'),'&quot;','"') ,#AD008C]$`,
+        `$[replace(...),replace(replace(replace('abc','%26lt;','<'),'%26gt;','>'),'%26quot;','%22') ,#AD008C]$`,
+      ],
     ])('should properly encode segments in: %p', (input, expected) => {
       expect(encodeStringSegments(input, true)).toBe(expected);
     });
