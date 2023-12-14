3f4be3c9dbd3fb66242c5345376723af642b1037
ek <1350074+ek68794998@users.noreply.github.com>
2023-12-14 12:09:24 -0800

    Re-enable parseSegments test cases

--- libs/designer-ui/src/lib/editor/base/utils/__test__/parsesegments.spec.ts
+++ libs/designer-ui/src/lib/editor/base/utils/__test__/parsesegments.spec.ts
@@ -1,4 +1,4 @@
-/*
+import type { ValueSegment } from '@microsoft/designer-client-services-logic-apps';
 import { encodeStringSegmentTokensInLexicalContext } from '../parsesegments';
 
 describe('lib/editor/base/utils/parseSegments', () => {
@@ -8,12 +8,15 @@ describe('lib/editor/base/utils/parseSegments', () => {
       [`$[concat(...),concat('&lt;'),#AD008C]$`, `$[concat(...),concat('%26lt;'),#AD008C]$`],
       [`text$[concat(...),concat('&lt;'),#AD008C]$text`, `text$[concat(...),concat('%26lt;'),#AD008C]$text`],
       [
-        `$[replace(...),replace(replace(replace('abc','&lt;','<'),'&gt;','>'),'&quot;','"') ,#AD008C]$`,
-        `$[replace(...),replace(replace(replace('abc','%26lt;','<'),'%26gt;','>'),'%26quot;','%22') ,#AD008C]$`,
+        `$[replace(...),replace(replace(replace('abc','&lt;','<'),'&gt;','>'),'&quot;','"'),#AD008C]$`,
+        `$[replace(...),replace(replace(replace('abc','%26lt;','<'),'%26gt;','>'),'%26quot;','%22'),#AD008C]$`,
       ],
     ])('should properly encode segments in: %p', (input, expected) => {
-      expect(encodeStringSegmentTokensInLexicalContext(input)).toBe(expected);
+      const nodeMap = new Map<string, ValueSegment>();
+      nodeMap.set(`concat('&lt;')`, {} as unknown as ValueSegment);
+      nodeMap.set(`replace(replace(replace('abc','&lt;','<'),'&gt;','>'),'&quot;','"')`, {} as unknown as ValueSegment);
+
+      expect(encodeStringSegmentTokensInLexicalContext(input, nodeMap)).toBe(expected);
     });
   });
 });
-*/
