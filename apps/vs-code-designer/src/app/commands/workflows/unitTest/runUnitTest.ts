89379c6d65ef2566f353ebbed2ce2cfec61c4817
ccastrotrejo <ccastrotrejo@microsoft.com>
2023-12-18 15:29:35 -0800

    Update paths to run unit tests

--- apps/vs-code-designer/src/app/commands/workflows/unitTest/runUnitTest.ts
+++ apps/vs-code-designer/src/app/commands/workflows/unitTest/runUnitTest.ts
@@ -3,35 +3,13 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import { localize } from '../../../../localize';
+import { getLogicAppProjectRoot } from '../../../utils/codeless/connection';
 import { type IAzureConnectorsContext } from '../azureConnectorWizard';
 import * as child from 'child_process';
+import * as path from 'path';
 import * as vscode from 'vscode';
 
-function getPathToRoot(dirItems: string[]) {
-  const DIRS_TO_ROOT = 4;
-  let path = dirItems[0];
-  for (let i = 1; i < dirItems.length - DIRS_TO_ROOT; i++) {
-    path = path.concat(`\\${dirItems[i]}`);
-  }
-  return path;
-}
-
-function getWorkflowName(dirItems: string[]) {
-  const DIRS_TO_WORKFLOWNAME = 1;
-  return dirItems[dirItems.length - DIRS_TO_WORKFLOWNAME - 1];
-}
-
-function getPathToExe(dirItems: string[]) {
-  const DIRS_TO_TESTS_FOLDER = 2;
-  let path = dirItems[0];
-  for (let i = 1; i < dirItems.length - DIRS_TO_TESTS_FOLDER; i++) {
-    path = path.concat(`\\${dirItems[i]}`);
-  }
-  const UNIT_TEST_EXE_NAME = 'LogicAppsTest.exe';
-  return `${path}\\${UNIT_TEST_EXE_NAME}`;
-}
-
-export async function runUnitTest(_context: IAzureConnectorsContext, node: vscode.Uri): Promise<void> {
+export async function runUnitTest(context: IAzureConnectorsContext, node: vscode.Uri): Promise<void> {
   const options: vscode.ProgressOptions = {
     location: vscode.ProgressLocation.Notification,
     title: localize('azureFunctions.runUnitTest', 'Running Unit Test...'),
@@ -39,22 +17,22 @@ export async function runUnitTest(_context: IAzureConnectorsContext, node: vscod
 
   await vscode.window.withProgress(options, async () => {
     const pathToUnitTest = node.fsPath;
-    const dirItems = pathToUnitTest.split('\\');
+    const projectPath: string | undefined = await getLogicAppProjectRoot(this.context, pathToUnitTest);
+    const workflowName = path.basename(path.dirname(pathToUnitTest));
+
+    // TODO(ccastrotrejo): Need to confirm with BE and Nidhi on how we are going to execute the unit tests.
+    const UNIT_TEST_EXE_NAME = 'LogicAppsTest.exe';
+    const pathToExe = path.join(projectPath, UNIT_TEST_EXE_NAME);
 
     try {
-      const res = child.spawn(getPathToExe(dirItems), [
-        '-PathToRoot',
-        getPathToRoot(dirItems),
-        '-workflowName',
-        getWorkflowName(dirItems),
-        '-pathToUnitTest',
-        pathToUnitTest,
-      ]);
+      const res = child.spawn(pathToExe, ['-PathToRoot', projectPath, '-workflowName', workflowName, '-pathToUnitTest', pathToUnitTest]);
+
       for await (const chunk of res.stdout) {
         vscode.window.showInformationMessage(`${chunk}`);
       }
     } catch (error) {
       vscode.window.showErrorMessage(`${localize('runFailure', 'Error Running Unit Test.')} ${error.message}`, localize('OK', 'OK'));
+      context.telemetry.properties.errorMessage = error.message;
       throw error;
     }
   });
