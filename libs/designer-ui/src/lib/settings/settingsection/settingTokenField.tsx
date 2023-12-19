aec12861bcb485d293b75b3468b9be0deda39605
Ram Gupta <ramgupta95@gmail.com>
2023-12-19 09:34:39 -0800

    fix(designer): Add description for Condition action (#3865)
    * Add description for Condition action
    
    * Move description to querybuilder editor
    
    * Remove extra file
    
    ---------
    
    Co-authored-by: Ram Gupta <Ram.Gupta@microsoft.com>

--- libs/designer-ui/src/lib/settings/settingsection/settingTokenField.tsx
+++ libs/designer-ui/src/lib/settings/settingsection/settingTokenField.tsx
@@ -274,6 +274,7 @@ export const TokenField = ({
           tokenMapping={tokenMapping}
           loadParameterValueFromString={loadParameterValueFromString}
           getTokenPicker={getTokenPicker}
+          showDescription={true}
         />
       );
 
