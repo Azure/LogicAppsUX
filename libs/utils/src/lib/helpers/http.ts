d92bcbd504b12606c857179945610477a327d9ff
Riley Evans <rllyy97@gmail.com>
2024-01-18 11:02:57 -0600

    Dynamic data in combobox now shows error message details when available

--- libs/utils/src/lib/helpers/http.ts
+++ libs/utils/src/lib/helpers/http.ts
@@ -1,4 +1,4 @@
-export type HTTP_METHODS = typeof HTTP_METHODS[keyof typeof HTTP_METHODS];
+export type HTTP_METHODS = (typeof HTTP_METHODS)[keyof typeof HTTP_METHODS];
 export const HTTP_METHODS = {
   GET: 'GET',
   PUT: 'PUT',
@@ -6,3 +6,12 @@ export const HTTP_METHODS = {
   PATCH: 'PATCH',
   DELETE: 'DELETE',
 } as const;
+
+export const parseErrorMessage = (error: any, defaultErrorMessage?: string): string =>
+  error?.message ??
+  error?.Message ??
+  error?.error?.message ??
+  error.content.message ??
+  error?.responseText ??
+  defaultErrorMessage ??
+  'Unknown error';
