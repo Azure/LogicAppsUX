6e3f0efb16d812a4b45ba68e3d5eae6388afdd1a
Riley Evans <rllyy97@gmail.com>
2024-02-02 16:06:17 -0600

    Moved service initialization into useEffect

--- libs/designer/src/lib/core/BJSWorkflowProvider.tsx
+++ libs/designer/src/lib/core/BJSWorkflowProvider.tsx
@@ -9,7 +9,7 @@ import type { AppDispatch } from './store';
 import type { LogicAppsV2 } from '@microsoft/utils-logic-apps';
 import { equals } from '@microsoft/utils-logic-apps';
 import { useDeepCompareEffect } from '@react-hookz/web';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 export interface BJSWorkflowProviderProps {
@@ -33,18 +33,21 @@ const DataProviderInner: React.FC<BJSWorkflowProviderProps> = ({ workflow, child
 export const BJSWorkflowProvider: React.FC<BJSWorkflowProviderProps> = (props) => {
   const wrapped = useContext(ProviderWrappedContext);
   const dispatch = useDispatch<AppDispatch>();
-  const designerOptionsInitialized = useAreDesignerOptionsInitialized();
   const servicesInitialized = useAreServicesInitialized();
+  const designerOptionsInitialized = useAreDesignerOptionsInitialized();
+
   if (!wrapped) {
     throw new Error('BJSWorkflowProvider must be used inside of a DesignerProvider');
   }
 
+  useEffect(() => {
+    if (!servicesInitialized) {
+      dispatch(initializeServices(wrapped));
+    }
+  }, [dispatch, servicesInitialized, wrapped]);
+
   if (!designerOptionsInitialized) {
     return null;
-  }
-
-  if (!servicesInitialized) {
-    dispatch(initializeServices(wrapped));
   }
 
   return <DataProviderInner {...props} />;
