3762cf9df2742c4923831d0c1c0ae21ebdc73d9f
e.k <1350074+ek68794998@users.noreply.github.com>
2024-02-05 13:10:24 -0800

    fix(designer-ui): Ensure `isAdvanced: false` tokens are shown if far down in a list (#4064)
    * Move token picker section options filter to a helper fn
    
    * Fix bug where later options would not be visible
    
    * Add tests for `hasAdvanced`

---
+++ libs/designer-ui/src/lib/tokenpicker/tokenpickersection/__test__/tokenpickerhelpers.spec.ts
@@ -0,0 +1,101 @@
+import type { OutputToken } from '../..';
+import { getReducedTokenList, hasAdvanced } from '../tokenpickerhelpers';
+
+describe('ui/tokenPicker/tokenPickerSection helpers', () => {
+  describe('getReducedTokenList', () => {
+    it('should return a list of tokens with the advanced tokens filtered out', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: false, value: 'Value 1' } as OutputToken,
+        { isAdvanced: true, value: 'Value 2' } as OutputToken,
+        { isAdvanced: false, value: 'Value 3' } as OutputToken,
+        { isAdvanced: true, value: 'Value 4' } as OutputToken,
+      ];
+      const options = { hasSearchQuery: false, maxRowsShown: 2, showAllOptions: false };
+
+      const result = getReducedTokenList(tokens, options);
+
+      expect(result).toEqual([
+        { isAdvanced: false, value: 'Value 1' },
+        { isAdvanced: false, value: 'Value 3' },
+      ]);
+    });
+
+    it('should return a list of tokens with the advanced tokens filtered out and the max rows shown respected', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: false, value: 'Value 1' } as OutputToken,
+        { isAdvanced: true, value: 'Value 2' } as OutputToken,
+        { isAdvanced: false, value: 'Value 3' } as OutputToken,
+        { isAdvanced: true, value: 'Value 4' } as OutputToken,
+      ];
+      const options = { hasSearchQuery: false, maxRowsShown: 1, showAllOptions: false };
+
+      const result = getReducedTokenList(tokens, options);
+
+      expect(result).toEqual([{ isAdvanced: false, value: 'Value 1' }]);
+    });
+
+    it('should return a list of tokens with the search query presence respected', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: false, value: 'Value 1' } as OutputToken,
+        { isAdvanced: true, value: 'Value 2' } as OutputToken,
+        { isAdvanced: false, value: 'Value 3' } as OutputToken,
+        { isAdvanced: true, value: 'Value 4' } as OutputToken,
+      ];
+      const options = { hasSearchQuery: true, maxRowsShown: 2, showAllOptions: false };
+
+      const result = getReducedTokenList(tokens, options);
+
+      expect(result).toEqual([
+        { isAdvanced: false, value: 'Value 1' },
+        { isAdvanced: true, value: 'Value 2' },
+      ]);
+    });
+
+    it('should return a list of tokens with the "no more options" field respected', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: false, value: 'Value 1' } as OutputToken,
+        { isAdvanced: true, value: 'Value 2' } as OutputToken,
+        { isAdvanced: false, value: 'Value 3' } as OutputToken,
+        { isAdvanced: true, value: 'Value 4' } as OutputToken,
+      ];
+      const options = { hasSearchQuery: false, maxRowsShown: 1, showAllOptions: true };
+
+      const result = getReducedTokenList(tokens, options);
+
+      expect(result).toEqual([
+        { isAdvanced: false, value: 'Value 1' },
+        { isAdvanced: true, value: 'Value 2' },
+        { isAdvanced: false, value: 'Value 3' },
+        { isAdvanced: true, value: 'Value 4' },
+      ]);
+    });
+  });
+
+  describe('hasAdvanced', () => {
+    it('should return true if any of the tokens are advanced', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: false, value: 'Value 1' } as OutputToken,
+        { isAdvanced: true, value: 'Value 2' } as OutputToken,
+        { isAdvanced: false, value: 'Value 3' } as OutputToken,
+        { isAdvanced: true, value: 'Value 4' } as OutputToken,
+      ];
+
+      const result = hasAdvanced(tokens);
+
+      expect(result).toEqual(true);
+    });
+
+    it('should return false if none of the tokens are advanced', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: false, value: 'Value 1' } as OutputToken,
+        { isAdvanced: false, value: 'Value 2' } as OutputToken,
+        { isAdvanced: false, value: 'Value 3' } as OutputToken,
+        { isAdvanced: false, value: 'Value 4' } as OutputToken,
+      ];
+
+      const result = hasAdvanced(tokens);
+
+      expect(result).toEqual(false);
+    });
+  });
+});
