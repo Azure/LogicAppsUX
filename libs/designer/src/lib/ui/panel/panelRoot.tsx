38ed3cfef31bccfefcc9488d8b07d716d8fc7a15
ccastrotrejo <ccastrotrejo@microsoft.com>
2024-02-05 18:45:14 -0800

    Initial implementation

--- libs/designer/src/lib/ui/panel/panelRoot.tsx
+++ libs/designer/src/lib/ui/panel/panelRoot.tsx
@@ -11,11 +11,11 @@ import { RecommendationPanelContext } from './recommendation/recommendationPanel
 import { WorkflowParametersPanel } from './workflowParametersPanel/workflowParametersPanel';
 import { WorkflowParametersPanelFooter } from './workflowParametersPanel/workflowParametersPanelFooter';
 import { Panel, PanelType } from '@fluentui/react';
-import { Spinner } from '@fluentui/react-components';
+import { Spinner, makeStyles, mergeClasses, shorthands, tokens } from '@fluentui/react-components';
 import { isUndefined } from '@microsoft/applicationinsights-core-js';
 import type { CommonPanelProps, CustomPanelLocation } from '@microsoft/designer-ui';
 import { PanelLocation, PanelSize } from '@microsoft/designer-ui';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 export interface PanelRootProps {
@@ -28,15 +28,46 @@ const layerProps = {
   eventBubblingEnabled: true,
 };
 
+const useStyles = makeStyles({
+  resizer: {
+    ...shorthands.borderLeft('1px', 'solid', tokens.colorNeutralBackground5),
+
+    width: '8px',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    cursor: 'col-resize',
+    resize: 'horizontal',
+
+    ':hover': {
+      borderLeftWidth: '4px',
+    },
+  },
+
+  resizerActive: {
+    borderLeftWidth: '4px',
+    borderLeftColor: tokens.colorNeutralBackground5Pressed,
+  },
+});
+
 export const PanelRoot = (props: PanelRootProps): JSX.Element => {
   const { panelLocation, customPanelLocations } = props;
   const dispatch = useDispatch<AppDispatch>();
   const isDarkMode = useIsDarkMode();
+  const styles = useStyles();
 
   const collapsed = useIsPanelCollapsed();
   const currentPanelMode = useCurrentPanelMode();
-
-  const [width, setWidth] = useState<PanelSize>(PanelSize.Auto);
+  const [isResizing, setIsResizing] = useState(false);
+  const [width, setWidth] = useState<any>(PanelSize.Auto);
+  const [mouseX, setMouseX] = useState(0);
+  const startResizing = useCallback((test: any) => {
+    setMouseX(test.clientX);
+    return setIsResizing(true);
+  }, []);
+  const stopResizing = useCallback(() => setIsResizing(false), []);
+  const animationFrame = useRef<number>(0);
 
   useEffect(() => {
     setWidth(collapsed ? PanelSize.Auto : PanelSize.Medium);
@@ -70,6 +101,30 @@ export const PanelRoot = (props: PanelRootProps): JSX.Element => {
     </div>
   );
 
+  const resize = useCallback(
+    ({ clientX }: MouseEvent) => {
+      animationFrame.current = requestAnimationFrame(() => {
+        if (isResizing) {
+          const newSize = mouseX - clientX;
+          console.log('charlie', newSize, mouseX);
+          setWidth(newSize.toString() + 'px');
+        }
+      });
+    },
+    [isResizing, mouseX]
+  );
+
+  useEffect(() => {
+    window.addEventListener('mousemove', resize);
+    window.addEventListener('mouseup', stopResizing);
+
+    return () => {
+      cancelAnimationFrame(animationFrame.current);
+      window.removeEventListener('mousemove', resize);
+      window.removeEventListener('mouseup', stopResizing);
+    };
+  }, [resize, stopResizing]);
+
   return (!isLoadingPanel && isUndefined(currentPanelMode)) || currentPanelMode === 'Operation' ? (
     <NodeDetailsPanel {...commonPanelProps} />
   ) : (
@@ -77,7 +132,7 @@ export const PanelRoot = (props: PanelRootProps): JSX.Element => {
       className={`msla-panel-root-${currentPanelMode}`}
       isLightDismiss
       isBlocking={!isLoadingPanel && !nonBlockingPanels.includes(currentPanelMode ?? '')}
-      type={commonPanelProps.panelLocation === PanelLocation.Right ? PanelType.medium : PanelType.customNear}
+      type={commonPanelProps.panelLocation === PanelLocation.Right ? PanelType.custom : PanelType.customNear}
       isOpen={!collapsed}
       onDismiss={dismissPanel}
       hasCloseButton={false}
@@ -93,6 +148,7 @@ export const PanelRoot = (props: PanelRootProps): JSX.Element => {
         },
       })}
     >
+      <div className={mergeClasses(styles.resizer, isResizing && styles.resizerActive)} onMouseDown={startResizing} />
       {
         isLoadingPanel ? (
           <LoadingComponent />
