321b8ccb427d3649f542ec2160b1e5f5b5e291bc
danielle <dacogbur@microsoft.com>
2024-02-12 15:45:11 -0800

    added moon and simple rollup config

---
+++ libs/utils/rollup.config.ts
@@ -0,0 +1,20 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import  commonjs  from '@rollup/plugin-commonjs';
+
+export default {
+    input: 'src/index.ts',
+    output: 
+        {
+            format: 'cjs',
+            dir: '.',
+        }
+    ,
+    plugins: [
+       typescript({'tsconfig': './tsconfig.lib.json'}), nodeResolve({ modulePaths: ['../../node_modules']}), commonjs()
+    ],
+    external: [
+        // Add any external dependencies here
+    ],
+    // Add any additional Rollup configuration options here
+};
