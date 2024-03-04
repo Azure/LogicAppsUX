534950e57c29d218341c28205d971c7ec02502e6
danielle <dacogbur@microsoft.com>
2024-03-03 18:33:49 -0800

    external node modules are correct

--- libs/designer-ui/rollup.config.ts
+++ libs/designer-ui/rollup.config.ts
@@ -5,19 +5,30 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
 
+const externalFn = (str, parent, _isResolved) => {
+  if (str.includes('logic-apps-shared')) {
+    //console.log(`Parent: ${parent} ${str}`);
+    return true;
+  }
+  if (str.includes('node_modules')) {
+    console.log(`Parent: ${parent} ${str}`);
+    return true;
+  }
+  return false;
+};
+
 export default {
   input: 'src/index.ts',
+  external: externalFn, //['@microsoft/logic-apps-shared', 'libs/logic-apps-shared/src/index.ts', '/node_modules/'],
   output: {
     format: 'esm',
-    dir: '../../dist/libs/designer-ui',
+    dir: '../../dist/rollup/lib/designer-ui',
   },
   plugins: [
-    typescript({ tsconfig: './tsconfig.lib.json' }),
+    typescript({ tsconfig: './tsconfig.lib.json', include: ['**/designer-ui/src/**'], exclude: ['**/logic-apps-shared/**'] }),
     postcss(),
     image(),
-    nodeResolve({
-      modulePaths: ['../../node_modules'],
-    }),
+    nodeResolve(),
     commonjs(),
     json(),
   ],
