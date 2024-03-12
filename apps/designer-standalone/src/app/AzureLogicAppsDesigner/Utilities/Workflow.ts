f2343b549ec0094a6aacf468516651b16b94c553
Riley Evans <rllyy97@gmail.com>
2024-03-12 11:53:39 -0500

    Reverted app settings resolve change

--- apps/designer-standalone/src/app/AzureLogicAppsDesigner/Utilities/Workflow.ts
+++ apps/designer-standalone/src/app/AzureLogicAppsDesigner/Utilities/Workflow.ts
@@ -58,7 +58,11 @@ export class WorkflowUtility {
     return references;
   }
 
-  public static resolveConnectionsReferences(content: string, parameters: ParametersData | undefined): any {
+  public static resolveConnectionsReferences(
+    content: string,
+    parameters: ParametersData | undefined,
+    appsettings?: Record<string, string> | undefined
+  ): any {
     let result = content;
 
     if (parameters) {
@@ -66,6 +70,14 @@ export class WorkflowUtility {
         const parameterValue = parameters[parameterName].value !== undefined ? parameters[parameterName].value : '';
         result = replaceAllOccurrences(result, `@parameters('${parameterName}')`, parameterValue);
         result = replaceAllOccurrences(result, `@{parameters('${parameterName}')}`, parameterValue);
+      }
+    }
+
+    if (appsettings) {
+      for (const settingName of Object.keys(appsettings)) {
+        const settingValue = appsettings[settingName] !== undefined ? appsettings[settingName] : '';
+        result = replaceAllOccurrences(result, `@appsetting('${settingName}')`, settingValue);
+        result = replaceAllOccurrences(result, `@{appsetting('${settingName}')}`, settingValue);
       }
     }
 
