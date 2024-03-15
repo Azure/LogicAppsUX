bcddb680bf7bd28a8a41a66ed81c126c2266f231
danielle <dacogbur@microsoft.com>
2024-03-15 13:43:42 -0700

    external and package json for services

--- libs/services/designer-client-services/rollup.config.ts
+++ libs/services/designer-client-services/rollup.config.ts
@@ -2,15 +2,35 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import copy from 'rollup-plugin-copy';
+
+const externalFn = (str, _parent, _isResolved) => {
+  if (str.includes('logic-apps-shared')) {
+    return true;
+  }
+  if (str.includes('node_modules')) {
+    return true;
+  }
+  return false;
+};
+
+const distFolder = '../../../dist/rollup/libs/designer-client-services';
 
 export default {
   input: './src/index.ts',
+  external: externalFn,
   output: {
     format: 'esm',
-    dir: '../../../dist/rollup/libs/designer-client-services',
+    dir: distFolder,
     entryFileNames: 'index.esm.js',
 
     name: '@microsoft/designer-client-services',
   },
-  plugins: [typescript({ tsconfig: './tsconfig.lib.json' }), nodeResolve({ modulePaths: ['../../node_modules'] }), commonjs(), json()],
+  plugins: [
+    typescript({ tsconfig: './tsconfig.lib.json' }),
+    nodeResolve({ modulePaths: ['../../node_modules'] }),
+    commonjs(),
+    json(),
+    copy({ targets: [{ src: './package.json', dest: distFolder }] }),
+  ],
 };
