de9d8574b238140b194566784e020ba2c9f6c6e6
ccastrotrejo <ccastrotrejo@microsoft.com>
2024-03-18 17:01:52 -0700

    Fix import

--- apps/vs-code-designer/src/extensionVariables.ts
+++ apps/vs-code-designer/src/extensionVariables.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 import type DataMapperPanel from './app/commands/dataMapper/DataMapperPanel';
 import type { AzureAccountTreeItemWithProjects } from './app/tree/AzureAccountTreeItemWithProjects';
-import { type TestData } from './app/tree/unitTestTree/testTree';
+import { type TestData } from './app/tree/unitTestTree';
 import { dotnet, func, node, npm } from './constants';
 import type { Site } from '@azure/arm-appservice';
 import type { IAzExtOutputChannel } from '@microsoft/vscode-azext-utils';
