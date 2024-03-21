cfe59af9306a1faa24d05da9710ca21326673e31
danielle <dacogbur@microsoft.com>
2024-03-21 15:41:28 -0700

    able to run jest

--- libs/logic-apps-shared/jest.setup.js
+++ libs/logic-apps-shared/jest.setup.js
@@ -1,5 +1,6 @@
 import messages from '../services/intl/src/compiled-lang/strings.json';
 import { createIntl, createIntlCache } from 'react-intl';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as Intl from 'react-intl';
 
 // You can pass your messages to the IntlProvider. Optional: remove if unneeded.
@@ -8,6 +9,7 @@ const defaultLocale = 'en';
 
 const cache = createIntlCache();
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const intl = createIntl(
   {
     locale: defaultLocale,
