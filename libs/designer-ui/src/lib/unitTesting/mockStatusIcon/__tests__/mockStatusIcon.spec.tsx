0bb5f4a661b05bad3ec29e52ffd3823ed8926775
ccastrotrejo <ccastrotrejo@microsoft.com>
2024-03-22 16:07:17 -0700

    Add mock status icon snapshots

--- libs/designer-ui/src/lib/unitTesting/mockStatusIcon/__tests__/mockStatusIcon.spec.tsx
+++ libs/designer-ui/src/lib/unitTesting/mockStatusIcon/__tests__/mockStatusIcon.spec.tsx
@@ -1,36 +1,45 @@
-import TestRenderer from 'react-test-renderer';
 import { MockStatusIcon } from '../index';
+import TestRenderer from 'react-test-renderer';
 
 describe('ui/unitTesting/mockStatusIcon', () => {
   it('should render the empty status when no completed property is send.', () => {
     const mockStatusIcon = TestRenderer.create(
-      <MockStatusIcon id='test-id' nodeMockResults={{
-        output: {},
-        actionResult: '',
-      }}/>
-     );
+      <MockStatusIcon
+        id="test-id"
+        nodeMockResults={{
+          output: {},
+          actionResult: '',
+        }}
+      />
+    );
     expect(mockStatusIcon).toMatchSnapshot();
   });
 
   it('should render the completed status.', () => {
     const mockStatusIcon = TestRenderer.create(
-      <MockStatusIcon id='test-id' nodeMockResults={{
-        output: {},
-        actionResult: '',
-        isCompleted: true,
-      }}/>
-     );
+      <MockStatusIcon
+        id="test-id"
+        nodeMockResults={{
+          output: {},
+          actionResult: '',
+          isCompleted: true,
+        }}
+      />
+    );
     expect(mockStatusIcon).toMatchSnapshot();
   });
 
   it('should render the empty status.', () => {
     const mockStatusIcon = TestRenderer.create(
-      <MockStatusIcon id='test-id' nodeMockResults={{
-        output: {},
-        actionResult: '',
-        isCompleted: false,
-      }}/>
-     );
+      <MockStatusIcon
+        id="test-id"
+        nodeMockResults={{
+          output: {},
+          actionResult: '',
+          isCompleted: false,
+        }}
+      />
+    );
     expect(mockStatusIcon).toMatchSnapshot();
   });
 });
