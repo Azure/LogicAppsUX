b8122216fc1ce3592043b5d5caf5eb7d19f752f9
Eric-B-Wu <wue@microsoft.com>
2024-03-26 12:14:36 -0400

    formatting

--- libs/logic-apps-shared/src/parsers/lib/common/helpers/utils.ts
+++ libs/logic-apps-shared/src/parsers/lib/common/helpers/utils.ts
@@ -1,32 +1,16 @@
-import type {
-  EnumObject,
-  OutputMetadata,
-  ParameterDynamicSchema,
-  ParameterDynamicValues,
-} from "../../models/operation";
-import {
-  isDynamicTreeExtension,
-  isLegacyDynamicValuesTreeExtension,
-  DynamicSchemaType,
-  DynamicValuesType,
-} from "../../models/operation";
-import * as Constants from "../constants";
-import { OutputKeys } from "../constants";
-import { parseEx } from "./keysutility";
-import { getIntl } from "@microsoft/intl-logic-apps";
-import type { OpenAPIV2 } from "@microsoft/utils-logic-apps";
-import {
-  capitalizeFirstLetter,
-  equals,
-  isNullOrUndefined,
-} from "@microsoft/utils-logic-apps";
+import type { EnumObject, OutputMetadata, ParameterDynamicSchema, ParameterDynamicValues } from '../../models/operation';
+import { isDynamicTreeExtension, isLegacyDynamicValuesTreeExtension, DynamicSchemaType, DynamicValuesType } from '../../models/operation';
+import * as Constants from '../constants';
+import { OutputKeys } from '../constants';
+import { parseEx } from './keysutility';
+import { getIntl } from '@microsoft/intl-logic-apps';
+import type { OpenAPIV2 } from '@microsoft/utils-logic-apps';
+import { capitalizeFirstLetter, equals, isNullOrUndefined } from '@microsoft/utils-logic-apps';
 
 type SchemaObject = OpenAPIV2.SchemaObject;
 type Parameter = OpenAPIV2.ParameterObject;
 
-export function toSwaggerSchema(
-  parameter: Parameter | OpenAPIV2.ItemsObject
-): SchemaObject {
+export function toSwaggerSchema(parameter: Parameter | OpenAPIV2.ItemsObject): SchemaObject {
   const schema: SchemaObject = {
     format: parameter.format,
     description: (parameter as Parameter).description,
@@ -48,7 +32,7 @@ export function toSwaggerSchema(
   };
 
   for (const propertyName of Object.getOwnPropertyNames(parameter)) {
-    if (propertyName.startsWith("x-ms-")) {
+    if (propertyName.startsWith('x-ms-')) {
       schema[propertyName] = (parameter as Parameter)[propertyName];
     }
   }
@@ -56,18 +40,13 @@ export function toSwaggerSchema(
   return schema;
 }
 
-export function getEnum(
-  parameter: SchemaObject,
-  required: boolean | undefined
-): EnumObject[] | undefined {
+export function getEnum(parameter: SchemaObject, required: boolean | undefined): EnumObject[] | undefined {
   if (parameter.enum) {
-    const customEnum: EnumObject[] =
-      parameter[Constants.ExtensionProperties.CustomEnum];
+    const customEnum: EnumObject[] = parameter[Constants.ExtensionProperties.CustomEnum];
     if (customEnum) {
       return customEnum;
     } else {
-      const editorOptions =
-        parameter[Constants.ExtensionProperties.EditorOptions];
+      const editorOptions = parameter[Constants.ExtensionProperties.EditorOptions];
       if (editorOptions && editorOptions.items) {
         return editorOptions.items.map((item: any) => ({
           displayName: item.title,
@@ -75,7 +54,7 @@ export function getEnum(
         }));
       } else {
         return parameter.enum.map((item) => ({
-          displayName: !isNullOrUndefined(item) ? item.toString() : "",
+          displayName: !isNullOrUndefined(item) ? item.toString() : '',
           value: item,
         }));
       }
@@ -85,14 +64,14 @@ export function getEnum(
   if (parameter.type === Constants.Types.Boolean) {
     const intl = getIntl();
     const TrueDisplayName = intl.formatMessage({
-      defaultMessage: "Yes",
-      id: "k5tGEr",
-      description: "This is the boolean value for Yes",
+      defaultMessage: 'Yes',
+      id: 'k5tGEr',
+      description: 'This is the boolean value for Yes',
     });
     const FalseDisplayName = intl.formatMessage({
-      defaultMessage: "No",
-      id: "rAyuzv",
-      description: "This is the boolean value for No",
+      defaultMessage: 'No',
+      id: 'rAyuzv',
+      description: 'This is the boolean value for No',
     });
 
     return required
@@ -108,8 +87,8 @@ export function getEnum(
         ]
       : [
           {
-            displayName: "",
-            value: "",
+            displayName: '',
+            value: '',
           },
           {
             displayName: TrueDisplayName,
@@ -125,18 +104,13 @@ export function getEnum(
   return undefined;
 }
 
-export function getParameterDynamicValues(
-  parameter: SchemaObject
-): ParameterDynamicValues | undefined {
+export function getParameterDynamicValues(parameter: SchemaObject): ParameterDynamicValues | undefined {
   const parameterDynamicListExtension =
-    parameter[Constants.ExtensionProperties.DynamicList] ??
-    parameter?.items?.[Constants.ExtensionProperties.DynamicList];
+    parameter[Constants.ExtensionProperties.DynamicList] ?? parameter?.items?.[Constants.ExtensionProperties.DynamicList];
   const parameterDynamicValuesExtension =
-    parameter[Constants.ExtensionProperties.DynamicValues] ??
-    parameter?.items?.[Constants.ExtensionProperties.DynamicValues];
+    parameter[Constants.ExtensionProperties.DynamicValues] ?? parameter?.items?.[Constants.ExtensionProperties.DynamicValues];
   const parameterDynamicTreeExtension =
-    parameter[Constants.ExtensionProperties.DynamicTree] ??
-    parameter?.items?.[Constants.ExtensionProperties.DynamicTree];
+    parameter[Constants.ExtensionProperties.DynamicTree] ?? parameter?.items?.[Constants.ExtensionProperties.DynamicTree];
 
   if (parameterDynamicValuesExtension) {
     return {
@@ -162,11 +136,8 @@ export function getParameterDynamicValues(
   return undefined;
 }
 
-export function getParameterDynamicSchema(
-  parameter: SchemaObject
-): ParameterDynamicSchema | undefined {
-  const dynamicSchemaExtension =
-    parameter[Constants.ExtensionProperties.DynamicSchema];
+export function getParameterDynamicSchema(parameter: SchemaObject): ParameterDynamicSchema | undefined {
+  const dynamicSchemaExtension = parameter[Constants.ExtensionProperties.DynamicSchema];
   if (dynamicSchemaExtension) {
     return {
       type: DynamicSchemaType.LegacyDynamicSchema,
@@ -174,8 +145,7 @@ export function getParameterDynamicSchema(
     };
   }
 
-  const dynamicPropertiesExtension =
-    parameter[Constants.ExtensionProperties.DynamicProperties];
+  const dynamicPropertiesExtension = parameter[Constants.ExtensionProperties.DynamicProperties];
   if (dynamicPropertiesExtension) {
     return {
       type: DynamicSchemaType.DynamicProperties,
@@ -186,30 +156,19 @@ export function getParameterDynamicSchema(
   return undefined;
 }
 
-export function getArrayOutputMetadata(
-  schema: SchemaObject,
-  required: boolean,
-  excludeInternal: boolean,
-  prefix?: string
-): OutputMetadata {
-  if (schema.type === "array") {
+export function getArrayOutputMetadata(schema: SchemaObject, required: boolean, excludeInternal: boolean, prefix?: string): OutputMetadata {
+  if (schema.type === 'array') {
     return {
       array: {
-        collectionPath: prefix ?? "",
+        collectionPath: prefix ?? '',
         required,
       },
     };
-  } else if (schema.type === "object") {
+  } else if (schema.type === 'object') {
     const properties = schema.properties || {};
     const keys = Object.keys(properties).filter((key) => {
       const property = properties[key];
-      return !(
-        excludeInternal &&
-        equals(
-          property[Constants.ExtensionProperties.Visibility],
-          Constants.Visibility.Internal
-        )
-      );
+      return !(excludeInternal && equals(property[Constants.ExtensionProperties.Visibility], Constants.Visibility.Internal));
     });
 
     // TODO: Currently this method returns the array details of the output where the trigger needs to do splitOn.
@@ -218,8 +177,7 @@ export function getArrayOutputMetadata(
     // dynamicparametershelper.getSchemaProcessorOptionsForDynamicOutputs needs to update accordingly.
     if (keys.length === 1) {
       const firstKey = keys[0];
-      const propertyRequired =
-        required && (schema.required || []).indexOf(firstKey) !== -1;
+      const propertyRequired = required && (schema.required || []).indexOf(firstKey) !== -1;
       const details = getArrayOutputMetadata(
         properties[firstKey],
         propertyRequired,
@@ -234,25 +192,14 @@ export function getArrayOutputMetadata(
   return {};
 }
 
-export function getEditorForParameter(
-  parameter: SchemaObject,
-  dynamicValues: ParameterDynamicValues | undefined
-): string | undefined {
+export function getEditorForParameter(parameter: SchemaObject, dynamicValues: ParameterDynamicValues | undefined): string | undefined {
   if (!dynamicValues) return parameter[Constants.ExtensionProperties.Editor];
-  if (parameter?.type === Constants.Types.Array)
-    return parameter[Constants.ExtensionProperties.Editor]; // If the parameter is in an array, break out so we render the array editor
-  if (
-    isLegacyDynamicValuesTreeExtension(dynamicValues) ||
-    isDynamicTreeExtension(dynamicValues)
-  )
-    return "filepicker";
-  return "combobox";
+  if (parameter?.type === Constants.Types.Array) return parameter[Constants.ExtensionProperties.Editor]; // If the parameter is in an array, break out so we render the array editor
+  if (isLegacyDynamicValuesTreeExtension(dynamicValues) || isDynamicTreeExtension(dynamicValues)) return 'filepicker';
+  return 'combobox';
 }
 
-type MakeDefinitionReducer = (
-  previous: Record<string, any>,
-  current: string
-) => Record<string, any>;
+type MakeDefinitionReducer = (previous: Record<string, any>, current: string) => Record<string, any>;
 
 const EmptyRefs = Object.freeze({
   $refs: {},
@@ -264,19 +211,13 @@ const EmptyRefs = Object.freeze({
  * @arg {Record<string, any>} metadata - A hash mapping cyclical JSON references to their raw schema.  A schema is raw if none of its $ref schema have been inlined
  * @return {SchemaObject}
  */
-export function dereferenceRefSchema(
-  $ref: string,
-  metadata: Record<string, any>
-): SchemaObject {
+export function dereferenceRefSchema($ref: string, metadata: Record<string, any>): SchemaObject {
   if (!$ref) {
     return {};
   }
 
   const { $refs }: Record<string, any> = { ...EmptyRefs, ...metadata };
-  const cyclicalDefinitions: Record<string, any> = Object.keys($refs).reduce(
-    makeDefinition($refs),
-    {}
-  );
+  const cyclicalDefinitions: Record<string, any> = Object.keys($refs).reduce(makeDefinition($refs), {});
   const cyclicalDefinitionType = cyclicalDefinitions[$ref];
   switch (cyclicalDefinitionType) {
     case Constants.Types.Array:
@@ -308,68 +249,67 @@ export function getKnownTitles(name: string): string {
   switch (name) {
     case OutputKeys.Body:
       return intl.formatMessage({
-        defaultMessage: "Body",
-        id: "ZCFMoe",
-        description: "Display name for body outputs",
+        defaultMessage: 'Body',
+        id: 'ZCFMoe',
+        description: 'Display name for body outputs',
       });
     case OutputKeys.Headers:
       return intl.formatMessage({
-        defaultMessage: "Headers",
-        id: "N4dEVo",
-        description: "Display name for headers in outputs",
+        defaultMessage: 'Headers',
+        id: 'N4dEVo',
+        description: 'Display name for headers in outputs',
       });
     case OutputKeys.Outputs:
       return intl.formatMessage({
-        defaultMessage: "Outputs",
-        id: "vT0DCP",
-        description: "Display name for operation outputs",
+        defaultMessage: 'Outputs',
+        id: 'vT0DCP',
+        description: 'Display name for operation outputs',
       });
     case OutputKeys.Queries:
       return intl.formatMessage({
-        defaultMessage: "Queries",
-        id: "w16qh+",
-        description: "Display name for queries in outputs",
+        defaultMessage: 'Queries',
+        id: 'w16qh+',
+        description: 'Display name for queries in outputs',
       });
     case OutputKeys.StatusCode:
       return intl.formatMessage({
-        defaultMessage: "Status Code",
-        id: "x10E1p",
-        description: "Display name for status code in outputs",
+        defaultMessage: 'Status Code',
+        id: 'x10E1p',
+        description: 'Display name for status code in outputs',
       });
     case OutputKeys.Item:
       return intl.formatMessage({
-        defaultMessage: "Item",
-        id: "oDHXKh",
-        description: "Display name for item output",
+        defaultMessage: 'Item',
+        id: 'oDHXKh',
+        description: 'Display name for item output',
       });
     case OutputKeys.PathParameters:
       return intl.formatMessage({
-        defaultMessage: "Path Parameters",
-        id: "BJNUxN",
-        description:
-          "Display name for relative path parameters in trigger outputs",
+        defaultMessage: 'Path Parameters',
+        id: 'BJNUxN',
+        description: 'Display name for relative path parameters in trigger outputs',
       });
     default:
       // eslint-disable-next-line no-case-declarations
       const segments = parseEx(name);
-      return segments.length ? String(segments[segments.length - 1].value) : "";
+      return segments.length ? String(segments[segments.length - 1].value) : '';
   }
 }
 
 export function getKnownTitlesFromKey(key: string): string | undefined {
   switch (key?.toLowerCase()) {
-    case "$.body":
+    case '$.body':
       return getKnownTitles(OutputKeys.Body);
-    case "$.headers":
+    case '$.headers':
       return getKnownTitles(OutputKeys.Headers);
-    case "$.queries":
+    case '$.queries':
       return getKnownTitles(OutputKeys.Queries);
-    case "$.pathparameters":
+    case '$.pathparameters':
       return getKnownTitles(OutputKeys.PathParameters);
-    case "$.statuscode":
+    case '$.statuscode':
       return getKnownTitles(OutputKeys.StatusCode);
     default:
-      if (key.startsWith("$.x-ms-")) {
+      if (key.startsWith('$.x-ms-')) {
         return replaceXmsTitle(key);
       }
       return undefined;
@@ -378,8 +318,8 @@ export function getKnownTitlesFromKey(key: string): string | undefined {
 
 export function replaceXmsTitle(key: string): string {
   return key
-    .replace("$.x-ms-", "")
-    .split("-")
+    .replace('$.x-ms-', '')
+    .split('-')
     .map((word) => capitalizeFirstLetter(word))
-    .join(" ");
+    .join(' ');
 }
