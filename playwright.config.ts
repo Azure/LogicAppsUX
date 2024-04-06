c832bac2d1c27c748d6e1bf0962e0b0440746cdd
Travis Harris <travisharris@microsoft.com>
2024-04-06 12:16:53 -0700

    Lets try a real test

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     baseURL: 'http://localhost:4200',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
