b9988ea730350cfcb54446713162a5bed60b2df5
Travis Harris <travisharris@microsoft.com>
2024-04-22 17:31:38 -0400

    Working test with real data

---
+++ e2e/designer/mock-copypastescope.spec.ts
@@ -0,0 +1,206 @@
+import { test, expect } from '@playwright/test';
+
+test(
+  'Mock: Expect Copy and Paste of Scopes to work on single workflow',
+  {
+    tag: '@mock',
+  },
+  async ({ page, browserName, context }) => {
+    if (browserName === 'webkit') {
+      context.grantPermissions(['clipboard-read'], { origin: 'http://localhost:4200' })
+    }
+    await page.goto('/');
+    await page.getByText('Select an option').click();
+    await page.getByRole('option', { name: 'Conditionals', exact: true }).click();
+    await page.getByRole('button', { name: 'Toolbox' }).click();
+    await page.waitForLoadState('networkidle');
+    await page.getByTestId('rf__node-Condition-#scope').getByRole('button', { name: 'Condition' }).click({
+      button: 'right'
+    });
+    await page.getByRole('menuitem', { name: 'Copy Entire Action ⌘+C' }).click();
+    await page.getByTestId('rf__edge-Initialize_variable-Condition').getByLabel('Insert a new step between').click();
+    await page.getByRole('menuitem', { name: 'Paste an action' }).click();
+    const serialized: any = await page.evaluate(() => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const state = (window as any).DesignerStore.getState();
+          resolve((window as any).DesignerModule.serializeBJSWorkflow(state));
+        }, 5000);
+      });
+    });
+    expect(serialized.definition).toEqual(verificationWorkflow);
+  }
+);
+
+const verificationWorkflow = {
+  "$schema": "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#",
+  "actions": {
+    "Condition": {
+      "type": "If",
+      "expression": {
+        "or": [
+          {
+            "equals": [
+              "abc@microsoft.com",
+              "@variables('goalOwner')"
+            ]
+          },
+          {
+            "equals": [
+              "@null",
+              "@variables('goalOwner')"
+            ]
+          },
+          {
+            "not": {
+              "equals": [
+                "@true",
+                "@variables('goalOwner')"
+              ]
+            }
+          },
+          {
+            "and": [
+              {
+                "not": {
+                  "endsWith": [
+                    "@{concat(concat(concat(concat())))}",
+                    "@variables('goalOwner')"
+                  ]
+                }
+              },
+              {
+                "equals": [
+                  null,
+                  "@variables('goalOwner')"
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      "actions": {
+        "Terminate": {
+          "type": "Terminate",
+          "inputs": {
+            "runStatus": "Succeeded"
+          }
+        }
+      },
+      "else": {
+        "actions": {
+          "Terminate_2": {
+            "type": "Terminate",
+            "inputs": {
+              "runStatus": "Failed",
+              "runError": {
+                "code": "CreateAndGetGoalFailed",
+                "message": "Created goal does not match expected goal"
+              }
+            }
+          }
+        }
+      },
+      "runAfter": {
+        "Condition-copy": [
+          "SUCCEEDED"
+        ]
+      }
+    },
+    "Initialize_variable": {
+      "type": "InitializeVariable",
+      "inputs": {
+        "variables": [
+          {
+            "name": "goalOwner",
+            "type": "string"
+          }
+        ]
+      },
+      "runAfter": {}
+    },
+    "Condition-copy": {
+      "type": "If",
+      "expression": {
+        "or": [
+          {
+            "equals": [
+              "abc@microsoft.com",
+              "@variables('goalOwner')"
+            ]
+          },
+          {
+            "equals": [
+              "@null",
+              "@variables('goalOwner')"
+            ]
+          },
+          {
+            "not": {
+              "equals": [
+                "@true",
+                "@variables('goalOwner')"
+              ]
+            }
+          },
+          {
+            "and": [
+              {
+                "not": {
+                  "endsWith": [
+                    "@{concat(concat(concat(concat())))}",
+                    "@variables('goalOwner')"
+                  ]
+                }
+              },
+              {
+                "equals": [
+                  null,
+                  "@variables('goalOwner')"
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      "actions": {
+        "Terminate_1": {
+          "type": "Terminate",
+          "inputs": {
+            "runStatus": "Succeeded"
+          }
+        }
+      },
+      "else": {
+        "actions": {
+          "Terminate_3": {
+            "type": "Terminate",
+            "inputs": {
+              "runStatus": "Failed",
+              "runError": {
+                "code": "CreateAndGetGoalFailed",
+                "message": "Created goal does not match expected goal"
+              }
+            }
+          }
+        }
+      },
+      "runAfter": {
+        "Initialize_variable": [
+          "Succeeded"
+        ]
+      }
+    }
+  },
+  "contentVersion": "1.0.0.0",
+  "outputs": {},
+  "triggers": {
+    "Recurrence": {
+      "type": "Recurrence",
+      "recurrence": {
+        "frequency": "Month",
+        "interval": 1
+      }
+    }
+  }
+}
---
+++ e2e/designer/real-api/copypastescope.spec.ts
@@ -0,0 +1,205 @@
+import { expect, test } from '@playwright/test';
+import { beforeEach } from 'node:test';
+
+test.describe('Copy and Paste of Scopes', {
+  tag: '@real',
+}, () => {
+
+  beforeEach(async () => {
+  });
+  test(
+    'Expect Copy and Paste of Scopes to work on single workflow',
+
+    async ({ page, context, browserName }) => {
+      if (browserName === 'webkit') {
+        context.grantPermissions(['clipboard-read'], { origin: 'http://localhost:4200' })
+      }
+      await page.goto('/');
+      await page.getByPlaceholder('Select an App').click({ timeout: 20000 });
+      await page.getByPlaceholder('Select an App').fill(`Good-Test-Case`, { timeout: 20000 });
+      await page.getByPlaceholder('Select an App').press('Enter', { timeout: 20000 });
+      await page.getByLabel('Workflow').locator('span').filter({ hasText: '' }).click({ timeout: 20000 });
+      await page.getByRole('option', { name: 'empty' }).click({ timeout: 20000 });
+      await page.getByRole('button', { name: 'Toolbox' }).click({ timeout: 20000 });
+
+      await page.waitForLoadState('networkidle');
+      await page.getByTestId('rf__node-For_each-#scope').getByRole('button', { name: 'For each' }).click({
+        button: 'right'
+      });
+      await page.getByRole('menuitem', { name: 'Copy Entire Action ⌘+C' }).click();
+      await page.getByTestId('rf__edge-For_each-Filter_array').getByLabel('Insert a new step between For').click();
+      await page.getByRole('menuitem', { name: 'Paste an action' }).click();
+      const serialized: any = await page.evaluate(() => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const state = (window as any).DesignerStore.getState();
+            resolve((window as any).DesignerModule.serializeBJSWorkflow(state));
+          }, 5000);
+        });
+      });
+      expect(serialized.definition).toEqual(verificationWorkflow);
+    }
+  );
+});
+
+const verificationWorkflow = {
+
+  "$schema": "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#",
+  "actions": {
+    "Initialize_ArrayVariable": {
+      "type": "InitializeVariable",
+      "inputs": {
+        "variables": [
+          {
+            "name": "ArrayVariable",
+            "type": "array"
+          }
+        ]
+      },
+      "runAfter": {}
+    },
+    "Filter_array": {
+      "type": "Query",
+      "inputs": {
+        "from": "@body('Parse_JSON')",
+        "where": "@not(contains(length(split(item(), '|')?[0]),length(split(item(), '|')?[0])))"
+      },
+      "runAfter": {
+        "For_each-copy": [
+          "SUCCEEDED"
+        ]
+      }
+    },
+    "HTTP": {
+      "type": "Http",
+      "inputs": {
+        "uri": "http://test.com",
+        "method": "GET",
+        "body": "@variables('ArrayVariable')"
+      },
+      "runAfter": {
+        "Filter_array": [
+          "SUCCEEDED"
+        ]
+      },
+      "runtimeConfiguration": {
+        "contentTransfer": {
+          "transferMode": "Chunked"
+        }
+      }
+    },
+    "For_each": {
+      "type": "foreach",
+      "foreach": "@triggerOutputs()?['body']?['array']",
+      "actions": {
+        "Parse_JSON": {
+          "type": "ParseJson",
+          "inputs": {
+            "content": "@{triggerBody()?['string']}@{variables('ArrayVariable')}@{item()?['item1']}",
+            "schema": {
+              "type": "array",
+              "items": {
+                "type": "object",
+                "properties": {
+                  "document": {
+                    "type": "string"
+                  },
+                  "min": {
+                    "type": "integer"
+                  },
+                  "policy": {
+                    "type": "string"
+                  }
+                },
+                "required": [
+                  "document",
+                  "min",
+                  "policy"
+                ]
+              }
+            }
+          }
+        }
+      },
+      "runAfter": {
+        "Initialize_ArrayVariable": [
+          "SUCCEEDED"
+        ]
+      }
+    },
+    "For_each-copy": {
+      "type": "foreach",
+      "foreach": "@triggerOutputs()?['body']?['array']",
+      "actions": {
+        "Parse_JSON_1": {
+          "type": "ParseJson",
+          "inputs": {
+            "content": "@{triggerBody()?['string']}@{variables('ArrayVariable')}@{item()?['item1']}",
+            "schema": {
+              "type": "array",
+              "items": {
+                "type": "object",
+                "properties": {
+                  "document": {
+                    "type": "string"
+                  },
+                  "min": {
+                    "type": "integer"
+                  },
+                  "policy": {
+                    "type": "string"
+                  }
+                },
+                "required": [
+                  "document",
+                  "min",
+                  "policy"
+                ]
+              }
+            }
+          }
+        }
+      },
+      "runAfter": {
+        "For_each": [
+          "SUCCEEDED"
+        ]
+      }
+    }
+  },
+  "contentVersion": "1.0.0.0",
+  "outputs": {},
+  "triggers": {
+    "manual": {
+      "type": "Request",
+      "kind": "Http",
+      "inputs": {
+        "schema": {
+          "type": "object",
+          "properties": {
+            "array": {
+              "type": "array",
+              "items": {
+                "type": "object",
+                "properties": {
+                  "item1": {
+                    "type": "string"
+                  }
+                },
+                "required": [
+                  "item1"
+                ]
+              }
+            },
+            "string": {
+              "type": "string"
+            },
+            "number": {
+              "type": "integer"
+            }
+          }
+        }
+      }
+    }
+  }
+}
