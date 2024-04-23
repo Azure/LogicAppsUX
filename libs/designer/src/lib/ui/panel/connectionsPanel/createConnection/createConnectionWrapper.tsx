4b60f37973cf53c1eacdc13203cc746b66e2c0b2
preetriti1 <48265693+preetriti1@users.noreply.github.com>
2024-04-23 15:38:43 -0700

    feat(designer): Adding MSI capability for Azure functions (#4662)
    * feat(designer): Adding MSI capability for Azure functions
    
    * Updating for back compatibility
    
    ---------
    
    Co-authored-by: Priti Sambandam <psamband@microsoft.com>

--- libs/designer/src/lib/ui/panel/connectionsPanel/createConnection/createConnectionWrapper.tsx
+++ libs/designer/src/lib/ui/panel/connectionsPanel/createConnection/createConnectionWrapper.tsx
@@ -23,7 +23,6 @@ import {
   getAssistedConnectionProps,
   getConnectionParametersForAzureConnection,
   getSupportedParameterSets,
-  isUserAssignedIdentitySupportedForInApp,
 } from '../../../../core/utils/connectors/connections';
 import { CreateConnection } from './createConnection';
 import { Spinner } from '@fluentui/react-components';
@@ -34,7 +33,6 @@ import {
   WorkflowService,
   getIconUriFromConnector,
   getRecordEntry,
-  safeSetObjectPropertyValue,
   type ConnectionCreationInfo,
   type ConnectionParametersMetadata,
   type Connection,
@@ -160,24 +158,6 @@ export const CreateConnectionWrapper = () => {
       }
 
       try {
-        // Assign connection parameters from resource selector experience
-        if (assistedConnectionProps) {
-          const assistedParams = await getConnectionParametersForAzureConnection(
-            operationManifest?.properties.connection?.type,
-            selectedSubResource
-          );
-          outputParameterValues = { ...outputParameterValues, ...assistedParams };
-        }
-
-        // Assign identity selected in parameter values for in-app connectors
-        if (
-          isUserAssignedIdentitySupportedForInApp(connector.properties.capabilities) &&
-          identitySelected &&
-          identitySelected !== constants.SYSTEM_ASSIGNED_MANAGED_IDENTITY
-        ) {
-          safeSetObjectPropertyValue(outputParameterValues, ['identity'], identitySelected);
-        }
-
         // If oauth, find the oauth parameter and assign the redirect url
         if (isOAuthConnection && selectedParameterSet) {
           const oAuthParameter = Object.entries(selectedParameterSet?.parameters).find(
@@ -189,6 +169,16 @@ export const CreateConnectionWrapper = () => {
             const redirectUrl = oAuthParameterObj?.oAuthSettings?.redirectUrl;
             outputParameterValues[oAuthParameterKey] = redirectUrl;
           }
+        }
+
+        // Assign connection parameters from resource selector experience
+        if (assistedConnectionProps) {
+          outputParameterValues = await getConnectionParametersForAzureConnection(
+            operationManifest?.properties.connection?.type,
+            selectedSubResource,
+            outputParameterValues,
+            !!selectedParameterSet // TODO: Should remove this when backend updates all connection parameters for functions and apim
+          );
         }
 
         const connectionInfo: ConnectionCreationInfo = {
@@ -318,11 +308,7 @@ export function getConnectionParameterSetValues(
     values: Object.keys(outputParameterValues).reduce((acc: any, key) => {
       // eslint-disable-next-line no-param-reassign
       acc[key] = {
-        value:
-          outputParameterValues[key] ??
-          // Avoid 'undefined', which causes the 'value' property to be removed when serializing as JSON object,
-          // and breaks contracts validation.
-          null,
+        value: outputParameterValues[key],
       };
       return acc;
     }, {}),
