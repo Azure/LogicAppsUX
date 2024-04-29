912f54043a36984ca1967efcff79f1cd652f2ab6
Travis Harris <hartra344@users.noreply.github.com>
2024-04-29 12:00:06 -0700

    test(e2e): Clean up and fix up some mock e2e test using newer playwright apis (#4723)
    * test(e2e): Fix up E2E tests a bit
    
    * test(e2e): fix up e2e tests
    
    * fix some tests
    
    * fix(Designer): Copy Paste Fixes (#4725)
    
    * fixScopePaste
    
    * remove logs
    
    * fix test
    
    * fix tests
    
    * i forgot to update the original one
    
    * readd tests
    
    * readd tests
    
    ---------
    
    Co-authored-by: Eric Wu <95886809+Eric-B-Wu@users.noreply.github.com>

--- e2e/designer/real-api/sanity-test.spec.ts
+++ e2e/designer/real-api/sanity-test.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@playwright/test';
-import { GoToWorkflow } from '../utils/GoToWorkflow';
+import { GoToRealWorkflow } from '../utils/GoToWorkflow';
 
 test.describe(
   'Sanity Check',
@@ -9,16 +9,16 @@ test.describe(
   () => {
     test('Sanity Check', async ({ page, request, browserName }) => {
       await page.goto('/');
-      await GoToWorkflow(page, `wapp-lauxtest2${browserName}`, 'testWorkflow1');
+      await GoToRealWorkflow(page, `wapp-lauxtest2${browserName}`, 'testWorkflow1');
       await page.getByTestId('card-When a HTTP request is received').getByLabel('When a HTTP request is').click();
       await page.getByRole('combobox', { name: 'Method' }).click();
       await page.getByRole('option', { name: 'GET' }).click();
       await page.getByLabel('Insert a new step after When').click();
       await page.getByRole('menuitem', { name: 'Add an action' }).click();
-      await page.getByPlaceholder('Search').fill('response',);
+      await page.getByPlaceholder('Search').fill('response');
       await page.getByLabel('Response This is an incoming').click();
       await page.getByLabel('Body').getByRole('paragraph').click();
-      await page.getByLabel('Body').fill('Test Body',);
+      await page.getByLabel('Body').fill('Test Body');
       await page.getByRole('menuitem', { name: 'Save Save' }).click();
 
       await page.waitForResponse((resp) => resp.url().includes('/deployWorkflowArtifacts') && resp.status() === 200);
