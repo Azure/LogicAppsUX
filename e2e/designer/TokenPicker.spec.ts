01b95b3cb0a36bd895ebaff18b3ab09c7e1fdde7
Travis Harris <travisharris@microsoft.com>
2024-04-29 22:31:27 -0700

    Add test for regression

---
+++ e2e/designer/TokenPicker.spec.ts
@@ -0,0 +1,29 @@
+import test, { expect } from '@playwright/test';
+import { GoToMockWorkflow } from './utils/GoToWorkflow';
+
+test.describe(
+  'Token Picker Tests',
+  {
+    tag: '@mock',
+  },
+  async () => {
+    test('Should serialize the workflow after deserializing it and match', async ({ page }) => {
+      await page.goto('/');
+
+      await GoToMockWorkflow(page, 'Panel');
+      await page.getByLabel('Parse JSON operation, Data').click();
+      await page.getByLabel('Content').click();
+      await page.locator('button').filter({ hasText: '' }).click();
+      await expect(page.getByRole('button', { name: 'EILCO Admin Nominations-OCSA' })).toBeVisible();
+      await page.getByPlaceholder('Search').click();
+      await page.getByPlaceholder('Search').fill('OCSA');
+      await expect(page.getByRole('button', { name: 'EILCO Admin Nominations-OCSA' })).toBeVisible();
+      await page.getByPlaceholder('Search').click();
+      await page.getByPlaceholder('Search').fill('_L2');
+      await expect(page.getByRole('button', { name: 'EILCO Admin Nominations-OCSA' })).toBeVisible();
+      await page.getByPlaceholder('Search').click();
+      await page.getByPlaceholder('Search').fill('OCSR');
+      await expect(page.getByRole('button', { name: 'EILCO Admin Nominations-OCSA' })).toBeVisible();
+    });
+  }
+);
