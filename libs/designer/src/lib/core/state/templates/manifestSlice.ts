a16526645f4ab226729c403fc57eea668bdffcef
Elaina Lee <hyehwalee@microsoft.com>
2024-05-01 11:46:50 -0700

    added submodule properly & added loading manifestNames

--- libs/designer/src/lib/core/state/templates/manifestSlice.ts
+++ libs/designer/src/lib/core/state/templates/manifestSlice.ts
@@ -1,27 +1,55 @@
 import type { Manifest } from '@microsoft/logic-apps-shared';
 import type { PayloadAction } from '@reduxjs/toolkit';
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 export interface ManifestState {
+  availableManifestNames: ManifestName[];
   availableManifests?: Record<ManifestName, Manifest>;
 }
 
 type ManifestName = string;
 
-export const initialManifestState: ManifestState = {};
+export const initialManifestState: ManifestState = {
+  availableManifestNames: [],
+};
+
+export const loadManifestNames = createAsyncThunk('manifest/loadManifestNames', async () => {
+  return loadManifestsFromGithub();
+});
 
 export const manifestSlice = createSlice({
   name: 'manifest',
   initialState: initialManifestState,
   reducers: {
+    setAvailableManifestsNames: (state, action: PayloadAction<ManifestName[] | undefined>) => {
+      if (action.payload) {
+        state.availableManifestNames = action.payload;
+      }
+    },
     setAvailableManifests: (state, action: PayloadAction<Record<ManifestName, Manifest> | undefined>) => {
       if (action.payload) {
         state.availableManifests = action.payload;
       }
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(loadManifestNames.fulfilled, (state, action) => {
+      state.availableManifestNames = action.payload ?? [];
+    });
+
+    builder.addCase(loadManifestNames.rejected, (state) => {
+      // TODO change to null for error handling case
+      state.availableManifestNames = [];
+    });
+  },
 });
 
-export const { setAvailableManifests } = manifestSlice.actions;
-
-export default manifestSlice.reducer;
+const loadManifestsFromGithub = async (): Promise<ManifestName[] | undefined> => {
+  try {
+    const manifestNames: ManifestName[] = await import('../../templates/samples/manifest.json');
+    return (manifestNames as any)?.default ?? manifestNames;
+  } catch (ex) {
+    console.error(ex);
+    return undefined;
+  }
+};
--- libs/designer/src/lib/core/templates/TemplatesDataProvider.tsx
+++ libs/designer/src/lib/core/templates/TemplatesDataProvider.tsx
@@ -1,8 +1,10 @@
 import { TemplatesWrappedContext } from './TemplatesDesignerContext';
 // import { Theme as ThemeType } from '@microsoft/logic-apps-shared';
 import type React from 'react';
-import { useContext } from 'react';
-// import { useDispatch } from 'react-redux';
+import { useContext, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import type { AppDispatch } from '../state/templates/store';
+import { loadManifestNames } from '../state/templates/manifestSlice';
 
 export interface TemplatesDataProviderProps {
   kinds?: ('stateful' | 'stateless')[];
@@ -17,10 +19,14 @@ const DataProviderInner = ({
   //   theme = ThemeType.Light,
   children,
 }: TemplatesDataProviderProps) => {
-  //   const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch<AppDispatch>();
 
   console.log('Kinds and SKUs; TODO: only show those qualified ones', kinds, skus);
   //TODO: load templates from ./samples folder.
+
+  useEffect(() => {
+    dispatch(loadManifestNames());
+  }, [dispatch]);
 
   return <>{children}</>;
 };
