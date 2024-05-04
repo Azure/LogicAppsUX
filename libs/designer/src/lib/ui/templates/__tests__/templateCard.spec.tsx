b4dcce44a0ea3392cf0d7d963d076a3ae90e5538
Elaina Lee <hyehwalee@microsoft.com>
2024-05-03 20:36:49 -0700

    divided tests into templateDesigner and templateCard

--- libs/designer/src/lib/ui/templates/__tests__/templateCard.spec.tsx
+++ libs/designer/src/lib/ui/templates/__tests__/templateCard.spec.tsx
@@ -3,8 +3,8 @@ import type { AppStore } from '../../../core/state/templates/store';
 import { setupStore, type RootState } from '../../../core/state/templates/store';
 import type { Template } from '@microsoft/logic-apps-shared';
 import { renderWithProviders } from '../../../__test__/template-test-utils';
-import { TemplatesDesigner } from '../TemplatesDesigner';
 import { screen } from '@testing-library/react';
+import { TemplateCard } from '../cards/templateCard';
 
 describe('ui/templates/templatesDesigner', () => {
   let store: AppStore;
@@ -62,7 +62,7 @@ describe('ui/templates/templatesDesigner', () => {
     };
   });
 
-  it('Fetches templates and display the title and description', async () => {
+  it('Renders TemplateCard and loads template state correctly on buttons click', async () => {
     minimalStoreData = {
       manifest: {
         availableTemplateNames: ['template1', 'template2'],
@@ -74,11 +74,37 @@ describe('ui/templates/templatesDesigner', () => {
     };
     store = setupStore(minimalStoreData);
 
-    renderWithProviders(<TemplatesDesigner />, { store });
+    renderWithProviders(<TemplateCard templateName="template1" />, { store });
 
-    expect(screen.getByText(/Template 1/i)).toBeDefined();
-    expect(screen.getByText(/Template 1 Description/i)).toBeDefined();
-    expect(screen.getByText(/Template 2/i)).toBeDefined();
-    expect(screen.getByText(/Template 2 Description/i)).toBeDefined();
+    expect(screen.getByText('Create Workflow')).toBeDefined();
+    expect(screen.getByText('Quick View')).toBeDefined();
+    screen.getByText('Create Workflow').click();
+    expect(store.getState().template.templateName).toBe('template1');
+  });
+
+  it('Renders TemplateCard and Opens the right panel', async () => {
+    minimalStoreData = {
+      manifest: {
+        availableTemplateNames: ['template1', 'template2'],
+        availableTemplates: {
+          template1: template1Manifest,
+          template2: template2Manifest,
+        },
+      },
+    };
+    store = setupStore(minimalStoreData);
+
+    renderWithProviders(<TemplateCard templateName="template2" />, { store });
+
+    expect(screen.getByText('Create Workflow')).toBeDefined();
+    expect(screen.getByText('Quick View')).toBeDefined();
+    screen.getByText('Create Workflow').click();
+    expect(store.getState().panel.isOpen).toBe(true);
+    expect(store.getState().panel.currentPanelView).toBe('createWorkflow');
+    store.dispatch({ type: 'panel/closePanel' });
+    expect(store.getState().panel.isOpen).toBe(false);
+    screen.getByText('Quick View').click();
+    expect(store.getState().panel.isOpen).toBe(true);
+    expect(store.getState().panel.currentPanelView).toBe('quickView');
   });
 });
