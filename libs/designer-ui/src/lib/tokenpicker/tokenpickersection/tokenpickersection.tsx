10a84ce9c62f5a5d891ea62ff49835134f9c541f
Eric Wu <95886809+Eric-B-Wu@users.noreply.github.com>
2024-05-06 17:39:12 -0400

    fix(Designer): Only show dropdown for array editor when there are options (#4783)
    change\

--- libs/designer-ui/src/lib/tokenpicker/tokenpickersection/tokenpickersection.tsx
+++ libs/designer-ui/src/lib/tokenpicker/tokenpickersection/tokenpickersection.tsx
@@ -66,7 +66,7 @@ export const TokenPickerSection = ({
       style={{
         maxHeight: fullScreen
           ? windowDimensions.height - (expressionEditorCurrentHeight + 287)
-          : Math.min(windowDimensions.height - (expressionEditorCurrentHeight + 197), 180),
+          : Math.min(windowDimensions.height - (expressionEditorCurrentHeight + 197), 540),
       }}
     >
       {searchQuery && noItems ? <TokenPickerNoMatches /> : null}
