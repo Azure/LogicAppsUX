696d2ea9d2b3833d9a4dd230203ec5ac1718bec7
Travis Harris <hartra344@users.noreply.github.com>
2024-05-07 00:59:35 -0700

    test(designer): Add tests for display of scope ids (#4792)
    * test(designer): Add tests for display of scope ids
    
    * test(designer): Add tests for display of scope ids

--- libs/logic-apps-shared/src/utils/src/lib/helpers/__test__/stringFunctions.spec.ts
+++ libs/logic-apps-shared/src/utils/src/lib/helpers/__test__/stringFunctions.spec.ts
@@ -1,11 +1,29 @@
-import { labelCase } from '../stringFunctions';
-import { describe, vi, beforeEach, afterEach, beforeAll, afterAll, it, test, expect } from 'vitest';
-describe('lib/helpers/stringFunctions', () => {
+import { idDisplayCase, labelCase } from '../stringFunctions';
+import { describe, it, expect } from 'vitest';
+describe('label_case', () => {
   it('should replace _ with spaces', () => {
     expect(labelCase('Test_Test2')).toEqual('Test Test2');
   });
 
   it('should replace all _ with spaces', () => {
     expect(labelCase('Test_Test2_Test3_Test4')).toEqual('Test Test2 Test3 Test4');
+  });
+});
+
+describe('idDisplayCase', () => {
+  it('should correctly format a string with an ID tag', () => {
+    expect(idDisplayCase('Test_ID-#Scope')).toEqual('Test ID');
+  });
+
+  it('should correctly format a string without an ID tag', () => {
+    expect(idDisplayCase('Test_ID')).toEqual('Test ID');
+  });
+
+  it('should handle an empty string', () => {
+    expect(idDisplayCase('')).toEqual('');
+  });
+
+  it('should handle a string with only an ID tag', () => {
+    expect(idDisplayCase('-#Scope')).toEqual('');
   });
 });
