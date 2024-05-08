1580602ddaa9662c44111e4f0b1d1c033c7acb20
Eric Wu <95886809+Eric-B-Wu@users.noreply.github.com>
2024-05-08 17:11:03 -0400

    feat(designer): Adding options to switch between Local Time and UTC for monitoring view (#4775)
    * add UTC to monitoring times
    
    * moved changes
    
    * update
    
    * update specs
    
    * fix
    
    * Update snapshots
    
    * see if this works
    
    * remove unnecssary code
    
    * Revert "fix(Designer): Updated HTML Editor to support newline characters (for dynamic content) (#4635)"
    
    This reverts commit f4e1f8bd2e287e4f254c55d0f0b9ea03593cb1a0.
    
    * testing if this fixes
    
    * more attempts
    
    * more attempts
    
    * adding unit tests
    
    * readding file and praying it works
    
    * please give me some coverage!
    
    ---------
    
    Co-authored-by: Travis Harris <travisharris@microsoft.com>
    Co-authored-by: Travis Harris <hartra344@users.noreply.github.com>

--- libs/designer-ui/src/lib/monitoring/values/__test__/number.spec.tsx
+++ libs/designer-ui/src/lib/monitoring/values/__test__/number.spec.tsx
@@ -3,6 +3,7 @@ import { getTestIntl } from '../../../__test__/intl-test-helper';
 import { NumberValue } from '../number';
 import type { ValueProps } from '../types';
 import { describe, vi, beforeEach, afterEach, beforeAll, afterAll, it, test, expect } from 'vitest';
+import React from 'react';
 describe('ui/monitoring/values/number', () => {
   let props: ValueProps, renderer: ReactShallowRenderer.ShallowRenderer;
 
