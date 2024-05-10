e76f41474a65af26a98d6b8004ca5c519c259ef0
ccastrotrejo <ccastrotrejo@microsoft.com>
2024-05-10 08:22:53 -0700

    Update tests and snapshots

--- libs/designer-ui/src/lib/unitTesting/assertionsPanel/__tests__/assertion.spec.tsx
+++ libs/designer-ui/src/lib/unitTesting/assertionsPanel/__tests__/assertion.spec.tsx
@@ -1,30 +1,28 @@
+// biome-ignore lint/correctness/noUnusedImports: actually is used
+import React from 'react';
 import { Assertion, type AssertionProps } from '../index';
 import { initializeIcons } from '@fluentui/react';
 import TestRenderer from 'react-test-renderer';
 import * as ReactShallowRenderer from 'react-test-renderer/shallow';
+import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest';
 
 describe('ui/unitTesting/assertionsPanel/assertion', () => {
-  let minimal: AssertionProps, renderer: ReactShallowRenderer.ShallowRenderer;
+  let minimal: AssertionProps;
+  let renderer: ReactShallowRenderer.ShallowRenderer;
 
   beforeEach(() => {
     minimal = {
+      id: 'test-id',
       assertion: {
         id: 'test-id',
         name: 'test-name',
         description: 'test-description',
-        expression: {
-          items: {
-            type: 'group',
-            items: [],
-          },
-        },
+        expression: '@equals(1, 1)',
         isEditable: true,
       },
-      onAssertionDelete: jest.fn(),
-      onAssertionUpdate: jest.fn(),
-      getTokenPicker: jest.fn(),
-      tokenMapping: {},
-      loadParameterValueFromString: jest.fn(),
+      onAssertionDelete: vi.fn(),
+      onAssertionUpdate: vi.fn(),
+      getConditionExpression: vi.fn(),
       validationErrors: {},
       isInverted: false,
     };
@@ -50,26 +48,7 @@ describe('ui/unitTesting/assertionsPanel/assertion', () => {
 
   it('should render the component with validation errors and error traffic light dot', () => {
     minimal.assertion.name = '';
-    minimal.assertion.expression = {
-      items: {
-        type: 'group',
-        items: [
-          {
-            type: 'row',
-            checked: false,
-            operand1: [],
-            operator: 'equals',
-            operand2: [
-              {
-                id: 'C0531439-5F53-4351-82E3-845772813C23',
-                type: 'literal',
-                value: '1',
-              },
-            ],
-          },
-        ],
-      },
-    };
+    minimal.assertion.expression = '';
 
     minimal.validationErrors = {
       name: 'Must provide the Assertion name.',
