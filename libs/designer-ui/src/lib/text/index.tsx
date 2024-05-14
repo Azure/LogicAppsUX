c480d36df3146237b9be657efde9f5b2d171f184
ccastrotrejo <ccastrotrejo@microsoft.com>
2024-05-13 17:25:08 -0700

    Add large size and add classname as prop

--- libs/designer-ui/src/lib/text/index.tsx
+++ libs/designer-ui/src/lib/text/index.tsx
@@ -6,25 +6,30 @@ const BuiltInText = ({
   size,
   weight,
   style,
+  className,
 }: {
   text: string;
   size: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 1000;
   weight: 'regular' | 'medium' | 'semibold' | 'bold';
   style?: CSSProperties;
+  className?: string;
 }) => {
   return (
-    <Text size={size} weight={weight} style={style}>
+    <Text size={size} weight={weight} style={style} className={className}>
       {text}
     </Text>
   );
 };
 
-export const XLargeText = ({ text, style }: { text: string; style?: CSSProperties }) => {
-  return <BuiltInText text={text} size={500} weight={'medium'} style={style} />;
+export const XLargeText = ({ text, style, className }: { text: string; style?: CSSProperties; className?: string }) => {
+  return <BuiltInText text={text} size={500} weight={'medium'} style={style} className={className} />;
 };
-export const MediumText = ({ text, style }: { text: string; style?: CSSProperties }) => {
-  return <BuiltInText text={text} size={300} weight={'regular'} style={style} />;
+export const LargeText = ({ text, style, className }: { text: string; style?: CSSProperties; className?: string }) => {
+  return <BuiltInText text={text} size={400} weight={'regular'} style={style} className={className} />;
 };
-export const SmallText = ({ text, style }: { text: string; style?: CSSProperties }) => {
-  return <BuiltInText text={text} size={200} weight={'regular'} style={style} />;
+export const MediumText = ({ text, style, className }: { text: string; style?: CSSProperties; className?: string }) => {
+  return <BuiltInText text={text} size={300} weight={'regular'} style={style} className={className} />;
+};
+export const SmallText = ({ text, style, className }: { text: string; style?: CSSProperties; className?: string }) => {
+  return <BuiltInText text={text} size={200} weight={'regular'} style={style} className={className} />;
 };
