0c33913543eb785c4094566df2e0208d4d96589e
ccastrotrejo <ccastrotrejo@microsoft.com>
2024-05-14 10:16:21 -0700

    Add snapshots for text component

--- libs/designer-ui/src/lib/text/index.tsx
+++ libs/designer-ui/src/lib/text/index.tsx
@@ -1,19 +1,18 @@
 import { Text } from '@fluentui/react-components';
 import type { CSSProperties } from 'react';
 
-const BuiltInText = ({
-  text,
-  size,
-  weight,
-  style,
-  className,
-}: {
+export interface TextProps {
   text: string;
-  size: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 1000;
-  weight: 'regular' | 'medium' | 'semibold' | 'bold';
   style?: CSSProperties;
   className?: string;
-}) => {
+}
+
+interface BuiltInTextProps extends TextProps {
+  size: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 1000;
+  weight: 'regular' | 'medium' | 'semibold' | 'bold';
+}
+
+const BuiltInText = ({ text, size, weight, style, className }: BuiltInTextProps) => {
   return (
     <Text size={size} weight={weight} style={style} className={className}>
       {text}
@@ -21,18 +20,18 @@ const BuiltInText = ({
   );
 };
 
-export const XXLargeText = ({ text, style, className }: { text: string; style?: CSSProperties; className?: string }) => {
+export const XXLargeText = ({ text, style, className }: TextProps) => {
   return <BuiltInText text={text} size={700} weight={'semibold'} style={style} className={className} />;
 };
-export const XLargeText = ({ text, style, className }: { text: string; style?: CSSProperties; className?: string }) => {
+export const XLargeText = ({ text, style, className }: TextProps) => {
   return <BuiltInText text={text} size={500} weight={'medium'} style={style} className={className} />;
 };
-export const LargeText = ({ text, style, className }: { text: string; style?: CSSProperties; className?: string }) => {
+export const LargeText = ({ text, style, className }: TextProps) => {
   return <BuiltInText text={text} size={400} weight={'regular'} style={style} className={className} />;
 };
-export const MediumText = ({ text, style, className }: { text: string; style?: CSSProperties; className?: string }) => {
+export const MediumText = ({ text, style, className }: TextProps) => {
   return <BuiltInText text={text} size={300} weight={'regular'} style={style} className={className} />;
 };
-export const SmallText = ({ text, style, className }: { text: string; style?: CSSProperties; className?: string }) => {
+export const SmallText = ({ text, style, className }: TextProps) => {
   return <BuiltInText text={text} size={200} weight={'regular'} style={style} className={className} />;
 };
