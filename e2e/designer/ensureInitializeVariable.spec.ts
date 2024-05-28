6f470fc30e707b6130cc7621bf2bb73a91761afc
preetriti1 <48265693+preetriti1@users.noreply.github.com>
2024-05-28 11:15:46 -0700

    fix(designer): Using custom Label with ariaLabel for parameter editors for a11y (#4889)
    Co-authored-by: Priti Sambandam <psamband@microsoft.com>

--- e2e/designer/ensureInitializeVariable.spec.ts
+++ e2e/designer/ensureInitializeVariable.spec.ts
@@ -16,24 +16,24 @@ test(
     await page.getByPlaceholder('Search').fill('initialize variable');
     await page.getByLabel('Initialize variable').click();
     await page.getByRole('paragraph').click();
-    await page.getByLabel('Name').fill('test');
+    await page.getByLabel('Name*').fill('test');
     await page.getByPlaceholder('Enter initial value').click();
     await page.getByRole('option', { name: 'true' }).click();
     await page.getByText('Boolean').click();
     await page.getByRole('option', { name: 'Integer' }).click();
-    await page.getByLabel('Value').getByRole('paragraph').click();
-    await page.getByLabel('Value').fill('test');
+    await page.getByTestId('msla-setting-token-editor-stringeditor-value').click();
+    await page.getByTestId('msla-setting-token-editor-stringeditor-value').getByRole('paragraph').fill('test');
     await page
       .locator('#msla-node-details-panel-Initialize_variable div')
-      .filter({ hasText: 'NametestAdd dynamic data or' })
+      .filter({ hasText: 'Name*testAdd dynamic data or' })
       .first()
       .click();
     await expect(page.getByRole('alert')).toContainText('Enter a valid integer.');
-    await page.getByLabel('Value').getByRole('paragraph').click();
-    await page.getByLabel('Value').fill('12');
+    await page.getByTestId('msla-setting-token-editor-stringeditor-value').click();
+    await page.getByTestId('msla-setting-token-editor-stringeditor-value').getByRole('paragraph').fill('12');
     await page
       .locator('#msla-node-details-panel-Initialize_variable div')
-      .filter({ hasText: 'NametestAdd dynamic data or' })
+      .filter({ hasText: 'Name*testAdd dynamic data or' })
       .first()
       .click();
 
