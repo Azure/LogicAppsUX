df6b7162f599111e05bbd4fca47522badbd5f51d
Travis Harris <travisharris@microsoft.com>
2024-06-06 17:57:42 -0700

    chore(git): Rename templatesManifestFolder

--- libs/designer/src/lib/core/state/templates/store.ts
+++ libs/designer/src/lib/core/state/templates/store.ts
@@ -18,7 +18,7 @@ export const setupStore = (preloadedState?: Partial<RootState>) => {
 };
 
 export const templateStore = setupStore();
-export const templatesPathFromState = '../../templates/samples';
+export const templatesPathFromState = '../../templates/templateFiles';
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = AppStore['dispatch'];
