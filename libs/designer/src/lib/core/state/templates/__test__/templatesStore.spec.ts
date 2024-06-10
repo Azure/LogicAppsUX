e929d4dc4011b1a73bfac9dde8fdd0eb6c16bc05
Elaina Lee <hyehwalee@microsoft.com>
2024-06-10 15:13:45 -0700

    added manifest slice tests on the same file for templatesStore

--- libs/designer/src/lib/core/state/templates/__test__/templatesStore.spec.ts
+++ libs/designer/src/lib/core/state/templates/__test__/templatesStore.spec.ts
@@ -1,6 +1,7 @@
 import { describe, beforeAll, it, expect } from 'vitest';
 import { AppStore, setupStore } from '../store';
 import { changeCurrentTemplateName, updateKind, updateWorkflowName } from '../templateSlice';
+import { manifestSlice, setavailableTemplates, setavailableTemplatesNames } from '../manifestSlice';
 
 describe('template store reducers', () => {
   let store: AppStore;
@@ -19,11 +20,15 @@ describe('template store reducers', () => {
         },
         connections: [],
       },
+      manifest: {
+        availableTemplateNames: undefined,
+      },
     });
   });
 
-  it('initial state tests for template slice', async () => {
+  it('initial state tests for template store', async () => {
     expect(store.getState().template.workflowName).toBe('');
+    expect(store.getState().manifest.availableTemplateNames).toBe(undefined);
   });
 
   it('update state call tests for template slice', async () => {
@@ -32,6 +37,35 @@ describe('template store reducers', () => {
 
     store.dispatch(updateWorkflowName('workflowName1'));
     expect(store.getState().template.workflowName).toBe('workflowName1');
+
+    store.dispatch(updateKind('kind1'));
+    expect(store.getState().template.kind).toBe('kind1');
+  });
+
+  it('update state call tests for manifest slice', async () => {
+    store.dispatch(setavailableTemplatesNames(['templateName1']));
+    expect(store.getState().manifest.availableTemplateNames).toEqual(['templateName1']);
+
+    const manifest1 = {
+      title: 'title',
+      description: 'description',
+      skus: ['sku1'],
+      kinds: ['kind1'],
+      artifacts: [],
+      parameters: [],
+      connections: [],
+    };
+    store.dispatch(
+      setavailableTemplates({
+        workflowName1: manifest1,
+        workflowName2: manifest1,
+      })
+    );
+    expect(store.getState().manifest.availableTemplates).toEqual({
+      workflowName1: manifest1,
+      workflowName2: manifest1,
+    });
+    expect(store.getState().manifest.availableTemplates?.['workflowName1']).toEqual(manifest1);
 
     store.dispatch(updateKind('kind1'));
     expect(store.getState().template.kind).toBe('kind1');
