027b8b70fbeaa1acdf035b12a23eef4404366527
e.k <1350074+ek68794998@users.noreply.github.com>
2024-06-13 18:06:48 -0700

    fix(designer-ui): Remove problematic call to `setIsValuePlaintext` (#4979)
    * Remove problematic call to `setIsValuePlaintext`
    
    * Clean up an unused variable
    
    * Ensure 'safe for lexical' is checked when switching view

--- libs/designer-ui/src/lib/html/plugins/toolbar/helper/HTMLChangePlugin.tsx
+++ libs/designer-ui/src/lib/html/plugins/toolbar/helper/HTMLChangePlugin.tsx
@@ -22,31 +22,20 @@ import { $getRoot } from 'lexical';
 export interface HTMLChangePluginProps {
   isValuePlaintext: boolean;
   setIsSwitchFromPlaintextBlocked: (value: boolean) => void;
-  setIsValuePlaintext: (isValuePlaintext: boolean) => void;
   setValue: (newVal: ValueSegment[]) => void;
 }
 
-export const HTMLChangePlugin = ({
-  isValuePlaintext,
-  setIsSwitchFromPlaintextBlocked,
-  setIsValuePlaintext,
-  setValue,
-}: HTMLChangePluginProps) => {
+export const HTMLChangePlugin = ({ isValuePlaintext, setIsSwitchFromPlaintextBlocked, setValue }: HTMLChangePluginProps) => {
   const onChange = (editorState: EditorState, editor: LexicalEditor) => {
     const nodeMap = new Map<string, ValueSegment>();
-    let isNewValuePlaintext = isValuePlaintext;
 
     editorState.read(() => {
       const editorString = getChildrenNodes($getRoot(), nodeMap);
       const isSafeForLexical = isHtmlStringValueSafeForLexical(editorString, nodeMap);
 
       setIsSwitchFromPlaintextBlocked(!isSafeForLexical);
-      if (!isSafeForLexical) {
-        isNewValuePlaintext = true;
-        setIsValuePlaintext(isNewValuePlaintext);
-      }
 
-      convertEditorState(editor, nodeMap, { isValuePlaintext: isNewValuePlaintext }).then(setValue);
+      convertEditorState(editor, nodeMap, { isValuePlaintext }).then(setValue);
     });
   };
   return <OnChangePlugin ignoreSelectionChange onChange={onChange} />;
