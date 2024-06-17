455666089fc34e5f12245e6862128249852f1aae
Krrish Mittal <krrishmittal15@gmail.com>
2024-06-17 14:00:33 -0700

    remove redundant func

--- libs/data-mapper-v2/src/components/addSchema/tree/SchemaTree.tsx
+++ libs/data-mapper-v2/src/components/addSchema/tree/SchemaTree.tsx
@@ -1,14 +1,10 @@
-import {
-  type SchemaExtended,
-  SchemaType,
-  equals,
-} from "@microsoft/logic-apps-shared";
-import { Tree, mergeClasses } from "@fluentui/react-components";
-import { useStyles } from "./styles";
-import { useState, useMemo, useRef, useEffect } from "react";
-import { useIntl } from "react-intl";
-import RecursiveTree from "./RecursiveTree";
-import { flattenSchemaIntoSortArray, flattenSchemaNodeMap } from "../../../utils";
+import { type SchemaExtended, SchemaType, equals } from '@microsoft/logic-apps-shared';
+import { Tree, mergeClasses } from '@fluentui/react-components';
+import { useStyles } from './styles';
+import { useState, useMemo, useRef, useEffect } from 'react';
+import { useIntl } from 'react-intl';
+import RecursiveTree from './RecursiveTree';
+import { flattenSchemaNodeMap } from '../../../utils';
 
 export type SchemaTreeProps = {
   schemaType?: SchemaType;
@@ -22,33 +18,25 @@ export const SchemaTree = (props: SchemaTreeProps) => {
     schema: { schemaTreeRoot },
   } = props;
 
-  const isLeftDirection = useMemo(
-    () => equals(schemaType, SchemaType.Source),
-    [schemaType]
-  );
+  const isLeftDirection = useMemo(() => equals(schemaType, SchemaType.Source), [schemaType]);
   const [openKeys, setOpenKeys] = useState<Set<string>>(new Set());
   const intl = useIntl();
   const treeRef = useRef<HTMLDivElement | null>(null);
-  const flattenedScehma = useMemo(() => flattenSchemaNodeMap(schemaTreeRoot), [schemaTreeRoot]);
+  const flattenedScehmaMap = useMemo(() => flattenSchemaNodeMap(schemaTreeRoot), [schemaTreeRoot]);
 
   const treeAriaLabel = intl.formatMessage({
-    defaultMessage: "Schema tree",
-    id: "t2Xi1/",
-    description: "tree showing schema nodes",
+    defaultMessage: 'Schema tree',
+    id: 't2Xi1/',
+    description: 'tree showing schema nodes',
   });
 
   useEffect(() => {
-    const allNodes = flattenSchemaIntoSortArray(schemaTreeRoot);
-    setOpenKeys(new Set<string>(allNodes));
-  }, [schemaTreeRoot, setOpenKeys]);
+    setOpenKeys(new Set<string>(Object.keys(flattenedScehmaMap).filter((key) => flattenedScehmaMap[key].children.length > 0)));
+  }, [flattenedScehmaMap, setOpenKeys]);
   return schemaTreeRoot ? (
     <Tree
       ref={treeRef}
-      className={
-        isLeftDirection
-          ? mergeClasses(styles.leftWrapper, styles.wrapper)
-          : mergeClasses(styles.rightWrapper, styles.wrapper)
-      }
+      className={isLeftDirection ? mergeClasses(styles.leftWrapper, styles.wrapper) : mergeClasses(styles.rightWrapper, styles.wrapper)}
       aria-label={treeAriaLabel}
     >
       <RecursiveTree
@@ -56,7 +44,7 @@ export const SchemaTree = (props: SchemaTreeProps) => {
         isLeftDirection={isLeftDirection}
         setOpenKeys={setOpenKeys}
         openKeys={openKeys}
-        flattenedScehmaMap={flattenedScehma}
+        flattenedScehmaMap={flattenedScehmaMap}
       />
     </Tree>
   ) : null;
