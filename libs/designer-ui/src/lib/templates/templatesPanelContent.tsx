abcb6c00425a4b1d750aba2ade383becdd829dc3
Elaina Lee <hyehwalee@microsoft.com>
2024-06-17 14:01:48 -0700

    added styling and disabled, selected into tab title area

--- libs/designer-ui/src/lib/templates/templatesPanelContent.tsx
+++ libs/designer-ui/src/lib/templates/templatesPanelContent.tsx
@@ -1,14 +1,23 @@
-import { TabList, Tab, OverflowItem } from '@fluentui/react-components';
+import { Icon, css } from '@fluentui/react';
+import { TabList, Tab, Text, Button } from '@fluentui/react-components';
 import type { SelectTabData, SelectTabEvent } from '@fluentui/react-components';
 
 export interface TemplatesPanelContentProps {
   panelType: string;
+  isSequence?: boolean;
   tabs: any[];
   selectedTab?: string;
   selectTab: (tabId: string) => void;
 }
-export const TemplatesPanelContent = ({ panelType, tabs = [], selectedTab, selectTab }: TemplatesPanelContentProps): JSX.Element => {
+export const TemplatesPanelContent = ({
+  panelType,
+  isSequence = true,
+  tabs = [],
+  selectedTab,
+  selectTab,
+}: TemplatesPanelContentProps): JSX.Element => {
   const selectedTabId = selectedTab ?? tabs[0]?.id;
+  const selectedTabOrder = tabs?.find((tab) => tab.id === selectedTabId)?.order ?? 0;
 
   const onTabSelected = (e?: SelectTabEvent, data?: SelectTabData): void => {
     if (data) {
@@ -20,17 +29,36 @@ export const TemplatesPanelContent = ({ panelType, tabs = [], selectedTab, selec
   return (
     <div id={`msla-templates-panel-${panelType}`} className="msla-templates-panel">
       <TabList selectedValue={selectedTabId} onTabSelect={onTabSelected} style={{ margin: '0px -12px' }}>
-        {tabs.map(({ id, visible, title }) =>
+        {tabs.map(({ id, visible, title, order }, index) =>
           visible ? (
-            <OverflowItem key={id} id={id} priority={id === selectedTabId ? 2 : 1}>
-              <Tab value={id} role={'tab'}>
-                {title}
-              </Tab>
-            </OverflowItem>
+            <Tab
+              key={id}
+              id={id}
+              className={css('msla-templates-panel-tabName', id === selectedTabId && 'selected')}
+              value={id}
+              role={'tab'}
+              disabled={isSequence ? order > selectedTabOrder : selectedTab !== id}
+            >
+              {isSequence && (
+                <Text className="msla-templates-panel-tabName-index">
+                  {order < selectedTabOrder ? <Icon iconName="accept" /> : index + 1}
+                </Text>
+              )}
+              <Text className="msla-templates-panel-tabName-title">{title}</Text>
+            </Tab>
           ) : null
         )}
       </TabList>
-      <div className="msla-panel-content-container">{tabs.find((tab) => tab.id === selectedTabId)?.content}</div>
+      <div className="msla-panel-content-container">
+        {tabs.find((tab) => tab.id === selectedTabId)?.content}
+        <Button
+          onClick={() => {
+            console.log('--');
+          }}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 };
