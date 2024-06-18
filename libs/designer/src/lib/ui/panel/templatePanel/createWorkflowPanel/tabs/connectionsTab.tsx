8a64f12f40e49aa45d9add0ebf3e1d3ca48da5e2
Elaina-Lee <144840522+Elaina-Lee@users.noreply.github.com>
2024-06-18 11:32:56 -0700

    feat(Templates): Styling for Header, Tabs, Footer (#4992)
    * moved tab styling to designer-ui
    
    * added styling and disabled, selected into tab title area
    
    * enforced templatePanelTab props with type
    
    * added footer
    
    * moved panelTabs to upper level for panel footer
    
    * fully enabled footer for quickview panel
    
    * moved quickviewPanel header to use TemplatesPanelContent
    
    * added template panel header
    
    * changed reviewCreate tab to intl and create button
    
    * defined a global handle dismiss panel
    
    * brings to the valid next tab for sequence
    
    * added next button blocked for parameters and name state tab on missing info
    
    * fixed tests
    
    * fixed minor nits, addressing rest in next pr due to current pr size

--- libs/designer/src/lib/ui/panel/templatePanel/createWorkflowPanel/tabs/connectionsTab.tsx
+++ libs/designer/src/lib/ui/panel/templatePanel/createWorkflowPanel/tabs/connectionsTab.tsx
@@ -1,22 +1,18 @@
-import { isNullOrUndefined } from '@microsoft/logic-apps-shared';
-import type { RootState } from '../../../../../core/state/templates/store';
+import type { AppDispatch, RootState } from '../../../../../core/state/templates/store';
 import { useSelector } from 'react-redux';
 import type { IntlShape } from 'react-intl';
 import constants from '../../../../../common/constants';
 import { DisplayConnections } from '../../../../templates/connections/displayConnections';
+import type { TemplatePanelTab } from '@microsoft/designer-ui';
+import { selectPanelTab } from '../../../../../core/state/templates/panelSlice';
 
 export const ConnectionsPanel: React.FC = () => {
-  const { connections, manifest } = useSelector((state: RootState) => state.template);
+  const { connections } = useSelector((state: RootState) => state.template);
 
-  return isNullOrUndefined(manifest) ? null : (
-    <div>
-      Connections Tab Placeholder
-      {connections ? <DisplayConnections connections={connections} /> : <>PLACEHOLDER: no connections to be made</>}
-    </div>
-  );
+  return <DisplayConnections connections={connections} />;
 };
 
-export const connectionsTab = (intl: IntlShape) => ({
+export const connectionsTab = (intl: IntlShape, dispatch: AppDispatch, nextTabId: string): TemplatePanelTab => ({
   id: constants.TEMPLATE_PANEL_TAB_NAMES.CONNECTIONS,
   title: intl.formatMessage({
     defaultMessage: 'Connections',
@@ -29,7 +25,17 @@ export const connectionsTab = (intl: IntlShape) => ({
     description: 'An accessability label that describes the connections tab',
   }),
   visible: true,
-  content: <ConnectionsPanel />,
   order: 0,
-  icon: 'Info',
+  content: <ConnectionsPanel />,
+  footerContent: {
+    primaryButtonText: intl.formatMessage({
+      defaultMessage: 'Next',
+      id: '0UfxUM',
+      description: 'Button text for moving to the next tab in the create workflow panel',
+    }),
+    primaryButtonOnClick: () => {
+      dispatch(selectPanelTab(nextTabId));
+    },
+    primaryButtonDisabled: false,
+  },
 });
