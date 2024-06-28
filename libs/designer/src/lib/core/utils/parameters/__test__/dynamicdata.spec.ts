b7ee35d2ddc4a28967d5e49e6fe51c1fdf292200
preetriti1 <48265693+preetriti1@users.noreply.github.com>
2024-06-28 01:56:45 -0700

    fix(designer): Designer removes object type dynmic parameter when only template expressions are present (#5041)
    Co-authored-by: Priti Sambandam <psamband@microsoft.com>

---
+++ libs/designer/src/lib/core/utils/parameters/__test__/dynamicdata.spec.ts
@@ -0,0 +1,96 @@
+import { getDynamicInputsFromSchema } from '../dynamicdata';
+import { InitOperationManifestService } from '@microsoft/logic-apps-shared';
+import { expect, describe, test } from 'vitest';
+
+describe('DynamicData', () => {
+  describe('getDynamicInputsFromSchema', () => {
+    const manifestService: any = {
+      isSupported: () => true,
+      getOperationManifest() {
+        return Promise.resolve({ properties: { inputsLocationSwapMap: [] } } as any);
+      },
+    };
+    const dynamicSchema = {
+      type: 'object',
+      properties: {
+        details: { type: 'object', properties: { name: { type: 'string' }, code: { type: 'number' } } },
+        id: { type: 'string' },
+      },
+    };
+    const dynamicParameter = {
+      key: 'inputs.$.dynamicData',
+      name: 'dynamicData',
+      type: 'any',
+      dynamicSchema: {
+        extension: {
+          dynamicState: { extension: { operationId: 'getApimSchema' }, isInput: true },
+          parameters: { operationId: { parameterRefernce: 'apiManagement.operationId', required: true } },
+        },
+        type: 'DynamicProperties',
+      },
+    };
+    test('should return parent parameter in dynamic schema when containing template expression', async () => {
+      InitOperationManifestService(manifestService);
+
+      const dynamicInputs = await getDynamicInputsFromSchema(
+        dynamicSchema,
+        dynamicParameter,
+        { connectorId: '/connectionProviders/test', operationId: 'test', type: 'Test' },
+        ['inputs.$.operationId'],
+        { inputs: { operationId: 'SomeValue', dynamicData: { id: 'abc', details: '@triggerBody()' } } }
+      );
+
+      expect(dynamicInputs).toBeDefined();
+      expect(dynamicInputs.length).toEqual(2);
+      expect(dynamicInputs).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ key: 'inputs.$.dynamicData.details', value: '@triggerBody()' }),
+          expect.objectContaining({ key: 'inputs.$.dynamicData.id', value: 'abc' }),
+        ])
+      );
+    });
+
+    test('should return leaf parameters in dynamic schema when property values are present in definition', async () => {
+      InitOperationManifestService(manifestService);
+
+      const dynamicInputs = await getDynamicInputsFromSchema(
+        dynamicSchema,
+        dynamicParameter,
+        { connectorId: '/connectionProviders/test', operationId: 'test', type: 'ApiManagement' },
+        ['inputs.$.operationId'],
+        { inputs: { operationId: 'SomeValue', dynamicData: { id: 'abc', details: { name: 'test', code: 123 } } } }
+      );
+
+      expect(dynamicInputs).toBeDefined();
+      expect(dynamicInputs.length).toEqual(3);
+      expect(dynamicInputs).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ key: 'inputs.$.dynamicData.details.name', value: 'test' }),
+          expect.objectContaining({ key: 'inputs.$.dynamicData.details.code', value: 123 }),
+          expect.objectContaining({ key: 'inputs.$.dynamicData.id', value: 'abc' }),
+        ])
+      );
+    });
+
+    test('should return only leaf parameters in dynamic schema for newly added action and not present in definition', async () => {
+      InitOperationManifestService(manifestService);
+
+      const dynamicInputs = await getDynamicInputsFromSchema(
+        dynamicSchema,
+        dynamicParameter,
+        { connectorId: '/connectionProviders/test', operationId: 'test', type: 'ApiManagement' },
+        ['inputs.$.operationId']
+      );
+
+      expect(dynamicInputs).toBeDefined();
+      expect(dynamicInputs.length).toEqual(3);
+      expect(dynamicInputs).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ key: 'inputs.$.dynamicData.details.name' }),
+          expect.objectContaining({ key: 'inputs.$.dynamicData.details.code' }),
+          expect.objectContaining({ key: 'inputs.$.dynamicData.id' }),
+        ])
+      );
+    });
+  });
+});
