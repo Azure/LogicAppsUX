361882ea9c68f4445bcdd2049f56217f6538e7ba
Jaden-Codes <Jaden.mathyou@gmail.com>
2024-07-02 17:16:29 -0700

    feat(vscode): parameters changed to raw/key and organized cloudToLocal

--- apps/vs-code-designer/src/app/commands/createNewCodeProject/cloudToLocal.ts
+++ apps/vs-code-designer/src/app/commands/createNewCodeProject/cloudToLocal.ts
@@ -1,5 +1,7 @@
 // Used createNewCodeProject.ts as a template to create this file
 // This file is used to take a zipped Logic App from the desktop and unzip to the local workspace
+// Reorganized file with constants at the top, grouped functions, and a main cloudToLocalInternal to call everything
+
 import {
   extensionCommand,
   funcVersionSetting,
@@ -32,12 +34,88 @@ import * as path from 'path';
 import AdmZip = require('adm-zip');
 import * as fs from 'fs';
 
+// Constants
 const openFolder = true;
 const DELIMITER = '/';
 const SUBSCRIPTION_INDEX = 2;
 const MANAGED_API_LOCATION_INDEX = 6;
 const MANAGED_CONNECTION_RESOURCE_GROUP_INDEX = 4;
 
+// Function to create an AdmZip instance
+function createAdmZipInstance(zipFilePath: string) {
+  return new AdmZip(zipFilePath);
+}
+
+// Function to get zip entries
+function getZipEntries(zipFilePath: string) {
+  const zip = createAdmZipInstance(zipFilePath);
+  return zip.getEntries();
+}
+
+// Function to deep merge objects
+function deepMergeObjects(target: any, source: any): any {
+  const result = { ...target };
+  for (const key of Object.keys(source)) {
+    if (source[key] instanceof Object && key in target) {
+      result[key] = deepMergeObjects(target[key], source[key]);
+    } else {
+      result[key] = source[key];
+    }
+  }
+  return result;
+}
+
+// Function to extract connection details
+function extractConnectionDetails(connection: ConnectionReferenceModel): any {
+  const details = [];
+  if (connection) {
+    const managedApiConnections = connection['managedApiConnections'];
+    for (const connKey in managedApiConnections) {
+      if (Object.prototype.hasOwnProperty.call(managedApiConnections, connKey)) {
+        const idPath = managedApiConnections[connKey]['api']['id'];
+        const connectionidPath = managedApiConnections[connKey]['connection']['id'];
+        const apiIdParts = idPath.split(DELIMITER);
+        const connectionidParts = connectionidPath.split(DELIMITER);
+        if (apiIdParts) {
+          const detail = {
+            WORKFLOWS_SUBSCRIPTION_ID: apiIdParts[SUBSCRIPTION_INDEX],
+            WORKFLOWS_LOCATION_NAME: apiIdParts[MANAGED_API_LOCATION_INDEX],
+            WORKFLOWS_RESOURCE_GROUP_NAME: connectionidParts[MANAGED_CONNECTION_RESOURCE_GROUP_INDEX],
+          };
+          details.push(detail);
+        }
+      }
+    }
+  }
+  return details;
+}
+
+// Function to change authentication type to Raw
+function changeAuthTypeToRaw(connections: ConnectionReferenceModel, connectionspath: string): void {
+  if (connections) {
+    const managedApiConnections = connections['managedApiConnections'];
+    for (const connKey in managedApiConnections) {
+      if (Object.prototype.hasOwnProperty.call(managedApiConnections, connKey)) {
+        const authType = managedApiConnections[connKey]['authentication']['type'];
+        if (authType === 'ManagedServiceIdentity') {
+          console.log(`Changing type for ${connKey} from ${authType} to Raw`);
+          managedApiConnections[connKey]['authentication']['type'] = 'Raw';
+          managedApiConnections[connKey]['authentication']['scheme'] = 'Key';
+          managedApiConnections[connKey]['authentication']['parameter'] = `@appsetting('${connKey}-connectionKey')`;
+        }
+      }
+    }
+    const data = JSON.stringify(connections, null, 2);
+    try {
+      fs.writeFileSync(connectionspath, data, 'utf8');
+      console.log('Connections updated and saved to file.');
+    } catch (error) {
+      console.error('Failed to write connections to file:', error);
+    }
+  }
+}
+
+// Main function to orchestrate the cloud to local process
 export async function cloudToLocalInternal(
   context: IActionContext,
   options: ICreateFunctionOptions = {
@@ -51,7 +129,7 @@ export async function cloudToLocalInternal(
   }
 ): Promise<void> {
   addLocalFuncTelemetry(context);
-  showPreviewWarning(extensionCommand.cloudToLocal); //Show warning if command is set to preview
+  showPreviewWarning(extensionCommand.cloudToLocal);
 
   const language: ProjectLanguage | string = (options.language as ProjectLanguage) || getGlobalSetting(projectLanguageSetting);
   const version: string = options.version || getGlobalSetting(funcVersionSetting) || (await tryGetLocalFuncVersion()) || latestGAVersion;
@@ -60,10 +138,9 @@ export async function cloudToLocalInternal(
     language,
     version: tryParseFuncVersion(version),
     projectTemplateKey,
-    projectPath: options.folderPath, // Set the projectPath property to the folderPath option
+    projectPath: options.folderPath,
   });
 
-  //If suppressOpenFolder is true, set the open behavior to don't open. Otherwise, get the open behavior from the workspace settings.
   if (options.suppressOpenFolder) {
     wizardContext.openBehavior = OpenBehavior.dontOpen;
   } else if (!wizardContext.openBehavior) {
@@ -71,7 +148,6 @@ export async function cloudToLocalInternal(
     context.telemetry.properties.openBehaviorFromSetting = String(!!wizardContext.openBehavior);
   }
 
-  // Create a new Azure wizard with the appropriate steps for Cloud to Local
   const wizard: AzureWizard<IFunctionWizardContext> = new AzureWizard(wizardContext, {
     title: localize('cloudToLocal', 'Import zip into new Workspace'),
     promptSteps: [
@@ -91,85 +167,28 @@ export async function cloudToLocalInternal(
   } catch (error) {
     console.error('Error during wizard execution:', error);
   }
-
-  // Factory function to create an AdmZip instance
-  function createAdmZipInstance(zipFilePath: string) {
-    return new AdmZip(zipFilePath);
-  }
-
-  // Function to abstract away the direct use of the AdmZip constructor
-  function getZipEntries(zipFilePath: string) {
-    const zip = createAdmZipInstance(zipFilePath);
-    return zip.getEntries(); // Returns all entries (files and folders) within the zip file
-  }
-
   const zipFilePath = ZipFileStep.zipFilePath;
   const zipEntries = getZipEntries(zipFilePath);
-
+  const connectionspath = path.join(wizardContext.workspacePath, 'connections.json');
   let zipSettings = {};
-
-  // Check if the local.settings.json file exists in the zip and read its contents
   const localSettingsEntry = zipEntries.find((entry) => entry.entryName === 'local.settings.json');
+  const localSettingsPath = path.join(wizardContext.workspacePath, 'local.settings.json');
+  let localSettings: { Values?: any } = {};
+
   if (localSettingsEntry) {
     const zipSettingsContent = localSettingsEntry.getData().toString('utf8');
     zipSettings = JSON.parse(zipSettingsContent);
   }
-  // Merge local.settings.json files
-  const localSettingsPath = path.join(wizardContext.workspacePath, 'local.settings.json');
-  let localSettings: { Values?: any } = {};
 
-  // Check if a local local.settings.json file exists
   if (fs.existsSync(localSettingsPath)) {
-    // If it does, read its contents
     const localSettingsContent = fs.readFileSync(localSettingsPath, 'utf8');
     localSettings = JSON.parse(localSettingsContent);
-  }
-
-  function deepMergeObjects(target: any, source: any): any {
-    const result = { ...target }; // Create a shallow copy of the target to avoid modifying the original object
-    for (const key of Object.keys(source)) {
-      if (source[key] instanceof Object && key in target) {
-        // If the value is an object and the key exists in the target, recurse
-        result[key] = deepMergeObjects(target[key], source[key]);
-      } else {
-        // For non-object values or keys not present in the target, directly assign to the result
-        result[key] = source[key];
-      }
-    }
-    return result;
-  }
-
-  localSettings = deepMergeObjects(zipSettings, localSettings);
-
-  function extractConnectionDetails(connection: ConnectionReferenceModel): any {
-    const details = [];
-    if (connection) {
-      const managedApiConnections = connection['managedApiConnections'];
-      for (const connection in managedApiConnections) {
-        if (Object.prototype.hasOwnProperty.call(managedApiConnections, connection)) {
-          const idPath = managedApiConnections[connection]['api']['id'];
-          const connectionidPath = managedApiConnections[connection]['connection']['id'];
-          const apiIdParts = idPath.split(DELIMITER);
-          const connectionidParts = connectionidPath.split(DELIMITER);
-          if (apiIdParts) {
-            const detail = {
-              WORKFLOWS_SUBSCRIPTION_ID: apiIdParts[SUBSCRIPTION_INDEX], // Extract subscription ID from API ID
-              WORKFLOWS_LOCATION_NAME: apiIdParts[MANAGED_API_LOCATION_INDEX], // Extract location from API ID
-              WORKFLOWS_RESOURCE_GROUP_NAME: connectionidParts[MANAGED_CONNECTION_RESOURCE_GROUP_INDEX], // Extract resource group from Connection ID
-            };
-            details.push(detail);
-          }
-        }
-      }
-    }
-    return details;
   }
 
   async function fetchConnections() {
     const instance = new ZipFileStep();
     try {
       const connection = await instance.getConnectionsJsonContent(wizardContext as IFunctionWizardContext);
-
       const connectionDetails = extractConnectionDetails(connection);
       return connectionDetails;
     } catch (error) {
@@ -179,38 +198,28 @@ export async function cloudToLocalInternal(
 
   async function mergeAndWriteConnections() {
     try {
-      const connectionsValues = await fetchConnections(); // Wait for the connection details
-
-      // Assuming connectionsValues is an array with the connection details as its first element
+      const connectionsValues = await fetchConnections();
       const connectionDetail = connectionsValues[0];
-
-      // Prepare the new object to include in Values
       const newValues = {
-        ...localSettings.Values, // Spread existing values
-        ...connectionDetail, // Add the connection detail under "0"
+        ...localSettings.Values,
+        ...connectionDetail,
       };
-
       const finalObject = {
-        ...localSettings, // Spread other properties of localSettings
-        Values: newValues, // Override Values with newValues
+        ...localSettings,
+        Values: newValues,
       };
-
-      // Write the merged contents back to the local.settings.json file
       fs.writeFileSync(localSettingsPath, JSON.stringify(finalObject, null, 2));
       console.log(`Successfully wrote to ${localSettingsPath}`);
     } catch (error) {
       console.error('Error writing file:', error);
     }
   }
-  mergeAndWriteConnections();
 
-  // Write the merged contents back to the local local.settings.json file
-  try {
-    fs.writeFileSync(localSettingsPath, JSON.stringify(localSettings, null, 2));
-    console.log(`Successfully writ to ${localSettingsPath}`);
-  } catch (error) {
-    console.error('Error writing file:', error);
-  }
+  localSettings = deepMergeObjects(zipSettings, localSettings);
+  await mergeAndWriteConnections();
+  const instance = new ZipFileStep();
+  const connection = await instance.getConnectionsJsonContent(wizardContext as IFunctionWizardContext);
+  changeAuthTypeToRaw(connection, connectionspath);
 
   window.showInformationMessage(localize('finishedCreating', 'Finished creating project.'));
 }
