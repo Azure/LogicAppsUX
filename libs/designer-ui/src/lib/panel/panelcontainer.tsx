6916f1c41e860c77e3197c8a1054b07cf45ea3b9
EK <1350074+ek68794998@users.noreply.github.com>
2024-07-26 15:30:13 -0700

    Improve UI for pinning when using left-side panel

--- libs/designer-ui/src/lib/panel/panelcontainer.tsx
+++ libs/designer-ui/src/lib/panel/panelcontainer.tsx
@@ -14,8 +14,6 @@ import type { CommonPanelProps } from './panelUtil';
 import { PanelLocation, PanelScope, PanelSize } from './panelUtil';
 import type { PanelNodeData } from './types';
 
-const horizontalPadding = '2rem';
-
 export type PanelContainerProps = {
   noNodeSelected: boolean;
   panelScope: PanelScope;
@@ -105,7 +103,6 @@ export const PanelContainer = ({
           canResubmit={canResubmit}
           onUnpinAction={canUnpin ? onUnpinAction : undefined}
           resubmitOperation={() => resubmitOperation?.(nodeId)}
-          horizontalPadding={horizontalPadding}
           commentChange={() => onCommentChange(nodeId)}
           toggleCollapse={toggleCollapse}
           onTitleChange={onTitleChange}
@@ -203,6 +200,7 @@ export const PanelContainer = ({
           <div
             className={mergeClasses(
               'msla-panel-container-nested',
+              `msla-panel-container-nested-${panelLocation.toLowerCase()}`,
               !isEmptyPane && pinnedNodeIfDifferent && 'msla-panel-container-nested-dual'
             )}
           >
