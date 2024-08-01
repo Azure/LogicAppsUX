7176a58500ea45890d1ef6414985db825c6e1974
alain-zhiyanov <t-azhiyanov@microsoft.com>
2024-08-01 10:22:41 -0700

    Update generateStaticWebApp.ts

--- apps/vs-code-designer/src/app/commands/GenerateStaticWebApp/generateStaticWebApp.ts
+++ apps/vs-code-designer/src/app/commands/GenerateStaticWebApp/generateStaticWebApp.ts
@@ -4,7 +4,6 @@ import { executeCommand } from '../../utils/funcCoreTools/cpUtils';
 import { ext } from '../../../extensionVariables';
 
 export async function generateStaticWebApp(context: IAzureConnectorsContext, node: vscode.Uri): Promise<void> {
-  console.log('generate SWA');
   //removing "/c: because that doesn't fit SWA cli format"
   const command: string = `node C:\\Users\\t-azhiyanov\\static-web-apps-cli-sifat\\dist\\cli\\bin.js generate ${node.path.substring(3)}`;
   executeCommand(ext.outputChannel, undefined, command);
