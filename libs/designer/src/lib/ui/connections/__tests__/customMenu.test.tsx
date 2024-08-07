00b1d07c5e8a86d44e55d7ffa396d6900ab169e0
Helen Liu <liuhelen@microsoft.com>
2024-08-07 15:02:48 -0400

    fix: fix test issue

--- libs/designer/src/lib/ui/connections/__tests__/customMenu.test.tsx
+++ libs/designer/src/lib/ui/connections/__tests__/customMenu.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import { CustomMenu, type CustomMenuProps } from '../customMenu';
