d7d410ef699b7c4ae85dc92579897a64f5326d32
Carlos Emiliano Castro Trejo <102700317+ccastrotrejo@users.noreply.github.com>
2024-08-15 16:11:34 -0700

    feat(designer): Open monitoring view panel from host service (#5401)
    * Add button and logic to render it
    
    * Add onclick footer link
    
    * Move button to component and align right
    
    * Update component name
    
    * Update render decision
    
    * Move button to panel header
    
    * Update styles
    
    * Resolve comments
    
    * Update snapshots

--- libs/designer/src/lib/core/state/workflow/workflowSlice.ts
+++ libs/designer/src/lib/core/state/workflow/workflowSlice.ts
@@ -24,6 +24,7 @@ import {
 import type { RelationshipIds } from '../panel/panelInterfaces';
 import type { ErrorMessage, SpecTypes, WorkflowState, WorkflowKind } from './workflowInterfaces';
 import { getWorkflowNodeFromGraphState } from './workflowSelectors';
+import type { BoundParameters } from '@microsoft/logic-apps-shared';
 import {
   LogEntryLevel,
   LoggerService,
@@ -325,6 +326,22 @@ export const workflowSlice = createSlice({
       };
       nodeMetadata.runData = nodeRunData as LogicAppsV2.WorkflowRunAction;
     },
+    setRunDataInputOutputs: (
+      state: WorkflowState,
+      action: PayloadAction<{ nodeId: string; inputs: BoundParameters; outputs: BoundParameters }>
+    ) => {
+      const { nodeId, inputs, outputs } = action.payload;
+      const nodeMetadata = getRecordEntry(state.nodesMetadata, nodeId);
+      if (!nodeMetadata) {
+        return;
+      }
+      const nodeRunData = {
+        ...nodeMetadata.runData,
+        inputs: inputs,
+        outputs: outputs,
+      };
+      nodeMetadata.runData = nodeRunData as LogicAppsV2.WorkflowRunAction;
+    },
     addSwitchCase: (state: WorkflowState, action: PayloadAction<{ caseId: string; nodeId: string }>) => {
       if (!state.graph) {
         return; // log exception
@@ -535,6 +552,7 @@ export const {
   setRepetitionRunData,
   setIsWorkflowDirty,
   setHostErrorMessages,
+  setRunDataInputOutputs,
 } = workflowSlice.actions;
 
 export default workflowSlice.reducer;
