1516db1f01c954d052cf7c885dc3248983486963
Krrish Mittal <krrishmittal15@gmail.com>
2024-08-21 09:05:54 -0700

    add edge on hover

--- libs/data-mapper-v2/src/core/state/selectors/selectors.ts
+++ libs/data-mapper-v2/src/core/state/selectors/selectors.ts
@@ -5,7 +5,7 @@ import { getReactFlowNodeId } from '../../../utils';
 export const useSelectedNode = (nodeId: string) =>
   useSelector((state: RootState) => state.dataMap.present.curDataMapOperation.selectedItemConnectedNodes[nodeId]);
 
-export const useActiveEdge = (edgeId: string) =>
+export const useSelectedEdge = (edgeId: string) =>
   useSelector((state: RootState) => state.dataMap.present.curDataMapOperation.selectedItemConnectedNodes[edgeId]);
 
 export const useLooping = (edgeId?: string) => {
@@ -24,4 +24,9 @@ export const useHoverNode = (nodeId: string) => {
 export const useHoverFunctionNode = (nodeId: string) => {
   const hoverState = useSelector((state: RootState) => state.dataMap.present.curDataMapOperation.state?.hover);
   return hoverState && hoverState.type === 'function' && hoverState.id === nodeId;
+};
+
+export const useHoverEdge = (edgeId: string) => {
+  const hoverState = useSelector((state: RootState) => state.dataMap.present.curDataMapOperation.state?.hover);
+  return hoverState && hoverState.type === 'edge' && hoverState.id === edgeId;
 };
