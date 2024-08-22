adc7fc194bca49670e2f9ef37906d49253963e8b
Krrish Mittal <krrishmittal15@gmail.com>
2024-08-22 07:59:40 -0700

    fix(Data Mapper): Remove parent scrolling (scrolling is enabled individually in schemas) (#5465)
    remove parent scroll

--- libs/data-mapper-v2/src/ui/styles.ts
+++ libs/data-mapper-v2/src/ui/styles.ts
@@ -7,7 +7,7 @@ export const useStyles = makeStyles({
     height: '100vh',
     minHeight: 'calc(100vh - 50px)',
     maxHeight: 'calc(100vh - 40px)',
-    ...shorthands.overflow('scroll'),
+    ...shorthands.overflow('hidden'),
     ...shorthands.flex(1, 1, '1px'),
   },
   dataMapperFunctionPanel: {
