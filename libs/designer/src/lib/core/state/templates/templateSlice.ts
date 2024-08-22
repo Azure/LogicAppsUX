f5b8f81782e1fbec7d55b2ccdb54a555cbe49cc4
Elaina-Lee <144840522+Elaina-Lee@users.noreply.github.com>
2024-08-22 15:52:47 -0700

    fix(Templates): Image not Rendering in Prod + Sort By Text (#5461)
    * fixed
    
    * localized sort by text

--- libs/designer/src/lib/core/state/templates/templateSlice.ts
+++ libs/designer/src/lib/core/state/templates/templateSlice.ts
@@ -290,13 +290,6 @@ const loadTemplateFromGithub = async (templateName: string, manifest: Template.M
     const templateManifest: Template.Manifest =
       manifest ?? (await import(`./../../templates/templateFiles/${templateName}/manifest.json`)).default;
 
-    const imagesWithPaths: Record<string, string> = {};
-    for (const imageType of Object.keys(templateManifest.images)) {
-      imagesWithPaths[imageType] = (
-        await import(`./../../templates/templateFiles/${templateName}/${templateManifest.images[imageType]}.png`)
-      ).default;
-    }
-
     const parametersDefinitions = templateManifest.parameters?.reduce((result: Record<string, Template.ParameterDefinition>, parameter) => {
       result[parameter.name] = {
         ...parameter,
@@ -312,7 +305,7 @@ const loadTemplateFromGithub = async (templateName: string, manifest: Template.M
       kind: templateManifest.kinds?.length === 1 ? templateManifest.kinds[0] : undefined,
       parameterDefinitions: parametersDefinitions,
       connections: templateManifest.connections,
-      images: imagesWithPaths,
+      images: templateManifest.images,
       errors: {
         workflow: undefined,
         kind: undefined,
