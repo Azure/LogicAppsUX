a3a71042b4af5d8f70df6ed6b89802ddc83b8398
Aleksandra (Aleks) Dziewulska <jokerka17@gmail.com>
2024-08-28 09:45:03 -0700

    chore(meta): Revert "fix(designer): Dynamic input parameters have extra $ paramete… (#5533)
    Revert "fix(designer): Dynamic input parameters have extra $ parameter for parent key when aliases are present (#5360)"
    
    This reverts commit 2e27773cd24e5a67fd33a1656a789b3ec0d8c10a.
    
    Co-authored-by: Aleks Dziewulska (from Dev Box) <aleksd@microsoft.com>

--- libs/logic-apps-shared/src/parsers/lib/common/schemaprocessor.ts
+++ libs/logic-apps-shared/src/parsers/lib/common/schemaprocessor.ts
@@ -320,7 +320,7 @@ export class SchemaProcessor {
       const key = keyPrefix || this.options.keyPrefix || '$';
       const description = schema.description;
       schemaProperties.push({
-        alias: this.options.useAliasedIndexing ? schema[SwaggerConstants.ExtensionProperties.Alias] : undefined,
+        alias: schema[SwaggerConstants.ExtensionProperties.Alias],
         default: schema.default,
         description: description,
         dynamicValues,
@@ -502,7 +502,7 @@ export class SchemaProcessor {
     const type = (schema.type as string) || SwaggerConstants.Types.Any;
     const visibility = this._getVisibility(schema);
     const groupName = this._getGroupName(schema);
-    const alias = this.options.useAliasedIndexing ? schema[SwaggerConstants.ExtensionProperties.Alias] : undefined;
+    const alias = schema[SwaggerConstants.ExtensionProperties.Alias];
 
     // Exclude read-only parameters from input schema, i.e., objects in Swagger body parameters.
     if (isInputSchema && this._isReadOnlyParameter(schema)) {
