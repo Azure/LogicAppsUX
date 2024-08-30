da776ada3b0a5d00925eeb4e382991c84555f5e2
Elaina Lee <hyehwalee@microsoft.com>
2024-08-30 16:27:13 -0700

    put component hook function to helper and wrote unit tests

--- libs/designer/src/lib/ui/connections/dropzone.tsx
+++ libs/designer/src/lib/ui/connections/dropzone.tsx
@@ -29,6 +29,7 @@ import { AllowDropTarget } from './dynamicsvgs/allowdroptarget';
 import { BlockDropTarget } from './dynamicsvgs/blockdroptarget';
 import { retrieveClipboardData } from '../../core/utils/clipboard';
 import { setEdgeContextMenuData } from '../../core/state/designerView/designerViewSlice';
+import { canDropItem } from './helpers';
 
 export interface DropZoneProps {
   graphId: string;
@@ -115,32 +116,8 @@ export const DropZone: React.FC<DropZoneProps> = memo(({ graphId, parentId, chil
         graphId?: string;
         isScope?: boolean;
       }) => {
-        // This supports preventing moving a node with a dependency above its upstream node
-        for (const dec of item.dependencies ?? []) {
-          if (!upstreamNodes.has(dec)) {
-            return false;
-          }
-        }
-
-        const sanitizedItemId = removeIdTag(item.id);
-
-        if (item.isScope) {
-          if (upstreamScopes.has(sanitizedItemId)) {
-            return false;
-          }
-        }
-
-        for (const node of upstreamNodes) {
-          if (
-            upstreamNodesDependencies[node].has(item.id) ||
-            (upstreamNodesDependencies[item.id] && upstreamNodesDependencies[item.id].has(node))
-          ) {
-            return false;
-          }
-        }
-        // TODO: Support preventing moving a node below downstream output
-        // TODO: Support calculating dependencies when dragging of scopes
-        return sanitizedItemId !== childId && sanitizedItemId !== parentId;
+        const hello = canDropItem(item, upstreamNodes, upstreamNodesDependencies, upstreamScopes, childId, parentId);
+        return hello;
       },
       collect: (monitor) => ({
         isOver: monitor.isOver(),
