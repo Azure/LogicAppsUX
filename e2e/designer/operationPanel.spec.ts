1fe1cdb07d1b813299c20d1457ee77d8adc85540
EK <1350074+ek68794998@users.noreply.github.com>
2024-09-04 12:46:14 -0700

    Add tests for panel collapse/expand

--- e2e/designer/operationPanel.spec.ts
+++ e2e/designer/operationPanel.spec.ts
@@ -7,32 +7,113 @@ test.describe(
     tag: '@mock',
   },
   () => {
-    test('Can select a node to be shown in the operation panel from fresh state', async ({ page }) => {
+    test('Can collapse and expand panel with no node selected or pinned', async ({ page }) => {
+      const validatePanel = async (state: 'open' | 'closed') => {
+        if (state === 'open') {
+          await expect(page.locator('.msla-panel-container-nested .msla-panel-select-card-container-empty')).toBeVisible();
+          return;
+        }
+
+        await expect(page.locator('.msla-panel-container-nested')).not.toBeVisible();
+      };
+
+      await page.goto('/');
+      await GoToMockWorkflow(page, 'Panel');
+
+      // Panel should be closed.
+      await validatePanel('closed');
+
+      // Expand the panel and verify.
+      await page.getByTestId('msla-panel-header-collapse-nav').click();
+      await validatePanel('open');
+
+      // Collapse the panel and verify.
+      await page.getByTestId('msla-panel-header-collapse-nav').click();
+      await validatePanel('closed');
+    });
+
+    test('Can select a node, and collapse & expand the panel', async ({ page }) => {
+      const validatePanel = async (state: 'open' | 'closed') => {
+        if (state === 'open') {
+          await expect(page.locator('.msla-panel-container-nested .msla-panel-layout-selected')).toBeVisible();
+          await expect(page.locator('.msla-panel-card-header input#Initialize_ArrayVariable-title')).toBeVisible();
+          await expect(page.locator('.msla-panel-card-header button[aria-label="Unpin action"]')).not.toBeVisible();
+          return;
+        }
+
+        await expect(page.locator('.msla-panel-container-nested')).not.toBeVisible();
+      };
+
       await page.goto('/');
       await GoToMockWorkflow(page, 'Panel');
 
       // Left-click on 'Initialize ArrayVariable' node.
       await page.getByTestId('card-initialize_arrayvariable').click();
 
-      // Node panel tab should be open with 'Initialize Variable' action.
-      await expect(page.locator('.msla-panel-card-header input#Initialize_ArrayVariable-title')).toBeVisible();
-      await expect(page.locator('.msla-panel-card-header button[aria-label="Unpin action"]')).not.toBeVisible();
+      // Panel should be open, with 'Initialize Variable' action.
+      await validatePanel('open');
+
+      // Collapse the panel and verify.
+      await page.getByTestId('msla-panel-header-collapse-nav').click();
+      await validatePanel('closed');
+
+      // Expand the panel and verify.
+      await page.getByTestId('msla-panel-header-collapse-nav').click();
+      await validatePanel('open');
     });
 
-    test('Can pin a node to operation panel from fresh state', async ({ page }) => {
+    test('Can pin a node, and collapse & expand the panel', async ({ page }) => {
+      const validatePanel = async (state: 'open' | 'closed') => {
+        if (state === 'open') {
+          await expect(page.locator('.msla-panel-container-nested .msla-panel-layout-selected')).toBeVisible();
+          await expect(page.locator('.msla-panel-card-header input#Initialize_ArrayVariable-title')).toBeVisible();
+          await expect(page.locator('.msla-panel-card-header button[aria-label="Unpin action"]')).toBeVisible();
+          return;
+        }
+
+        await expect(page.locator('.msla-panel-container-nested')).not.toBeVisible();
+      };
+
       await page.goto('/');
       await GoToMockWorkflow(page, 'Panel');
 
-      // Right-click on 'Initialize ArrayVariable' node, and select 'Pin' from the menu.
+      // Left-click on 'Initialize ArrayVariable' node.
       await page.getByTestId('card-initialize_arrayvariable').click({ button: 'right' });
       await page.getByTestId('msla-pin-menu-option').click();
 
-      // Node panel tab should be open with 'Initialize Variable' action.
-      await expect(page.locator('.msla-panel-card-header input#Initialize_ArrayVariable-title')).toBeVisible();
-      await expect(page.locator('.msla-panel-card-header button[aria-label="Unpin action"]')).toBeVisible();
+      // Panel should be open, with 'Initialize Variable' action.
+      await validatePanel('open');
+
+      // Collapse the panel and verify.
+      await page.getByTestId('msla-panel-header-collapse-nav').click();
+      await validatePanel('closed');
+
+      // Expand the panel and verify.
+      await page.getByTestId('msla-panel-header-collapse-nav').click();
+      await validatePanel('open');
     });
 
-    test('Can have both selected and pinned operations open', async ({ page }) => {
+    test('Can have both selected and pinned operations open, and collapse & expand the panel', async ({ page }) => {
+      const validatePanel = async (state: 'open' | 'closed') => {
+        if (state === 'open') {
+          // Node panel tab should be open with 'Parse JSON' node.
+          await expect(page.locator('.msla-panel-layout-selected .msla-panel-card-header input#Parse_JSON-title')).toBeVisible();
+          await expect(
+            page.locator('.msla-panel-layout-selected .msla-panel-card-header button[aria-label="Unpin action"]')
+          ).not.toBeVisible();
+
+          // Node panel tab should also be open with 'Initialize Variable' action pinned.
+          await expect(
+            page.locator('.msla-panel-layout-pinned .msla-panel-card-header input#Initialize_ArrayVariable-title')
+          ).toBeVisible();
+          await expect(page.locator('.msla-panel-layout-pinned .msla-panel-card-header button[aria-label="Unpin action"]')).toBeVisible();
+
+          return;
+        }
+
+        await expect(page.locator('.msla-panel-container-nested')).not.toBeVisible();
+      };
+
       await page.goto('/');
       await GoToMockWorkflow(page, 'Panel');
 
@@ -43,13 +124,16 @@ test.describe(
       await page.getByTestId('card-initialize_arrayvariable').click({ button: 'right' });
       await page.getByTestId('msla-pin-menu-option').click();
 
-      // Node panel tab should be open with 'Parse JSON' node.
-      await expect(page.locator('.msla-panel-layout-selected .msla-panel-card-header input#Parse_JSON-title')).toBeVisible();
-      await expect(page.locator('.msla-panel-layout-selected .msla-panel-card-header button[aria-label="Unpin action"]')).not.toBeVisible();
+      // Panel should be open, with 'Initialize Variable' and 'Parse JSON' actions.
+      await validatePanel('open');
 
-      // Node panel tab should also be open with 'Initialize Variable' action pinned.
-      await expect(page.locator('.msla-panel-layout-pinned .msla-panel-card-header input#Initialize_ArrayVariable-title')).toBeVisible();
-      await expect(page.locator('.msla-panel-layout-pinned .msla-panel-card-header button[aria-label="Unpin action"]')).toBeVisible();
+      // Collapse the panel and verify.
+      await page.getByTestId('msla-panel-header-collapse-nav').click();
+      await validatePanel('closed');
+
+      // Expand the panel and verify.
+      await page.getByTestId('msla-panel-header-collapse-nav').click();
+      await validatePanel('open');
     });
 
     test('Can switch between different tabs independently in selected/pinned panels', async ({ page }) => {
