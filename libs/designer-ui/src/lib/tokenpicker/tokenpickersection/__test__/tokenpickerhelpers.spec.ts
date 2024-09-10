06e0990ec0b910189710c51533152931e172ae95
pranaydubeymicrosoft <135278983+pranaydubeymicrosoft@users.noreply.github.com>
2024-09-09 18:51:58 -0700

    fix(designer-ui): Token picker should display all output tokens when fewer options than max rows shown (#5618)
    * don't display show more if less than min items
    
    * add test case for token picker helper function
    
    * remove hasAdvanced function + tests as unused
    
    * edge cases for getReducedTokenList
    
    ---------
    
    Co-authored-by: Pranay Dubey <pradubey@microsoft.com>
    Co-authored-by: Eric Wu <95886809+Eric-B-Wu@users.noreply.github.com>

--- libs/designer-ui/src/lib/tokenpicker/tokenpickersection/__test__/tokenpickerhelpers.spec.ts
+++ libs/designer-ui/src/lib/tokenpicker/tokenpickersection/__test__/tokenpickerhelpers.spec.ts
@@ -1,6 +1,6 @@
 import type { OutputToken } from '../..';
-import { getReducedTokenList, hasAdvanced } from '../tokenpickerhelpers';
-import { describe, vi, beforeEach, afterEach, beforeAll, afterAll, it, test, expect } from 'vitest';
+import { getReducedTokenList } from '../tokenpickerhelpers';
+import { describe, it, expect } from 'vitest';
 describe('ui/tokenPicker/tokenPickerSection helpers', () => {
   describe('getReducedTokenList', () => {
     it('should return a list of tokens with the advanced tokens filtered out', () => {
@@ -69,33 +69,239 @@ describe('ui/tokenPicker/tokenPickerSection helpers', () => {
         { isAdvanced: true, value: 'Value 4' },
       ]);
     });
-  });
 
-  describe('hasAdvanced', () => {
-    it('should return true if any of the tokens are advanced', () => {
+    it('should return all tokens with the "max rows shown" field respected', () => {
       const tokens: OutputToken[] = [
-        { isAdvanced: false, value: 'Value 1' } as OutputToken,
+        { isAdvanced: true, value: 'Value 1' } as OutputToken,
         { isAdvanced: true, value: 'Value 2' } as OutputToken,
-        { isAdvanced: false, value: 'Value 3' } as OutputToken,
+        { isAdvanced: true, value: 'Value 3' } as OutputToken,
         { isAdvanced: true, value: 'Value 4' } as OutputToken,
       ];
+      const options = { hasSearchQuery: false, maxRowsShown: 6, showAllOptions: false };
 
-      const result = hasAdvanced(tokens);
+      const result = getReducedTokenList(tokens, options);
 
-      expect(result).toEqual(true);
+      expect(result).toEqual([
+        { isAdvanced: true, value: 'Value 1' },
+        { isAdvanced: true, value: 'Value 2' },
+        { isAdvanced: true, value: 'Value 3' },
+        { isAdvanced: true, value: 'Value 4' },
+      ]);
     });
 
-    it('should return false if none of the tokens are advanced', () => {
+    it('should return all tokens when there are 0 non-advanced tokens and 1 advanced token', () => {
+      const tokens: OutputToken[] = [{ isAdvanced: true, value: 'Value 1' } as OutputToken];
+      const options = { hasSearchQuery: false, maxRowsShown: 6, showAllOptions: false };
+
+      const result = getReducedTokenList(tokens, options);
+
+      expect(result).toEqual([{ isAdvanced: true, value: 'Value 1' }]);
+    });
+
+    it('should return all tokens when there are 0 non-advanced tokens and 5 advanced tokens', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: true, value: 'Value 1' } as OutputToken,
+        { isAdvanced: true, value: 'Value 2' } as OutputToken,
+        { isAdvanced: true, value: 'Value 3' } as OutputToken,
+        { isAdvanced: true, value: 'Value 4' } as OutputToken,
+        { isAdvanced: true, value: 'Value 5' } as OutputToken,
+      ];
+      const options = { hasSearchQuery: false, maxRowsShown: 6, showAllOptions: false };
+
+      const result = getReducedTokenList(tokens, options);
+
+      expect(result).toEqual([
+        { isAdvanced: true, value: 'Value 1' },
+        { isAdvanced: true, value: 'Value 2' },
+        { isAdvanced: true, value: 'Value 3' },
+        { isAdvanced: true, value: 'Value 4' },
+        { isAdvanced: true, value: 'Value 5' },
+      ]);
+    });
+
+    it('should return all tokens when there are 0 non-advanced tokens and 6 advanced tokens', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: true, value: 'Value 1' } as OutputToken,
+        { isAdvanced: true, value: 'Value 2' } as OutputToken,
+        { isAdvanced: true, value: 'Value 3' } as OutputToken,
+        { isAdvanced: true, value: 'Value 4' } as OutputToken,
+        { isAdvanced: true, value: 'Value 5' } as OutputToken,
+        { isAdvanced: true, value: 'Value 6' } as OutputToken,
+      ];
+      const options = { hasSearchQuery: false, maxRowsShown: 6, showAllOptions: false };
+
+      const result = getReducedTokenList(tokens, options);
+
+      expect(result).toEqual([
+        { isAdvanced: true, value: 'Value 1' },
+        { isAdvanced: true, value: 'Value 2' },
+        { isAdvanced: true, value: 'Value 3' },
+        { isAdvanced: true, value: 'Value 4' },
+        { isAdvanced: true, value: 'Value 5' },
+        { isAdvanced: true, value: 'Value 6' },
+      ]);
+    });
+
+    it('should return the first 6 tokens when there are 0 non-advanced tokens and 7 advanced tokens', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: true, value: 'Value 1' } as OutputToken,
+        { isAdvanced: true, value: 'Value 2' } as OutputToken,
+        { isAdvanced: true, value: 'Value 3' } as OutputToken,
+        { isAdvanced: true, value: 'Value 4' } as OutputToken,
+        { isAdvanced: true, value: 'Value 5' } as OutputToken,
+        { isAdvanced: true, value: 'Value 6' } as OutputToken,
+        { isAdvanced: true, value: 'Value 7' } as OutputToken,
+      ];
+      const options = { hasSearchQuery: false, maxRowsShown: 6, showAllOptions: false };
+
+      const result = getReducedTokenList(tokens, options);
+
+      expect(result).toEqual([
+        { isAdvanced: true, value: 'Value 1' },
+        { isAdvanced: true, value: 'Value 2' },
+        { isAdvanced: true, value: 'Value 3' },
+        { isAdvanced: true, value: 'Value 4' },
+        { isAdvanced: true, value: 'Value 5' },
+        { isAdvanced: true, value: 'Value 6' },
+      ]);
+    });
+    it('should return all tokens when there are 5 non-advanced tokens and 0 advanced tokens', () => {
       const tokens: OutputToken[] = [
         { isAdvanced: false, value: 'Value 1' } as OutputToken,
         { isAdvanced: false, value: 'Value 2' } as OutputToken,
         { isAdvanced: false, value: 'Value 3' } as OutputToken,
         { isAdvanced: false, value: 'Value 4' } as OutputToken,
+        { isAdvanced: false, value: 'Value 5' } as OutputToken,
       ];
+      const options = { hasSearchQuery: false, maxRowsShown: 6, showAllOptions: false };
 
-      const result = hasAdvanced(tokens);
+      const result = getReducedTokenList(tokens, options);
 
-      expect(result).toEqual(false);
+      expect(result).toEqual([
+        { isAdvanced: false, value: 'Value 1' },
+        { isAdvanced: false, value: 'Value 2' },
+        { isAdvanced: false, value: 'Value 3' },
+        { isAdvanced: false, value: 'Value 4' },
+        { isAdvanced: false, value: 'Value 5' },
+      ]);
+    });
+
+    it('should return all tokens when there are 6 non-advanced tokens and 0 advanced tokens', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: false, value: 'Value 1' } as OutputToken,
+        { isAdvanced: false, value: 'Value 2' } as OutputToken,
+        { isAdvanced: false, value: 'Value 3' } as OutputToken,
+        { isAdvanced: false, value: 'Value 4' } as OutputToken,
+        { isAdvanced: false, value: 'Value 5' } as OutputToken,
+        { isAdvanced: false, value: 'Value 6' } as OutputToken,
+      ];
+      const options = { hasSearchQuery: false, maxRowsShown: 6, showAllOptions: false };
+
+      const result = getReducedTokenList(tokens, options);
+
+      expect(result).toEqual([
+        { isAdvanced: false, value: 'Value 1' },
+        { isAdvanced: false, value: 'Value 2' },
+        { isAdvanced: false, value: 'Value 3' },
+        { isAdvanced: false, value: 'Value 4' },
+        { isAdvanced: false, value: 'Value 5' },
+        { isAdvanced: false, value: 'Value 6' },
+      ]);
+    });
+
+    it('should return the first 6 tokens when there are 7 non-advanced tokens and 0 advanced tokens', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: false, value: 'Value 1' } as OutputToken,
+        { isAdvanced: false, value: 'Value 2' } as OutputToken,
+        { isAdvanced: false, value: 'Value 3' } as OutputToken,
+        { isAdvanced: false, value: 'Value 4' } as OutputToken,
+        { isAdvanced: false, value: 'Value 5' } as OutputToken,
+        { isAdvanced: false, value: 'Value 6' } as OutputToken,
+        { isAdvanced: false, value: 'Value 7' } as OutputToken,
+      ];
+      const options = { hasSearchQuery: false, maxRowsShown: 6, showAllOptions: false };
+
+      const result = getReducedTokenList(tokens, options);
+
+      expect(result).toEqual([
+        { isAdvanced: false, value: 'Value 1' },
+        { isAdvanced: false, value: 'Value 2' },
+        { isAdvanced: false, value: 'Value 3' },
+        { isAdvanced: false, value: 'Value 4' },
+        { isAdvanced: false, value: 'Value 5' },
+        { isAdvanced: false, value: 'Value 6' },
+      ]);
+    });
+
+    it('should return all tokens when there are 5 non-advanced tokens and 1 advanced token', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: false, value: 'Value 1' } as OutputToken,
+        { isAdvanced: false, value: 'Value 2' } as OutputToken,
+        { isAdvanced: false, value: 'Value 3' } as OutputToken,
+        { isAdvanced: false, value: 'Value 4' } as OutputToken,
+        { isAdvanced: false, value: 'Value 5' } as OutputToken,
+        { isAdvanced: true, value: 'Value 6' } as OutputToken,
+      ];
+      const options = { hasSearchQuery: false, maxRowsShown: 6, showAllOptions: false };
+
+      const result = getReducedTokenList(tokens, options);
+
+      expect(result).toEqual([
+        { isAdvanced: false, value: 'Value 1' },
+        { isAdvanced: false, value: 'Value 2' },
+        { isAdvanced: false, value: 'Value 3' },
+        { isAdvanced: false, value: 'Value 4' },
+        { isAdvanced: false, value: 'Value 5' },
+        { isAdvanced: true, value: 'Value 6' },
+      ]);
+    });
+
+    it('should return all tokens when there are 6 non-advanced tokens and 1 advanced token', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: false, value: 'Value 1' } as OutputToken,
+        { isAdvanced: false, value: 'Value 2' } as OutputToken,
+        { isAdvanced: false, value: 'Value 3' } as OutputToken,
+        { isAdvanced: false, value: 'Value 4' } as OutputToken,
+        { isAdvanced: false, value: 'Value 5' } as OutputToken,
+        { isAdvanced: false, value: 'Value 6' } as OutputToken,
+        { isAdvanced: true, value: 'Value 7' } as OutputToken,
+      ];
+      const options = { hasSearchQuery: false, maxRowsShown: 6, showAllOptions: false };
+
+      const result = getReducedTokenList(tokens, options);
+
+      expect(result).toEqual([
+        { isAdvanced: false, value: 'Value 1' },
+        { isAdvanced: false, value: 'Value 2' },
+        { isAdvanced: false, value: 'Value 3' },
+        { isAdvanced: false, value: 'Value 4' },
+        { isAdvanced: false, value: 'Value 5' },
+        { isAdvanced: false, value: 'Value 6' },
+      ]);
+    });
+
+    it('should return the first 6 tokens when there are 5 non-advanced tokens and 2 advanced tokens', () => {
+      const tokens: OutputToken[] = [
+        { isAdvanced: false, value: 'Value 1' } as OutputToken,
+        { isAdvanced: false, value: 'Value 2' } as OutputToken,
+        { isAdvanced: false, value: 'Value 3' } as OutputToken,
+        { isAdvanced: false, value: 'Value 4' } as OutputToken,
+        { isAdvanced: false, value: 'Value 5' } as OutputToken,
+        { isAdvanced: true, value: 'Value 6' } as OutputToken,
+        { isAdvanced: true, value: 'Value 7' } as OutputToken,
+      ];
+      const options = { hasSearchQuery: false, maxRowsShown: 6, showAllOptions: false };
+
+      const result = getReducedTokenList(tokens, options);
+
+      expect(result).toEqual([
+        { isAdvanced: false, value: 'Value 1' },
+        { isAdvanced: false, value: 'Value 2' },
+        { isAdvanced: false, value: 'Value 3' },
+        { isAdvanced: false, value: 'Value 4' },
+        { isAdvanced: false, value: 'Value 5' },
+        { isAdvanced: true, value: 'Value 6' },
+      ]);
     });
   });
 });
