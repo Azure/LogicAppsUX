97c0cc510198113fd7966214755eeec915882e78
preetriti1 <48265693+preetriti1@users.noreply.github.com>
2024-09-10 19:34:56 -0700

    fix(designer): Fixing null type of output for tokens filtering (#5627) (#5634)
    Co-authored-by: Priti Sambandam <psamband@microsoft.com>

--- libs/designer/src/lib/ui/panel/nodeDetailsPanel/tabs/parametersTab/index.tsx
+++ libs/designer/src/lib/ui/panel/nodeDetailsPanel/tabs/parametersTab/index.tsx
@@ -20,6 +20,7 @@ import type { AppDispatch, RootState } from '../../../../../core/store';
 import { getConnectionReference } from '../../../../../core/utils/connectors/connections';
 import { isRootNodeInGraph } from '../../../../../core/utils/graph';
 import {
+  getTypeForTokenFiltering,
   loadDynamicTreeItemsForParameter,
   loadDynamicValuesForParameter,
   loadParameterValueFromString,
@@ -350,10 +351,7 @@ const ParameterSection = ({
   ): JSX.Element => {
     const parameterType =
       editorType ?? (nodeInputs.parameterGroups[group.id].parameters.find((param) => param.id === parameterId) ?? {})?.type;
-    const supportedTypes: string[] = getPropertyValue(
-      constants.TOKENS,
-      parameterType && parameterType in constants.TOKENS ? parameterType : constants.SWAGGER.TYPE.ANY
-    );
+    const supportedTypes: string[] = getPropertyValue(constants.TOKENS, getTypeForTokenFiltering(parameterType));
 
     const filteredTokenGroup = tokenGroup.map((group) => ({
       ...group,
