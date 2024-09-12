411f03170c8baeb30a714cbddf8c43685461fcc0
Brian Lam <lambrian@microsoft.com>
2024-09-11 23:06:45 -0700

    fix(vscode): Ensure workflow design time starts with multiple logic apps (#5632)
    * Ensures that the workflow design time is started even with multiple logic apps in a workspace
    
    * Remove unneeded changes

--- apps/vs-code-designer/src/main.ts
+++ apps/vs-code-designer/src/main.ts
@@ -63,8 +63,8 @@ export async function activate(context: vscode.ExtensionContext) {
       const errorMessage = `Error downloading and extracting the Logic Apps Standard extension bundle: ${error.message}`;
       activateContext.telemetry.properties.errorMessage = errorMessage;
     }
-    promptParameterizeConnections(activateContext);
-    verifyLocalConnectionKeys(activateContext);
+    promptParameterizeConnections(activateContext, true);
+    verifyLocalConnectionKeys(activateContext, true);
     await startOnboarding(activateContext);
 
     ext.extensionVersion = getExtensionVersion();
