24f340dca00a10d31e7a9408327244c3eb01622a
Carlos Emiliano Castro Trejo <ccastrotrejo@microsoft.com>
2024-09-18 09:18:47 -0700

    Fix import

--- libs/designer/src/lib/core/parsers/binders/inputs/default.ts
+++ libs/designer/src/lib/core/parsers/binders/inputs/default.ts
@@ -1,6 +1,6 @@
 import type { BoundParameters } from '@microsoft/logic-apps-shared';
 import { Binder } from '../binder';
-import { parseInputs } from 'lib/core/utils/monitoring';
+import { parseInputs } from '../../../utils/monitoring';
 
 export default class DefaultInputsBinder extends Binder {
   bind(inputs: any): BoundParameters {
