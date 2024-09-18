e4b12902429963c999f153a5c4e10b32f3ca364c
Carlos Emiliano Castro Trejo <ccastrotrejo@microsoft.com>
2024-09-18 11:31:11 -0700

    Remove export

--- libs/designer/src/lib/core/utils/monitoring/binders/inputs/index.ts
+++ libs/designer/src/lib/core/utils/monitoring/binders/inputs/index.ts
@@ -1,4 +1,3 @@
 export { default as ApiConnectionInputsBinder } from './apiconnection';
 export { default as DefaultInputsBinder } from './default';
-export { default as SendToBatchInputsBinder } from './sendtobatch';
 export { ManifestInputsBinder } from './manifest';
