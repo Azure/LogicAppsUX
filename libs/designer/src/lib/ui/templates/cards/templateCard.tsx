8d86a84a773f536a05ecf603448c978c2de718e9
Elaina Lee <hyehwalee@microsoft.com>
2024-09-19 13:28:02 -0700

    half way styling

--- libs/designer/src/lib/ui/templates/cards/templateCard.tsx
+++ libs/designer/src/lib/ui/templates/cards/templateCard.tsx
@@ -11,6 +11,8 @@ import { getUniqueConnectors } from '../../../core/templates/utils/helper';
 import { useIntl } from 'react-intl';
 import type { OperationInfo } from '@microsoft/logic-apps-shared';
 import { getBuiltInOperationInfo, isBuiltInOperation, LogEntryLevel, LoggerService } from '@microsoft/logic-apps-shared';
+import MicrosoftIcon from '../../../common/images/templates/microsoft.svg';
+import PeopleCommunityIcon from '../../../common/images/templates/people_community.svg';
 
 interface TemplateCardProps {
   templateName: string;
@@ -29,6 +31,25 @@ export const TemplateCard = ({ templateName }: TemplateCardProps) => {
   }));
   const templateManifest = templates?.[templateName];
 
+  const intlText = {
+    TEMPLATE_LOADING: intl.formatMessage({ defaultMessage: 'Loading....', description: 'Loading text', id: 'cZ60Tk' }),
+    NO_CONNECTORS: intl.formatMessage({
+      defaultMessage: 'This template does not have connectors',
+      description: 'Accessibility text to inform user this template does not contain connectors',
+      id: 'aI9W5L',
+    }),
+    COMMUNITY_AUTHORED: intl.formatMessage({
+      defaultMessage: 'Community Authored',
+      description: 'Label text for community authored templates',
+      id: 'F+cOLr',
+    }),
+    MICROSOFT_AUTHORED: intl.formatMessage({
+      defaultMessage: 'Microsoft Authored',
+      description: 'Label text for Microsoft authored templates',
+      id: 'rEQceE',
+    }),
+  };
+
   const onSelectTemplate = () => {
     LoggerService().log({
       level: LogEntryLevel.Trace,
@@ -42,11 +63,7 @@ export const TemplateCard = ({ templateName }: TemplateCardProps) => {
   };
 
   if (!templateManifest) {
-    return (
-      <DocumentCard className="msla-template-card-wrapper">
-        {intl.formatMessage({ defaultMessage: 'Loading....', description: 'Loading text', id: 'cZ60Tk' })}
-      </DocumentCard>
-    );
+    return <DocumentCard className="msla-template-card-wrapper">{intlText.TEMPLATE_LOADING}</DocumentCard>;
   }
 
   const { title, details, featuredOperations, connections } = templateManifest as Manifest;
@@ -81,55 +98,59 @@ export const TemplateCard = ({ templateName }: TemplateCardProps) => {
     root: { display: 'inline-block', maxWidth: 1000 },
   };
 
+  const isMicrosoftAuthored = details?.By === 'Microsoft';
+
   return (
     <DocumentCard className="msla-template-card-wrapper" styles={cardStyles} onClick={onSelectTemplate} aria-label={title}>
-      <div className="msla-template-card-data">
-        <Text size={400} weight="semibold" align="start" className="msla-template-card-title">
-          {title}
-        </Text>
-        <div className="msla-template-card-tags">
-          {['By', 'Type', 'Trigger'].map((key: string) => {
-            if (!details[key]) {
-              return null;
-            }
-            return (
-              <Text key={key} size={300} className="msla-template-card-tag">
-                {key}: {details[key]}
-              </Text>
-            );
-          })}
+      <div className="msla-template-card-authored-wrapper">
+        <div className="msla-template-card-authored">
+          {isMicrosoftAuthored ? (
+            <img src={MicrosoftIcon} alt="Microsoft" width={16} />
+          ) : (
+            <img src={PeopleCommunityIcon} alt="Community" width={16} />
+          )}
+          <Text size={200} weight="semibold" align="start" className="msla-template-card-authored-label">
+            {isMicrosoftAuthored ? intlText.MICROSOFT_AUTHORED : intlText.COMMUNITY_AUTHORED}
+          </Text>
         </div>
       </div>
 
-      <hr className="msla-templates-break" />
+      <div className="msla-template-card-body">
+        <Text size={400} weight="semibold" align="start" className="msla-template-card-title">
+          {title}
+        </Text>
 
-      <div className="msla-template-card-connectors-wrapper">
-        <div className="msla-template-card-connectors">
-          <Text size={300} weight="medium" align="start" className="msla-template-card-connectors-title">
-            {intl.formatMessage({ defaultMessage: 'Connectors', description: 'Connectors section title', id: '0OC7ag' })}
-          </Text>
-          <div className="msla-template-card-connectors-list">
-            {connectorsToShow.length > 0 ? (
-              connectorsToShow.map((info) => (
-                <ConnectorIcon
-                  key={info.connectorId}
-                  connectorId={info.connectorId}
-                  operationId={info.operationId}
-                  classes={{ root: 'msla-template-card-connector', icon: 'msla-template-card-connector-icon' }}
-                />
-              ))
-            ) : (
-              <Text className="msla-template-card-connectors-emptyText">
-                {intl.formatMessage({
-                  defaultMessage: 'This template does not have connectors',
-                  description: 'Accessibility text to inform user this template does not contain connectors',
-                  id: 'aI9W5L',
-                })}
-              </Text>
-            )}
-            {showOverflow ? (
-              <IconButton className="msla-template-card-connector-overflow" onRenderMenuIcon={onRenderMenuIcon} menuProps={menuProps} />
-            ) : null}
+        <div className="msla-template-card-connectors-wrapper">
+          <div className="msla-template-card-tags">
+            {['Type', 'Trigger'].map((key: string) => {
+              if (!details[key]) {
+                return null;
+              }
+              return (
+                <Text key={key} size={300} className="msla-template-card-tag">
+                  {details[key]}
+                </Text>
+              );
+            })}
+          </div>
+          <div className="msla-template-card-connectors">
+            <div className="msla-template-card-connectors-list">
+              {connectorsToShow.length > 0 ? (
+                connectorsToShow.map((info) => (
+                  <ConnectorIcon
+                    key={info.connectorId}
+                    connectorId={info.connectorId}
+                    operationId={info.operationId}
+                    classes={{ root: 'msla-template-card-connector', icon: 'msla-template-card-connector-icon' }}
+                  />
+                ))
+              ) : (
+                <Text className="msla-template-card-connectors-emptyText">{intlText.NO_CONNECTORS}</Text>
+              )}
+              {showOverflow ? (
+                <IconButton className="msla-template-card-connector-overflow" onRenderMenuIcon={onRenderMenuIcon} menuProps={menuProps} />
+              ) : null}
+            </div>
           </div>
         </div>
       </div>
