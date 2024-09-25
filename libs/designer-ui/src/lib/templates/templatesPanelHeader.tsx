659eb93b3d3a24cacae9d7e74ae9e232f2f6074e
Elaina Lee <hyehwalee@microsoft.com>
2024-09-25 16:35:36 -0700

    moved both headers to Designer UI Header for consistency

--- libs/designer-ui/src/lib/templates/templatesPanelHeader.tsx
+++ libs/designer-ui/src/lib/templates/templatesPanelHeader.tsx
@@ -1,72 +1,15 @@
-import { Label } from '@fluentui/react';
 import { Text } from '@fluentui/react-components';
-import { ChevronDown16Regular, ChevronUp16Regular } from '@fluentui/react-icons';
-import { useState } from 'react';
-import { useIntl } from 'react-intl';
-import Markdown from 'react-markdown';
 
 export interface TemplatesPanelHeaderProps {
   title: string;
-  description: string;
+  children: React.ReactNode;
 }
 
-export const TemplatesPanelHeader = ({ title, description }: TemplatesPanelHeaderProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const intl = useIntl();
-  const intlText = {
-    CREATE_WORKFLOW: intl.formatMessage({
-      defaultMessage: 'Create a new workflow from template',
-      id: 'RZNabt',
-      description: 'Panel header title for creating the workflow',
-    }),
-    BY_MICROSOFT: intl.formatMessage({
-      defaultMessage: 'By Microsoft',
-      id: 'Xs7Uvt',
-      description: 'Panel description for stating it was created by Microsoft',
-    }),
-    TEMPLATE_DETAILS: intl.formatMessage({
-      defaultMessage: 'Template Details',
-      id: 'ZD8dme',
-      description: 'Panel description title for template details, allowing to click to read more',
-    }),
-    NAME: intl.formatMessage({
-      defaultMessage: 'Name',
-      id: 'DX9jWz',
-      description: 'Description label for template name',
-    }),
-    DESCRIPTION: intl.formatMessage({
-      defaultMessage: 'Description',
-      id: 'l9+EbH',
-      description: 'Description label for template description',
-    }),
-  };
-
+export const TemplatesPanelHeader = ({ title, children }: TemplatesPanelHeaderProps) => {
   return (
     <div className="msla-templates-panel-header">
-      <Text className="msla-templates-panel-header-title">{intlText.CREATE_WORKFLOW}</Text>
-      <div
-        className="msla-templates-panel-header-details-title"
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-      >
-        <Text className="msla-templates-panel-header-details-title-text">{intlText.TEMPLATE_DETAILS}</Text>
-        {isOpen ? <ChevronUp16Regular /> : <ChevronDown16Regular />}
-      </div>
-      {isOpen && (
-        <div className="msla-templates-panel-header-description-wrapper">
-          <div className="msla-templates-panel-header-description">
-            <Label className="msla-templates-panel-header-description-title">{intlText.NAME}</Label>
-            <Text className="msla-templates-panel-header-description-text">{title}</Text>
-          </div>
-          <div className="msla-templates-panel-header-description">
-            <Label className="msla-templates-panel-header-description-title">{intlText.DESCRIPTION}</Label>
-            <Markdown className="msla-templates-panel-header-description-text" linkTarget="_blank">
-              {description}
-            </Markdown>
-          </div>
-        </div>
-      )}
+      <Text className="msla-templates-panel-header-title">{title}</Text>
+      {children}
     </div>
   );
 };
--- libs/designer/src/lib/ui/panel/templatePanel/quickViewPanel/quickViewPanel.tsx
+++ libs/designer/src/lib/ui/panel/templatePanel/quickViewPanel/quickViewPanel.tsx
@@ -4,7 +4,7 @@ import { Text } from '@fluentui/react-components';
 import { Icon } from '@fluentui/react';
 import { useIntl } from 'react-intl';
 import { useState } from 'react';
-import { TemplatesPanelContent } from '@microsoft/designer-ui';
+import { TemplatesPanelContent, TemplatesPanelHeader } from '@microsoft/designer-ui';
 import { getQuickViewTabs } from '../../../../core/templates/utils/helper';
 import Markdown from 'react-markdown';
 
@@ -46,23 +46,24 @@ export const QuickViewPanelHeader = ({
   details,
 }: { title: string; description: string; details: Record<string, string> }) => {
   return (
-    <div className="msla-template-quickview-header">
-      <Text className="msla-template-panel-header">{title}</Text>
-      <div className="msla-template-quickview-tags">
-        {Object.keys(details).map((key: string, index: number, array: any[]) => {
-          return (
-            <div key={key}>
-              <Text className="msla-template-card-tag">
-                {key}: {details[key]}
-              </Text>
-              {index !== array.length - 1 ? (
-                <Icon style={{ padding: '3px 10px 3px 10px', color: '#dedede', fontSize: 10 }} iconName="LocationDot" />
-              ) : null}
-            </div>
-          );
-        })}
+    <TemplatesPanelHeader title={title}>
+      <div className="msla-template-quickview-header">
+        <div className="msla-template-quickview-tags">
+          {Object.keys(details).map((key: string, index: number, array: any[]) => {
+            return (
+              <div key={key}>
+                <Text className="msla-template-card-tag">
+                  {key}: {details[key]}
+                </Text>
+                {index !== array.length - 1 ? (
+                  <Icon style={{ padding: '3px 10px 3px 10px', color: '#dedede', fontSize: 10 }} iconName="LocationDot" />
+                ) : null}
+              </div>
+            );
+          })}
+        </div>
+        <Markdown linkTarget="_blank">{description}</Markdown>
       </div>
-      <Markdown linkTarget="_blank">{description}</Markdown>
-    </div>
+    </TemplatesPanelHeader>
   );
 };
