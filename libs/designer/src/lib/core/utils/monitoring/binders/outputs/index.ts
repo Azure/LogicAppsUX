82d86e91cb4d743dd27795545dc2ef3c901c47c0
Carlos Emiliano Castro Trejo <102700317+ccastrotrejo@users.noreply.github.com>
2024-09-25 09:32:54 -0700

    feat(designer): Bind outputs for monitoring view (#5723)
    * First commit
    
    * Send outputs and parser manifest
    
    * Merge function to bind inputs and outputs
    
    * Remove outputs
    
    * Add API connection outputs binder
    
    * Update outputs
    
    * Update values
    
    * Remove swagger
    
    * Remove extra code and reuse it
    
    * Fix rename

---
+++ libs/designer/src/lib/core/utils/monitoring/binders/outputs/index.ts
@@ -0,0 +1,3 @@
+export { default as DefaultOutputsBinder } from './default';
+export { default as ApiConnectionOutputsBinder } from './apiconnection';
+export { ManifestOutputsBinder } from './manifest';
