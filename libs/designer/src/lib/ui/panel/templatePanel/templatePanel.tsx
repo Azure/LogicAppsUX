859d7d71eaf243ea97d04381a37ae2b73c14ede1
Elaina-Lee <144840522+Elaina-Lee@users.noreply.github.com>
2024-09-26 10:49:19 -0700

    style(Templates): Template Context Pane Updates (#5758)
    * shifted logic of information
    
    * added css for the header description part
    
    * intltext header
    
    * moved both headers to Designer UI Header for consistency
    
    * moved css to panel.less
    
    * added icon for learn more

--- libs/designer/src/lib/ui/panel/templatePanel/templatePanel.tsx
+++ libs/designer/src/lib/ui/panel/templatePanel/templatePanel.tsx
@@ -3,9 +3,9 @@ import type { AppDispatch, RootState } from '../../../core/state/templates/store
 import { useCallback, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { closePanel } from '../../../core/state/templates/panelSlice';
-import { CreateWorkflowPanel } from './createWorkflowPanel/createWorkflowPanel';
+import { CreateWorkflowPanel, CreateWorkflowPanelHeader } from './createWorkflowPanel/createWorkflowPanel';
 import { QuickViewPanel, QuickViewPanelHeader } from './quickViewPanel/quickViewPanel';
-import { type TemplatePanelTab, TemplatesPanelFooter, TemplatesPanelHeader } from '@microsoft/designer-ui';
+import { type TemplatePanelTab, TemplatesPanelFooter } from '@microsoft/designer-ui';
 import { useCreateWorkflowPanelTabs } from './createWorkflowPanel/usePanelTabs';
 import { clearTemplateDetails } from '../../../core/state/templates/templateSlice';
 import { useIntl } from 'react-intl';
@@ -42,22 +42,6 @@ export const TemplatePanel = ({ onCreateClick }: { onCreateClick: () => Promise<
   );
 
   const selectedTabProps = selectedTabId ? currentPanelTabs?.find((tab) => tab.id === selectedTabId) : currentPanelTabs[0];
-
-  const intlText = useMemo(() => {
-    return {
-      CREATE_WORKFLOW: intl.formatMessage({
-        defaultMessage: 'Create a new workflow',
-        id: 'Y9VTmA',
-        description: 'Panel header title for creating the workflow',
-      }),
-      BY_MICROSOFT: intl.formatMessage({
-        defaultMessage: 'By Microsoft',
-        id: 'Xs7Uvt',
-        description: 'Panel description for stating it was created by Microsoft',
-      }),
-    };
-  }, [intl]);
-
   const layerProps = {
     hostId: 'msla-layer-host',
     eventBubblingEnabled: true,
@@ -68,12 +52,9 @@ export const TemplatePanel = ({ onCreateClick }: { onCreateClick: () => Promise<
       currentPanelView === 'quickView' ? (
         <QuickViewPanelHeader title={templateTitle} description={templateDescription} details={manifest?.details ?? {}} />
       ) : (
-        <TemplatesPanelHeader
-          title={currentPanelView === 'createWorkflow' ? intlText.CREATE_WORKFLOW : templateTitle}
-          description={currentPanelView === 'createWorkflow' ? templateDescription : intlText.BY_MICROSOFT}
-        />
+        <CreateWorkflowPanelHeader title={templateTitle} description={templateDescription} />
       ),
-    [currentPanelView, templateTitle, templateDescription, manifest?.details, intlText.CREATE_WORKFLOW, intlText.BY_MICROSOFT]
+    [currentPanelView, templateTitle, templateDescription, manifest?.details]
   );
   const onRenderFooterContent = useCallback(
     () => (selectedTabProps?.footerContent ? <TemplatesPanelFooter {...selectedTabProps?.footerContent} /> : null),
