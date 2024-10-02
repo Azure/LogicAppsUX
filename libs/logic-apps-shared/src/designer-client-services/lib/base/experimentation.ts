83945a69fbb3cfc6a22d1b871e928456108e06fc
Krrish Mittal <krrishmittal15@gmail.com>
2024-10-02 16:30:03 -0700

    fix(Designer): Update types for experimentation and add a new for value (#5801)
    update types

--- libs/logic-apps-shared/src/designer-client-services/lib/base/experimentation.ts
+++ libs/logic-apps-shared/src/designer-client-services/lib/base/experimentation.ts
@@ -1,11 +1,11 @@
 import type { IExperimentationService } from '../experimentation';
 
 export class BaseExperimentationService implements IExperimentationService {
-  isFeatureEnabled(_featureGateName: string, _type: 'extension' | 'shell') {
+  isFeatureEnabled(_featureGateName: string, _assignmentType: 'extension' | 'shell') {
     return Promise.resolve(false);
   }
 
-  getFeatureValue(_featureGateName: string, _type: 'extension' | 'shell') {
+  getFeatureValue(_featureGateName: string, _assignmentType: 'extension' | 'shell', _type: 'string' | 'number' | 'boolean') {
     return Promise.resolve(undefined);
   }
 }
