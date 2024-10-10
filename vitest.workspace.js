e372d6f848f43c516beb77025d4b309ba0609a03
danielle <dacogbur@microsoft.com>
2024-10-10 15:47:44 -0700

    test extension works

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -1,1 +1,9 @@
-export default ['libs/', 'apps/'];
+import { defineWorkspace } from 'vitest/config';
+
+export default defineWorkspace([
+  'libs/*',
+  'libs/data-mapper-v2/vitest.config.ts',
+  'libs/designer/vitest.config.ts',
+  'libs/designer-ui/vitest.config.ts',
+  'libs/vscode-extension/vitest.config.ts',
+]);
