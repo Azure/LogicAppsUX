d315f48da20d6a09c2e113ff1a1492839e783fb1
danielle <dacogbur@microsoft.com>
2024-10-10 22:03:40 -0700

    updated docs and cleaned up config

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -1,9 +1,3 @@
 import { defineWorkspace } from 'vitest/config';
 
-export default defineWorkspace([
-  'libs/*',
-  'libs/data-mapper-v2/vitest.config.ts',
-  'libs/designer/vitest.config.ts',
-  'libs/designer-ui/vitest.config.ts',
-  'libs/vscode-extension/vitest.config.ts',
-]);
+export default defineWorkspace(['libs/*', 'apps/vs-code-designer/vitest.config.ts']);
