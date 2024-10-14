dd734cb20615f8feb700e2d3127780b12a2d59f4
pranaydubeymicrosoft <135278983+pranaydubeymicrosoft@users.noreply.github.com>
2024-10-14 13:59:30 -0700

    feat(Designer): Add option for multiselect and dropdown for dynamic parameter (#5863)
    * add advanced text parameter options for dynamically added parameter
    
    * fix menu bug
    
    * remove extra comment
    
    * remove unused interface
    
    * fix new entry behavior
    
    * fix broken test
    
    * address comments
    
    * improve code readability
    
    ---------
    
    Co-authored-by: Pranay Dubey <pradubey@microsoft.com>

--- libs/designer-ui/src/lib/dynamicallyaddedparameter/index.tsx
+++ libs/designer-ui/src/lib/dynamicallyaddedparameter/index.tsx
@@ -2,6 +2,7 @@ import type { IContextualMenuProps } from '@fluentui/react';
 import { DirectionalHint, IconButton, TextField, TooltipHost } from '@fluentui/react';
 import type React from 'react';
 import { useIntl } from 'react-intl';
+import StringStack from './plugins/stringstack';
 
 export const DynamicallyAddedParameterType = {
   Text: 'TEXT',
@@ -10,6 +11,7 @@ export const DynamicallyAddedParameterType = {
   Boolean: 'BOOLEAN',
   Number: 'NUMBER',
   Date: 'DATE',
+  Array: 'ARRAY',
 } as const;
 export type DynamicallyAddedParameterTypeType = (typeof DynamicallyAddedParameterType)[keyof typeof DynamicallyAddedParameterType];
 
@@ -22,15 +24,42 @@ export interface DynamicallyAddedParameterProps {
   descriptionPlaceholder?: string;
   renderDescriptionField?: boolean;
   required?: boolean;
+  shouldDisplayAddDropdownOption?: boolean;
+  shouldDisplayAddMultiSelectOption?: boolean;
+  shouldDisplayRemoveDropdownOption?: boolean;
+  shouldDisplayRemoveMultiSelectOption?: boolean;
   onTitleChange: (schemaKey: string, newValue: string | undefined) => void;
   onRequiredToggle?: (schemaKey: string) => void;
   onDescriptionChange?: (schemaKey: string, newValue: string | undefined) => void;
   onDelete: (schemaKey: string) => void;
   onRenderValueField: (schemaKey: string) => JSX.Element;
+  onStringDropdownListToggle?: (schemaKey: string) => void;
+  onStringMultiSelectListToggle?: (schemaKey: string) => void;
+  onStringListUpdate?: (schemaKey: string, newValue: string[]) => void;
+  stringListValues?: (schemaKey: string) => string[];
+  isDynamicParameterMultiSelect?: (schemaKey: string) => boolean;
+  isDynamicParameterDropdown?: (schemaKey: string) => boolean;
 }
 
 export const DynamicallyAddedParameter = (props: DynamicallyAddedParameterProps): JSX.Element => {
-  const { icon, required, schemaKey, title, titlePlaceholder, onDelete, onRenderValueField, onRequiredToggle } = props;
+  const {
+    icon,
+    required,
+    schemaKey,
+    title,
+    titlePlaceholder,
+    onDelete,
+    onRenderValueField,
+    onRequiredToggle,
+    onStringMultiSelectListToggle,
+    onStringDropdownListToggle,
+    shouldDisplayAddDropdownOption,
+    shouldDisplayAddMultiSelectOption,
+    shouldDisplayRemoveDropdownOption,
+    shouldDisplayRemoveMultiSelectOption,
+    onStringListUpdate = () => {},
+    stringListValues = () => [],
+  } = props;
   const intl = useIntl();
 
   const renderMenuButton = (): JSX.Element => {
@@ -58,6 +87,30 @@ export const DynamicallyAddedParameter = (props: DynamicallyAddedParameterProps)
       description: 'Make the dynamic parameter corresponding to this row required',
     });
 
+    const addDropdownText = intl.formatMessage({
+      defaultMessage: 'Add a drop-down list of options',
+      id: 'VFnuA2',
+      description: 'Add a drop-down list of options to the text input dynamic parameter',
+    });
+
+    const removeDropdownText = intl.formatMessage({
+      defaultMessage: 'Remove list of options',
+      id: 'DbxZhS',
+      description: 'Remove the drop-down list of options for the text input dynamic parameter',
+    });
+
+    const addMultiSelectText = intl.formatMessage({
+      defaultMessage: 'Add a multi-select list of options',
+      id: 'dU0RgS',
+      description: 'Add a multi-select list of options to the text input dynamic parameter',
+    });
+
+    const removeMultiSelectText = intl.formatMessage({
+      defaultMessage: 'Remove list of multi options',
+      id: 'oxcHvs',
+      description: 'Remove multi-select list of options to the text input dynamic parameter',
+    });
+
     const baseParameterRequiredProps = {
       iconProps: { iconName: 'CheckboxComposite' },
       onClick: () => {
@@ -76,10 +129,58 @@ export const DynamicallyAddedParameter = (props: DynamicallyAddedParameterProps)
         }
       : null;
 
+    const baseParameterMultiSelectProps = {
+      iconProps: { iconName: 'BulletedTreeList' },
+      onClick: () => {
+        if (onStringMultiSelectListToggle) {
+          onStringMultiSelectListToggle(schemaKey);
+        }
+        return true;
+      },
+    };
+
+    const addMultiSelectItem = {
+      ...baseParameterMultiSelectProps,
+      key: 'dynamicallyaddedparameter_menu_add_multiselect',
+      text: addMultiSelectText,
+    };
+
+    const removeMultiSelectItem = {
+      ...baseParameterMultiSelectProps,
+      key: 'dynamicallyaddedparameter_menu_remove_multiselect',
+      text: removeMultiSelectText,
+    };
+
+    const baseParameterDropdownProps = {
+      iconProps: { iconName: 'BulletedTreeList' },
+      onClick: () => {
+        if (onStringDropdownListToggle) {
+          onStringDropdownListToggle(schemaKey);
+        }
+        return true;
+      },
+    };
+
+    const addDropdownItem = {
+      ...baseParameterDropdownProps,
+      key: 'dynamicallyaddedparameter_menu_add_dropdown',
+      text: addDropdownText,
+    };
+
+    const removeDropdownItem = {
+      ...baseParameterDropdownProps,
+      key: 'dynamicallyaddedparameter_menu_remove_dropdown',
+      text: removeDropdownText,
+    };
+
     const menuProps: IContextualMenuProps | undefined = {
       shouldFocusOnMount: true,
       items: [
         ...(parameterRequiredItem ? [parameterRequiredItem] : []),
+        ...(shouldDisplayAddMultiSelectOption && !isDynamicParameterDropdown ? [addMultiSelectItem] : []),
+        ...(shouldDisplayRemoveMultiSelectOption ? [removeMultiSelectItem] : []),
+        ...(shouldDisplayAddDropdownOption && !isDynamicParameterMultiSelect ? [addDropdownItem] : []),
+        ...(shouldDisplayRemoveDropdownOption ? [removeDropdownItem] : []),
         {
           iconProps: { iconName: 'Delete' },
           key: 'dynamicallyaddedparameter_menu_delete',
@@ -107,6 +208,27 @@ export const DynamicallyAddedParameter = (props: DynamicallyAddedParameterProps)
       </TooltipHost>
     );
   };
+
+  const multiSelectTitleText = intl.formatMessage({
+    defaultMessage: 'Multi-select list of options',
+    id: 'Ccbm2Y',
+    description: 'Multi-select list of options',
+  });
+
+  const dropdownTitleText = intl.formatMessage({
+    defaultMessage: 'Drop-down list of options',
+    id: 'B6BjCl',
+    description: 'Drop-down list of options',
+  });
+
+  const isDynamicParameterMultiSelect = props?.isDynamicParameterMultiSelect?.(schemaKey) || false;
+  const isDynamicParameterDropdown = props?.isDynamicParameterDropdown?.(schemaKey) || false;
+  const isAdvancedDynamicStringParameter = isDynamicParameterMultiSelect || isDynamicParameterDropdown;
+  const advancedStringParameterTitle = isAdvancedDynamicStringParameter
+    ? isDynamicParameterMultiSelect
+      ? multiSelectTitleText
+      : dropdownTitleText
+    : '';
 
   const renderDynamicParameterContainer = (): JSX.Element => {
     const iconStyle = {
@@ -144,6 +266,16 @@ export const DynamicallyAddedParameter = (props: DynamicallyAddedParameterProps)
             />
           </div>
         )}
+        {isAdvancedDynamicStringParameter ? (
+          <div className="msla-dynamic-added-param-token-picker">
+            <StringStack
+              advancedStringParameterTitle={advancedStringParameterTitle}
+              initialStrings={stringListValues}
+              schemaKey={schemaKey}
+              onStringListUpdate={onStringListUpdate}
+            />
+          </div>
+        ) : undefined}
       </>
     );
   };
--- libs/designer-ui/src/lib/floatingactionmenu/floatingactionmenuinputs/index.tsx
+++ libs/designer-ui/src/lib/floatingactionmenu/floatingactionmenuinputs/index.tsx
@@ -1,3 +1,4 @@
+import constants from '../../constants';
 import type { DynamicallyAddedParameterProps, DynamicallyAddedParameterTypeType } from '../../dynamicallyaddedparameter';
 import { DynamicallyAddedParameter } from '../../dynamicallyaddedparameter';
 import { generateDynamicParameterKey } from '../../dynamicallyaddedparameter/helper';
@@ -27,6 +28,17 @@ type DynamicallyAddedParameterInputsEmailProperties = DynamicallyAddedParameterI
   format: string;
 };
 
+type DynamicallyAddedParameterInputsTextProperties = DynamicallyAddedParameterInputsPropertiesBase & {
+  enum: string[];
+};
+
+type DynamicallyAddedParameterInputsArrayProperties = DynamicallyAddedParameterInputsPropertiesBase & {
+  items: {
+    enum: string[];
+    type: string;
+  };
+};
+
 type DynamicallyAddedParameterInputsFileProperties = DynamicallyAddedParameterInputsPropertiesBase & {
   properties: {
     name: {
@@ -43,7 +55,9 @@ export type DynamicallyAddedParameterInputsProperties =
   | DynamicallyAddedParameterInputsPropertiesBase
   | DynamicallyAddedParameterInputsDateProperties
   | DynamicallyAddedParameterInputsEmailProperties
-  | DynamicallyAddedParameterInputsFileProperties;
+  | DynamicallyAddedParameterInputsFileProperties
+  | DynamicallyAddedParameterInputsTextProperties
+  | DynamicallyAddedParameterInputsArrayProperties;
 
 export type DynamicallyAddedParameterInputsModel = DynamicallyAddedParameterProps & {
   required: boolean;
@@ -96,6 +110,94 @@ export const FloatingActionMenuInputs = (props: FloatingActionMenuInputsProps): 
     }
   };
 
+  const onStringDropdownListToggle = (schemaKey: string) => {
+    const { onChange } = props;
+    if (onChange) {
+      const indexOfModelToUpdate = dynamicParameterModels.findIndex((model) => model.schemaKey === schemaKey);
+      const modelToUpdate = dynamicParameterModels[indexOfModelToUpdate];
+      const newProperties =
+        (modelToUpdate.properties as DynamicallyAddedParameterInputsTextProperties).enum === undefined
+          ? { ...modelToUpdate.properties, enum: ['First option'] }
+          : { ...modelToUpdate.properties, enum: undefined };
+
+      dynamicParameterModels[indexOfModelToUpdate] = safeSetObjectPropertyValue(
+        modelToUpdate,
+        ['properties'],
+        newProperties
+      ) as DynamicallyAddedParameterInputsModel;
+      const value = serialize(dynamicParameterModels, props.isRequestApiConnectionTrigger);
+      onChange({ value });
+    }
+  };
+
+  const onStringDropdownListUpdate = (schemaKey: string, newValue: string[]) => {
+    const { onChange } = props;
+    if (onChange) {
+      const indexOfModelToUpdate = dynamicParameterModels.findIndex((model) => model.schemaKey === schemaKey);
+      const modelToUpdate = dynamicParameterModels[indexOfModelToUpdate];
+      const newProperties = { ...modelToUpdate.properties, enum: newValue };
+
+      dynamicParameterModels[indexOfModelToUpdate] = safeSetObjectPropertyValue(
+        modelToUpdate,
+        ['properties'],
+        newProperties
+      ) as DynamicallyAddedParameterInputsModel;
+      const value = serialize(dynamicParameterModels, props.isRequestApiConnectionTrigger);
+      onChange({ value });
+    }
+  };
+
+  const onStringMultiSelectListToggle = (schemaKey: string) => {
+    const { onChange } = props;
+    if (onChange) {
+      const indexOfModelToUpdate = dynamicParameterModels.findIndex((model) => model.schemaKey === schemaKey);
+      const modelToUpdate = dynamicParameterModels[indexOfModelToUpdate];
+      const currentParameterType = modelToUpdate.properties.type;
+      const newParameterType =
+        currentParameterType === constants.SWAGGER.TYPE.STRING ? constants.SWAGGER.TYPE.ARRAY : constants.SWAGGER.TYPE.STRING;
+
+      onDynamicallyAddedParameterChange(schemaKey, 'type', newParameterType);
+
+      const newProperties =
+        currentParameterType === constants.SWAGGER.TYPE.STRING
+          ? { ...modelToUpdate.properties, items: { enum: ['First option'], type: constants.SWAGGER.TYPE.STRING } }
+          : { ...modelToUpdate.properties, items: undefined };
+
+      safeSetObjectPropertyValue(modelToUpdate, ['properties'], newProperties);
+      const value = serialize(dynamicParameterModels, props.isRequestApiConnectionTrigger);
+      onChange({ value });
+    }
+  };
+
+  const onStringMultiSelectListUpdate = (schemaKey: string, newValue: string[]) => {
+    const { onChange } = props;
+    if (onChange) {
+      const indexOfModelToUpdate = dynamicParameterModels.findIndex((model) => model.schemaKey === schemaKey);
+      const modelToUpdate = dynamicParameterModels[indexOfModelToUpdate];
+      const newProperties = { ...modelToUpdate.properties, items: { enum: newValue, type: constants.SWAGGER.TYPE.STRING } };
+
+      safeSetObjectPropertyValue(modelToUpdate, ['properties'], newProperties);
+      const value = serialize(dynamicParameterModels, props.isRequestApiConnectionTrigger);
+      onChange({ value });
+    }
+  };
+
+  const isDynamicParameterDropdown = (schemaKey: string): boolean => {
+    const model = dynamicParameterModels.find((model) => model.schemaKey === schemaKey);
+    if (model && (model.properties as DynamicallyAddedParameterInputsTextProperties).enum) {
+      return true;
+    }
+    return false;
+  };
+
+  const isDynamicParameterMultiSelect = (schemaKey: string): boolean => {
+    const model = dynamicParameterModels.find((model) => model.schemaKey === schemaKey);
+    if (model && (model.properties as DynamicallyAddedParameterInputsArrayProperties).items) {
+      return true;
+    }
+    return false;
+  };
+
   const onDynamicallyAddedParameterDelete = (schemaKey: string) => {
     const { onChange } = props;
     if (onChange) {
@@ -118,16 +220,53 @@ export const FloatingActionMenuInputs = (props: FloatingActionMenuInputsProps): 
     return <TextField className="msla-dynamic-added-param-value-TextField" value={description} onChange={onDescriptionChange} />;
   };
 
+  const stringListValues = (schemaKey: string): string[] => {
+    const model = dynamicParameterModels.find((model) => model.schemaKey === schemaKey);
+    if (model && (model.properties as DynamicallyAddedParameterInputsTextProperties).enum) {
+      return (model.properties as DynamicallyAddedParameterInputsTextProperties).enum;
+    }
+    if (model && (model.properties as DynamicallyAddedParameterInputsArrayProperties).items) {
+      return (model.properties as DynamicallyAddedParameterInputsArrayProperties).items.enum;
+    }
+    return [];
+  };
+
+  const getStringListUpdateHandler = (schemaKey: string, newValue: string[]) => {
+    if (isDynamicParameterDropdown(schemaKey)) {
+      return onStringDropdownListUpdate(schemaKey, newValue);
+    }
+    if (isDynamicParameterMultiSelect(schemaKey)) {
+      return onStringMultiSelectListUpdate(schemaKey, newValue);
+    }
+    return undefined;
+  };
+
   const dynamicParameterModels: DynamicallyAddedParameterInputsModel[] = deserialize(
     props.initialValue,
     props.isRequestApiConnectionTrigger
-  ).map((model) => ({
-    ...model,
-    onTitleChange: onDynamicallyAddedParameterTitleChange,
-    onRequiredToggle: onDynamicallyAddedParameterRequiredToggle,
-    onDelete: onDynamicallyAddedParameterDelete,
-    onRenderValueField,
-  }));
+  ).map((model) => {
+    const isTextHint = model.properties['x-ms-content-hint'] === 'TEXT';
+    const isArrayOrStringType = ['array', 'string'].includes(model.properties.type);
+    const isDropdownSchema = (model.properties as DynamicallyAddedParameterInputsTextProperties).enum;
+    const isMultiSelectSchema = (model.properties as DynamicallyAddedParameterInputsArrayProperties).items;
+    return {
+      ...model,
+      onTitleChange: onDynamicallyAddedParameterTitleChange,
+      onRequiredToggle: onDynamicallyAddedParameterRequiredToggle,
+      onStringDropdownListToggle: model.properties.type === 'string' ? onStringDropdownListToggle : undefined,
+      onStringMultiSelectListToggle: isArrayOrStringType ? onStringMultiSelectListToggle : undefined,
+      onDelete: onDynamicallyAddedParameterDelete,
+      onRenderValueField,
+      isDynamicParameterMultiSelect,
+      isDynamicParameterDropdown,
+      onStringListUpdate: getStringListUpdateHandler,
+      shouldDisplayAddDropdownOption: isTextHint && !isDropdownSchema,
+      shouldDisplayAddMultiSelectOption: isTextHint && !isMultiSelectSchema,
+      shouldDisplayRemoveDropdownOption: isTextHint && !!isDropdownSchema,
+      shouldDisplayRemoveMultiSelectOption: model.properties.type === 'array' && !!isMultiSelectSchema,
+      stringListValues: isArrayOrStringType ? stringListValues : undefined,
+    };
+  });
 
   const addNewDynamicallyAddedParameter = (item: FloatingActionMenuItem) => {
     const { icon, type: floatingActionMenuItemType } = item;
@@ -137,17 +276,29 @@ export const FloatingActionMenuInputs = (props: FloatingActionMenuInputsProps): 
       item.type
     );
     const properties = createDynamicallyAddedParameterProperties(floatingActionMenuItemType, schemaKey);
-    dynamicParameterModels.push({
+    const isTextHint = properties['x-ms-content-hint'] === 'TEXT';
+    const isArrayOrStringType = ['array', 'string'].includes(properties.type);
+
+    const newModel: DynamicallyAddedParameterInputsModel = {
       icon,
       title: properties.title,
       schemaKey,
       properties,
       required: true,
       onRequiredToggle: onDynamicallyAddedParameterRequiredToggle,
+      onStringDropdownListToggle: properties.type === 'string' ? onStringDropdownListToggle : undefined,
+      onStringMultiSelectListToggle: isArrayOrStringType ? onStringMultiSelectListToggle : undefined,
       onTitleChange: onDynamicallyAddedParameterTitleChange,
       onDelete: onDynamicallyAddedParameterDelete,
       onRenderValueField,
-    });
+      shouldDisplayAddDropdownOption: isTextHint,
+      shouldDisplayAddMultiSelectOption: isTextHint,
+      onStringListUpdate: getStringListUpdateHandler,
+      stringListValues: isTextHint ? stringListValues : undefined,
+      isDynamicParameterMultiSelect: isDynamicParameterMultiSelect,
+    };
+
+    dynamicParameterModels.push(newModel);
   };
 
   const onMenuItemSelected = (selectedItem: FloatingActionMenuItem) => {
@@ -180,7 +331,6 @@ export const FloatingActionMenuInputs = (props: FloatingActionMenuInputsProps): 
     >
       {dynamicParameterModels.map((model) => {
         const { required, properties, ...props } = model;
-
         return properties['x-ms-dynamically-added'] === true ? (
           <DynamicallyAddedParameter {...props} required={required} key={props.schemaKey} />
         ) : null;
