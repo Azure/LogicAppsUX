4c904b8de2ca72a19ffbe9515d9ef7a98ac5efe3
Priti Sambandam <psamband@microsoft.com>
2024-10-17 18:23:37 -0700

    Handling connectorId being undefined

--- libs/designer/src/lib/core/utils/parameters/helper.ts
+++ libs/designer/src/lib/core/utils/parameters/helper.ts
@@ -3980,6 +3980,6 @@ export function validateUntilAction(
 }
 
 export function shouldEncodeParameterValueForOperationBasedOnMetadata(operationInfo: OperationInfo): boolean {
-  const { connectorId } = operationInfo;
-  return !connectorId.toLowerCase().includes('commondataservice');
+  const { connectorId } = operationInfo ?? {};
+  return !(connectorId?.toLowerCase().includes('commondataservice'));
 }
