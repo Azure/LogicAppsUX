86f1502c3770e854314bba2021480d44b7fb5919
Joaquim Malcampo <jmalcampo@microsoft.com>
2024-11-04 16:19:36 -0500

    changed logic to excluding files and rename select folder step

--- apps/vs-code-designer/src/app/commands/createNewCodeProject/CodeProjectBase/ExtractPackageStep.ts
+++ apps/vs-code-designer/src/app/commands/createNewCodeProject/CodeProjectBase/ExtractPackageStep.ts
@@ -7,19 +7,31 @@ import { rimraf } from 'rimraf';
 
 export class ExtractPackageStep extends AzureWizardPromptStep<IFunctionWizardContext> {
   public async prompt(context: IFunctionWizardContext): Promise<void> {
-    const packageName = path.basename(context.packagePath, path.extname(context.packagePath));
-
     try {
       const data: Buffer | Buffer[] = fs.readFileSync(context.packagePath);
       await unzipLogicAppArtifacts(data, context.projectPath);
 
-      const excludedFiles = [`${packageName}.csproj`, '.vscode', 'obj', 'bin', 'local.settings.json', 'host.json'];
-      for (const file of excludedFiles) {
-        if (fs.existsSync(path.join(context.projectPath, file))) {
-          rimraf.sync(path.join(context.projectPath, file));
-          context.telemetry.properties.excludedFile = `Excluded ${file} from package`;
+      const projectFiles = fs.readdirSync(context.projectPath);
+      const filesToExclude = [];
+      const excludedFiles = ['.vscode', 'obj', 'bin', 'local.settings.json', 'host.json'];
+      const excludedExt = ['.csproj'];
+
+      projectFiles.forEach((fileName) => {
+        if (excludedExt.includes(path.extname(fileName))) {
+          filesToExclude.push(path.join(context.projectPath, fileName));
         }
-      }
+      });
+
+      excludedFiles.forEach((excludedFile) => {
+        if (fs.existsSync(path.join(context.projectPath, excludedFile))) {
+          filesToExclude.push(path.join(context.projectPath, excludedFile));
+        }
+      });
+
+      filesToExclude.forEach((path) => {
+        rimraf.sync(path);
+        context.telemetry.properties.excludedFile = `Excluded ${path.basename} from package`;
+      });
     } catch (error) {
       context.telemetry.properties.error = error.message;
       console.error(`Failed to extract contents of package to ${context.projectPath}`, error);
