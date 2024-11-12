532c421afe2084e786a683180b14830667a74c9e
Joaquim Malcampo <jmalcampo@microsoft.com>
2024-11-12 10:23:39 -0500

    changed test

--- apps/vs-code-designer/src/app/commands/createNewCodeProject/__test__/cloudToLocal.test.ts
+++ apps/vs-code-designer/src/app/commands/createNewCodeProject/__test__/cloudToLocal.test.ts
@@ -30,8 +30,6 @@ describe('extractConnectionDetails and ChangAuthToRaw are being tested for cloud
       const newconnection = JSON.stringify(connectionObject);
       const parsedconnection: ConnectionReferenceModel = JSON.parse(newconnection);
 
-      const details = extractConnectionDetails(parsedconnection);
-
       const expectedDetails = [
         {
           WORKFLOWS_LOCATION_NAME: 'eastus2',
@@ -40,51 +38,9 @@ describe('extractConnectionDetails and ChangAuthToRaw are being tested for cloud
         },
       ];
 
-      expect(details).toEqual(expectedDetails);
-    });
-  });
-
-  describe('ChangeAuthToRaw for ConnectionReferenceModel', () => {
-    it('should change authentication type to Raw', () => {
-      // Setup
-      let connectionsJsonContent = {
-        managedApiConnections: {
-          applicationinsights: {
-            api: {
-              id: '/subscriptions/346751b2-0de1-405c-ad29-acb7ba73797f/providers/Microsoft.Web/locations/eastus2/managedApis/applicationinsights',
-            },
-            connection: {
-              id: '/subscriptions/346751b2-0de1-405c-ad29-acb7ba73797f/resourceGroups/vs-code-debug/providers/Microsoft.Web/connections/applicationinsights',
-            },
-            authentication: {
-              type: 'ManagedServiceIdentity',
-            },
-          },
-        },
-      };
-      const jsonString = JSON.stringify(connectionsJsonContent);
-      const connection: ConnectionReferenceModel = JSON.parse(jsonString);
-
-      changeAuthTypeToRaw(connection);
-
-      const expectedConnection = JSON.stringify({
-        managedApiConnections: {
-          applicationinsights: {
-            api: {
-              id: '/subscriptions/346751b2-0de1-405c-ad29-acb7ba73797f/providers/Microsoft.Web/locations/eastus2/managedApis/applicationinsights',
-            },
-            connection: {
-              id: '/subscriptions/346751b2-0de1-405c-ad29-acb7ba73797f/resourceGroups/vs-code-debug/providers/Microsoft.Web/connections/applicationinsights',
-            },
-            authentication: {
-              type: 'Raw',
-              scheme: 'Key',
-              parameter: "@appsetting('applicationinsights-connectionKey')",
-            },
-          },
-        },
+      extractConnectionDetails(parsedconnection).then((result) => {
+        expect(result).toEqual(expectedDetails);
       });
-      expect(JSON.stringify(connection)).toEqual(expectedConnection);
     });
   });
 });
