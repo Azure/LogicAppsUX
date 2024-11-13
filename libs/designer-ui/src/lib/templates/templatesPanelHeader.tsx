ad5e8d89888bcddaef38c878a32afaab6fa0f054
Elaina Lee <144840522+Elaina-Lee@users.noreply.github.com>
2024-11-13 11:28:12 -0800

    feat(Templates): Detail page dismiss button back to templates designer (#6081)
    added back to template library button on overview page

--- libs/designer-ui/src/lib/templates/templatesPanelHeader.tsx
+++ libs/designer-ui/src/lib/templates/templatesPanelHeader.tsx
@@ -1,14 +1,30 @@
-import { Text } from '@fluentui/react-components';
+import { Link, Text } from '@fluentui/react-components';
+import { ArrowLeft16Filled } from '@fluentui/react-icons';
+import { useIntl } from 'react-intl';
 
 export interface TemplatesPanelHeaderProps {
   title: string;
+  onBackClick?: () => void;
   children: React.ReactNode;
 }
 
-export const TemplatesPanelHeader = ({ title, children }: TemplatesPanelHeaderProps) => {
+export const TemplatesPanelHeader = ({ title, onBackClick, children }: TemplatesPanelHeaderProps) => {
+  const intl = useIntl();
   return (
     <div className="msla-templates-panel-header">
-      <Text className="msla-templates-panel-header-title">{title}</Text>
+      <div className="msla-templates-panel-header-title-section">
+        <Text className="msla-templates-panel-header-title">{title}</Text>
+        {onBackClick && (
+          <Link as="button" className="msla-templates-panel-header-back-button" onClick={onBackClick}>
+            <ArrowLeft16Filled />
+            {intl.formatMessage({
+              defaultMessage: 'Back to template library',
+              id: '7adnmH',
+              description: 'Button to navigate back to the template library',
+            })}
+          </Link>
+        )}
+      </div>
       {children}
     </div>
   );
