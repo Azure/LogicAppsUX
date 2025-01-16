96adb24735d68ad307d29cf40c8cb1cbfe1ea5f7
Carlos Emiliano Castro Trejo <102700317+ccastrotrejo@users.noreply.github.com>
2025-01-15 20:18:06 -0800

    fix(designer): Correctly add inactive class for subgraph cards (#6408)
    * Fix subgraph run data
    
    * Update graph container logic

--- libs/designer/src/lib/ui/CustomNodes/GraphContainerNode.tsx
+++ libs/designer/src/lib/ui/CustomNodes/GraphContainerNode.tsx
@@ -1,10 +1,10 @@
 import { useMonitoringView, useReadOnly } from '../../core/state/designerOptions/designerOptionsSelectors';
 import { useIsNodeSelectedInOperationPanel } from '../../core/state/panel/panelSelectors';
-import { useActionMetadata, useIsLeafNode, useNodeMetadata, useRunData } from '../../core/state/workflow/workflowSelectors';
+import { useActionMetadata, useIsLeafNode, useNodeMetadata, useParentRunId, useRunData } from '../../core/state/workflow/workflowSelectors';
 import { DropZone } from '../connections/dropzone';
 import { css } from '@fluentui/react';
 import { GraphContainer } from '@microsoft/designer-ui';
-import { SUBGRAPH_TYPES, useNodeSize, useNodeLeafIndex, isNullOrUndefined } from '@microsoft/logic-apps-shared';
+import { SUBGRAPH_TYPES, useNodeSize, useNodeLeafIndex, isNullOrUndefined, removeIdTag } from '@microsoft/logic-apps-shared';
 import { memo } from 'react';
 import { Handle, Position, type NodeProps } from '@xyflow/react';
 
@@ -19,7 +19,9 @@ const GraphContainerNode = ({ targetPosition = Position.Top, sourcePosition = Po
   const showLeafComponents = !readOnly && actionMetadata?.type && isLeaf;
   const isSubgraphContainer = nodeMetadata?.subgraphType !== undefined;
   const hasFooter = nodeMetadata?.subgraphType === SUBGRAPH_TYPES.UNTIL_DO;
-  const runData = useRunData(id);
+  const graphContainerId = isSubgraphContainer ? removeIdTag(id) : id;
+  const parentRunId = useParentRunId(graphContainerId);
+  const runData = useRunData(isSubgraphContainer ? (parentRunId ?? '') : graphContainerId);
 
   const nodeSize = useNodeSize(id);
 
