47dce724681829f9e292ddfc792e2de731692754
Carlos Emiliano Castro Trejo <ccastrotrejo@microsoft.com>
2025-01-22 11:47:27 -0800

    Update e2e and unit test

--- e2e/designer/monitoringView.spec.ts
+++ e2e/designer/monitoringView.spec.ts
@@ -12,42 +12,7 @@ test.describe(
       await GoToMockWorkflow(page, 'Monitoring view conditional');
       await LoadRunFile(page, 'normalState');
 
-      // // Open workflow parameters panel
-
       await expect(page.getByTestId('msla-pill-Condition 2-status')).toBeVisible();
-
-      // await page.getByText('Workflow Parameters', { exact: true }).click();
-      // await expect(page.locator('.msla-workflow-parameters-heading')).toBeVisible();
-
-      // // Verify existing float parameter was loaded properly
-      // await page.getByRole('button', { name: 'Float Parameter' }).click();
-      // await expect(page.getByText('Name*Float Parameter')).toBeVisible();
-      // await expect(page.getByText('Type*Float')).toBeVisible();
-      // await expect(page.getByText('Value*9.9')).toBeVisible();
-      // await page.getByRole('button', { name: 'Float Parameter' }).click();
-
-      // // Create new parameter
-      // await page.getByRole('button', { name: 'Create parameter' }).click();
-      // await expect(page.getByRole('button', { name: 'New parameter' })).toBeVisible();
-      // const paramId = await page.getByRole('button', { name: 'New parameter' }).getAttribute('id');
-      // // Give param a name
-      // await page.getByTestId(`${paramId}-name`).click();
-      // await page.keyboard.type('PlaywrightParam');
-      // // Verify name changed
-      // await expect(page.getByRole('button', { name: 'PlaywrightParam' })).toBeVisible();
-      // // Change param type
-      // await page.getByTestId(`${paramId}-type`).getByText('Array').click();
-      // await page.getByRole('option', { name: 'String' }).click();
-      // // Change param value
-      // await page.getByTestId(`${paramId}-value`).click();
-      // await page.getByTestId(`${paramId}-value`).fill('Hello');
-      // // Delete param
-      // await page.getByTestId(`${paramId}-parameter-delete-button`).click();
-      // await expect(page.getByRole('button', { name: 'PlaywrightParam' })).not.toBeVisible();
-
-      // // Close workflow parameters panel
-      // await page.getByLabel('Close panel').click();
-      // await expect(page.locator('.msla-workflow-parameters-heading')).not.toBeVisible();
     });
 
     test('Sanity check for loading state', async ({ page }) => {
