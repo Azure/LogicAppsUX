854c61ce89c984b24a8845fbab2277150608979c
Carlos Emiliano Castro Trejo <ccastrotrejo@microsoft.com>
2025-01-23 11:53:05 -0800

    Add rest of unit tests

--- libs/vscode-extension/src/lib/services/__test__/httpClient.spec.ts
+++ libs/vscode-extension/src/lib/services/__test__/httpClient.spec.ts
@@ -51,7 +51,7 @@ describe('HttpClient', () => {
     });
   });
 
-  it('should make a successful GET request without arm resourceid', async () => {
+  it('should make a successful GET request with arm resourceid', async () => {
     const responseData = { data: 'test-data' };
     (axios as any).mockResolvedValue({ data: responseData, status: 200 });
 
@@ -87,7 +87,7 @@ describe('HttpClient', () => {
   });
 
   it('should make a POST request', async () => {
-    const responseData = { data: JSON.stringify({ result: 'test-result' }) };
+    const responseData = { data: { result: 'test-result' } };
     (axios as any).mockResolvedValue({ data: responseData, status: 200 });
 
     const options: HttpRequestOptions<unknown> = {
@@ -98,7 +98,7 @@ describe('HttpClient', () => {
 
     const result = await httpClient.post(options);
 
-    expect(result).toEqual({ data: '{"result":"test-result"}' });
+    expect(result).toEqual({ data: { result: 'test-result' } });
     expect(axios).toHaveBeenCalledWith({
       method: 'POST',
       url: `${baseUrl}/test-post`,
@@ -114,8 +114,49 @@ describe('HttpClient', () => {
     });
   });
 
+  it('should make a successful POST request with arm resourceid', async () => {
+    const responseData = { data: { result: 'test-result' } };
+    (axios as any).mockResolvedValue({ data: responseData, status: 200 });
+
+    const options: HttpRequestOptions<unknown> = {
+      uri: '/test/subscriptions/subscription-test/test-get',
+      headers: {},
+      content: { key: 'value' },
+    };
+
+    const result = await httpClient.post(options);
+
+    expect(result).toEqual(responseData);
+    expect(axios).toHaveBeenCalledWith({
+      method: 'POST',
+      url: `${apiHubBaseUrl}/test/subscriptions/subscription-test/test-get`,
+      headers: {
+        Authorization: accessToken,
+        'Content-Type': 'application/json',
+        'x-ms-user-agent': 'LogicAppsDesigner/(host vscode 1.0.0)',
+      },
+      data: options.content,
+      content: { key: 'value' },
+      uri: '/test/subscriptions/subscription-test/test-get',
+      commandName: 'Designer.httpClient.post',
+    });
+  });
+
+  it('should throw an error when POST request returns an error', async () => {
+    const errorMessage = 'Network Error';
+    (axios as any).mockRejectedValueOnce(new Error(errorMessage)); // Simulate error
+
+    const options: HttpRequestOptions<unknown> = {
+      uri: '/test/subscriptions/subscription-test/test-get',
+      headers: {},
+      content: { key: 'value' },
+    };
+
+    await expect(httpClient.post(options)).rejects.toThrow(errorMessage);
+  });
+
   it('should make a PUT request', async () => {
-    const responseData = { data: JSON.stringify({ result: 'test-result' }) };
+    const responseData = { data: { result: 'test-result' } };
     (axios as any).mockResolvedValue({ data: responseData, status: 200 });
 
     const options: HttpRequestOptions<unknown> = {
@@ -126,20 +167,61 @@ describe('HttpClient', () => {
 
     const result = await httpClient.put(options);
 
-    expect(result).toEqual({ data: '{"result":"test-result"}' });
-    expect(axios).toHaveBeenCalledWith(
-      expect.objectContaining({
-        method: 'PUT',
-        url: `${baseUrl}/test-put`,
-        headers: expect.objectContaining({
-          Authorization: '',
-          'Content-Type': 'application/json',
-          'x-ms-user-agent': 'LogicAppsDesigner/(host vscode 1.0.0)',
-        }),
-        uri: '/test-post',
-        data: options.content,
-      })
-    );
+    expect(result).toEqual(responseData);
+    expect(axios).toHaveBeenCalledWith({
+      method: 'PUT',
+      url: `${baseUrl}/test-put`,
+      content: { key: 'value' },
+      headers: {
+        Authorization: '',
+        'Content-Type': 'application/json',
+        'x-ms-user-agent': 'LogicAppsDesigner/(host vscode 1.0.0)',
+      },
+      uri: '/test-put',
+      data: options.content,
+      commandName: 'Designer.httpClient.put',
+    });
+  });
+
+  it('should make a successful PUT request with arm resourceid', async () => {
+    const responseData = { data: { result: 'test-result' } };
+    (axios as any).mockResolvedValue({ data: responseData, status: 200 });
+
+    const options: HttpRequestOptions<unknown> = {
+      uri: '/test/subscriptions/subscription-test/test-get',
+      headers: {},
+      content: { key: 'value' },
+    };
+
+    const result = await httpClient.put(options);
+
+    expect(result).toEqual(responseData);
+    expect(axios).toHaveBeenCalledWith({
+      method: 'PUT',
+      url: `${apiHubBaseUrl}/test/subscriptions/subscription-test/test-get`,
+      content: { key: 'value' },
+      headers: {
+        Authorization: accessToken,
+        'Content-Type': 'application/json',
+        'x-ms-user-agent': 'LogicAppsDesigner/(host vscode 1.0.0)',
+      },
+      uri: '/test/subscriptions/subscription-test/test-get',
+      data: options.content,
+      commandName: 'Designer.httpClient.put',
+    });
+  });
+
+  it('should throw an error when PUT request returns an error', async () => {
+    const errorMessage = { error: { message: 'Not Found' } };
+    (axios as any).mockRejectedValueOnce({ response: { status: 404, data: errorMessage } }); // Simulate 404 error
+
+    const options: HttpRequestOptions<unknown> = {
+      uri: '/test-put-not-found',
+      headers: {},
+      content: { key: 'value' },
+    };
+
+    await expect(httpClient.put(options)).rejects.toEqual({ status: 404, ...errorMessage });
   });
 
   it('should make a DELETE request', async () => {
@@ -154,14 +236,25 @@ describe('HttpClient', () => {
     const result = await httpClient.delete(options);
 
     expect(result).toEqual(responseData);
-    expect(axios).toHaveBeenCalledWith(
-      expect.objectContaining({
-        method: 'DELETE',
-        url: `${baseUrl}/test-delete`,
-        headers: expect.objectContaining({
-          Authorization: accessToken,
-        }),
-      })
-    );
+    expect(axios).toHaveBeenCalledWith({
+      method: 'DELETE',
+      uri: '/test-delete',
+      headers: {
+        'x-ms-user-agent': 'LogicAppsDesigner/(host vscode 1.0.0)',
+        Authorization: accessToken,
+      },
+    });
+  });
+
+  it('should throw an error when DELETE request returns an error', async () => {
+    const errorMessage = 'Network Error';
+    (axios as any).mockRejectedValueOnce(new Error(errorMessage)); // Simulate error
+
+    const options: HttpRequestOptions<unknown> = {
+      uri: '/test-delete',
+      headers: {},
+    };
+
+    await expect(httpClient.delete(options)).rejects.toThrow(errorMessage);
   });
 });
