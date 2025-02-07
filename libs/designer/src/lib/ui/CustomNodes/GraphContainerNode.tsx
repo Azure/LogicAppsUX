06e3cfc444e252e06c69c00c35ea8ded1e173a50
Riley Evans <rllyy97@gmail.com>
2025-02-06 18:15:58 -0600

    fix(Designer): hotfix/5.18 - Fixed issues with loop component memoization (#6568)
    * Cherry-picked loops pages fix
    
    * Loop pager visibility in do-until action (cherry-pick)
    
    * Added missing test function
    
    * Fixed tests
    
    ---------
    
    Co-authored-by: Carlos Emiliano Castro Trejo <102700317+ccastrotrejo@users.noreply.github.com>

--- libs/designer/src/lib/ui/CustomNodes/GraphContainerNode.tsx
+++ libs/designer/src/lib/ui/CustomNodes/GraphContainerNode.tsx
@@ -21,10 +21,8 @@ const GraphContainerNode = ({ targetPosition = Position.Top, sourcePosition = Po
   const hasFooter = nodeMetadata?.subgraphType === SUBGRAPH_TYPES.UNTIL_DO;
   const graphContainerId = isSubgraphContainer ? removeIdTag(id) : id;
   const parentRunId = useParentRunId(graphContainerId);
-  const runData = useRunData(isSubgraphContainer ? (parentRunId ?? '') : graphContainerId);
-
+  const runData = useRunData(isSubgraphContainer ? (parentRunId ?? graphContainerId) : graphContainerId);
   const nodeSize = useNodeSize(id);
-
   const nodeLeafIndex = useNodeLeafIndex(id);
 
   return (
@@ -37,7 +35,7 @@ const GraphContainerNode = ({ targetPosition = Position.Top, sourcePosition = Po
         }}
       >
         <Handle className="node-handle top" type="target" position={targetPosition} isConnectable={false} />
-        <GraphContainer active={isMonitoringView ? !isNullOrUndefined(runData?.status) : true} selected={selected} />
+        <GraphContainer id={id} active={isMonitoringView ? !isNullOrUndefined(runData?.status) : true} selected={selected} />
         <Handle className="node-handle bottom" type="source" position={sourcePosition} isConnectable={false} />
       </div>
       {showLeafComponents && (
--- libs/designer/src/lib/ui/common/LoopsPager/LoopsPager.tsx
+++ libs/designer/src/lib/ui/common/LoopsPager/LoopsPager.tsx
@@ -3,10 +3,16 @@ import type { AppDispatch } from '../../../core';
 import { useActionMetadata, useNodeMetadata, useRunInstance } from '../../../core/state/workflow/workflowSelectors';
 import { setRunIndex } from '../../../core/state/workflow/workflowSlice';
 import { getForeachItemsCount } from './helper';
-import { RunService, FindPreviousAndNextPage, isNullOrUndefined, type LogicAppsV2 } from '@microsoft/logic-apps-shared';
+import {
+  RunService,
+  FindPreviousAndNextPage,
+  isNullOrUndefined,
+  type LogicAppsV2,
+  replaceWhiteSpaceWithUnderscore,
+} from '@microsoft/logic-apps-shared';
 import type { PageChangeEventArgs, PageChangeEventHandler } from '@microsoft/designer-ui';
 import { Pager } from '@microsoft/designer-ui';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useDispatch } from 'react-redux';
 
@@ -17,96 +23,101 @@ export interface LoopsPagerProps {
 }
 
 export const LoopsPager = ({ metadata, scopeId, collapsed }: LoopsPagerProps) => {
-  const [failedRepetitions, setFailedRepetitions] = useState<number[]>([]);
   const runInstance = useRunInstance();
   const dispatch = useDispatch<AppDispatch>();
   const actionMetadata = useActionMetadata(scopeId);
-  const normalizedType = actionMetadata?.type.toLowerCase();
+  const normalizedType = useMemo(() => actionMetadata?.type.toLowerCase(), [actionMetadata]);
   const nodeMetadata = useNodeMetadata(scopeId);
   const currentPage = useMemo(() => nodeMetadata?.runIndex ?? 0, [nodeMetadata]);
 
-  const forEachItemsCount = getForeachItemsCount(metadata?.runData);
+  const forEachItemsCount = useMemo(() => getForeachItemsCount(metadata?.runData), [metadata?.runData]);
 
-  const getFailedRunScopeRepetitions = () => {
-    return RunService().getScopeRepetitions({ nodeId: scopeId, runId: runInstance?.id }, constants.FLOW_STATUS.FAILED);
-  };
-
-  const onRunRepetitionsSuccess = async (repetitionValues: { value: LogicAppsV2.RunRepetition[] }) => {
-    const { value } = repetitionValues;
-    const sortedFailedRepetitions: number[] = value
-      .reduce((acc: number[], current: LogicAppsV2.RunRepetition) => {
+  const {
+    isError,
+    isLoading,
+    data: failedRepetitions,
+  } = useQuery<any>(
+    ['runRepetitions', { nodeId: scopeId, runId: runInstance?.id }],
+    async () => {
+      let failedRunRepetitions: LogicAppsV2.RunRepetition[] = [];
+      try {
+        const { value } = await RunService().getScopeRepetitions({ nodeId: scopeId, runId: runInstance?.id }, constants.FLOW_STATUS.FAILED);
+        failedRunRepetitions = value;
+      } catch {
+        failedRunRepetitions = [];
+      }
+      const _failedRepetitions: number[] = failedRunRepetitions.reduce((acc: number[], current: LogicAppsV2.RunRepetition) => {
         const scopeObject = current.properties?.repetitionIndexes?.find((item) => item.scopeName === scopeId);
         const indexOfFail = isNullOrUndefined(scopeObject) ? undefined : scopeObject.itemIndex;
         acc.push(indexOfFail ?? []);
         return acc;
-      }, [])
-      .sort();
-
-    setFailedRepetitions(sortedFailedRepetitions.sort((a, b) => a - b));
-  };
-
-  const onRunRepetitionsError = async () => {
-    setFailedRepetitions([]);
-  };
-
-  const { isError, refetch, isLoading } = useQuery<any>(
-    ['runRepetitions', { nodeId: scopeId, runId: runInstance?.id }],
-    getFailedRunScopeRepetitions,
+      }, []);
+      return _failedRepetitions.sort((a, b) => a - b);
+    },
     {
       refetchOnWindowFocus: false,
-      initialData: null,
-      refetchOnMount: true,
-      onSuccess: onRunRepetitionsSuccess,
-      onError: onRunRepetitionsError,
-      enabled: normalizedType === constants.NODE.TYPE.FOREACH,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      enabled: normalizedType === constants.NODE.TYPE.FOREACH || normalizedType === constants.NODE.TYPE.UNTIL,
     }
   );
 
-  useEffect(() => {
-    if (normalizedType === constants.NODE.TYPE.FOREACH) {
-      refetch();
-    }
-  }, [runInstance?.id, refetch, scopeId, normalizedType]);
+  const findPreviousAndNextFailed = useCallback(
+    (page: number) => FindPreviousAndNextPage(page, failedRepetitions ?? []),
+    [failedRepetitions]
+  );
 
-  const findPreviousAndNextFailed = useCallback((page: number) => FindPreviousAndNextPage(page, failedRepetitions), [failedRepetitions]);
+  const onPagerChange: PageChangeEventHandler = useCallback(
+    (page: PageChangeEventArgs) => {
+      dispatch(setRunIndex({ page: page.value - 1, nodeId: scopeId }));
+    },
+    [dispatch, scopeId]
+  );
+
+  const onClickNextFailed: PageChangeEventHandler = useCallback(
+    (page: PageChangeEventArgs) => {
+      const { nextFailedRepetition } = findPreviousAndNextFailed(page.value - 1);
+      dispatch(setRunIndex({ page: nextFailedRepetition, nodeId: scopeId }));
+    },
+    [dispatch, findPreviousAndNextFailed, scopeId]
+  );
+
+  const onClickPreviousFailed: PageChangeEventHandler = useCallback(
+    (page: PageChangeEventArgs) => {
+      const { prevFailedRepetition } = findPreviousAndNextFailed(page.value - 1);
+      dispatch(setRunIndex({ page: prevFailedRepetition, nodeId: scopeId }));
+    },
+    [dispatch, findPreviousAndNextFailed, scopeId]
+  );
+
+  const failedIterationProps = useMemo(
+    () =>
+      (failedRepetitions ?? []).length > 0
+        ? {
+            max: failedRepetitions.length >= 1 ? failedRepetitions[failedRepetitions.length - 1] + 1 : 0,
+            min: failedRepetitions[0] + 1 >= 1 ? failedRepetitions[0] + 1 : 1,
+            onClickNext: onClickNextFailed,
+            onClickPrevious: onClickPreviousFailed,
+          }
+        : undefined,
+    [failedRepetitions, onClickNextFailed, onClickPreviousFailed]
+  );
 
   if (!forEachItemsCount || isError || collapsed) {
     return null;
   }
 
-  const onPagerChange: PageChangeEventHandler = (page: PageChangeEventArgs) => {
-    dispatch(setRunIndex({ page: page.value - 1, nodeId: scopeId }));
-  };
-
-  const onClickNextFailed: PageChangeEventHandler = (page: PageChangeEventArgs) => {
-    const { nextFailedRepetition } = findPreviousAndNextFailed(page.value - 1);
-    dispatch(setRunIndex({ page: nextFailedRepetition, nodeId: scopeId }));
-  };
-
-  const onClickPreviousFailed: PageChangeEventHandler = (page: PageChangeEventArgs) => {
-    const { prevFailedRepetition } = findPreviousAndNextFailed(page.value - 1);
-    dispatch(setRunIndex({ page: prevFailedRepetition, nodeId: scopeId }));
-  };
-
-  const failedIterationProps =
-    failedRepetitions.length > 0
-      ? {
-          max: failedRepetitions.length >= 1 ? failedRepetitions[failedRepetitions.length - 1] + 1 : 0,
-          min: failedRepetitions[0] + 1 >= 1 ? failedRepetitions[0] + 1 : 1,
-          onClickNext: onClickNextFailed,
-          onClickPrevious: onClickPreviousFailed,
-        }
-      : undefined;
-
   return isLoading ? null : (
-    <Pager
-      current={currentPage + 1}
-      onChange={onPagerChange}
-      max={forEachItemsCount}
-      maxLength={forEachItemsCount.toString().length + 1}
-      min={1}
-      readonlyPagerInput={false}
-      failedIterationProps={failedIterationProps}
-    />
+    <div data-automation-id={`msla-pager-v2-${replaceWhiteSpaceWithUnderscore(scopeId)}`}>
+      <Pager
+        current={currentPage + 1}
+        onChange={onPagerChange}
+        max={forEachItemsCount}
+        maxLength={forEachItemsCount.toString().length + 1}
+        min={1}
+        readonlyPagerInput={false}
+        failedIterationProps={failedIterationProps}
+      />
+    </div>
   );
 };
