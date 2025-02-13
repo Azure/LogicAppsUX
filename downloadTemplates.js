3c3b3ff86c20b5742981d48f9ca2048b615d5e68
Tony Tang <tonytang@microsoft.com>
2025-02-13 15:14:37 -0800

    Pull template from main branch

--- downloadTemplates.js
+++ downloadTemplates.js
@@ -3,7 +3,7 @@ import { existsSync, writeFile, createWriteStream } from 'fs';
 import { mkdir, rm } from 'fs/promises';
 import client from 'https';
 
-const releaseBranch = 'release/20250203';
+const releaseBranch = 'main';
 
 const baseURL = `https://raw.githubusercontent.com/azure/LogicAppsTemplates/${releaseBranch}`;
 const sourceCodeURL = `https://github.com/Azure/LogicAppsTemplates/tree/${releaseBranch}`;
