7f61b5275b46942e3b2b759545487fdf45f6860b
Carlos Emiliano Castro Trejo <ccastrotrejo@microsoft.com>
2025-02-18 16:13:45 -0500

    Update tests

--- e2e/designer/real-api/monitoring/NestedLoops/nestedLoops.spec.ts
+++ e2e/designer/real-api/monitoring/NestedLoops/nestedLoops.spec.ts
@@ -19,15 +19,29 @@ test.describe(
       await page.getByRole('menuitem', { name: 'Run History' }).click();
       await page.waitForTimeout(5000);
       await page.getByRole('gridcell', { name: '/18/2025, 3:25:03 PM' }).click();
-      await page.waitForTimeout(5000);
 
       // Check for outermost foreach
       await expect(page.getByTestId('msla-pill-foreach_status')).toBeVisible();
       await expect(page.getByTestId('msla-pager-v2-foreach')).toBeVisible();
 
-      // Check for innermost foreach
+      // Check for inner actions
+      await expect(page.getByTestId('card-Increment counter')).toBeVisible();
+      await expect(page.getByLabel('1.1 seconds. Succeeded')).toContainText('1s');
+      await expect(page.getByLabel('11.3 seconds. Succeeded')).toContainText('11s');
+
+      // Check for innermost loop
       await expect(page.getByTestId('msla-pill-foreach_2_status')).toBeVisible();
       await expect(page.getByTestId('msla-pager-v2-foreach_2')).toBeVisible();
+
+      // Open panel
+      await page.getByTestId('card-increment_counter').getByRole('button', { name: 'Increment counter' }).click();
+      await expect(page.getByLabel('Value', { exact: true }).locator('pre')).toContainText('1');
+
+      // Move to next iteration
+      await page.getByTestId('rf__node-Foreach-#scope').getByLabel('Next').click();
+      await expect(page.getByLabel('Value', { exact: true }).locator('pre')).toContainText('2');
+      await expect(page.getByLabel('0.1 seconds. Succeeded')).toContainText('0.1s');
+      await expect(page.getByLabel('13.3 seconds. Succeeded')).toContainText('13s');
     });
   }
 );
