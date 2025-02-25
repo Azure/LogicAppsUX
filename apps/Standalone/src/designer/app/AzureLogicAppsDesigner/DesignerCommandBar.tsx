d692353eac6bb2f7208c21454fa3846d812da6d6
Carlos Emiliano Castro Trejo <102700317+ccastrotrejo@users.noreply.github.com>
2025-02-25 09:28:22 -0500

    fix(designer): Update callback url accordingly to new name (#6656)
    * Update callback url accordingly to new name
    
    * Add e2e tests
    
    * Add e2e tests
    
    * Update snapshots

--- apps/Standalone/src/designer/app/AzureLogicAppsDesigner/DesignerCommandBar.tsx
+++ apps/Standalone/src/designer/app/AzureLogicAppsDesigner/DesignerCommandBar.tsx
@@ -130,7 +130,9 @@ export const DesignerCommandBar = ({
 
     if (!hasParametersErrors) {
       await saveWorkflow(serializedWorkflow, customCodeFilesWithData, () => dispatch(resetDesignerDirtyState(undefined)));
-      updateCallbackUrl(designerState, DesignerStore.dispatch);
+      if (Object.keys(serializedWorkflow?.definition?.triggers ?? {}).length > 0) {
+        updateCallbackUrl(designerState, dispatch);
+      }
     }
   });
 
