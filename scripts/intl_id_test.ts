87555a91eebc5b34799fcd8071667e7f7d593b33
Riley Evans <rllyy97@gmail.com>
2025-03-05 17:03:35 -0600

    Small modifications

--- scripts/intl_id_test.ts
+++ scripts/intl_id_test.ts
@@ -2,8 +2,12 @@ import crypto from 'crypto';
 import { format } from '../format';
 
 function generateFullSha512(content) {
-  return crypto.createHash('sha512').update(content).digest('hex');
+  return crypto.createHash('sha512').update(content).digest('hex').slice(0, 12);
 }
+
+// Expecting output to be: 66f029ee63f7
+// From here: apps\vs-code-react\src\app\designer\DesignerCommandBar\index.tsx
+// Seems to go through this file's format function somehow before being parsed by the eslint rule: format.js
 
 console.log(
   generateFullSha512(
