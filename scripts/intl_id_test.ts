2b3e1dad326ef582cfd250a56c4681a0a430002d
Riley Evans <rllyy97@gmail.com>
2025-03-06 12:23:49 -0600

    Small tweak

--- scripts/intl_id_test.ts
+++ scripts/intl_id_test.ts
@@ -23,40 +23,39 @@ var testFile = `
       );
     }
   }
-`
+`;
 const localIdMapping = {}; // Store old -> new ID mapping for this file
 
 const intlMessages = extractIntlMessages(testFile);
 
 // Iterate over each intl.formatMessage object
 intlMessages.forEach((message) => {
+  // Extract old ID, defaultMessage, description
+  const oldId = extractIntlProperty(message, 'id');
+  const defaultMessage = extractIntlProperty(message, 'defaultMessage');
+  const description = extractIntlProperty(message, 'description');
 
-	// Extract old ID, defaultMessage, description
-	const oldId = extractIntlProperty(message, 'id');
-	const defaultMessage = extractIntlProperty(message, 'defaultMessage');
-	const description = extractIntlProperty(message, 'description');
-
-	if (!oldId || !defaultMessage || !description) {
-		console.error(`❌ Invalid intl.formatMessage object found: ${message}`);
-	} else {
-		// Generate new ID based on defaultMessage and description
-		const newId = generateHexId(defaultMessage, description);
-		if (!localIdMapping[oldId] || localIdMapping[oldId] === newId) {
-			localIdMapping[oldId] = newId;
-		} else {
-			// Error emoji to indicate that the ID is already present
-			console.error(`❌ ID ${oldId} already exists in the mapping.`);
-		}
-	}
+  if (!oldId || !defaultMessage || !description) {
+    console.error(`❌ Invalid intl.formatMessage object found: ${message}`);
+  } else {
+    // Generate new ID based on defaultMessage and description
+    const newId = generateHexId(defaultMessage, description);
+    if (!localIdMapping[oldId] || localIdMapping[oldId] === newId) {
+      localIdMapping[oldId] = newId;
+    } else {
+      // Error emoji to indicate that the ID is already present
+      console.error(`❌ ID ${oldId} already exists in the mapping.`);
+    }
+  }
 });
 
 // Replace old IDs with new IDs
 Object.keys(localIdMapping).forEach((oldId) => {
-	// Regex to find the `id` key and replace its value
-	const regex = new RegExp(`(id:\\s*['"\`])${oldId}(['"\`])`);
-	// Replace the old ID with the new ID
-	const newId = localIdMapping[oldId];
-	testFile = testFile.replace(regex, `$1${newId}`);
-	console.log(`#> Replacing ID ${oldId} with ${newId}`);
-	console.log(`#> New Content: ${testFile.replace(regex, `$1${newId}`)}`);
+  // Regex to find the `id` key and replace its value
+  const regex = new RegExp(`(id:\\s*['"\`])${oldId}(['"\`])`);
+  // Replace the old ID with the new ID
+  const newId = localIdMapping[oldId];
+  testFile = testFile.replace(regex, `$1${newId}$2`);
+  console.log(`#> Replacing ID ${oldId} with ${newId}`);
+  console.log(`#> New Content: ${testFile.replace(regex, `$1${newId}`)}`);
 });
