8e798af02767860fcae4b8fa5510c87f08685d0c
danielle <dacogbur@microsoft.com>
2025-03-12 13:06:14 -0700

    revert accidental v1 change

--- libs/data-mapper/src/lib/core/state/Store.ts
+++ libs/data-mapper/src/lib/core/state/Store.ts
@@ -25,7 +25,6 @@ export const store = configureStore({
     modal: modalReducer,
     dataMap: undoable(dataMapReducer, {
       filter: includeAction(includedActions),
-      ignoreInitialState: true,
     }),
     app: appReducer,
   },
