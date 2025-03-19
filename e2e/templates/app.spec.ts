91e3fa1fba478443b5c5a3db9f37b16948c75e35
Elaina Lee <hyehwalee@microsoft.com>
2025-03-19 16:28:50 -0700

    fixed tests

--- e2e/templates/app.spec.ts
+++ e2e/templates/app.spec.ts
@@ -31,6 +31,7 @@ test.describe(
       await page.getByRole('tab', { name: 'Summary' }).click();
       await page.getByRole('button', { name: 'Use this template' }).click();
       await page.getByRole('tab', { name: 'Basics' }).click();
+      await expect(page.getByText('Workflow name*', { exact: true })).toBeVisible();
       await page.getByText('Stateless', { exact: true }).click();
       await page.getByRole('tab', { name: 'Review + create' }).click();
       await expect(page.getByText('Create a new workflow from template', { exact: true })).toBeVisible();
@@ -82,7 +83,7 @@ test.describe(
       await page.getByText('[Mock] Simple Parameters', { exact: false }).click();
       await page.getByRole('button', { name: 'Use this template' }).click();
       await page.getByRole('tab', { name: 'Basics' }).click();
-      await expect(page.getByText('Workflow name')).toBeVisible();
+      await expect(page.getByText('Workflow name*', { exact: true })).toBeVisible();
       await expect(page.getByText('State type')).not.toBeVisible();
 
       await page.getByRole('button', { name: 'Next' }).click();
@@ -104,7 +105,7 @@ test.describe(
       await expect(page.getByText('Subscription 1', { exact: true })).toBeVisible();
       await expect(page.getByText('SecondRG', { exact: true })).toBeVisible();
       await expect(page.getByText('East US', { exact: true })).toBeVisible();
-      await expect(page.getByText('Workflow name')).toBeVisible();
+      await expect(page.getByText('Workflow name*', { exact: true })).toBeVisible();
 
       await page.getByRole('button', { name: 'Next' }).click();
       await page.getByRole('tab', { name: 'Review + create' }).click();
@@ -120,7 +121,7 @@ test.describe(
       await expect(page.getByText('Subscription 1', { exact: true })).toBeVisible();
       await expect(page.getByText('SecondRG', { exact: true })).toBeVisible();
       await expect(page.getByText('East US', { exact: true })).toBeVisible();
-      await expect(page.getByText('Workflow name')).toBeVisible();
+      await expect(page.getByText('Workflow name*', { exact: true })).toBeVisible();
 
       await page.getByRole('button', { name: 'Next' }).click();
       await page.getByRole('tab', { name: 'Review + create' }).click();
