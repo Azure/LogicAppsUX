56e2d8ee9e9b0d6637963aacc67b374cdbbaa417
Elaina Lee <hyehwalee@microsoft.com>
2025-03-20 11:13:21 -0700

    removed additional describe blocks from tests

--- libs/designer-ui/src/lib/templates/__test__/templatesSection.spec.tsx
+++ libs/designer-ui/src/lib/templates/__test__/templatesSection.spec.tsx
@@ -38,76 +38,62 @@ describe('ui/templates/templatesSection', () => {
     expect(callout).toBeDefined();
   });
 
-  describe('title and description', () => {
-    it('should have title and empty children wrapper only', () => {
-      renderer.render(<TemplatesSection {...minimal1} />);
+  it('should have title and empty children wrapper only', () => {
+    renderer.render(<TemplatesSection {...minimal1} />);
 
-      const callout = renderer.getRenderOutput();
-      expect(callout.props.className).toBe(classNames.templatesSection);
-      const [first, second]: any[] = React.Children.toArray(callout.props.children);
-      expect(first.props.className).toBe(classNames.sectionTitle);
-      expect(second.props.className).toBe(classNames.sectionItems);
-    });
-
-    it('should have title, description and empty children wrapper only', () => {
-      renderer.render(
-        <TemplatesSection {...minimal1} description={'description'} descriptionLink={{ text: 'description', href: '/link' }} />
-      );
-
-      const callout = renderer.getRenderOutput();
-      expect(callout.props.className).toBe(classNames.templatesSection);
-      const [first, second, third]: any[] = React.Children.toArray(callout.props.children);
-      expect(first.props.className).toBe(classNames.sectionTitle);
-      expect(second.props.className).toBe(classNames.sectionDescription);
-      const [_desText, desLink]: any[] = React.Children.toArray(second.props.children);
-      expect(desLink.props.className).toBe(classNames.sectionDescriptionLink);
-      expect(third.props.className).toBe(classNames.sectionItems);
-    });
+    const callout = renderer.getRenderOutput();
+    expect(callout.props.className).toBe(classNames.templatesSection);
+    const [first, second]: any[] = React.Children.toArray(callout.props.children);
+    expect(first.props.className).toBe(classNames.sectionTitle);
+    expect(second.props.className).toBe(classNames.sectionItems);
   });
 
-  describe('title and description', () => {
-    it('should have title and empty children wrapper only', () => {
-      renderer.render(<TemplatesSection {...minimal1} />);
+  it('should have title, description and empty children wrapper only', () => {
+    renderer.render(
+      <TemplatesSection {...minimal1} description={'description'} descriptionLink={{ text: 'description', href: '/link' }} />
+    );
 
-      const callout = renderer.getRenderOutput();
-      expect(callout.props.className).toBe(classNames.templatesSection);
-      const [first, second]: any[] = React.Children.toArray(callout.props.children);
-      expect(first.props.className).toBe(classNames.sectionTitle);
-      expect(second.props.className).toBe(classNames.sectionItems);
-    });
+    const callout = renderer.getRenderOutput();
+    expect(callout.props.className).toBe(classNames.templatesSection);
+    const [first, second, third]: any[] = React.Children.toArray(callout.props.children);
+    expect(first.props.className).toBe(classNames.sectionTitle);
+    expect(second.props.className).toBe(classNames.sectionDescription);
+    const [_desText, desLink]: any[] = React.Children.toArray(second.props.children);
+    expect(desLink.props.className).toBe(classNames.sectionDescriptionLink);
+    expect(third.props.className).toBe(classNames.sectionItems);
+  });
 
-    it('text display section', () => {
-      renderer.render(
-        <TemplatesSection
-          title={'title'}
-          description={'description'}
-          items={[
-            {
-              type: 'text',
-              label: 'label1',
-              value: 'value 1',
-            },
-            {
-              type: 'text',
-              value: 'value 2',
-            },
-          ]}
-        />
-      );
+  it('text display section', () => {
+    renderer.render(
+      <TemplatesSection
+        title={'title'}
+        description={'description'}
+        items={[
+          {
+            type: 'text',
+            label: 'label1',
+            value: 'value 1',
+          },
+          {
+            type: 'text',
+            value: 'value 2',
+          },
+        ]}
+      />
+    );
 
-      const callout = renderer.getRenderOutput();
-      expect(callout.props.className).toBe(classNames.templatesSection);
-      const [_first, _second, third]: any[] = React.Children.toArray(callout.props.children);
-      expect(third.props.className).toBe(classNames.sectionItems);
-      const [firstText, secondText]: any[] = React.Children.toArray(third.props.children);
-      expect(firstText.props.className).toBe(classNames.sectionItem);
-      expect(secondText.props.className).toBe(classNames.sectionItem);
-      const [firstLabel, firstValue]: any[] = React.Children.toArray(firstText.props.children);
-      expect(firstLabel.props.className).toBe(classNames.sectionItemLabel);
-      expect(firstValue.props.className).toBe(classNames.sectionItemValue);
+    const callout = renderer.getRenderOutput();
+    expect(callout.props.className).toBe(classNames.templatesSection);
+    const [_first, _second, third]: any[] = React.Children.toArray(callout.props.children);
+    expect(third.props.className).toBe(classNames.sectionItems);
+    const [firstText, secondText]: any[] = React.Children.toArray(third.props.children);
+    expect(firstText.props.className).toBe(classNames.sectionItem);
+    expect(secondText.props.className).toBe(classNames.sectionItem);
+    const [firstLabel, firstValue]: any[] = React.Children.toArray(firstText.props.children);
+    expect(firstLabel.props.className).toBe(classNames.sectionItemLabel);
+    expect(firstValue.props.className).toBe(classNames.sectionItemValue);
 
-      const [secondValue]: any[] = React.Children.toArray(secondText.props.children);
-      expect(secondValue.props.className).toBe(classNames.sectionItemValue);
-    });
+    const [secondValue]: any[] = React.Children.toArray(secondText.props.children);
+    expect(secondValue.props.className).toBe(classNames.sectionItemValue);
   });
 });
