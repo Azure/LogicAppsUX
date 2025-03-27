aead700e154b8a7895004be418fc20ec6a91b6f8
preetriti1 <48265693+preetriti1@users.noreply.github.com>
2025-03-27 12:01:25 -0700

    feat(templates): Adding create consumption workflow from template helper as exportable method (#6877)
    * Adding a reset state action for templates
    
    * fix(templates): Adding a reload handler when a resource changes and services to reload
    
    * Adding CITs and also updated standard app template standalone
    
    * chore(release): 5.53.1
    
    * Revert "chore(release): 5.53.1"
    
    This reverts commit 4fc648d6e5dc5402cd8b27cd461cbe35f6f1f1fd.
    
    * Calling handler on every resource change to track resource validity
    
    * Updating the package version correctly
    
    * feat(Templates): Review tab improvements + Template provider useExternalRedux flag (#6829)
    
    feat(Templates): Review tab improvements + Template provider `useExternalRedux` flag (#6828)
    
    * Tweaked some review tab behavior
    
    * Review fix
    
    * chore(release): 5.53.2
    
    * chore(release): 5.53.3
    
    * chore(release): 5.53.4
    
    * Adding create workflow payload function
    
    * chore(release): 5.53.5
    
    * Updating gallery with cssoverrides
    
    * chore(release): 5.53.6
    
    * replacing icon to svg
    
    * chore(release): 5.53.7
    
    * Reverting the merge issues in changelog.md
    
    ---------
    
    Co-authored-by: Priti Sambandam <psamband@microsoft.com>
    Co-authored-by: release-automation-preetriti1@microsoft.com <preetriti1>
    Co-authored-by: Riley Evans <rllyy97@gmail.com>
    Co-authored-by: release-automation-rllyy97@microsoft.com <rllyy97>

--- libs/designer/src/lib/core/index.ts
+++ libs/designer/src/lib/core/index.ts
@@ -99,5 +99,6 @@ export type { AppDispatch as TemplatesAppDispatch, RootState as TemplatesRootSta
 export type { TemplateServiceOptions } from './templates/TemplatesDesignerContext';
 export { ConfigureTemplateWizard } from './configuretemplate/ConfigureTemplateWizard';
 export { validateParameters, validateConnections } from './state/templates/templateSlice';
+export { getConsumptionWorkflowPayloadForCreate } from './templates/utils/createhelper';
 export * from './state/modal/modalSelectors';
 export * from './state/modal/modalSlice';
