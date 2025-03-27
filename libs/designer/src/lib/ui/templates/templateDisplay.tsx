590b32289265d0ea219736b7a782477a01e60206
Riley Evans <rllyy97@gmail.com>
2025-03-27 15:35:06 -0500

    Made description a subtext

--- libs/designer/src/lib/ui/templates/templateDisplay.tsx
+++ libs/designer/src/lib/ui/templates/templateDisplay.tsx
@@ -11,31 +11,23 @@ const useStyles = makeStyles({
 
 export interface TemplateDisplayProps {
   titleLabel?: string;
-  descriptionLabel?: string;
   showDescription?: boolean;
   cssOverrides?: Record<string, string>;
 }
 
-export const TemplateDisplay = ({ titleLabel, descriptionLabel, showDescription, cssOverrides }: TemplateDisplayProps) => {
+export const TemplateDisplay = ({ titleLabel, showDescription, cssOverrides }: TemplateDisplayProps) => {
   const styles = useStyles();
   const { resourceStrings } = useTemplatesStrings();
   const templateTitle = useSelector((state: RootState) => state.template.manifest?.title);
   const templateDescription = useSelector((state: RootState) => state.template.manifest?.summary);
 
   return (
-    <>
-      <div className={mergeClasses('msla-templates-review-block', cssOverrides?.templateName)}>
-        <Text>{titleLabel ?? resourceStrings.TEMPLATE_NAME}</Text>
-        <Text weight="semibold" className={styles.actionName}>
-          {templateTitle}
-        </Text>
-      </div>
-      {showDescription && (
-        <div className={mergeClasses('msla-templates-review-block', cssOverrides?.templateDescription)}>
-          <Text>{descriptionLabel ?? resourceStrings.DESCRIPTION}</Text>
-          <Text weight="semibold">{templateDescription}</Text>
-        </div>
-      )}
-    </>
+    <div className={mergeClasses('msla-templates-review-block', cssOverrides?.templateName)}>
+      <Text>{titleLabel ?? resourceStrings.TEMPLATE_NAME}</Text>
+      <Text weight="semibold" className={styles.actionName}>
+        {templateTitle}
+      </Text>
+      {showDescription && <Text>{templateDescription}</Text>}
+    </div>
   );
 };
