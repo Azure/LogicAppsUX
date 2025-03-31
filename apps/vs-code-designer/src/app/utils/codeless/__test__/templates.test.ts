05a232eacaa8f7dd1028f1b403dddbec1de57f40
Carlos Emiliano Castro Trejo <ccastrotrejo@microsoft.com>
2025-03-31 11:22:55 -0700

    Update unit tests

--- apps/vs-code-designer/src/app/utils/codeless/__test__/templates.test.ts
+++ apps/vs-code-designer/src/app/utils/codeless/__test__/templates.test.ts
@@ -1,7 +1,7 @@
 import { type StandardApp, ProjectType } from '@microsoft/vscode-extension-logic-apps';
 import { describe, it, expect } from 'vitest';
 import { getCodelessWorkflowTemplate, getWorkflowTemplate } from '../templates';
-import { WorkflowKind } from '../../../../constants';
+import { WorkflowKind, WorkflowType } from '../../../../constants';
 import { isNullOrEmpty } from '@microsoft/logic-apps-shared';
 
 const methodName = 'testMethod';
@@ -32,14 +32,20 @@ describe('utils/codeless/templates', () => {
   });
 
   it('Should return an empty stateful workflow definition', () => {
-    const workflowDefinition: StandardApp = getCodelessWorkflowTemplate(true);
+    const workflowDefinition: StandardApp = getCodelessWorkflowTemplate(WorkflowType.stateful);
     expect(workflowDefinition.kind).toBe(WorkflowKind.stateful);
     expect(isNullOrEmpty(workflowDefinition.definition.actions)).toBe(true);
   });
 
   it('Should return an empty stateless workflow definition', () => {
-    const workflowDefinition: StandardApp = getCodelessWorkflowTemplate(false);
+    const workflowDefinition: StandardApp = getCodelessWorkflowTemplate(WorkflowType.stateless);
     expect(workflowDefinition.kind).toBe(WorkflowKind.stateless);
     expect(isNullOrEmpty(workflowDefinition.definition.actions)).toBe(true);
+  });
+
+  it('Should return an empty agentic workflow definition', () => {
+    const workflowDefinition: StandardApp = getCodelessWorkflowTemplate(WorkflowType.agentic);
+    expect(workflowDefinition.kind).toBe(WorkflowKind.agentic);
+    expect(Object.values(workflowDefinition?.definition?.actions ?? {}).some((action) => action.type === 'Agent')).toBe(true);
   });
 });
