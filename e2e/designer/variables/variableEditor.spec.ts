60e4f6d0b081d94c2674616bed68089c95d6d3de
Eric-B-Wu <wue@microsoft.com>
2025-04-01 14:36:55 -0700

    fix test

--- e2e/designer/variables/variableEditor.spec.ts
+++ e2e/designer/variables/variableEditor.spec.ts
@@ -43,7 +43,7 @@ test.describe(
 
         await page.waitForTimeout(100);
 
-        await page.getByTestId('name - 1 contenteditable').click();
+        await page.getByTestId('[data-testid="name - 1 contenteditable"]').click();
         await page.locator('[data-testid="name - 1 contenteditable"]').fill('newVariable');
         await page.getByText('Select variable type').click();
         await page.getByRole('option', { name: 'Integer' }).click();
