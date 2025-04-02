2e29b3709de0708cc4ccc5b76070cd82a22fca0e
Riley Evans <rllyy97@gmail.com>
2025-04-02 13:55:13 -0700

    feat(Designer): Removing template bundling from packages (#6948)
    * Removing templates bundling and upddated service code to read only from endpoint
    
    * Removed deprecated test
    
    ---------
    
    Co-authored-by: Priti Sambandam <psamband@microsoft.com>
    Co-authored-by: Riley Evans <rileyevans@microsoft.com>

--- e2e/templates/app.spec.ts
+++ e2e/templates/app.spec.ts
@@ -40,27 +40,6 @@ test.describe(
       await expect(page.getByText('State type')).toBeVisible();
     });
 
-    test('Should contain all templates when templates are loaded from packaged files', async ({ page }) => {
-      await page.goto('/templates');
-
-      await page.getByText('Local', { exact: true }).click();
-      await page.getByLabel('Categories').click();
-      await page.getByText('Automation', { exact: true }).click();
-
-      await page.waitForTimeout(10);
-
-      await page.getByText('Azure Business').click();
-      await page.getByRole('tab', { name: 'Workflow' }).click();
-      await page.getByRole('tab', { name: 'Summary' }).click();
-      await page.getByTestId('template-footer-primary-button').click();
-      await expect(page.getByText('Create a new workflow from template', { exact: true })).toBeVisible();
-      await page.getByRole('button', { name: 'Close' }).click();
-
-      await page.getByPlaceholder('Search').fill('test');
-      await page.waitForTimeout(5);
-      await expect(page.getByText('Test template', { exact: true })).not.toBeVisible();
-    });
-
     test('Should only contain the mock templates when templates are loaded from azure endpoint', async ({ page }) => {
       await page.goto('/templates');
       await page.getByText('Local', { exact: true }).click();
