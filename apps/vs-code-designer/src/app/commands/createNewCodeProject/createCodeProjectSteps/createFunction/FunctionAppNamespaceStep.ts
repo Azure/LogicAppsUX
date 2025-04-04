01211d3971a51b2bf0ff1bac3f9edc1ed88a86a8
andrew-eldridge <eldridga@email.sc.edu>
2025-04-04 18:25:44 -0400

    add test for FunctionAppNamespaceStep

--- apps/vs-code-designer/src/app/commands/createNewCodeProject/createCodeProjectSteps/createFunction/FunctionAppNamespaceStep.ts
+++ apps/vs-code-designer/src/app/commands/createNewCodeProject/createCodeProjectSteps/createFunction/FunctionAppNamespaceStep.ts
@@ -2,6 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import { ext } from '../../../../../extensionVariables';
 import { localize } from '../../../../../localize';
 import { AzureWizardPromptStep } from '@microsoft/vscode-azext-utils';
 import type { IProjectWizardContext } from '@microsoft/vscode-extension-logic-apps';
@@ -15,6 +16,10 @@ export class FunctionAppNamespaceStep extends AzureWizardPromptStep<IProjectWiza
       prompt: localize('methodNamePrompt', 'Provide a namespace for functions project'),
       validateInput: async (input: string): Promise<string | undefined> => await this.validateNamespace(input),
     });
+
+    ext.outputChannel.appendLog(
+      localize('functionAppNamespaceSet', `Function App project namespace set to ${context.functionAppNamespace}`)
+    );
   }
 
   public shouldPrompt(_context: IProjectWizardContext): boolean {
