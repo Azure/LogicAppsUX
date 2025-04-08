c94327e6a42a858c75fd2bba9f73765e9aa72994
Eric-B-Wu <wue@microsoft.com>
2025-04-08 10:29:00 -0700

    fix array editor first

--- libs/designer-ui/src/lib/agentinstruction/index.tsx
+++ libs/designer-ui/src/lib/agentinstruction/index.tsx
@@ -84,7 +84,7 @@ export const AgentInstructionEditor = ({
         <NavigateIcon style={{ position: 'relative', top: '2px' }} />
       </Link>
       <div className="msla-agent-instruction-editors">
-        <Label text={systemPromptLabel} />
+        <Label text={systemPromptLabel} isRequiredField />
         <StringEditor
           {...props}
           className={css(className, 'msla-agent-instruction-system-editor editor-custom')}
@@ -95,6 +95,7 @@ export const AgentInstructionEditor = ({
         <Label text={userItemLabel} />
         <ArrayEditor
           {...props}
+          isRequired={false}
           label={userItemLabel}
           placeholder={userPlaceholder}
           itemSchema={{ key: 'userMessage', type: 'string' }}
