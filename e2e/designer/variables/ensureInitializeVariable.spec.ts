cccd3b5d01b5df9bdbf7ce0992d895f5715de490
Eric-B-Wu <wue@microsoft.com>
2025-04-25 10:13:25 -0700

    fix e2e tests

--- e2e/designer/variables/ensureInitializeVariable.spec.ts
+++ e2e/designer/variables/ensureInitializeVariable.spec.ts
@@ -12,8 +12,7 @@ test(
     await GoToMockWorkflow(page, 'Recurrence');
     await page.getByLabel('Insert a new step after').click();
     await page.getByText('Add an action').click();
-    await page.getByPlaceholder('Search').click();
-    await page.getByPlaceholder('Search').fill('initialize variable');
+    await page.getByPlaceholder('Search for an action or').fill('initialize variable');
     await page.getByLabel('Initialize variables').click();
     await page.getByRole('textbox', { name: 'Enter variable name' }).getByRole('paragraph').click();
     await page.getByRole('textbox', { name: 'Enter variable name' }).fill('test');
