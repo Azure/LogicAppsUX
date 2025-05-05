30d01fe68c80080553ca4f4ed9edc6d0ef24c5cb
danielle <dacogbur@microsoft.com>
2025-05-05 14:25:08 -0700

    removed dupes from create classes

--- apps/vs-code-designer/src/app/commands/createCodeful/CodefulWorkflowCreateStep.ts
+++ apps/vs-code-designer/src/app/commands/createCodeful/CodefulWorkflowCreateStep.ts
@@ -4,24 +4,21 @@
  *--------------------------------------------------------------------------------------------*/
 
 import type { IActionContext } from '@microsoft/vscode-azext-utils';
-import { DialogResponses, nonNullProp, parseError } from '@microsoft/vscode-azext-utils';
+import { nonNullProp } from '@microsoft/vscode-azext-utils';
 import { WorkflowProjectType, MismatchBehavior, WorkerRuntime } from '@microsoft/vscode-extension-logic-apps';
 import type { IFunctionWizardContext, IHostJsonV2 } from '@microsoft/vscode-extension-logic-apps';
 import * as fse from 'fs-extra';
 import * as path from 'path';
-import type { MessageItem } from 'vscode';
 import { WorkflowCreateStepBase } from '../createCodeless/createCodelessSteps/WorkflowCreateStepBase';
 import { getCodefulWorkflowTemplate } from '../../utils/codeless/templates';
 //import { addFolderToBuildPath, addNugetPackagesToBuildFile, getDotnetBuildFile, suppressJavaScriptBuildWarnings, updateFunctionsSDKVersion, writeBuildFileToDisk } from '../../utils/codeless/updateBuildFile';
 import { writeFormattedJson } from '../../utils/fs';
-import { localize } from 'vscode-nls';
 import {
   hostFileName,
   extensionBundleId,
   defaultVersionRange,
   azureWebJobsStorageKey,
   localEmulatorConnectionString,
-  localSettingsFileName,
   workerRuntimeKey,
   functionsInprocNet8Enabled,
   codefulWorkflowFileName,
@@ -30,7 +27,6 @@ import {
 } from '../../../constants';
 import { setLocalAppSetting } from '../../utils/appSettings/localSettings';
 import { validateDotnetInstalled } from '../../utils/dotnet/executeDotnetTemplateCommand';
-import { parseJson } from '../../utils/parseJson';
 import { switchToDotnetProject } from '../workflows/switchToDotnetProject';
 import * as vscode from 'vscode';
 import { createConnectionsJson } from '../../utils/codeless/connection';
@@ -102,48 +98,4 @@ export class CodefulWorkflowCreateStep extends WorkflowCreateStepBase<IFunctionW
       MismatchBehavior.Overwrite
     );
   }
-
-  private async getHostJson(context: IFunctionWizardContext, hostJsonPath: string, allowOverwrite = false): Promise<IHostJsonV2> {
-    return this.getJsonFromFile(context, hostJsonPath, { version: '2.0' }, allowOverwrite);
-  }
-
-  private async getJsonFromFile<T extends object>(
-    context: IFunctionWizardContext,
-    filePath: string,
-    defaultValue: T,
-    allowOverwrite = false
-  ): Promise<T> {
-    if (await fse.pathExists(filePath)) {
-      const data: string = (await fse.readFile(filePath)).toString();
-      if (emptyStringTest.test(data)) {
-        try {
-          return parseJson(data);
-        } catch (error) {
-          if (allowOverwrite) {
-            const message: string = localize(
-              'failedToParseWithOverwrite',
-              'Failed to parse "{0}": {1}. Overwrite?',
-              localSettingsFileName,
-              parseError(error).message
-            );
-            const overwriteButton: MessageItem = { title: localize('overwrite', 'Overwrite') };
-            // Overwrite is the only button and cancel automatically throws, so no need to check result
-            await context.ui.showWarningMessage(message, { modal: true }, overwriteButton, DialogResponses.cancel);
-          } else {
-            const message: string = localize(
-              'failedToParse',
-              'Failed to parse "{0}": {1}.',
-              localSettingsFileName,
-              parseError(error).message
-            );
-            throw new Error(message);
-          }
-        }
-      }
-    }
-
-    return defaultValue;
-  }
 }
-
-const emptyStringTest = /[^\s]/;
