8e90f910290699200c4c2dc176db2539a2ffad59
Travis Vu <hartra344@users.noreply.github.com>
2025-06-10 08:05:05 -0500

    fix(designer): use semantic h2 heading for Add a trigger panel header (#7494)

--- libs/designer-ui/src/lib/text/index.tsx
+++ libs/designer-ui/src/lib/text/index.tsx
@@ -5,6 +5,7 @@ export interface TextProps {
   text: string;
   style?: CSSProperties;
   className?: string;
+  as?: 'b' | 'em' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'i' | 'p' | 'pre' | 'span' | 'strong' | undefined;
 }
 
 interface BuiltInTextProps extends TextProps {
@@ -12,26 +13,26 @@ interface BuiltInTextProps extends TextProps {
   weight: 'regular' | 'medium' | 'semibold' | 'bold';
 }
 
-const BuiltInText = ({ text, size, weight, style, className }: BuiltInTextProps) => {
+const BuiltInText = ({ text, size, weight, style, className, as }: BuiltInTextProps) => {
   return (
-    <Text size={size} weight={weight} style={style} className={className}>
+    <Text as={as} size={size} weight={weight} style={style} className={className}>
       {text}
     </Text>
   );
 };
 
-export const XXLargeText = ({ text, style, className }: TextProps) => {
-  return <BuiltInText text={text} size={700} weight={'semibold'} style={style} className={className} />;
+export const XXLargeText = ({ text, style, className, as }: TextProps) => {
+  return <BuiltInText as={as} text={text} size={700} weight={'semibold'} style={style} className={className} />;
 };
-export const XLargeText = ({ text, style, className }: TextProps) => {
-  return <BuiltInText text={text} size={500} weight={'medium'} style={style} className={className} />;
+export const XLargeText = ({ text, style, className, as }: TextProps) => {
+  return <BuiltInText as={as} text={text} size={500} weight={'medium'} style={style} className={className} />;
 };
-export const LargeText = ({ text, style, className }: TextProps) => {
-  return <BuiltInText text={text} size={400} weight={'regular'} style={style} className={className} />;
+export const LargeText = ({ text, style, className, as }: TextProps) => {
+  return <BuiltInText as={as} text={text} size={400} weight={'regular'} style={style} className={className} />;
 };
-export const MediumText = ({ text, style, className }: TextProps) => {
-  return <BuiltInText text={text} size={300} weight={'regular'} style={style} className={className} />;
+export const MediumText = ({ text, style, className, as }: TextProps) => {
+  return <BuiltInText as={as} text={text} size={300} weight={'regular'} style={style} className={className} />;
 };
-export const SmallText = ({ text, style, className }: TextProps) => {
-  return <BuiltInText text={text} size={200} weight={'regular'} style={style} className={className} />;
+export const SmallText = ({ text, style, className, as }: TextProps) => {
+  return <BuiltInText as={as} text={text} size={200} weight={'regular'} style={style} className={className} />;
 };
