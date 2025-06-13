19255d1c13d15608e73b513b1ccd22a2b7ad3af9
Krrish Mittal <krrishmittal15@gmail.com>
2025-06-12 19:00:23 -0700

    add create new entry point and popover

--- apps/Standalone/src/designer/app/LocalDesigner/customEditorService.tsx
+++ apps/Standalone/src/designer/app/LocalDesigner/customEditorService.tsx
@@ -1,6 +1,7 @@
 import type { IEditorParameterInfo, IEditorService } from '@microsoft/logic-apps-shared';
 import { CustomEditorIncrementVariable } from './customEditor/CustomEditorIncrementVariable';
 import { CustomEditorInitializeVariable } from './customEditor/CustomEditorInitializeVariable';
+import { CustomDeploymentModelResource } from '@microsoft/logic-apps-designer';
 
 export class CustomEditorService implements IEditorService {
   public areCustomEditorsEnabled = false;
@@ -29,6 +30,25 @@ export class CustomEditorService implements IEditorService {
         hideLabel: true,
         editor,
         editorOptions,
+      };
+    }
+
+    return undefined;
+  };
+
+  public getNewResourceEditor = (props: IEditorParameterInfo) => {
+    const { operationInfo, parameter } = props;
+    const { connectorId } = operationInfo ?? {};
+    const { parameterName } = parameter ?? {};
+
+    // if (!this.areCustomEditorsEnabled) {
+    //   return undefined;
+    // }
+
+    if (connectorId === 'connectionProviders/agent' && parameterName === 'deploymentId') {
+      return {
+        EditorComponent: CustomDeploymentModelResource,
+        hideLabel: false,
       };
     }
 
