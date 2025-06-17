ceef3363a3b4ace9cceffec06255f90ce92e7b7d
Carlos Emiliano Castro Trejo <ccastrotrejo@microsoft.com>
2025-06-17 10:58:20 -0400

    Fix rules directory

--- apps/vs-code-designer/src/app/commands/deploy/hybridLogicApp/connectToSMB.ts
+++ apps/vs-code-designer/src/app/commands/deploy/hybridLogicApp/connectToSMB.ts
@@ -17,6 +17,7 @@ import {
   mapsDirectory,
   parametersFileName,
   Platform,
+  rulesDirectory,
   schemasDirectory,
   workflowFileName,
 } from '../../../../constants';
@@ -101,7 +102,7 @@ const uploadArtifactsFiles = async (projectPath: string | undefined, smbFolderPa
   }
 
   if (artifactsFiles.rules.length > 0) {
-    await uploadFiles(artifactsFiles.rules, path.join(smbFolderPath, artifactsDirectory, mapsDirectory));
+    await uploadFiles(artifactsFiles.rules, path.join(smbFolderPath, artifactsDirectory, rulesDirectory));
   }
 };
 
