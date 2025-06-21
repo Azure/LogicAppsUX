bfd57672cea8bfd8dd93abefb1449cab0ffa0d67
Elaina Lee <hyehwalee@microsoft.com>
2025-06-20 18:03:03 -0700

    took templateQuery out of if statement

--- libs/designer/src/lib/core/actions/bjsworkflow/configuretemplate.ts
+++ libs/designer/src/lib/core/actions/bjsworkflow/configuretemplate.ts
@@ -438,9 +438,8 @@ const saveWorkflowsInTemplateInternal = async (
 
     if (updateTemplateManifest || newState) {
       await service.updateTemplate(templateId, templateManifest, newState);
-      resetTemplateQuery(templateId);
     }
-
+    resetTemplateQuery(templateId);
     resetTemplateWorkflowsQuery(templateId, /* clearRawData */ true);
     dispatch(setApiValidationErrors({ error: undefined, source: 'workflows' }));
   } catch (error: any) {
