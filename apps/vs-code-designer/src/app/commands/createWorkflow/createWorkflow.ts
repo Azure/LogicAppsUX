315bd139cbef931d4dec50acd7818185bf51588b
danielle <dacogbur@microsoft.com>
2025-06-25 16:11:32 -0700

    removed codeful for release

--- apps/vs-code-designer/src/app/commands/createWorkflow/createWorkflow.ts
+++ apps/vs-code-designer/src/app/commands/createWorkflow/createWorkflow.ts
@@ -70,7 +70,7 @@ export async function createWorkflow(
     projectTemplateKey,
   });
 
-  // wizardContext.isCodeless = true; // default to codeless workflow, disabling codeful option until Public Preview
+  wizardContext.isCodeless = true; // default to codeless workflow, disabling codeful option until Public Preview
 
   const wizard: AzureWizard<IFunctionWizardContext> = new AzureWizard(wizardContext, {
     promptSteps: [
