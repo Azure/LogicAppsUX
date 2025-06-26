2016911879dc38e08cba7e48577a5a3c10f80a40
Priti Sambandam <psamband@microsoft.com>
2025-06-26 15:05:56 -0700

    refactor(designer): Refactoring how subscriptions are filtered in loading test apps in test page

--- apps/Standalone/src/designer/app/AzureLogicAppsDesigner/Queries/FetchConsumptionApps.ts
+++ apps/Standalone/src/designer/app/AzureLogicAppsDesigner/Queries/FetchConsumptionApps.ts
@@ -3,11 +3,10 @@ import { environment } from '../../../../environments/environment';
 import type { Data as FetchLogicAppsData } from '../Models/LogicAppAppTypes';
 import { fetchAppsByQuery } from '../Utilities/resourceUtilities';
 
-const buildQuery = (subscriptionId?: string) => {
+const buildQuery = () => {
   const baseQuery = `
     resources
-    | where ${subscriptionId ? `subscriptionId == '${subscriptionId}' and` : ''}
-      (type =~ 'microsoft.logic/workflows' or (type =~ 'microsoft.web/sites' and kind contains 'workflowapp'))
+    | where (type =~ 'microsoft.logic/workflows' or (type =~ 'microsoft.web/sites' and kind contains 'workflowapp'))
     | extend plan = case(kind contains 'workflowapp', 'Standard', 'Consumption')
     | where plan =~ 'consumption'
   `;
@@ -16,19 +15,15 @@ const buildQuery = (subscriptionId?: string) => {
 
 export const useFetchConsumptionApps = (subscriptionIds?: string[]) => {
   return useQuery<FetchLogicAppsData[]>(
-    ['listAllLogicApps', 'consumption', subscriptionIds ?? 'all'],
+    ['listAllLogicApps', 'consumption', (subscriptionIds ?? ['all']).join(',')],
     async () => {
       if (!environment.armToken) {
         return [];
       }
 
-      const queries = subscriptionIds?.length
-        ? subscriptionIds.map((id) => fetchAppsByQuery(buildQuery(id)))
-        : [fetchAppsByQuery(buildQuery())];
+      const results = await fetchAppsByQuery(buildQuery(), subscriptionIds);
 
-      const results = await Promise.all(queries);
-
-      return results.flat().map((item: any) => ({
+      return results.map((item: any) => ({
         id: item[0],
         name: item[1],
         location: item[5],
