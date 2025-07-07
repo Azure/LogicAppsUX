f6b7f0abe5c9a24b5b49fa73c36e1e18edfdc7a5
Carlos Emiliano Castro Trejo <102700317+ccastrotrejo@users.noreply.github.com>
2025-07-07 08:46:45 -0700

    feat(vscode): Add .NET 8 in-process settings to Logic Apps creation (#7723)
    * Update settings
    
    * Add settings to logic apps creation

--- apps/vs-code-designer/src/app/commands/dataMapper/FxWorkflowRuntime.ts
+++ apps/vs-code-designer/src/app/commands/dataMapper/FxWorkflowRuntime.ts
@@ -1,6 +1,6 @@
 import { WorkerRuntime } from '@microsoft/vscode-extension-logic-apps';
 import {
-  ProjectDirectoryPath,
+  ProjectDirectoryPathKey,
   appKindSetting,
   designTimeDirectoryName,
   designerStartApi,
@@ -64,7 +64,7 @@ export async function startBackendRuntime(projectPath: string, context: IActionC
           designTimeDirectory.fsPath,
           {
             [appKindSetting]: logicAppKind,
-            [ProjectDirectoryPath]: projectPath,
+            [ProjectDirectoryPathKey]: projectPath,
             [workerRuntimeKey]: WorkerRuntime.Node,
           },
           true
