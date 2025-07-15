0418381110886c5b54ac58419108e82037706d06
Carlos Emiliano Castro Trejo <ccastrotrejo@microsoft.com>
2025-07-15 10:19:40 -0400

    Update selected list

--- apps/vs-code-react/src/app/export/workflowsSelection/selectedList.tsx
+++ apps/vs-code-react/src/app/export/workflowsSelection/selectedList.tsx
@@ -1,17 +1,19 @@
 import type { WorkflowsList } from '../../../run-service';
 import type { RootState } from '../../../state/store';
-import { IconButton, Shimmer } from '@fluentui/react';
-import type { IIconProps } from '@fluentui/react';
 import { LargeText, MediumText, XLargeText } from '@microsoft/designer-ui';
 import { useMemo } from 'react';
 import { useIntl } from 'react-intl';
 import { useSelector } from 'react-redux';
 import { useExportStyles } from '../exportStyles';
+import { Button, Skeleton, SkeletonItem } from '@fluentui/react-components';
+import { bundleIcon, DismissFilled, DismissRegular } from '@fluentui/react-icons';
 
 export interface ISelectedListProps {
   isLoading: boolean;
   deselectWorkflow: (workflowKey: string) => void;
 }
+
+const DismissIcon = bundleIcon(DismissFilled, DismissRegular);
 
 export const SelectedList: React.FC<ISelectedListProps> = ({ isLoading, deselectWorkflow }) => {
   const intl = useIntl();
@@ -26,20 +28,40 @@ export const SelectedList: React.FC<ISelectedListProps> = ({ isLoading, deselect
       id: 'fDpDnc',
       description: 'Selected logic apps title',
     }),
+    SHIMMER_LABEL: intl.formatMessage({
+      defaultMessage: 'Loading selected logic apps',
+      id: '0KDnLw',
+      description: 'Shimmer label for loading selected logic apps',
+    }),
+    CANCEL_LABEL: intl.formatMessage({
+      defaultMessage: 'Cancel',
+      id: '0GT0SI',
+      description: 'Cancel button label',
+    }),
   };
 
   const shimmerList = useMemo(() => {
-    return new Array(5).fill(0).map((_element, index) => {
-      return <Shimmer className={styles.exportWorkflowsPanelSelectedListItemShimmer} key={index} />;
-    });
-  }, [styles.exportWorkflowsPanelSelectedListItemShimmer]);
+    return (
+      <Skeleton key="shimmer" aria-label={intlText.SHIMMER_LABEL}>
+        {new Array(5).fill(0).map((_element, index) => {
+          return <SkeletonItem className={styles.exportWorkflowsPanelSelectedListItemShimmer} key={index} />;
+        })}
+      </Skeleton>
+    );
+  }, [intlText.SHIMMER_LABEL, styles.exportWorkflowsPanelSelectedListItemShimmer]);
 
   const renderItems = useMemo(() => {
     const getList = (list: WorkflowsList[]) => {
       return list.map((workflow: WorkflowsList) => {
         const { name, resourceGroup } = workflow;
-        const deselectIcon: IIconProps = { iconName: 'Cancel' };
-        const deselectButton = <IconButton iconProps={deselectIcon} aria-label="cancel" onClick={() => deselectWorkflow(workflow.key)} />;
+        const deselectButton = (
+          <Button
+            appearance="transparent"
+            aria-label={intlText.CANCEL_LABEL}
+            onClick={() => deselectWorkflow(workflow.key)}
+            icon={<DismissIcon />}
+          />
+        );
 
         return (
           <div key={workflow.key} className={styles.exportWorkflowsPanelSelectedListItem}>
@@ -62,6 +84,7 @@ export const SelectedList: React.FC<ISelectedListProps> = ({ isLoading, deselect
     isLoading,
     shimmerList,
     selectedWorkflows,
+    intlText.CANCEL_LABEL,
     styles.exportWorkflowsPanelSelectedListItem,
     styles.exportWorkflowsPanelSelectedListItemText,
     styles.exportWorkflowsPanelSelectedListItemSubtext,
