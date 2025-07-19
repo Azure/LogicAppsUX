05f2a1b5802af2052c83243ae689b4d13c5a0aea
Carlos Emiliano Castro Trejo <ccastrotrejo@microsoft.com>
2025-07-19 17:08:05 -0400

    Add validation

--- apps/vs-code-react/src/app/export/summary/newResourceGroup.tsx
+++ apps/vs-code-react/src/app/export/summary/newResourceGroup.tsx
@@ -3,13 +3,13 @@ import { isNameValid } from './helper';
 import type { IDropdownOption } from '@fluentui/react';
 import { useBoolean } from '@fluentui/react-hooks';
 import { MediumText } from '@microsoft/designer-ui';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import type { ChangeEvent } from 'react';
 import { useIntl } from 'react-intl';
 import { useSelector } from 'react-redux';
 import { useExportStyles } from '../exportStyles';
 import type { InputOnChangeData } from '@fluentui/react-components';
-import { Button, Input, Label, Link, Popover, PopoverSurface, PopoverTrigger, useId } from '@fluentui/react-components';
+import { Button, Field, Input, Label, Link, Popover, PopoverSurface, PopoverTrigger, useId } from '@fluentui/react-components';
 
 interface INewResourceGroupProps {
   onAddNewResourceGroup: (selectedOption: IDropdownOption) => void;
@@ -26,58 +26,65 @@ export const NewResourceGroup: React.FC<INewResourceGroupProps> = ({ onAddNewRes
   const { exportData } = workflowState;
   const { location } = exportData;
 
-  const intlText = {
-    CREATE_NEW: intl.formatMessage({
-      defaultMessage: 'Create new',
-      id: 'XKQ/Lw',
-      description: 'Create new text',
+  const intlText = useMemo(
+    () => ({
+      CREATE_NEW: intl.formatMessage({
+        defaultMessage: 'Create new',
+        id: 'XKQ/Lw',
+        description: 'Create new text',
+      }),
+      RESOURCE_GROUP_DESCRIPTION: intl.formatMessage({
+        defaultMessage: 'A resource group is a container that holds related resources for an Azure solution.',
+        id: 'fU98Nt',
+        description: 'Deploy managed connections warning text',
+      }),
+      OK: intl.formatMessage({
+        defaultMessage: 'OK',
+        id: '70cHmm',
+        description: 'OK button',
+      }),
+      CANCEL: intl.formatMessage({
+        defaultMessage: 'Cancel',
+        id: 'qz9XeG',
+        description: 'Cancel button',
+      }),
+      NAME: intl.formatMessage({
+        defaultMessage: 'Name',
+        id: '1kGBdP',
+        description: 'Name button',
+      }),
+      INVALID_CHARS: intl.formatMessage({
+        defaultMessage: 'The name can contain only alphanumeric characters or the following symbols: . _ - ( )',
+        id: '9vOq5B',
+        description: 'RResource group name - invalid characters error',
+      }),
+      INVALID_ENDING_CHAR: intl.formatMessage({
+        defaultMessage: `The name can't end with a period.`,
+        id: 'Pe0eMX',
+        description: 'Resource group name ending error',
+      }),
+      INVALID_EXISTING_NAME: intl.formatMessage({
+        defaultMessage: 'A resource group with the same name already exists in the selected subscription.',
+        id: 'KtGlzI',
+        description: 'Resource group existing name error',
+      }),
+      NEW: intl.formatMessage({
+        defaultMessage: 'New',
+        id: 'm3H+gL',
+        description: 'New text',
+      }),
     }),
-    RESOURCE_GROUP_DESCRIPTION: intl.formatMessage({
-      defaultMessage: 'A resource group is a container that holds related resources for an Azure solution.',
-      id: 'fU98Nt',
-      description: 'Deploy managed connections warning text',
-    }),
-    OK: intl.formatMessage({
-      defaultMessage: 'OK',
-      id: '70cHmm',
-      description: 'OK button',
-    }),
-    CANCEL: intl.formatMessage({
-      defaultMessage: 'Cancel',
-      id: 'qz9XeG',
-      description: 'Cancel button',
-    }),
-    NAME: intl.formatMessage({
-      defaultMessage: 'Name',
-      id: '1kGBdP',
-      description: 'Name button',
-    }),
-    INVALID_CHARS: intl.formatMessage({
-      defaultMessage: 'The name can contain only alphanumeric characters or the following symbols: . _ - ( )',
-      id: '9vOq5B',
-      description: 'RResource group name - invalid characters error',
-    }),
-    INVALID_ENDING_CHAR: intl.formatMessage({
-      defaultMessage: `The name can't end with a period.`,
-      id: 'Pe0eMX',
-      description: 'Resource group name ending error',
-    }),
-    INVALID_EXISTING_NAME: intl.formatMessage({
-      defaultMessage: 'A resource group with the same name already exists in the selected subscription.',
-      id: 'KtGlzI',
-      description: 'Resource group existing name error',
-    }),
-    NEW: intl.formatMessage({
-      defaultMessage: 'New',
-      id: 'm3H+gL',
-      description: 'New text',
-    }),
-  };
+    [intl]
+  );
 
   const onChangeName = (_ev: ChangeEvent<HTMLInputElement>, data: InputOnChangeData) => {
-    const newName = typeof data.value === 'string' ? data.value.trim() : '';
+    const newName = typeof data.value === 'string' ? data.value : '';
     setName(newName);
   };
+
+  const validation = useMemo(() => {
+    return isNameValid(name, intlText, resourceGroups);
+  }, [name, intlText, resourceGroups]);
 
   const onClickOk = () => {
     toggleIsCalloutVisible();
@@ -93,16 +100,18 @@ export const NewResourceGroup: React.FC<INewResourceGroupProps> = ({ onAddNewRes
       <PopoverSurface tabIndex={-1}>
         <MediumText text={intlText.RESOURCE_GROUP_DESCRIPTION} style={{ display: 'block' }} />
         <div className={styles.exportSummaryNewResourceGroup}>
-          <Label htmlFor={resourceGroupInputId} size={'small'}>
-            {intlText.NAME}
-          </Label>
-          <Input autoFocus={true} value={name} id={resourceGroupInputId} onChange={onChangeName} />
+          <Field validationState={validation.validationError ? 'error' : 'none'} validationMessage={validation.validationError} required>
+            <Label htmlFor={resourceGroupInputId} size={'small'}>
+              {intlText.NAME}
+            </Label>
+            <Input autoFocus={true} value={name} id={resourceGroupInputId} onChange={onChangeName} />
+          </Field>
         </div>
         <Button
           appearance="primary"
           className={styles.exportSummaryConnectionsButton}
           aria-label={intlText.OK}
-          disabled={!isNameValid(name, intlText, resourceGroups).validName}
+          disabled={!validation.validName}
           onClick={onClickOk}
         >
           {intlText.OK}
