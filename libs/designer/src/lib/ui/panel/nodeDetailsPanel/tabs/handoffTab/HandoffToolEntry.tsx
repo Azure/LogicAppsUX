2667fb8d32e03590a11894cd2d7513344f58bbf0
Riley Evans <rllyy97@gmail.com>
2025-08-12 11:22:06 -0500

    fix(Designer): Fixed handoff description dirty state issue (#8048)
    Fixed handoff description dirty state issue

--- libs/designer/src/lib/ui/panel/nodeDetailsPanel/tabs/handoffTab/HandoffToolEntry.tsx
+++ libs/designer/src/lib/ui/panel/nodeDetailsPanel/tabs/handoffTab/HandoffToolEntry.tsx
@@ -86,6 +86,7 @@ export const HandoffToolEntry = ({ agentId, toolId }: HandoffToolEntryProps) => 
               },
             },
           ],
+          isUserAction: true,
         })
       );
     },
