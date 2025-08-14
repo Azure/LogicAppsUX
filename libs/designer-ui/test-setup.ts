66acf1844897389b7d8aabcd42eb8302277c0ee2
Eric-B-Wu <wue@microsoft.com>
2025-08-13 18:26:22 -0700

    fix test

--- libs/designer-ui/test-setup.ts
+++ libs/designer-ui/test-setup.ts
@@ -4,6 +4,16 @@ import { afterEach, beforeEach } from 'vitest';
 import { mockUseIntl } from './src/lib/__test__/intl-test-helper';
 import { initializeIcons } from '@fluentui/react';
 
+// Suppress console errors from tabster/NodeFilter that don't affect test results
+const originalConsoleError = console.error;
+console.error = (...args) => {
+  const message = args.join(' ');
+  if (message.includes('NodeFilter is not defined') || message.includes('Node is not defined')) {
+    return; // Suppress these specific errors
+  }
+  originalConsoleError.apply(console, args);
+};
+
 initializeIcons();
 // https://testing-library.com/docs/react-testing-library/api#cleanup
 afterEach(() => cleanup());
