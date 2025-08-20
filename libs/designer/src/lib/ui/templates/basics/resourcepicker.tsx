44eb97bddaaea64674cf5f849a3ed0f5f57b39cc
Elaina Lee <hyehwalee@microsoft.com>
2025-08-20 16:50:24 -0700

    migrated to TemplateResourcePicker using ResourcePicker

--- libs/designer/src/lib/ui/templates/basics/resourcepicker.tsx
+++ libs/designer/src/lib/ui/templates/basics/resourcepicker.tsx
@@ -1,9 +1,5 @@
 import type { AppDispatch, RootState } from '../../../core/state/templates/store';
 import { useDispatch, useSelector } from 'react-redux';
-import { useIntl } from 'react-intl';
-import { Option, Field, Dropdown } from '@fluentui/react-components';
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import { useLocations, useLogicApps, useResourceGroups, useSubscriptions } from '../../../core/queries/resource';
 import {
   setLocation,
   setLogicAppDetails,
@@ -11,234 +7,30 @@ import {
   setSubscription,
   setWorkflowAppDetails,
 } from '../../../core/state/templates/workflowSlice';
-import { type LogicAppResource, type Resource, type Template, equals } from '@microsoft/logic-apps-shared';
-import { useTemplatesStrings } from '../templatesStrings';
-import { useAllLogicApps } from '../../../core/configuretemplate/utils/queries';
-// import { ResourcePicker } from '../../common/resourcepicker/resourcepicker';
+import { type BaseResourcePickerProps, ResourcePicker } from '../../common/resourcepicker/resourcepicker';
 
-export interface ResourcePickerProps {
-  viewMode?: 'default' | 'alllogicapps';
-  onSelectApp?: (value: LogicAppResource) => void;
-  lockField?: Template.ResourceFieldId;
-}
-
-export const TemplateResourcePicker = ({ viewMode = 'default', onSelectApp, lockField }: ResourcePickerProps) => {
+export const TemplateResourcePicker = (props: BaseResourcePickerProps) => {
   const dispatch = useDispatch<AppDispatch>();
-  const isDefaultMode = viewMode === 'default';
   const { subscriptionId, resourceGroup, location, workflowAppName, logicAppName, isConsumption } = useSelector(
     (state: RootState) => state.workflow
   );
-  const { data: subscriptions, isLoading } = useSubscriptions();
-  const { data: resourceGroups, isLoading: isResourceGroupLoading } = useResourceGroups(subscriptionId ?? '');
-  const { data: locations, isLoading: islocationLoading } = useLocations(subscriptionId ?? '');
-  const { data: logicApps, isLoading: isLogicAppsLoading } = useLogicApps(
-    subscriptionId ?? '',
-    resourceGroup ?? '',
-    isDefaultMode && !isConsumption
-  );
-  const { data: allLogicApps, isLoading: isAllLogicAppsLoading } = useAllLogicApps(
-    subscriptionId ?? '',
-    resourceGroup ?? '',
-    !isDefaultMode
-  );
-
-  const intl = useIntl();
-  const intlText = useMemo(
-    () => ({
-      SECTION_DESCRIPTION: intl.formatMessage({
-        defaultMessage: 'Select the resource location for your workflow',
-        id: 'e1+Gqi',
-        description: 'Description for resource location section.',
-      }),
-      VALIDATION_ERROR: intl.formatMessage({
-        defaultMessage: 'Please select a valid resource',
-        id: 'nJfJNU',
-        description: 'Validation error message when a resource is not selected',
-      }),
-      ALL_LOGIC_APPS: intl.formatMessage({
-        defaultMessage: 'Logic app instance',
-        id: 'IpD27y',
-        description: 'Label field for logic app instance',
-      }),
-    }),
-    [intl]
-  );
-
-  const { resourceStrings } = useTemplatesStrings();
-  const onLogicAppSelect = useCallback(
-    (value: string) => {
-      const app = logicApps?.find((app) => equals(app.name, value));
-      dispatch(setWorkflowAppDetails({ name: value, location: app?.location ?? '' }));
-    },
-    [dispatch, logicApps]
-  );
-
-  const onLogicAppInstanceSelect = useCallback(
-    (value: string) => {
-      const app = allLogicApps?.find((app) => equals(app.name, value));
-      dispatch(setLogicAppDetails({ name: value, location: app?.location ?? '', plan: app?.plan ?? '' }));
-
-      if (app) {
-        onSelectApp?.(app);
-      }
-    },
-    [dispatch, allLogicApps, onSelectApp]
-  );
 
   return (
-    <div>
-      {/* <ResourcePicker
-        {...props}
-      /> */}
-      <ResourceField
-        id="subscriptionId"
-        label={resourceStrings.SUBSCRIPTION}
-        onSelect={(value) => dispatch(setSubscription(value))}
-        defaultKey={subscriptionId}
-        isLoading={isLoading}
-        resources={subscriptions ?? []}
-        errorMessage={subscriptionId ? '' : intlText.VALIDATION_ERROR}
-        lockField={lockField === 'subscription' || lockField === 'resourcegroup' || lockField === 'resource'}
-      />
-      <ResourceField
-        id="resourceGroupName"
-        label={resourceStrings.RESOURCE_GROUP}
-        onSelect={(value) => dispatch(setResourceGroup(value))}
-        defaultKey={resourceGroup}
-        isLoading={isResourceGroupLoading}
-        resources={resourceGroups ?? []}
-        errorMessage={resourceGroup ? '' : intlText.VALIDATION_ERROR}
-        lockField={lockField === 'resourcegroup' || lockField === 'resource'}
-      />
-      {isDefaultMode && isConsumption ? (
-        <ResourceField
-          id="location"
-          label={resourceStrings.LOCATION}
-          onSelect={(value) => dispatch(setLocation(value))}
-          defaultKey={location}
-          isLoading={islocationLoading}
-          resources={locations ?? []}
-          errorMessage={location ? '' : intlText.VALIDATION_ERROR}
-          lockField={lockField === 'location' || lockField === 'resource'}
-        />
-      ) : null}
-      {isDefaultMode && !isConsumption ? (
-        <ResourceField
-          id="logicapp"
-          label={resourceStrings.LOGIC_APP}
-          onSelect={onLogicAppSelect}
-          defaultKey={workflowAppName ?? ''}
-          isLoading={isLogicAppsLoading}
-          resources={(logicApps ?? []).map((app) => ({
-            id: app.id,
-            name: app.name,
-            displayName: app.name,
-          }))}
-          errorMessage={workflowAppName ? '' : intlText.VALIDATION_ERROR}
-          lockField={lockField === 'resource'}
-        />
-      ) : null}
-      {isDefaultMode ? null : (
-        <ResourceField
-          id="alllogicapp"
-          label={intlText.ALL_LOGIC_APPS}
-          onSelect={onLogicAppInstanceSelect}
-          defaultKey={logicAppName ?? ''}
-          isLoading={isAllLogicAppsLoading}
-          resources={(allLogicApps ?? []).map((app) => ({
-            id: app.id,
-            name: app.name,
-            displayName: equals(app.plan, 'consumption') ? `${app.name} (Consumption)` : `${app.name} (Standard)`,
-          }))}
-          errorMessage={logicAppName ? '' : intlText.VALIDATION_ERROR}
-          lockField={lockField === 'resource'}
-        />
-      )}
-    </div>
-  );
-};
-
-const ResourceField = ({
-  id,
-  label,
-  resources,
-  defaultKey,
-  errorMessage,
-  isLoading,
-  onSelect,
-  lockField,
-}: {
-  id: string;
-  label: string;
-  defaultKey: string;
-  resources: Resource[];
-  onSelect: (value: any) => void;
-  isLoading?: boolean;
-  errorMessage?: string;
-  lockField: boolean;
-}) => {
-  const intl = useIntl();
-  const texts = {
-    LOADING: intl.formatMessage({
-      defaultMessage: 'Loading resources ...',
-      id: 'IMWSjN',
-      description: 'Loading text',
-    }),
-    NO_ITEMS: intl.formatMessage({
-      defaultMessage: 'No resources found',
-      id: 'yytPY3',
-      description: 'No items to select text',
-    }),
-  };
-
-  const sortedResources = useMemo(() => resources.sort((a, b) => a.displayName.localeCompare(b.displayName)), [resources]);
-
-  const [selectedResource, setSelectedResource] = useState<string | undefined>('');
-  useEffect(() => {
-    if (!isLoading) {
-      const resource = resources.find((resource) => equals(resource.name, defaultKey))?.displayName;
-      if (!resource && !!defaultKey) {
-        onSelect('');
-      }
-
-      if (resource !== selectedResource) {
-        setSelectedResource(resource);
-      }
-    }
-  }, [resources, defaultKey, onSelect, isLoading, selectedResource]);
-
-  return (
-    <div style={{ marginBottom: '12px' }}>
-      <Field
-        className="msla-templates-tab-label"
-        label={label}
-        required={true}
-        validationMessage={errorMessage}
-        validationState={errorMessage ? 'error' : 'none'}
-      >
-        <Dropdown
-          style={{ width: '100%' }}
-          id={id}
-          onOptionSelect={(e, option) => onSelect(option?.optionValue)}
-          disabled={isLoading || (lockField && !!selectedResource)}
-          value={selectedResource}
-          selectedOptions={[defaultKey]}
-          size="small"
-          placeholder={isLoading ? texts.LOADING : ''}
-        >
-          {!isLoading && !sortedResources.length ? (
-            <Option key={'no-items'} value={'#noitem#'} disabled>
-              {texts.NO_ITEMS}
-            </Option>
-          ) : (
-            sortedResources.map((resource) => (
-              <Option key={resource.id} value={resource.name}>
-                {resource.displayName}
-              </Option>
-            ))
-          )}
-        </Dropdown>
-      </Field>
-    </div>
+    <ResourcePicker
+      {...props}
+      resourceState={{
+        subscriptionId,
+        resourceGroup,
+        location,
+        workflowAppName,
+        logicAppName,
+        isConsumption,
+      }}
+      onSubscriptionSelect={(value) => dispatch(setSubscription(value))}
+      onResourceGroupSelect={(value) => dispatch(setResourceGroup(value))}
+      onLocationSelect={(value) => dispatch(setLocation(value))}
+      onLogicAppSelect={(value) => dispatch(setWorkflowAppDetails(value))}
+      onLogicAppInstanceSelect={(value) => dispatch(setLogicAppDetails(value))}
+    />
   );
 };
