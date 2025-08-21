fec7091cac6f068d2f311fdd5ffb04cc5b723d71
Carlos Emiliano Castro Trejo <ccastrotrejo@microsoft.com>
2025-08-21 10:11:43 -0400

    revert back vitest

--- apps/vs-code-designer/vitest.config.ts
+++ apps/vs-code-designer/vitest.config.ts
@@ -7,12 +7,14 @@ export default defineConfig({
   resolve: {
     alias: {
       vscode: path.resolve(path.join(__dirname, 'node_modules', '@types', 'vscode', 'index.d.ts')),
+      '@microsoft/vscode-azext-azureauth/out/src/getSessionFromVSCode': '/__mocks__/vscode-azext-azureauth.ts',
     },
   },
   test: {
     name: packageJson.name,
     environment: 'node',
-    globals: true,
+    setupFiles: ['test-setup.ts'],
+    coverage: { enabled: true, provider: 'istanbul', include: ['src/**/*'], reporter: ['html', 'cobertura'] },
     restoreMocks: true,
   },
 });
