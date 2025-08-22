51ec4e65110f14e75b6bab0e1f19484d528b2673
Elaina Lee <hyehwalee@microsoft.com>
2025-08-21 17:33:58 -0700

    initialized resource service and verified service being used

--- libs/designer/src/lib/core/clonetostandard/clonedataprovider.tsx
+++ libs/designer/src/lib/core/clonetostandard/clonedataprovider.tsx
@@ -1,11 +1,13 @@
 import type React from 'react';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import type { AppDispatch } from '../state/clonetostandard/store';
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, RootState } from '../state/clonetostandard/store';
 import { type ResourceState, setResourceData } from '../state/clonetostandard/resourceslice';
+import { initializeCloneServices, type CloneServiceOptions } from '../actions/bjsworkflow/clone';
 
 export interface ExportDataProviderProps {
   resourceDetails: ResourceState;
+  services: CloneServiceOptions;
   children?: React.ReactNode;
 }
 
@@ -13,12 +15,25 @@ const DataProviderInner = ({ children }: { children?: React.ReactNode }) => {
   return <>{children}</>;
 };
 
-export const CloneDataProvider = ({ resourceDetails, children }: ExportDataProviderProps) => {
+export const CloneDataProvider = ({ resourceDetails, services, children }: ExportDataProviderProps) => {
   const dispatch = useDispatch<AppDispatch>();
+  const { servicesInitialized } = useSelector((state: RootState) => ({
+    servicesInitialized: state.cloneOptions.servicesInitialized,
+  }));
+
+  useEffect(() => {
+    if (!servicesInitialized) {
+      dispatch(initializeCloneServices(services));
+    }
+  }, [dispatch, servicesInitialized, services]);
 
   useEffect(() => {
     dispatch(setResourceData(resourceDetails));
   }, [dispatch, resourceDetails]);
+
+  if (!servicesInitialized) {
+    return null;
+  }
 
   return <DataProviderInner>{children}</DataProviderInner>;
 };
