b0d952fb35f8b438c5653f0924393e9e12208686
Elaina Lee <hyehwalee@microsoft.com>
2025-08-26 17:18:31 -0700

    finished styles

--- libs/designer/src/lib/ui/clonetostandard/logicapps/styles.ts
+++ libs/designer/src/lib/ui/clonetostandard/logicapps/styles.ts
@@ -4,23 +4,6 @@ const borderStyle = `1px solid ${tokens.colorNeutralBackground6}`;
 const mainGap = '16px';
 
 export const useCloneTabStyles = makeStyles({
-  wizardContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    background: tokens.colorNeutralBackground1,
-    height: '90vh',
-  },
-
-  scrollableContent: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
-    gap: '32px',
-    overflow: 'auto',
-    padding: '16px',
-  },
-
-  /////
   tabContainer: {
     padding: `${mainGap} 0`,
     display: 'flex',
@@ -59,7 +42,6 @@ export const useCloneTabStyles = makeStyles({
   sectionDescription: {
     marginTop: '8px',
   },
-  /////
 
   content: {
     flex: 1,
@@ -68,73 +50,5 @@ export const useCloneTabStyles = makeStyles({
     overflow: 'hidden',
     paddingTop: mainGap,
     maxWidth: '50%',
-  },
-
-  footer: {
-    padding: `${tokens.spacingVerticalM} 0px`,
-    borderTop: `1px solid ${tokens.colorNeutralStroke2}`,
-    flexShrink: 0,
-  },
-});
-
-const connectorButtonGap = '8px';
-
-// Connectors / Operations section styles
-export const useConnectorSectionStyles = makeStyles({
-  emptyState: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    padding: '40px',
-    textAlign: 'center',
-  },
-
-  emptyStateIcon: {
-    fontSize: '64px',
-    color: tokens.colorNeutralForeground3,
-    '& svg': {
-      width: '64px',
-      height: '64px',
-    },
-  },
-
-  tableStyle: {
-    width: '100%',
-    margin: '0 auto',
-  },
-
-  connectorIcon: {
-    width: '24px',
-    height: '24px',
-    objectFit: 'contain',
-    marginRight: connectorButtonGap,
-  },
-
-  descriptionSection: {
-    verticalAlign: 'top',
-    display: '-webkit-box',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    WebkitLineClamp: 2,
-    WebkitBoxOrient: 'vertical',
-  },
-
-  icon: {
-    marginRight: connectorButtonGap,
-  },
-
-  iconText: {
-    verticalAlign: 'top',
-  },
-
-  iconTextCell: {
-    alignItems: 'center',
-    display: 'flex',
-  },
-
-  iconsCell: {
-    textAlign: 'right',
   },
 });
