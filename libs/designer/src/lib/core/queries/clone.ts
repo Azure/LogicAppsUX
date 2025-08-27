195ff6a426aad1f73e62380f88fb1e18927bf2c2
Elaina Lee <hyehwalee@microsoft.com>
2025-08-27 15:58:15 -0700

    enable query only if values are present

--- libs/designer/src/lib/core/queries/clone.ts
+++ libs/designer/src/lib/core/queries/clone.ts
@@ -2,7 +2,13 @@ import { CloneService } from '@microsoft/logic-apps-shared';
 import { useQuery } from '@tanstack/react-query';
 
 export const useExistingWorkflowNamesOfResource = (subscriptionId: string, resourceGroup: string, logicAppName: string) => {
-  return useQuery(['getExistingWorkflowNames', subscriptionId, resourceGroup, logicAppName], async () => {
-    return (await CloneService()?.getExistingWorkflowNames?.({ subscriptionId, resourceGroup, logicAppName })) ?? [];
-  });
+  return useQuery(
+    ['getExistingWorkflowNames', subscriptionId, resourceGroup, logicAppName],
+    async () => {
+      return (await CloneService()?.getExistingWorkflowNames?.({ subscriptionId, resourceGroup, logicAppName })) ?? [];
+    },
+    {
+      enabled: Boolean(subscriptionId && resourceGroup && logicAppName),
+    }
+  );
 };
