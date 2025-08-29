5827c3e650182ae1a8a4eb3735d0fedbec20d943
Eric-B-Wu <wue@microsoft.com>
2025-08-29 10:46:34 -0700

    UI update to placeholder node

--- libs/designer-v2/src/lib/ui/CustomNodes/PlaceholderNode.tsx
+++ libs/designer-v2/src/lib/ui/CustomNodes/PlaceholderNode.tsx
@@ -3,7 +3,7 @@ import type { AppDispatch } from '../../core';
 import { useReadOnly } from '../../core/state/designerOptions/designerOptionsSelectors';
 import { useIsNodeSelectedInOperationPanel } from '../../core/state/panel/panelSelectors';
 import { expandDiscoveryPanel } from '../../core/state/panel/panelSlice';
-import { AddActionCard, ADD_CARD_TYPE, NoActionCard } from '@microsoft/designer-ui';
+import { ADD_CARD_TYPE, NoActionCard, AddActionCardV2 } from '@microsoft/designer-ui';
 import { guid } from '@microsoft/logic-apps-shared';
 import { memo, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
@@ -25,7 +25,11 @@ const PlaceholderNode = ({ id }: NodeProps) => {
   return (
     <div>
       <DefaultHandle type="target" />
-      {isReadOnly ? <NoActionCard /> : <AddActionCard addCardType={ADD_CARD_TYPE.TRIGGER} onClick={openAddNodePanel} selected={selected} />}
+      {isReadOnly ? (
+        <NoActionCard />
+      ) : (
+        <AddActionCardV2 addCardType={ADD_CARD_TYPE.TRIGGER} onClick={openAddNodePanel} selected={selected} />
+      )}
       <DefaultHandle type="source" />
     </div>
   );
