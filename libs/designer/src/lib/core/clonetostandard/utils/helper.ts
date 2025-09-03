bb3a335a9eb0083c90327b28e6341d57d7a2385d
Elaina Lee <hyehwalee@microsoft.com>
2025-09-02 17:08:19 -0700

    added case insensitive - ness

--- libs/designer/src/lib/core/clonetostandard/utils/helper.ts
+++ libs/designer/src/lib/core/clonetostandard/utils/helper.ts
@@ -1,9 +1,11 @@
+import { equals } from '@microsoft/logic-apps-shared';
+
 export const getCloneWorkflowName = (baseName: string, existingWorkflowNames?: string[]): string => {
   let counter = 1;
   const newNameWithClone = `${baseName}_clone`;
   let newUniqueName = newNameWithClone;
 
-  while (existingWorkflowNames?.includes(newUniqueName)) {
+  while (existingWorkflowNames?.some((name) => equals(name, newUniqueName))) {
     newUniqueName = `${newNameWithClone}${counter}`;
     counter++;
   }
