5b4f7b487c935f39c6a377c06bb2dabcdb8f0660
Carlos Emiliano Castro Trejo <ccastrotrejo@microsoft.com>
2025-09-26 14:06:01 -0400

    Add agent isolated worker

--- apps/vs-code-designer/src/app/commands/createProject/createProjectSteps/agentCodefulFilesStep.ts
+++ apps/vs-code-designer/src/app/commands/createProject/createProjectSteps/agentCodefulFilesStep.ts
@@ -2,25 +2,12 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import {
-  dotnetExtensionId,
-  functionsExtensionId,
-  vscodeFolderName,
-  extensionsFileName,
-  launchFileName,
-  settingsFileName,
-  tasksFileName,
-  extensionCommand,
-  assetsFolderName,
-} from '../../../../constants';
+import { assetsFolderName } from '../../../../constants';
 import { AzureWizardPromptStep } from '@microsoft/vscode-azext-utils';
-import type { FuncVersion, IProjectWizardContext } from '@microsoft/vscode-extension-logic-apps';
-import { TargetFramework } from '@microsoft/vscode-extension-logic-apps';
+import type { IProjectWizardContext } from '@microsoft/vscode-extension-logic-apps';
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import { getDebugConfigs, updateDebugConfigs } from '../../../utils/vsCodeConfig/launch';
-import { getContainingWorkspace, isMultiRootWorkspace } from '../../../utils/workspace';
-import { getDebugConfiguration } from '../../../utils/debug';
+import AdmZip from 'adm-zip';
 
 /**
  * This class represents a prompt step that allows the user to set up an Azure Function project.
@@ -55,6 +42,8 @@ export class AgentCodefulFilesStep extends AzureWizardPromptStep<IProjectWizardC
 
     // Create the .csproj file inside the functions folder
     await this.createCsprojFile(context.projectPath, logicAppName);
+
+    await this.createWorkerIsolated(context.projectPath);
   }
 
   /**
@@ -89,142 +78,13 @@ export class AgentCodefulFilesStep extends AzureWizardPromptStep<IProjectWizardC
     await fs.writeFile(csprojFilePath, templateContent);
   }
 
-  /**
-   * Creates the Visual Studio Code configuration files in the .vscode folder of the specified functions app.
-   * @param functionFolderPath The path to the functions folder.
-   * @param targetFramework The target framework of the functions app.
-   * @param funcVersion The version of the functions app.
-   * @param logicAppName The name of the logic app.
-   * @param isNewLogicAppProject Indicates if the logic app project is new.
-   */
-  private async createVscodeConfigFiles(
-    functionFolderPath: string,
-    targetFramework: TargetFramework,
-    funcVersion: FuncVersion,
-    logicAppName: string,
-    isNewLogicAppProject: boolean
-  ): Promise<void> {
-    await fs.ensureDir(functionFolderPath);
-    const vscodePath: string = path.join(functionFolderPath, vscodeFolderName);
-    await fs.ensureDir(vscodePath);
-
-    await this.generateExtensionsJson(vscodePath);
-
-    // Update launch config for existing logic app project (new projects will be created with the correct config)
-    if (!isNewLogicAppProject) {
-      await this.updateLogicAppLaunchJson(vscodePath, targetFramework, funcVersion, logicAppName);
+  private async createWorkerIsolated(targetFolder: string): Promise<void> {
+    const zipFile = path.join(__dirname, assetsFolderName, 'LSPServer', 'AgentIsolatedWorker.zip');
+    try {
+      const zip = new AdmZip(zipFile);
+      await zip.extractAllTo(targetFolder, /* overwrite */ true, /* Permissions */ true);
+    } catch (error) {
+      throw new Error(`Error extracting worker isolated: ${error}`);
     }
-
-    await this.generateSettingsJson(vscodePath, targetFramework);
-
-    await this.generateTasksJson(vscodePath);
-  }
-
-  /**
-   * Generates the extensions.json file in the specified folder.
-   * @param folderPath The path to the folder where the extensions.json file should be generated.
-   */
-  private async generateExtensionsJson(folderPath: string): Promise<void> {
-    const filePath = path.join(folderPath, extensionsFileName);
-    const content = {
-      recommendations: [functionsExtensionId, dotnetExtensionId],
-    };
-    await fs.writeJson(filePath, content, { spaces: 2 });
-  }
-
-  /**
-   * Updates the launch.json file for the logic app corresponding to this functions app.
-   * @param folderPath The functions app folder path.
-   * @param targetFramework The target framework of the functions app.
-   * @param funcVersion The version of the functions app.
-   * @param logicAppName The name of the logic app.
-   */
-  private async updateLogicAppLaunchJson(
-    folderPath: string,
-    targetFramework: TargetFramework,
-    funcVersion: FuncVersion,
-    logicAppName: string
-  ): Promise<void> {
-    const logicAppLaunchJsonPath = path.join(folderPath, '..', '..', logicAppName, vscodeFolderName, launchFileName);
-    const logicAppWorkspaceFolder = getContainingWorkspace(logicAppLaunchJsonPath);
-    const debugConfigs = getDebugConfigs(logicAppWorkspaceFolder);
-    const updatedDebugConfigs = debugConfigs.some((debugConfig) => debugConfig.type === 'logicapp')
-      ? debugConfigs.map((debugConfig) => {
-          // Update the logic app debug configuration to use the correct runtime for custom code
-          if (debugConfig.type === 'logicapp') {
-            return {
-              ...debugConfig,
-              customCodeRuntime: targetFramework === TargetFramework.Net8 ? 'coreclr' : 'clr',
-              isCodeless: true,
-            };
-          }
-          return debugConfig;
-        })
-      : [
-          getDebugConfiguration(funcVersion, logicAppName, targetFramework),
-          ...debugConfigs.filter(
-            (debugConfig) => debugConfig.request !== 'attach' || debugConfig.processId !== `\${command:${extensionCommand.pickProcess}}`
-          ),
-        ];
-
-    if (isMultiRootWorkspace()) {
-      let launchJsonContent: any;
-      if (await fs.pathExists(logicAppLaunchJsonPath)) {
-        launchJsonContent = await fs.readJson(logicAppLaunchJsonPath);
-        launchJsonContent['configurations'] = updatedDebugConfigs;
-      } else {
-        launchJsonContent = {
-          version: '0.2.0',
-          configurations: updatedDebugConfigs,
-        };
-      }
-      await fs.writeJson(logicAppLaunchJsonPath, launchJsonContent, { spaces: 2 });
-    } else {
-      updateDebugConfigs(logicAppWorkspaceFolder, updatedDebugConfigs);
-    }
-  }
-
-  /**
-   * Generates the settings.json file in the specified folder.
-   * @param folderPath The path to the folder where the settings.json file should be generated.
-   * @param targetFramework The target framework of the functions app.
-   */
-  private async generateSettingsJson(folderPath: string, targetFramework: TargetFramework): Promise<void> {
-    const filePath = path.join(folderPath, settingsFileName);
-    const content = {
-      'azureFunctions.deploySubpath': `bin/Release/${targetFramework ?? TargetFramework.NetFx}/publish`,
-      'azureFunctions.projectLanguage': 'C#',
-      'azureFunctions.projectRuntime': '~4',
-      'debug.internalConsoleOptions': 'neverOpen',
-      'azureFunctions.preDeployTask': 'publish (functions)',
-      'azureFunctions.templateFilter': 'Core',
-      'azureFunctions.showTargetFrameworkWarning': false,
-      'azureFunctions.projectSubpath': `bin\\Release\\${targetFramework ?? TargetFramework.NetFx}\\publish`,
-    };
-    await fs.writeJson(filePath, content, { spaces: 2 });
-  }
-
-  /**
-   * Generates the tasks.json file in the specified folder.
-   * @param folderPath The path to the folder where the tasks.json file should be generated.
-   */
-  private async generateTasksJson(folderPath: string): Promise<void> {
-    const filePath = path.join(folderPath, tasksFileName);
-    const content = {
-      version: '2.0.0',
-      tasks: [
-        {
-          label: 'build',
-          command: '${config:azureLogicAppsStandard.dotnetBinaryPath}',
-          type: 'process',
-          args: ['build', '${workspaceFolder}'],
-          group: {
-            kind: 'build',
-            isDefault: true,
-          },
-        },
-      ],
-    };
-    await fs.writeJson(filePath, content, { spaces: 2 });
   }
 }
