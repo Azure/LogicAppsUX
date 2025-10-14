d3227ef9371c7091a3ed0e0bc5aba922e2a54edf
William Groover <wgroover@microsoft.com>
2025-10-14 12:13:33 -0700

    initialize.ts nit

--- libs/designer-v2/src/lib/core/actions/bjsworkflow/initialize.ts
+++ libs/designer-v2/src/lib/core/actions/bjsworkflow/initialize.ts
@@ -606,19 +606,11 @@ export const updateAgentUrlInInputs = async ({ type, kind }: NodeOperation, node
       const parameter = getParameterFromName(nodeInputs, 'agentUrl');
       if (parameter && agentUrlInfo) {
         parameter.value = [createLiteralValueSegment(agentUrlInfo.agentUrl)];
-        if (agentUrlInfo.queryParams) {
-          parameter.editorOptions = {
-            ...parameter.editorOptions,
-            chatUrl: agentUrlInfo.chatUrl,
-            queryParams: agentUrlInfo.queryParams,
-          };
-        } else {
-          // Even without queryParams, ensure editorOptions preserves showAgentViewer from manifest
-          parameter.editorOptions = {
-            ...parameter.editorOptions,
-            chatUrl: agentUrlInfo.chatUrl,
-          };
-        }
+        parameter.editorOptions = {
+          ...parameter.editorOptions,
+          chatUrl: agentUrlInfo.chatUrl,
+          ...(agentUrlInfo.queryParams ? { queryParams: agentUrlInfo.queryParams } : {}),
+        };
         return parameter;
       }
     } catch (error) {
