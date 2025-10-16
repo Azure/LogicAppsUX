03bf474f062ec41c2157536a3dd73f5da5822376
Brian Lam <lambrian@microsoft.com>
2025-10-16 13:34:43 -0700

    Updated function comments

--- apps/vs-code-designer/src/app/utils/verifyIsProject.ts
+++ apps/vs-code-designer/src/app/utils/verifyIsProject.ts
@@ -211,9 +211,7 @@ export async function tryGetLogicAppProjectRoot(
 
 /**
  * Checks root folder and subFolders one level down
- * If a single logic app project is found, return that path.
- * If multiple projects are found, prompt to pick the project.
- * TODO - this is checking every root folder and subfolders of non-logic app projects in the workspace, can we optimize this?
+ * Gets the folder of the first logic app found.
  */
 export async function getFirstLogicAppProjectRoot(workspaceFolder: WorkspaceFolder | string | undefined): Promise<string | undefined> {
   if (isNullOrUndefined(workspaceFolder)) {
