d1dc53c82094ddcb1ff4b089d68adede85d40412
Andrew Eldridge <aeldridge@microsoft.com>
2025-10-17 14:19:00 -0400

    feat(vscode): launch debugger on open overview (#8393)
    * launch debugger on open overview if launch config exists
    
    * add check for undefined workspace folder
    
    * additional safeguards
    
    * update isRuntimeUp check, ext.workflowRuntimePort can be set without runtime up

--- apps/vs-code-designer/src/app/utils/vsCodeConfig/launch.ts
+++ apps/vs-code-designer/src/app/utils/vsCodeConfig/launch.ts
@@ -4,10 +4,57 @@
  *--------------------------------------------------------------------------------------------*/
 import type { DebugConfiguration, WorkspaceConfiguration, WorkspaceFolder } from 'vscode';
 import { workspace } from 'vscode';
+import { localize } from '../../../localize';
+import { UserCancelledError, type IActionContext } from '@microsoft/vscode-azext-utils';
+import { getContainingWorkspace } from '../workspace';
+import { isNullOrUndefined } from '@microsoft/logic-apps-shared';
+import * as vscode from 'vscode';
 
 const configurationsKey = 'configurations';
 const launchKey = 'launch';
 const versionKey = 'version';
+
+/**
+ * Launches the debugger for a logic app project.
+ * @param {IActionContext} context - The action context.
+ * @param {string} projectPath - The project path.
+ * @returns {Promise<void>} - A promise that resolves when the debugger has started.
+ */
+export async function launchProjectDebugger(context: IActionContext, projectPath: string): Promise<void> {
+  const workspaceFolder = getContainingWorkspace(projectPath);
+  const debugConfig = await getDebugConfig(context, workspaceFolder);
+  if (!isNullOrUndefined(debugConfig)) {
+    await vscode.debug.startDebugging(workspaceFolder, debugConfig);
+  }
+}
+
+/**
+ * Gets a single debug configuration from the launch.json file.
+ * @param {IActionContext} context - The action context.
+ * @param {WorkspaceFolder} folder - The workspace folder.
+ * @param {boolean} shouldPromptOnMultipleConfigs - A flag indicating whether to prompt user for selection on multiple configs.
+ * @returns {Promise<DebugConfiguration | undefined>} - A promise that resolves to the debug configuration or undefined if one couldn't be retrieved.
+ */
+export async function getDebugConfig(
+  context: IActionContext,
+  folder: WorkspaceFolder,
+  shouldPromptOnMultipleConfigs = true
+): Promise<DebugConfiguration | undefined> {
+  if (isNullOrUndefined(folder)) {
+    return undefined;
+  }
+
+  const debugConfigs = getDebugConfigs(folder);
+  if (debugConfigs.length === 0) {
+    return undefined;
+  }
+
+  if (debugConfigs.length === 1 || !shouldPromptOnMultipleConfigs) {
+    return debugConfigs[0];
+  }
+
+  return await promptSelectDebugConfig(context, debugConfigs);
+}
 
 /**
  * Gets configurations property of launch.json file.
@@ -53,6 +100,37 @@ export function updateLaunchVersion(folder: WorkspaceFolder, version: string): v
  */
 export function isDebugConfigEqual(c1: DebugConfiguration, c2: DebugConfiguration): boolean {
   return c1.name === c2.name && c1.request === c2.request && isTypeEqual(c1.type, c2.type);
+}
+
+/**
+ * Prompts the user to select a debug configuration.
+ * @param {IActionContext} context - The action context.
+ * @param {DebugConfiguration[]} debugConfigs - The debug configuration options.
+ * @returns {Promise<DebugConfiguration>} - A promise that resolves to the selected debug configuration or undefined if one couldn't be retrieved.
+ */
+async function promptSelectDebugConfig(
+  context: IActionContext,
+  debugConfigs: DebugConfiguration[]
+): Promise<DebugConfiguration | undefined> {
+  if (isNullOrUndefined(debugConfigs) || debugConfigs.length === 0) {
+    return undefined;
+  }
+
+  const debugConfigPicks = debugConfigs.map((debugConfig) => ({
+    label: debugConfig.name,
+    data: debugConfig,
+  }));
+
+  const placeHolder = localize('selectDebugConfig', 'Select the debug configuration for your logic app project.');
+  try {
+    const selectedItem = await context.ui.showQuickPick(debugConfigPicks, { placeHolder });
+    return selectedItem?.data;
+  } catch (err) {
+    if (err instanceof UserCancelledError) {
+      return undefined;
+    }
+    throw err;
+  }
 }
 
 /**
