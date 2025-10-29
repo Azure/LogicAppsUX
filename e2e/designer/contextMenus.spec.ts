ded9d475322cf5276ce853fd59d6a6994c5ab2e9
Krrish Mittal <krrishmittal@Krrishs-MacBook-Pro.local>
2025-10-28 21:09:33 -0700

    context menu fixed

--- e2e/designer/contextMenus.spec.ts
+++ e2e/designer/contextMenus.spec.ts
@@ -13,15 +13,15 @@ test.describe(
 
       // Open trigger context menu
       await page.getByText('manual', { exact: true }).click({ button: 'right' });
-      expect(await page.getByText('Copy trigger', { exact: true }).isVisible()).toBeTruthy();
+      await expect(page.getByText('Copy trigger', { exact: true })).toBeVisible();
       await page.keyboard.press('Escape');
       // Open variable context menu
       await page.getByText('Initialize variable', { exact: true }).click({ button: 'right' });
-      expect(await page.getByText('Copy action', { exact: true }).isVisible()).toBeTruthy();
+      await expect(page.getByText('Copy action', { exact: true })).toBeVisible();
       await page.keyboard.press('Escape');
       // Open scope context menu
       await page.getByLabel('Scope operation', { exact: true }).click({ button: 'right' });
-      expect(await page.getByText('Copy entire action', { exact: true }).isVisible()).toBeTruthy();
+      await expect(page.getByText('Copy entire action', { exact: true })).toBeVisible();
       await page.keyboard.press('Escape');
     });
 
@@ -31,15 +31,15 @@ test.describe(
 
       // Click first edge button
       await page.getByLabel('Insert a new step between manual and Initialize variable').click();
-      expect(await page.getByText('Add an action', { exact: true }).isVisible()).toBeTruthy();
-      expect(await page.getByText('Add a parallel branch', { exact: true }).isVisible()).toBeTruthy();
+      await expect(page.getByText('Add an action', { exact: true })).toBeVisible();
+      await expect(page.getByText('Add a parallel branch', { exact: true })).toBeVisible();
       await page.keyboard.press('Escape');
       await page.getByLabel('Zoom view to fit').click({ force: true });
 
       // Click the last edge button
       await page.getByLabel('Insert a new step after Response').click();
-      expect(await page.getByText('Add an action', { exact: true }).isVisible()).toBeTruthy();
-      expect(await page.getByText('Add a parallel branch', { exact: true }).isVisible()).not.toBeTruthy();
+      await expect(page.getByText('Add an action', { exact: true })).toBeVisible();
+      await expect(page.getByText('Add a parallel branch', { exact: true })).not.toBeVisible();
       await page.keyboard.press('Escape');
     });
   }
