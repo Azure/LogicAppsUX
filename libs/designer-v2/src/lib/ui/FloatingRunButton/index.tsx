527afe2497aeea04cf44eab2ddff38e171c2d1a1
Riley Evans <rllyy97@gmail.com>
2025-11-04 19:22:16 -0600

    Improved run button loading and error states

--- libs/designer-v2/src/lib/ui/FloatingRunButton/index.tsx
+++ libs/designer-v2/src/lib/ui/FloatingRunButton/index.tsx
@@ -1,10 +1,18 @@
 import { useCallback, useMemo, useRef, useState } from 'react';
 import { useIntl } from 'react-intl';
 import { useMutation } from '@tanstack/react-query';
-import { Button, Spinner, SplitButton, Tooltip } from '@fluentui/react-components';
-import { bundleIcon, FlashFilled, FlashRegular, FlashSettingsFilled, FlashSettingsRegular } from '@fluentui/react-icons';
+import { Button, Spinner, SplitButton, Tooltip, Badge, MessageBar, MessageBarBody, MessageBarTitle } from '@fluentui/react-components';
+import { bundleIcon, FlashFilled, FlashRegular, FlashSettingsFilled, FlashSettingsRegular, ImportantFilled } from '@fluentui/react-icons';
 import type { Workflow } from '@microsoft/logic-apps-shared';
-import { canRunBeInvokedWithPayload, equals, HTTP_METHODS, isNullOrEmpty, RunService, WorkflowService } from '@microsoft/logic-apps-shared';
+import {
+  canRunBeInvokedWithPayload,
+  equals,
+  HTTP_METHODS,
+  isNullOrEmpty,
+  parseErrorMessage,
+  RunService,
+  WorkflowService,
+} from '@microsoft/logic-apps-shared';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   getCustomCodeFilesWithData,
@@ -20,6 +28,9 @@ import { PayloadPopover } from './payloadPopover';
 import { useIsA2AWorkflow } from '../../core/state/designerView/designerViewSelectors';
 import { ChatButton } from './chat';
 import constants from './../../common/constants';
+import { useFloatingRunButtonStyles } from './styles';
+
+const ErrorIcon = ImportantFilled;
 
 const RunIcon = bundleIcon(FlashFilled, FlashRegular);
 const RunWithPayloadIcon = bundleIcon(FlashSettingsFilled, FlashSettingsRegular);
@@ -48,6 +59,7 @@ export interface FloatingRunButtonProps {
   isDarkMode: boolean;
   isDraftMode?: boolean;
   isDisabled?: boolean;
+  tooltipOverride?: string;
 }
 
 export const FloatingRunButton = ({
@@ -58,8 +70,46 @@ export const FloatingRunButton = ({
   isDarkMode,
   isDraftMode,
   isDisabled,
+  tooltipOverride,
 }: FloatingRunButtonProps) => {
   const intl = useIntl();
+
+  const [runHasPayload, setRunHasPayload] = useState<boolean>(false);
+
+  const [runStatusMessage, setRunStatusMessage] = useState<string | null>(null);
+  const runStatusMessages = useMemo(
+    () => ({
+      saving: intl.formatMessage({
+        defaultMessage: 'Saving workflow...',
+        id: '2aC0Xh',
+        description: 'Status message displayed when the workflow is being saved',
+      }),
+      savingDraft: intl.formatMessage({
+        defaultMessage: 'Saving draft workflow...',
+        id: 'Z8uBn6',
+        description: 'Status message displayed when the draft workflow is being saved',
+      }),
+      running: intl.formatMessage({
+        defaultMessage: 'Running workflow...',
+        id: 'MrvtIU',
+        description: 'Status message displayed when the workflow is being run',
+      }),
+      runningDraft: intl.formatMessage({
+        defaultMessage: 'Running draft workflow...',
+        id: 'LX3q/+',
+        description: 'Status message displayed when the draft workflow is being run',
+      }),
+      error: intl.formatMessage({
+        defaultMessage: 'An error occurred',
+        id: 'cqHOAP',
+        description: 'Status message displayed when there is an error running the workflow',
+      }),
+    }),
+    [intl]
+  );
+
+  const styles = useFloatingRunButtonStyles();
+
   const dispatch = useDispatch();
   const isA2AWorkflow = useIsA2AWorkflow();
   const { operations: operationState, workflow } = useSelector((state: RootState) => state);
@@ -91,6 +141,8 @@ export const FloatingRunButton = ({
       let contentBody = payload?.body;
       const headers = payload?.headers ?? {};
 
+      setRunStatusMessage(runStatusMessages.runningDraft);
+
       // Try to parse body as JSON
       try {
         contentBody = contentBody ? JSON.parse(contentBody) : undefined;
@@ -105,148 +157,126 @@ export const FloatingRunButton = ({
       }
 
       try {
-        if (siteResourceId && workflowName) {
-          const callbackInfo: any = {
-            value: `${siteResourceId}/hostruntime/runtime/webhooks/workflow/api/management/workflows/${workflowName}/triggers/${triggerId}/${contentBody ? 'runDraftWithPayload' : 'runDraft'}`,
-            method: HTTP_METHODS.POST,
-          };
+        const callbackInfo: any = {
+          value: `${siteResourceId}/hostruntime/runtime/webhooks/workflow/api/management/workflows/${workflowName}/triggers/${triggerId}/${contentBody ? 'runDraftWithPayload' : 'runDraft'}`,
+          method: HTTP_METHODS.POST,
+        };
 
-          // Wait 0.5 seconds, running too fast after saving causes 500 error
-          await new Promise((resolve) => setTimeout(resolve, 500));
-          const runResponse = await RunService().runTrigger(callbackInfo, {
-            headers,
-            body: contentBody,
-          });
-          const runId = runResponse?.responseHeaders?.['x-ms-workflow-run-id'] ?? runResponse?.headers?.['x-ms-workflow-run-id'];
-          onRun?.(runId);
-        } else {
-          // TODO: Handle/throw error
-        }
+        // Wait 0.5 seconds, running too fast after saving causes 500 error
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        const runResponse = await RunService().runTrigger(callbackInfo, {
+          headers,
+          body: contentBody,
+        });
+        const runId = runResponse?.responseHeaders?.['x-ms-workflow-run-id'] ?? runResponse?.headers?.['x-ms-workflow-run-id'];
+        onRun?.(runId);
       } catch (error) {
-        console.error('Error running draft workflow:', error);
+        setRunStatusMessage(null);
+        throw error;
       }
+
+      setRunStatusMessage(null);
     },
-    [siteResourceId, workflowName, onRun]
+    [runStatusMessages, siteResourceId, workflowName, onRun]
   );
 
   const saveWorkflow = useCallback(async () => {
-    try {
-      const designerState = store.getState();
-      const serializedWorkflow = await serializeBJSWorkflow(designerState, {
-        skipValidation: false,
-        ignoreNonCriticalErrors: true,
-      });
-      const customCodeData = getCustomCodeFilesWithData(designerState.customCode);
+    const designerState = store.getState();
+    const serializedWorkflow = await serializeBJSWorkflow(designerState, {
+      skipValidation: false,
+      ignoreNonCriticalErrors: true,
+    });
+    const customCodeData = getCustomCodeFilesWithData(designerState.customCode);
 
-      const validationErrorsList: Record<string, boolean> = {};
-      const arr = Object.entries(designerState.operations.inputParameters);
-      for (const [id, nodeInputs] of arr) {
-        const hasValidationErrors = Object.values(nodeInputs.parameterGroups).some((parameterGroup) => {
-          return parameterGroup.parameters.some((parameter) => {
-            const validationErrors = validateParameter(parameter, parameter.value);
-            if (validationErrors.length > 0) {
-              dispatch(updateParameterValidation({ nodeId: id, groupId: parameterGroup.id, parameterId: parameter.id, validationErrors }));
-            }
-            return validationErrors.length;
-          });
+    const validationErrorsList: Record<string, boolean> = {};
+    const arr = Object.entries(designerState.operations.inputParameters);
+    for (const [id, nodeInputs] of arr) {
+      const hasValidationErrors = Object.values(nodeInputs.parameterGroups).some((parameterGroup) => {
+        return parameterGroup.parameters.some((parameter) => {
+          const validationErrors = validateParameter(parameter, parameter.value);
+          if (validationErrors.length > 0) {
+            dispatch(updateParameterValidation({ nodeId: id, groupId: parameterGroup.id, parameterId: parameter.id, validationErrors }));
+          }
+          return validationErrors.length;
         });
-        if (hasValidationErrors) {
-          validationErrorsList[id] = hasValidationErrors;
-        }
+      });
+      if (hasValidationErrors) {
+        validationErrorsList[id] = hasValidationErrors;
       }
+    }
 
-      const hasParametersErrors = !isNullOrEmpty(validationErrorsList);
+    const hasParametersErrors = !isNullOrEmpty(validationErrorsList);
 
-      if (!hasParametersErrors) {
-        return saveDraftWorkflow(
-          serializedWorkflow,
-          customCodeData as any,
-          () => dispatch(resetDesignerDirtyState(undefined) as any),
-          isDraftMode
-        );
-      }
-    } catch (error: any) {
-      console.error('Error saving workflow:', error);
+    if (!hasParametersErrors) {
+      return saveDraftWorkflow(
+        serializedWorkflow,
+        customCodeData as any,
+        () => dispatch(resetDesignerDirtyState(undefined) as any),
+        isDraftMode
+      );
     }
   }, [dispatch, saveDraftWorkflow, isDraftMode]);
 
   const {
     mutate: runMutate,
     isLoading: runIsLoading,
-    // error: runError,
-  } = useMutation(async () => {
+    error: runError,
+  } = useMutation(async (payload?: PayloadData | undefined) => {
     try {
+      setRunStatusMessage(isDraftMode ? runStatusMessages.savingDraft : runStatusMessages.saving);
       const saveResponse = await saveWorkflow();
       const triggerId = Object.keys(saveResponse?.definition?.triggers || {})?.[0];
       if (!triggerId) {
         return;
       }
 
+      setRunHasPayload(!!payload);
+
       if (isDraftMode) {
-        runDraftWorkflow(triggerId);
+        await runDraftWorkflow(triggerId, payload);
         return;
       }
 
       const callbackInfo = await WorkflowService().getCallbackUrl(triggerId);
-      const method = saveResponse?.definition?.triggers?.[triggerId]?.inputs?.method || 'POST';
-      callbackInfo.method = method;
+      callbackInfo.method = payload ? payload?.method : saveResponse?.definition?.triggers?.[triggerId]?.inputs?.method || 'POST';
 
       // Wait 0.5 seconds, running too fast after saving causes 500 error
       await new Promise((resolve) => setTimeout(resolve, 500));
-      const runResponse = await RunService().runTrigger(callbackInfo, undefined);
+      setRunStatusMessage(runStatusMessages.running);
+      const runResponse = await RunService().runTrigger(callbackInfo, payload);
       const runId = runResponse?.responseHeaders?.['x-ms-workflow-run-id'] ?? runResponse?.headers?.['x-ms-workflow-run-id'];
       onRun?.(runId);
-    } catch (_error: any) {
-      return;
+    } catch (error: any) {
+      setRunStatusMessage(null);
+      throw error;
     }
+    setRunStatusMessage(null);
   });
 
-  const {
-    mutate: runWithPayloadMutate,
-    isLoading: runWithPayloadIsLoading,
-    // error: runWithPayloadError,
-  } = useMutation(async (payload: PayloadData) => {
-    try {
-      const saveResponse = await saveWorkflow();
-      const triggerId = Object.keys(saveResponse?.definition?.triggers || {})?.[0];
-      if (!triggerId) {
-        return;
-      }
-
-      if (isDraftMode) {
-        runDraftWorkflow(triggerId, payload);
-        return;
-      }
-
-      const callbackInfo = await WorkflowService().getCallbackUrl(triggerId);
-      callbackInfo.method = payload?.method;
-
-      // Wait 0.5 seconds, running too fast after saving causes 500 error
-      await new Promise((resolve) => setTimeout(resolve, 500));
-      const runWithPayloadResponse = await RunService().runTrigger(callbackInfo, payload);
-      const runId = runWithPayloadResponse?.headers?.['x-ms-workflow-run-id'];
-      onRun?.(runId);
-    } catch (_error: any) {
-      return;
-    }
-  });
+  const mainButtonRef = useRef<HTMLButtonElement>(null);
+  const payloadButtonRef = useRef(null);
 
   const buttonCommonProps: any = {
     id: 'laux-v2-run-button',
+    ref: mainButtonRef,
     appearance: 'primary',
     shape: 'circular',
     size: 'large',
-    disabled: isDisabled || runIsLoading || runWithPayloadIsLoading || !isAllowedTriggerType,
-    style: {
-      position: 'absolute',
-      bottom: '16px',
-      left: '50%',
-      transform: 'translate(-50%, 0)',
-    },
+    disabled: isDisabled || runIsLoading || !isAllowedTriggerType,
   };
 
   const strings = useMemo(
     () => ({
+      NEUTRAL_TOOLTIP_PUBLISHED: intl.formatMessage({
+        defaultMessage: 'Run published workflow',
+        id: 'KV+9pl',
+        description: 'Tooltip for Run button when published workflow is shown',
+      }),
+      NEUTRAL_TOOLTIP_DRAFT: intl.formatMessage({
+        defaultMessage: 'Run draft workflow',
+        id: 'opvqoT',
+        description: 'Tooltip for Run button when draft workflow is shown',
+      }),
       RUN_TEXT: intl.formatMessage({
         defaultMessage: 'Run',
         id: 'd8JU5h',
@@ -257,45 +287,55 @@ export const FloatingRunButton = ({
         id: 'JrAqnE',
         description: 'Tooltip for Run with payload button',
       }),
+      ERROR_BADGE_TITLE: intl.formatMessage({
+        defaultMessage: 'Error running workflow',
+        id: 'SRN6ij',
+        description: 'Tooltip title when there is an error running the workflow',
+      }),
     }),
     [intl]
   );
 
-  const buttonRef = useRef(null);
+  const tooltipText = useMemo(
+    () => tooltipOverride ?? runStatusMessage ?? (isDraftMode ? strings.NEUTRAL_TOOLTIP_DRAFT : strings.NEUTRAL_TOOLTIP_PUBLISHED),
+    [tooltipOverride, runStatusMessage, isDraftMode, strings]
+  );
 
   const [popoverOpen, setPopoverOpen] = useState(false);
 
   if (isA2AWorkflow) {
     return (
-      <ChatButton
-        {...buttonCommonProps}
-        isDarkMode={isDarkMode}
-        isDraftMode={isDraftMode}
-        siteResourceId={siteResourceId}
-        workflowName={workflowName}
-        saveWorkflow={saveWorkflow}
-      />
+      <div className={styles.container}>
+        <ChatButton
+          {...buttonCommonProps}
+          isDarkMode={isDarkMode}
+          isDraftMode={isDraftMode}
+          siteResourceId={siteResourceId}
+          workflowName={workflowName}
+          saveWorkflow={saveWorkflow}
+        />
+      </div>
     );
   }
 
   if (canBeRunWithPayload) {
     return (
-      <>
-        <Tooltip positioning={{ target: buttonRef.current }} withArrow content={strings.RUN_PAYLOAD_TOOLTIP} relationship="description">
+      <div className={styles.container}>
+        <Tooltip withArrow content={tooltipText} relationship="description">
           <SplitButton
             {...buttonCommonProps}
             primaryActionButton={{
-              icon: runIsLoading ? <Spinner size="tiny" /> : <RunIcon />,
+              icon: runIsLoading && !runHasPayload ? <Spinner size="tiny" /> : <RunIcon />,
               onClick: () => {
-                runMutate();
+                runMutate(undefined);
               },
-              disabled: isDisabled || runIsLoading || runWithPayloadIsLoading || !isAllowedTriggerType || !triggerId,
+              disabled: isDisabled || runIsLoading || !isAllowedTriggerType || !triggerId,
             }}
             menuButton={{
-              icon: runWithPayloadIsLoading ? <Spinner size="tiny" /> : <RunWithPayloadIcon />,
+              icon: runIsLoading && runHasPayload ? <Spinner size="tiny" /> : <RunWithPayloadIcon />,
               onClick: () => setPopoverOpen(true),
-              ref: buttonRef,
-              disabled: isDisabled || runIsLoading || runWithPayloadIsLoading || !canBeRunWithPayload || !triggerId,
+              ref: payloadButtonRef,
+              disabled: isDisabled || runIsLoading || !canBeRunWithPayload || !triggerId,
             }}
           >
             {strings.RUN_TEXT}
@@ -305,16 +345,37 @@ export const FloatingRunButton = ({
           isDraftMode={isDraftMode}
           open={popoverOpen}
           setOpen={setPopoverOpen}
-          buttonRef={buttonRef}
-          onSubmit={runWithPayloadMutate}
+          buttonRef={payloadButtonRef}
+          onSubmit={runMutate}
         />
-      </>
+        {runError ? (
+          <Tooltip
+            relationship="description"
+            content={{
+              className: styles.errorTooltip,
+              children: (
+                <MessageBar intent="error" layout="multiline">
+                  <MessageBarBody>
+                    <MessageBarTitle>{strings.ERROR_BADGE_TITLE}</MessageBarTitle>
+                    <br />
+                    {parseErrorMessage(runError)}
+                  </MessageBarBody>
+                </MessageBar>
+              ),
+            }}
+          >
+            <Badge className={styles.errorBadge} color="danger" icon={<ErrorIcon />} />
+          </Tooltip>
+        ) : null}
+      </div>
     );
   }
 
   return (
-    <Button {...buttonCommonProps} icon={runIsLoading ? <Spinner size="tiny" /> : <RunIcon />} onClick={runMutate}>
-      {strings.RUN_TEXT}
-    </Button>
+    <div className={styles.container}>
+      <Button {...buttonCommonProps} icon={runIsLoading ? <Spinner size="tiny" /> : <RunIcon />} onClick={runMutate}>
+        {strings.RUN_TEXT}
+      </Button>
+    </div>
   );
 };
