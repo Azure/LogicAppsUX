c8a3a362665c79108977b1ff042f894ce6ba755c
Travis Vu <travisharris@microsoft.com>
2025-11-18 20:29:52 -0800

    Fix Test

--- libs/a2a-core/src/react/components/MessageInput/MessageInput.test.tsx
+++ libs/a2a-core/src/react/components/MessageInput/MessageInput.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
