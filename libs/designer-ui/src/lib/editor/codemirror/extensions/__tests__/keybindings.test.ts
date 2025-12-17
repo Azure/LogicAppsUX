71a82175b393d5f34086d6141c78f7dac1388d7d
Riley Evans <rllyy97@gmail.com>
2025-12-17 10:38:36 -0600

    Updated unit tests

--- libs/designer-ui/src/lib/editor/codemirror/extensions/__tests__/keybindings.test.ts
+++ libs/designer-ui/src/lib/editor/codemirror/extensions/__tests__/keybindings.test.ts
@@ -18,4 +18,20 @@ describe('createKeybindingExtensions', () => {
     // Base keybindings (default + history) are always included
     expect(extensions.length).toBeGreaterThan(0);
   });
+
+  it('should include indent with tab keybinding when indentWithTab is true', () => {
+    const extensions = createKeybindingExtensions({ indentWithTab: true });
+    // Should contain base keybindings (2) + tabSize (1) + keymap (1)
+    expect(extensions.length).toBe(4);
+    // This is a bit of a hack, but there's no easy way to inspect the keymap
+    const keymapExtension = extensions[3] as any;
+    expect(keymapExtension.value.length).toBe(2);
+    expect(keymapExtension.value[0].key).toBe('Tab');
+    expect(keymapExtension.value[1].key).toBe('Shift-Tab');
+  });
+
+  it('should not include indent with tab keybinding when indentWithTab is false', () => {
+    const extensions = createKeybindingExtensions({ indentWithTab: false });
+    expect(extensions.length).toBe(2);
+  });
 });
