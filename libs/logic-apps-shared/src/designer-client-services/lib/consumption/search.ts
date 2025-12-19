363a9b76cf1942f03eb6a8fc95119392bb3ff839
Bhavya . <bhavya@microsoft.com>
2025-12-19 15:38:50 +0530

    Add A2A trigger in search menu

--- libs/logic-apps-shared/src/designer-client-services/lib/consumption/search.ts
+++ libs/logic-apps-shared/src/designer-client-services/lib/consumption/search.ts
@@ -5,6 +5,7 @@ import type { BaseSearchServiceOptions } from '../base/search';
 import type { ContinuationTokenResponse } from '../common/azure';
 import type { QueryParameters } from '../httpClient';
 import * as OperationsData from './operations';
+import { a2aRequestGroup } from './operations/operationgroups';
 
 const ISE_RESOURCE_ID = 'properties/integrationServiceEnvironmentResourceId';
 
@@ -101,6 +102,7 @@ export class ConsumptionSearchService extends BaseSearchService {
       OperationsData.rosettaNetEncodeOperation,
       OperationsData.rosettaNetDecodeOperation,
       OperationsData.rosettaNetWairForResponseOperation,
+      OperationsData.a2aRequestOperation,
     ];
     return Promise.resolve([...clientBuiltInOperations, ...consumptionBuiltIn]);
   }
@@ -185,6 +187,7 @@ export const getBuiltInConnectorsInConsumption = (): Connector[] => {
     OperationsData.selectBatchWorkflowGroup,
     OperationsData.as2Group,
     OperationsData.rosettaNetGroup,
+    a2aRequestGroup,
   ];
   return [...clientBuiltInConnectors, ...consumptionBuiltIn];
 };
