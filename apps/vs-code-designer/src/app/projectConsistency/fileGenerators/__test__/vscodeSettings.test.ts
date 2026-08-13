0dacfeeb291b5899cebb19543138ce89d4d3b226
Andrew Eldridge <aeldridge@microsoft.com>
2026-08-13 12:56:39 -0400

    fix(vscode): Codeful project deploy fix (#9527)
    * fix codeful deploy issue (set deploySubpath to root and use zipIgnorePattern)
    
    * revert zipIgnorePattern, update .funcignore for codeful
    
    * add tests for codeful .funcignore changes

--- apps/vs-code-designer/src/app/projectConsistency/fileGenerators/__test__/vscodeSettings.test.ts
+++ apps/vs-code-designer/src/app/projectConsistency/fileGenerators/__test__/vscodeSettings.test.ts
@@ -55,8 +55,9 @@ describe('generateSettingsJson', () => {
       };
       const result = generateSettingsJson(config);
 
-      expect(result).toHaveProperty('azureLogicAppsStandard.deploySubpath');
+      expect(result).toHaveProperty('azureLogicAppsStandard.deploySubpath', '.');
       expect(result).not.toHaveProperty('azureLogicAppsStandard.preDeployTask');
+      expect(result).not.toHaveProperty('azureLogicAppsStandard.zipIgnorePattern');
       expect(result).toHaveProperty('omnisharp.enableMsBuildLoadProjectsOnDemand', false);
       expect(result).toHaveProperty('omnisharp.disableMSBuildDiagnosticWarning', true);
     });
